0d83faa2f1e19cfec4b90dc34ebb99a740004c09
Colin Cross <ccross@android.com>
2010-04-07 12:05:32 -0700

    fs: partitions: Add support for named partitions
    Adds a new file in /sys/block/<block>/<partition> called partition_name
    that contains the name of the partition, if specified by the partition
    handler.
    
    Change-Id: I6648ed95eabefd1d00edbfdfd99eeb971d15f4b3
    Signed-off-by: Colin Cross <ccross@android.com>

--- fs/partitions/check.h
+++ fs/partitions/check.h
@@ -1,6 +1,8 @@
 #include <linux/pagemap.h>
 #include <linux/blkdev.h>
 
+#define PART_NAME_SIZE 128
+
 /*
  * add_gd_partition adds a partitions details to the devices partition
  * description.
@@ -12,6 +14,7 @@ struct parsed_partitions {
 		sector_t from;
 		sector_t size;
 		int flags;
+		char name[PART_NAME_SIZE];
 	} parts[DISK_MAX_PARTS];
 	int next;
 	int limit;
@@ -29,13 +32,27 @@ static inline void *read_part_sector(struct parsed_partitions *state,
 }
 
 static inline void
-put_partition(struct parsed_partitions *p, int n, sector_t from, sector_t size)
+put_named_partition(struct parsed_partitions *p, int n, sector_t from,
+	sector_t size, const char *name, size_t name_size)
 {
 	if (n < p->limit) {
 		p->parts[n].from = from;
 		p->parts[n].size = size;
 		printk(" %s%d", p->name, n);
+		if (name) {
+			if (name_size > PART_NAME_SIZE - 1)
+				name_size = PART_NAME_SIZE - 1;
+			memcpy(p->parts[n].name, name, name_size);
+			p->parts[n].name[name_size] = 0;
+			printk(" (%s)", p->parts[n].name);
+		}
 	}
+}
+
+static inline void
+put_partition(struct parsed_partitions *p, int n, sector_t from, sector_t size)
+{
+	put_named_partition(p, n, from, size, NULL, 0);
 }
 
 extern int warn_no_part;
