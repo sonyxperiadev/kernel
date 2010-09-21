d46a586624b748822357166212586e2baaa4b4bb
David Xiao <dxiao@broadcom.com>
2010-09-20 20:22:24 -0700

    Added uart0 and uart1 into board file.
    Signed-off-by: David Xiao <dxiao@broadcom.com>

--- arch/arm/mach-kona/bcm2850_fpga.c
+++ arch/arm/mach-kona/bcm2850_fpga.c
@@ -28,12 +28,53 @@
 #include <linux/platform_device.h>
 #include <linux/sysdev.h>
 #include <linux/interrupt.h>
+#include <linux/serial_8250.h>
 
 #include <mach/hardware.h>
 #include <asm/mach/arch.h>
 #include <asm/mach-types.h>
 
 #include <plat/kona.h>
+
+/*
+ * todo: 8250 driver has problem autodetecting the UART type -> have to 
+ * use FIXED type
+ * confuses it as an XSCALE UART.  Problem seems to be that it reads
+ * bit6 in IER as non-zero sometimes when it's supposed to be 0.
+ */
+#define KONA_UART0_PA	UARTB_BASE_ADDR
+#define KONA_UART1_PA	UARTB2_BASE_ADDR
+
+#define BCM2850_FPGA_8250PORT(name)				\
+{								\
+	.membase    = (void __iomem *)(KONA_##name##_VA), 	\
+	.mapbase    = (resource_size_t)(KONA_##name##_PA),    	\
+	.irq	    = BCM_INT_ID_##name,               		\
+	.uartclk    = 13000000,					\
+	.regshift   = 2,					\
+	.iotype	    = UPIO_MEM,					\
+	.type	    = PORT_16550A,          			\
+	.flags	    = UPF_BOOT_AUTOCONF | UPF_FIXED_TYPE | UPF_SKIP_TEST,	\
+}
+
+static struct plat_serial8250_port bcm2850_fpga_uart_data[] = {
+	/* only two uart ports are available in FPGA at this moment */
+	BCM2850_FPGA_8250PORT(UART0),
+	BCM2850_FPGA_8250PORT(UART1),
+	{
+		.flags		= 0
+	},
+};
+
+static struct platform_device bcm2850_fpga_serial_device = {
+	.name		= "serial8250",
+	.id		= PLAT8250_DEV_PLATFORM,
+	.dev		= {
+		.platform_data = bcm2850_fpga_uart_data,
+	},
+};
+
+
 
 void __init bcm2850_fpga_map_io(void)
 {
@@ -42,10 +83,19 @@ void __init bcm2850_fpga_map_io(void)
 	kona_map_io();
 }
 
+static struct platform_device *bcm2850_fpga_devices[] __initdata = {
+	&bcm2850_fpga_serial_device,
+};
+
+static void __init bcm2850_fpga_add_devices(void)
+{
+	platform_add_devices(bcm2850_fpga_devices, ARRAY_SIZE(bcm2850_fpga_devices));
+}
+
 void __init bcm2850_fpga_init(void)
 {
 	/* FPGA machine specific initialisation */
-
+	bcm2850_fpga_add_devices();
 	return;
 }
 
