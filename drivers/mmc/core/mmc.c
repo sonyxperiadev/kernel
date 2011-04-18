9e885817f3dcdf41ebef28143d379220b9bb2055
Wei Zhong <weizhong@broadcom.com>
2011-04-18 11:11:47 -0700

    MMC: Fixes wrong sector size calculation by reverting f0b0e4bec1e89014f3dcef4da8bcf95428cc771c
    This is the same patch as
    http://android.git.kernel.org/?p=kernel/common.git;a=commitdiff;h=1cd64ef5c1becb1c70c432bdcae98fdb686a8173
    This fixes the MMC sector size mismatch between u-boot and kernel
    when boot section presents in MMC card.

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -243,11 +243,7 @@ static int mmc_read_ext_csd(struct mmc_card *card)
 			ext_csd[EXT_CSD_SEC_CNT + 1] << 8 |
 			ext_csd[EXT_CSD_SEC_CNT + 2] << 16 |
 			ext_csd[EXT_CSD_SEC_CNT + 3] << 24;
-		if (card->ext_csd.sectors) {
-			unsigned boot_sectors;
-			/* size is in 256K chunks, i.e. 512 sectors each */
-			boot_sectors = ext_csd[EXT_CSD_BOOT_SIZE_MULTI] * 512;
-			card->ext_csd.sectors -= boot_sectors;
+
 #ifdef CONFIG_MMC_BCM_SD
 		/* Cards with density > 2GiB are sector addressed */
 		if (card->ext_csd.sectors > (2u * 1024 * 1024 * 1024) / 512)
@@ -255,7 +251,6 @@ static int mmc_read_ext_csd(struct mmc_card *card)
 		if (card->ext_csd.sectors)
 #endif
 			mmc_card_set_blockaddr(card);
-		}
 	}
 
 	switch (ext_csd[EXT_CSD_CARD_TYPE] & EXT_CSD_CARD_TYPE_MASK) {
