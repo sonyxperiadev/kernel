ab2e4d41201fab5a995fab50d201cb82b7bc46f7
David X <dxiao@mobmm-ubuntu2.(none)>
2011-05-09 15:38:22 -0700

    Revert "Configured MEMC for optimized performance"
    This reverts commit 5dd720edb2623ac45ac8ad51a2260ae3bcddc39c.

--- arch/arm/mach-rhea/io_map.c
+++ arch/arm/mach-rhea/io_map.c
@@ -140,7 +140,6 @@ static struct map_desc rhea_io_desc[] __initdata =
 	IO_DESC( KONA_AHINTC_BASE_VA, SZ_4K ),
 	IO_DESC(KONA_AUXMIC_VA, SZ_4K),
 	IO_DESC(KONA_AUDIOH_VA, SZ_4K),
-	IO_DESC(KONA_MEMC0_NS_VA, SZ_4K),
 };
 
 
