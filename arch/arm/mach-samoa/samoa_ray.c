cca49714eae53918919fecd7a548a49fe2d71af1
jianzha <jianzha@broadcom.com>
2011-05-10 12:05:34 -0700

    Samoa BSP: code clock for samoa chip
    [Defect/Enhancement/New Feature]
    
    [Problem]
    Need to code clock enables for real Samoa chip for bring-up
    
    [Solution]
    added the code to take care of KPS_CCU change on Samoa
    
    [Reviewers]
    Jun Tang
    Huadong Shao
    
    [Testing]
    	[Test Cases]
    build & run samoa_ray_defconfig
    
    [Label/CL# Synced To]
    head
    
    [Platform]
    samoa_ray_defconfig
    CONFIG_MACH_SAMOA_RAY_TEST_ON_RHEA_RAY on and off
    
    
    [BROADCOM INTERNAL NOTES]

--- arch/arm/mach-samoa/samoa_ray.c
+++ arch/arm/mach-samoa/samoa_ray.c
@@ -45,8 +45,10 @@
 #include <linux/i2c/qt602240_ts.h>
 #endif
 #include <mach/rdb/brcm_rdb_kproc_clk_mgr_reg.h>
+#include <mach/rdb/brcm_rdb_bmdm_clk_mgr_reg.h>
 #include <mach/rdb/brcm_rdb_kps_clk_mgr_reg.h>
 #include <mach/rdb/brcm_rdb_kpm_clk_mgr_reg.h>
+#include <mach/rdb/brcm_rdb_khubaon_clk_mgr_reg.h>
 #include <mach/kona.h>
 #include <mach/samoa.h>
 #include <asm/mach/map.h>
@@ -226,33 +228,10 @@ static void __init samoa_ray_add_i2c_devices (void)
 #endif
 }
 
-static void enable_smi_display_clks(void)
-{
-#if 0
-	struct clk *smi_axi;
-	struct clk *mm_dma;
-	struct clk *smi;
-
-	smi_axi = clk_get (NULL, "smi_axi_clk");
-	mm_dma = clk_get (NULL, "mm_dma_axi_clk");
-
-	smi = clk_get (NULL, "smi_clk");
-	BUG_ON (!smi_axi || !smi || !mm_dma);
-
-
-	clk_set_rate (smi, 250000000);
-
-	clk_enable (smi_axi);
-	clk_enable (smi);
-	clk_enable(mm_dma);
-#endif
-}
 
 /* All Samoa Ray specific devices */ 
 static void __init samoa_ray_add_devices(void)
 {
-	enable_smi_display_clks();
-
 #ifdef CONFIG_KEYBOARD_BCM
 	bcm_kp_device.dev.platform_data = &bcm_keypad_data;
 #endif
@@ -271,6 +250,89 @@ EXPORT_SYMBOL(clk_enable);
 EXPORT_SYMBOL(clk_get_rate);
 EXPORT_SYMBOL(clk_set_rate);
 
+#define CLK_MGR_REG_WR_ACCESS_OFFSET 0
+#define CLK_MGR_REG_POLICY_CTL_OFFSET 0xc
+#define CLK_MGR_REG_LVM_EN_OFFSET     0x34
+void __init board_configure(void)
+{
+	u32 proc_clk_mgr_base_v;
+	u32 bmdm_clk_mgr_base_v;
+
+	proc_clk_mgr_base_v = (u32)ioremap(PROC_CLK_BASE_ADDR, 0x1000);
+	bmdm_clk_mgr_base_v = (u32)ioremap(BMDM_CCU_BASE_ADDR, 0x1000);
+
+	/* print proc_clk set up by the boot loader */
+	printk(KERN_ERR "KPROC: FREQ=0x%x\n", readl(proc_clk_mgr_base_v+KPROC_CLK_MGR_REG_POLICY_FREQ_OFFSET));
+	printk(KERN_ERR "KPROC: PLLARMA=0x%x\n", readl(proc_clk_mgr_base_v+KPROC_CLK_MGR_REG_PLLARMA_OFFSET));
+	printk(KERN_ERR "KPROC: PLLARMB=0x%x\n", readl(proc_clk_mgr_base_v+KPROC_CLK_MGR_REG_PLLARMB_OFFSET));
+	printk(KERN_ERR "KPROC: PLLARMC=0x%x\n", readl(proc_clk_mgr_base_v+KPROC_CLK_MGR_REG_PLLARMC_OFFSET));
+	printk(KERN_ERR "KPROC: PLLARMCTRL5=0x%x\n", readl(proc_clk_mgr_base_v+KPROC_CLK_MGR_REG_PLLARMCTRL5_OFFSET));
+	printk(KERN_ERR "KPROC: ARM_DIV=0x%x\n", readl(proc_clk_mgr_base_v+KPROC_CLK_MGR_REG_ARM_DIV_OFFSET));
+
+	/* print bmdm_clk set up by the boot loader */
+	printk(KERN_ERR "BMDM_CCU: FREQ=0x%x\n", readl(bmdm_clk_mgr_base_v+BMDM_CLK_MGR_REG_POLICY_FREQ_OFFSET));
+
+	/* print hubaon_timer */
+	printk(KERN_ERR "HAON_CCU: HUB_TIMER_DIV=0x%x\n", readl(KONA_AON_CLK_VA+KHUBAON_CLK_MGR_REG_HUB_TIMER_DIV_OFFSET));
+	/* print peri_timer */
+	printk(KERN_ERR "KPS_CCU: TIMERS_DIV=0x%x\n", readl(KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_TIMERS_DIV_OFFSET));
+
+
+	/* enable SDIO2 (for SD card) clocks before clock driver is ready */
+	printk(KERN_ERR "SDIO1_CLKGATE =0x%x\n", readl(KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_SDIO1_CLKGATE_OFFSET));
+
+	writel(0xA5A501, (KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_WR_ACCESS_OFFSET));
+	writel(readl(KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_KPM_POLICY3_MASK_OFFSET) |
+		KPM_CLK_MGR_REG_KPM_POLICY3_MASK_SDIO1_POLICY3_MASK_MASK,
+		(KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_KPM_POLICY3_MASK_OFFSET));
+	writel(0x1, (KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_LVM_EN_OFFSET));
+	while ((readl(KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_LVM_EN_OFFSET)&0x1) == 0x1 );
+	writel(0x5, (KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_POLICY_CTL_OFFSET));
+	writel(0x1f, (KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_SDIO1_CLKGATE_OFFSET));
+	printk(KERN_ERR "SDIO1_CLKGATE =0x%x\n", readl(KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_SDIO1_CLKGATE_OFFSET));
+	printk(KERN_ERR "SDIO1_DIV =0x%x\n", readl(KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_SDIO1_DIV_OFFSET));
+	printk(KERN_ERR "SDIO2_DIV =0x%x\n", readl(KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_SDIO2_DIV_OFFSET));
+
+	/* enable I2C clocks before clock driver is ready */
+	// BSC1 & BSC2 clocks
+	printk(KERN_ERR "I2C: BSC1_CLKGATE=0x%x\n", readl(KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_BSC1_CLKGATE_OFFSET));
+	printk(KERN_ERR "I2C: BSC2_CLKGATE=0x%x\n", readl(KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_BSC2_CLKGATE_OFFSET));
+#ifdef CONFIG_MACH_SAMOA_RAY_TEST_ON_RHEA_RAY
+	writel(0xA5A501, (KONA_KPS_CLK_VA+CLK_MGR_REG_WR_ACCESS_OFFSET));
+#else
+	/* On Samoa we use kpm to control kps clocks */
+#endif
+
+	writel(readl(KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_KPS_POLICY3_MASK_OFFSET) |
+		KPS_CLK_MGR_REG_KPS_POLICY3_MASK_BSC1_POLICY3_MASK_MASK |
+		KPS_CLK_MGR_REG_KPS_POLICY3_MASK_BSC2_POLICY3_MASK_MASK,
+		(KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_KPS_POLICY3_MASK_OFFSET));
+#ifdef CONFIG_MACH_SAMOA_RAY_TEST_ON_RHEA_RAY
+	writel(0x1, (KONA_KPS_CLK_VA+CLK_MGR_REG_LVM_EN_OFFSET));
+	while ((readl(KONA_KPS_CLK_VA+CLK_MGR_REG_LVM_EN_OFFSET)&0x1) == 0x1 );
+	writel(0x5, (KONA_KPS_CLK_VA+CLK_MGR_REG_POLICY_CTL_OFFSET));
+#else
+	/* Samoa chip is diffeent. Use kpm to control kps clocks */
+	writel(0x1, (KONA_KPM_CLK_VA+CLK_MGR_REG_LVM_EN_OFFSET));
+	while ((readl(KONA_KPM_CLK_VA+CLK_MGR_REG_LVM_EN_OFFSET)&0x1) == 0x1 );
+	writel(0x5, (KONA_KPM_CLK_VA+CLK_MGR_REG_POLICY_CTL_OFFSET));
+#endif
+	writel(0x30f, (KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_BSC1_CLKGATE_OFFSET));
+	writel(0x30f, (KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_BSC2_CLKGATE_OFFSET));
+
+	printk(KERN_ERR "I2C: BSC1_CLKGATE=0x%x\n", readl(KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_BSC1_CLKGATE_OFFSET));
+	printk(KERN_ERR "I2C: BSC2_CLKGATE=0x%x\n", readl(KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_BSC2_CLKGATE_OFFSET));
+	printk(KERN_ERR "I2C: BSC1_DIV=0x%x\n", readl(KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_BSC1_DIV_OFFSET));
+	printk(KERN_ERR "I2C: BSC2_DIV=0x%x\n", readl(KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_BSC2_DIV_OFFSET));
+
+	writel(0, (KONA_KPM_CLK_VA+CLK_MGR_REG_WR_ACCESS_OFFSET));
+#ifdef CONFIG_MACH_SAMOA_RAY_TEST_ON_RHEA_RAY
+	writel(0, (KONA_KPS_CLK_VA+CLK_MGR_REG_WR_ACCESS_OFFSET));
+#endif
+
+	iounmap((void *)proc_clk_mgr_base_v);
+	iounmap((void *)bmdm_clk_mgr_base_v);
+}
 
 #ifndef CONFIG_MACH_SAMOA_FPGA
 #include <linux/broadcom/bcm_fuse_memmap.h>
@@ -281,7 +343,7 @@ static void Comms_Start(void)
 {
     void __iomem *apcp_shmem = ioremap_nocache(IPC_BASE, IPC_SIZE);
     void __iomem *cp_boot_base;
-    
+
     if (!apcp_shmem) {
         printk(KERN_ERR "%s: ioremap shmem failed\n", __func__);
         return;
@@ -307,78 +369,13 @@ static void Comms_Start(void)
 
 void __init board_init(void)
 {
-	u32 proc_clk_mgr_base_v;
-
-	proc_clk_mgr_base_v = (u32)ioremap(PROC_CLK_BASE_ADDR, 0x1000);
-
-	/* print proc_clk set up by the boot loader */
-	printk(KERN_ERR "KPROC: FREQ=0x%x\n", readl(proc_clk_mgr_base_v+KPROC_CLK_MGR_REG_POLICY_FREQ_OFFSET));
-	printk(KERN_ERR "KPROC: PLLARMA=0x%x\n", readl(proc_clk_mgr_base_v+KPROC_CLK_MGR_REG_PLLARMA_OFFSET));
-	printk(KERN_ERR "KPROC: PLLARMB=0x%x\n", readl(proc_clk_mgr_base_v+KPROC_CLK_MGR_REG_PLLARMB_OFFSET));
-	printk(KERN_ERR "KPROC: PLLARMC=0x%x\n", readl(proc_clk_mgr_base_v+KPROC_CLK_MGR_REG_PLLARMC_OFFSET));
-	printk(KERN_ERR "KPROC: PLLARMCTRL5=0x%x\n", readl(proc_clk_mgr_base_v+KPROC_CLK_MGR_REG_PLLARMCTRL5_OFFSET));
-	printk(KERN_ERR "KPROC: ARM_DIV=0x%x\n", readl(proc_clk_mgr_base_v+KPROC_CLK_MGR_REG_ARM_DIV_OFFSET));
-
-	/* enable I2C clocks before clock driver is ready */
-#ifdef CONFIG_MACH_SAMOA_RAY_TEST_ON_RHEA_RAY
-#define KPS_CLK_MGR_REG_WR_ACCESS_OFFSET 0
-#define KPS_CLK_MGR_REG_POLICY_CTL_OFFSET 0xc
-#define KPS_CLK_MGR_REG_LVM_EN_OFFSET 0x34
-#define KPS_CLK_MGR_REG_POLICY3_MASK_OFFSET KPS_CLK_MGR_REG_KPS_POLICY3_MASK_OFFSET
-#define KPS_CLK_MGR_REG_POLICY3_MASK_BSC1_POLICY3_MASK_MASK KPS_CLK_MGR_REG_KPS_POLICY3_MASK_BSC1_POLICY3_MASK_MASK
-#define KPS_CLK_MGR_REG_POLICY3_MASK_BSC2_POLICY3_MASK_MASK KPS_CLK_MGR_REG_KPS_POLICY3_MASK_BSC2_POLICY3_MASK_MASK
-	// BSC1 & BSC2 clocks
-	printk(KERN_ERR "I2C: BSC1_CLKGATE=0x%x\n", readl(KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_BSC1_CLKGATE_OFFSET));
-	printk(KERN_ERR "I2C: BSC2_CLKGATE=0x%x\n", readl(KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_BSC2_CLKGATE_OFFSET));
-	writel(0xA5A501, (KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_WR_ACCESS_OFFSET));
-	writel(readl(KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_POLICY3_MASK_OFFSET) |
-		KPS_CLK_MGR_REG_POLICY3_MASK_BSC1_POLICY3_MASK_MASK |
-		KPS_CLK_MGR_REG_POLICY3_MASK_BSC2_POLICY3_MASK_MASK,
-		(KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_POLICY3_MASK_OFFSET));
-	writel(0x1, (KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_LVM_EN_OFFSET));
-	while ((readl(KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_LVM_EN_OFFSET)&0x1) == 0x1 );
-	writel(0x5, (KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_POLICY_CTL_OFFSET));
-	writel(0x30f, (KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_BSC1_CLKGATE_OFFSET));
-	writel(0x30f, (KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_BSC2_CLKGATE_OFFSET));
-	printk(KERN_ERR "I2C: WR_ACCESS=0x%x\n", readl(KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_WR_ACCESS_OFFSET));
-	printk(KERN_ERR "I2C: BSC1_CLKGATE=0x%x\n", readl(KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_BSC1_CLKGATE_OFFSET));
-	printk(KERN_ERR "I2C: BSC2_CLKGATE=0x%x\n", readl(KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_BSC2_CLKGATE_OFFSET));
-	printk(KERN_ERR "I2C: BSC1_DIV=0x%x\n", readl(KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_BSC1_DIV_OFFSET));
-	printk(KERN_ERR "I2C: BSC2_DIV=0x%x\n", readl(KONA_KPS_CLK_VA+KPS_CLK_MGR_REG_BSC2_DIV_OFFSET));
-
-#else
-	/* Samoa chip is diffeent */
-
-#endif
-
-	/* enable SDIO2 (for SD card) clocks before clock driver is ready */
-#ifdef CONFIG_MACH_SAMOA_RAY_TEST_ON_RHEA_RAY
-#define KPM_CLK_MGR_REG_POLICY3_MASK_OFFSET KPM_CLK_MGR_REG_KPM_POLICY3_MASK_OFFSET
-#define KPM_CLK_MGR_REG_POLICY3_MASK_SDIO1_POLICY3_MASK_MASK KPM_CLK_MGR_REG_KPM_POLICY3_MASK_SDIO1_POLICY3_MASK_MASK
-	printk(KERN_ERR "SDIO1_CLKGATE =0x%x\n", readl(KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_SDIO1_CLKGATE_OFFSET));
-
-	writel(0xA5A501, (KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_WR_ACCESS_OFFSET));
-	writel(readl(KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_POLICY3_MASK_OFFSET) |
-		KPM_CLK_MGR_REG_POLICY3_MASK_SDIO1_POLICY3_MASK_MASK,
-		(KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_POLICY3_MASK_OFFSET));
-	writel(0x1, (KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_LVM_EN_OFFSET));
-	while ((readl(KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_LVM_EN_OFFSET)&0x1) == 0x1 );
-	writel(0x5, (KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_POLICY_CTL_OFFSET));
-	writel(0x1f, (KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_SDIO1_CLKGATE_OFFSET));
-	printk(KERN_ERR "SDIO1_CLKGATE =0x%x\n", readl(KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_SDIO1_CLKGATE_OFFSET));
-	printk(KERN_ERR "SDIO1_DIV =0x%x\n", readl(KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_SDIO1_DIV_OFFSET));
-	printk(KERN_ERR "SDIO2_DIV =0x%x\n", readl(KONA_KPM_CLK_VA+KPM_CLK_MGR_REG_SDIO2_DIV_OFFSET));
-#else
-	/* Samoa chip is diffeent */
-
-#endif
-
-	iounmap((void *)proc_clk_mgr_base_v);
+	/* remove this call later */
+	board_configure();
 
 #ifndef CONFIG_MACH_SAMOA_FPGA
 	Comms_Start();
 #endif
-	
+
 	board_add_common_devices();
 	samoa_ray_add_devices();
 	return;
