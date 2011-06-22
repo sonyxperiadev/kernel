5093cc0b1285701027666a01229858cd1f0b448f
Henrik Jehg <hjehg@broadcom.com>
2011-06-22 04:50:11 -0700

    Adding more test cases, and KConfig options

--- include/linux/broadcom/bcm59055-adc_chipset_hw.h
+++ include/linux/broadcom/bcm59055-adc_chipset_hw.h
@@ -1,3 +1,18 @@
+/*****************************************************************************
+*  Copyright 2001 - 2008 Broadcom Corporation.  All rights reserved.
+*
+*  Unless you and Broadcom execute a separate written software license
+*  agreement governing use of this software, this software is licensed to you
+*  under the terms of the GNU General Public License version 2, available at
+*  http://www.gnu.org/licenses/old-license/gpl-2.0.html (the "GPL").
+*
+*  Notwithstanding the above, under no circumstances may you combine this
+*  software in any way with any other Broadcom software provided under a
+*  license other than the GPL, without Broadcom's express prior written
+*  consent.
+*
+*****************************************************************************/
+
 /*
 	VENDOR_ADC_PRODUCT_CONFIGURATION
 	Description: Used to determine if this configuration is here and it is to be used
