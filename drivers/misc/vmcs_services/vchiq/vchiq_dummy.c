bd0952f617b3d84003a552a2795a5793c18cba4c
Zhengran <zhengran@broadcom.com>
2011-06-23 18:31:56 -0700

    build vcos/vchiq into kernel; stub out ipc/sdma functions referenced by vchiq for now

---
+++ drivers/misc/vmcs_services/vchiq/vchiq_dummy.c
@@ -0,0 +1,35 @@
+/* dummy function for ipc/dma function */
+
+void chal_ipc_int_vcset(void) {}
+
+void chal_ipc_config(void){}
+
+void chal_icd_set_security(void){}
+
+void chal_ipc_int_secmode(void){}
+
+void chal_ipc_int_clr(void){}
+
+void chal_ipc_get_int_status(void){}
+
+void dma_mmap_init_map(void){}
+
+void dma_mmap_dma_is_supported(void){}
+
+void dma_mmap_map(void){}
+
+void dma_mmap_unmap(void){}
+
+void dma_mmap_memcpy(void){}
+
+void sdma_request_channel(void){}
+
+void sdma_set_device_handler(void){}
+
+void sdma_map_create_descriptor_ring(void){}
+
+void sdma_start_transfer(void){}
+
+void sdma_free_channel(void){}
+
+void sdma_mmap_unmap(void){}
