aaf7b240929fd14f7bdb750d654610e493ccf675
Srinivas Ramana <srinivasr@broadcom.com>
2011-06-27 17:24:03 +0530

    [clock] Changes to clock manager to accomodate PM and clock dependencies.
    [Defect / Enhancement / New Feature]
    Enhancement
    
    [Problem]
    clock modude need be upgraded to accomodate power manager dependecies and clock dependencies.
    [Solution]
    Made the clock manager module more generic and also changes the clock data
    structure to accomodate more dependencies and PM related fields.
    [Reviewers]
    Shameer P
    [Testing]
        [Test Cases]
    	Verified the below test cases
    	1. Boot up on Rhea ray board.
    	2. Clock enable/disable.
    	3. clock setrate/getrate
    
        [Label/CL# Synced To]
    	HEAD
        [Platform]
    	Rhea ray
    [BROADCOM INTERNAL NOTES]
    
    Signed-off-by: Srinivas Ramana <srinivasr@broadcom.com>

--- arch/arm/mach-rhea/rhea.c
+++ arch/arm/mach-rhea/rhea.c
@@ -73,16 +73,16 @@ void __init rhea_ray_timer_init (void)
 
 	/*
 	 * IMPORTANT:
-	 * If we have to use slave-timer as system timer, two modifications are required 
+	 * If we have to use slave-timer as system timer, two modifications are required
 	 * 1) modify the name of timer as, gpt_setup.name = "slave-timer";
 	 * 2) By default when the clock manager comes up it disables most of
 	 *    the clock. So if we switch to slave-timer we should prevent the
 	 *    clock manager from doing this. So, modify plat-kona/include/mach/clock.h
-	 * 
+	 *
 	 * By default aon-timer as system timer the following is the config
 	 * #define BCM2165x_CLK_TIMERS_FLAGS     (TYPE_PERI_CLK | SW_GATE | DISABLE_ON_INIT)
          * #define BCM2165x_CLK_HUB_TIMER_FLAGS  (TYPE_PERI_CLK | SW_GATE)
-	 * 
+	 *
 	 * change it as follows to use slave timer as system timer
 	 *
 	 * #define BCM2165x_CLK_TIMERS_FLAGS     (TYPE_PERI_CLK | SW_GATE)
@@ -91,7 +91,7 @@ void __init rhea_ray_timer_init (void)
 	gpt_setup.name   = "aon-timer";
 	gpt_setup.ch_num = 0;
 	gpt_setup.rate = CLOCK_TICK_RATE;
-	
+
 	/* Call the init function of timer module */
 	gp_timer_init(&gpt_setup);
 	profile_timer_init(IOMEM(KONA_PROFTMR_VA));
@@ -110,13 +110,10 @@ static int __init rhea_init(void)
 	rhea_l2x0_init();
 #endif
 
-#ifdef CONFIG_HAVE_CLK
-	clock_init();
-#endif
 	pinmux_init();
 
 #ifdef CONFIG_GPIOLIB
-	/* rhea has 4 banks of GPIO pins */ 
+	/* rhea has 4 banks of GPIO pins */
 	kona_gpio_init(4);
 #endif
 
