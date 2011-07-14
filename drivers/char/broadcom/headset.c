9ab27e41626eea6f7656e92a9fc45fb8d348a93e
Brendan Moran <bremoran@broadcom.com>
2011-07-14 12:16:50 -0700

    Initial addition of headset detection code.

---
+++ drivers/char/broadcom/headset.c
@@ -0,0 +1,440 @@
+/*****************************************************************************
+* Copyright 2011 Broadcom Corporation.  All rights reserved.
+*
+* Unless you and Broadcom execute a separate written software license
+* agreement governing use of this software, this software is licensed to you
+* under the terms of the GNU General Public License version 2, available at
+* http://www.broadcom.com/licenses/GPLv2.php (the "GPL").
+*
+* Notwithstanding the above, under no circumstances may you combine this
+* software in any way with any other Broadcom software provided under a
+* license other than the GPL, without Broadcom's express prior written
+* consent.
+*****************************************************************************/
+/**
+*
+*  @file    headset.c
+*
+*  @brief   Implements simple GPIO-based headset detection interface 
+*
+****************************************************************************/
+#ifndef CONFIG_BCM_HEADSET_SW
+#define CONFIG_BCM_HEADSET_SW y
+#endif
+/* ---- Include Files ---------------------------------------------------- */
+#include <linux/module.h>
+#include <linux/version.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/errno.h>
+#include <linux/interrupt.h>
+#include <linux/workqueue.h>
+#include <linux/spinlock.h>
+#include <linux/sched.h>
+#include <linux/fs.h>
+#include <linux/ioctl.h>
+#include <linux/poll.h>
+#include <linux/device.h>
+#include <linux/gpio.h>
+#include <linux/platform_device.h>
+#include <asm/gpio.h>
+#if defined(CONFIG_BCM_HEADSET_SW)
+   #include <linux/switch.h>
+#endif
+
+#include <linux/broadcom/bcm_major.h>
+#include <linux/broadcom/headset.h>
+#include <linux/broadcom/headset_cfg.h>
+
+/* ---- Public Variables ------------------------------------------------- */
+/* ---- Private Constants and Types -------------------------------------- */
+
+/* Headset channel info structure */
+struct headset_info
+{
+   atomic_t          avail;         /* Only allow single user because of select() */
+   int               changed;       /* Changed flag used by poll */
+   headset_state     state;         /* Current state */
+   wait_queue_head_t waitq;         /* wait queue */
+   struct headset_hw_cfg hw_cfg;    /* board hw configuration */
+};
+
+/* ---- Private Variables ------------------------------------------------ */
+
+#if CONFIG_SYSFS
+static struct class *headset_class;
+static struct device *headset_dev;
+#if defined(CONFIG_BCM_HEADSET_SW)
+static struct switch_dev headset_switch;
+#endif
+#endif
+
+/* Headset channel information */
+static struct headset_info gHeadset[1] =
+{
+   {
+      .avail         = ATOMIC_INIT( 1 ),
+   },
+};
+
+static spinlock_t detlock = SPIN_LOCK_UNLOCKED;
+
+/* ---- Private Function Prototypes -------------------------------------- */
+static int  headset_open( struct inode *inode, struct file *file );
+static int  headset_release( struct inode *inode, struct file *file );
+static long  headset_ioctl( struct file *file, unsigned int cmd, unsigned long arg );
+
+static unsigned int headset_poll( struct file *file, struct poll_table_struct *poll_table );
+
+#if defined(CONFIG_BCM_HEADSET_SW)
+static void setsw( struct work_struct *work );
+DECLARE_WORK(setsw_work, setsw);
+#endif
+
+/* File Operations (these are the device driver entry points) */
+struct file_operations headset_fops =
+{
+   .owner   = THIS_MODULE,
+   .open    = headset_open,
+   .release = headset_release,
+   .unlocked_ioctl = headset_ioctl,
+   .poll    = headset_poll,
+};
+
+/* ---- Functions -------------------------------------------------------- */
+
+/***************************************************************************/
+/**
+*  Helper function to schedule switch state changes for Android  
+*/
+#if defined(CONFIG_BCM_HEADSET_SW)
+static void setsw(struct work_struct *work){
+  struct headset_info *ch = gHeadset;
+  switch_set_state(&headset_switch,ch->state);  
+}
+#endif
+/***************************************************************************/
+/**
+*  Helper routine to check headset detection gpio  
+*/
+static void check_headset_det_gpio( struct headset_info *ch )
+{
+   if ( ch->hw_cfg.gpio_headset_det >= 0 )
+   {
+      int gpio_val;
+      unsigned long flags;
+
+      spin_lock_irqsave( &detlock, flags );
+      gpio_val = gpio_get_value( ch->hw_cfg.gpio_headset_det );
+      ch->state = gpio_val ? HEADSET_TOGGLE_A : HEADSET_UNPLUGGED;
+      ch->changed = 1;
+#if defined(CONFIG_BCM_HEADSET_SW)
+      schedule_work(&setsw_work);
+#endif
+      
+      
+      wake_up_interruptible( &ch->waitq );
+      spin_unlock_irqrestore( &detlock, flags );
+   }
+   else
+   {
+      ch->state = HEADSET_STATE_INIT;
+   }
+}
+
+/***************************************************************************/
+/**
+*  Driver open method
+*
+*  @remarks
+*/
+static int headset_open( struct inode *inode, struct file *file )
+{
+   struct headset_info *ch;
+
+   ch = gHeadset;
+
+   if ( ch->hw_cfg.gpio_headset_det < 0 )
+   {
+      /* Missing configuration */
+      return -EPERM;
+   }
+
+   /* Allow only 1 user because select() only works for single user */
+   if ( atomic_dec_and_test( &ch->avail ) == 0 )
+   {
+      atomic_inc( &ch->avail );
+      return -EBUSY;
+   }
+
+   file->private_data = ch;
+
+   check_headset_det_gpio( ch );
+
+   return 0;
+}
+
+/***************************************************************************/
+/**
+*  Driver release method
+*
+*  @remarks
+*/
+static int headset_release( struct inode *inode, struct file *file )
+{
+   struct headset_info *ch = file->private_data;
+
+   atomic_inc( &ch->avail );
+
+   return 0;
+}
+
+/***************************************************************************/
+/**
+*  Driver ioctl method
+*
+*  @remarks
+*/
+static long headset_ioctl( struct file *file, unsigned int cmd, unsigned long arg )
+{
+   struct headset_info *ch = file->private_data;
+
+   switch ( cmd )
+   {
+      case HEADSET_IOCTL_GET_STATE:
+         if ( copy_to_user( (unsigned long *)arg, &ch->state, sizeof(ch->state) ) != 0 )
+         {
+            return -EFAULT;
+         }
+         break;
+
+      default:
+         return -ENOTTY;
+   }
+   return 0;
+}
+
+/***************************************************************************/
+/**
+*  Driver poll method to support system select call
+*
+*  @remarks
+*/
+static unsigned int headset_poll(
+   struct file *file, 
+   struct poll_table_struct *poll_table 
+)
+{
+   struct headset_info *ch = file->private_data;
+
+   poll_wait( file, &ch->waitq, poll_table );
+
+   if ( ch->changed )
+   {
+      ch->changed = 0;
+      return POLLIN | POLLRDNORM;
+   }
+
+   return 0;
+}
+
+/***************************************************************************/
+/**
+*  Headset detection ISR
+*/
+static irqreturn_t headset_det_irq( int irq, void *dev_id )
+{
+   check_headset_det_gpio( dev_id );
+   return IRQ_HANDLED;
+}
+
+/***************************************************************************/
+/**
+*  Platform probe method
+*/
+static int __devinit headset_pltfm_probe(struct platform_device *pdev)
+{
+   struct headset_info *ch;
+   int ret;
+
+   BUG_ON( pdev == NULL );
+
+   if ( pdev->dev.platform_data == NULL )
+   {
+      dev_err( &pdev->dev, "platform_data missing\n" );
+      return -EFAULT;
+   }
+
+   ch = gHeadset;
+   memcpy( &ch->hw_cfg, (void *)pdev->dev.platform_data, sizeof(ch->hw_cfg) );
+
+   if ( ch->hw_cfg.gpio_headset_det >= 0 )
+   {
+      ret = gpio_request( ch->hw_cfg.gpio_headset_det, "headset det");
+      if ( ret < 0 )
+      {
+         dev_err( &pdev->dev, "Unable to request GPIO pin %d\n", ch->hw_cfg.gpio_headset_det );
+         return ret;
+      }
+
+      ret = request_irq( gpio_to_irq( ch->hw_cfg.gpio_headset_det ),
+            headset_det_irq, IRQF_TRIGGER_FALLING | IRQF_TRIGGER_RISING,
+            "headset_det", ch );
+      if ( ret )
+      {
+         dev_err( &pdev->dev, "Unable to request headset detect irq=%d for gpio=%d\n",
+               gpio_to_irq( ch->hw_cfg.gpio_headset_det ), ch->hw_cfg.gpio_headset_det );
+         goto err_free_cd_gpio;
+      }
+   }
+
+   return 0;
+
+err_free_cd_gpio:
+   gpio_free( ch->hw_cfg.gpio_headset_det );
+   return ret;
+}
+
+/***************************************************************************/
+/**
+*  Platform remove method
+*/
+static int __devexit headset_pltfm_remove( struct platform_device *pdev )
+{
+   struct headset_info *ch;
+   ch = gHeadset;
+
+   if ( ch->hw_cfg.gpio_headset_det >= 0 )
+   {
+      gpio_free( ch->hw_cfg.gpio_headset_det );
+   }
+   platform_set_drvdata( pdev, NULL );
+
+   return 0;
+}
+
+static struct platform_driver gPlatform_driver = 
+{
+   .driver = 
+   {
+      .name    = "bcmhana-headset",
+      .owner   = THIS_MODULE,
+   },
+   .probe      = headset_pltfm_probe,
+   .remove     = __devexit_p( headset_pltfm_remove ),
+};
+
+/***************************************************************************/
+/**
+*  Initialize APM headset detection
+*
+*  @remarks
+*/
+static int __init headset_init( void )
+{
+   int rc;
+   struct headset_info *ch;
+
+   init_waitqueue_head( &gHeadset[0].waitq );
+
+   rc = platform_driver_register( &gPlatform_driver );
+   if ( rc < 0 )
+   {
+      printk( "%s: failed to register platform driver\n", __FUNCTION__ );
+      return rc;
+   }
+
+   rc = register_chrdev( BCM_HEADSET_MAJOR, "headset", &headset_fops );
+   if ( rc < 0 )
+   {
+      printk( "failed for to register device major %d\n", BCM_HEADSET_MAJOR );
+      goto err_unregister_platform;
+   }
+
+#if CONFIG_SYSFS
+   headset_class = class_create( THIS_MODULE,"bcmhana-headset" );
+   if ( IS_ERR( headset_class ))
+   {
+      printk( "%s: Class create failed\n", __FUNCTION__ );
+      rc = -EFAULT;
+      goto err_unregister_chrdev;
+   }
+
+   headset_dev = device_create( headset_class, NULL, MKDEV(BCM_HEADSET_MAJOR, 0),
+         NULL, "headset0" );
+   if ( IS_ERR( headset_dev ))
+   {
+      printk(KERN_ERR "%s: Device create failed\n", __FUNCTION__ );
+      rc = -EFAULT;
+      goto err_class_destroy;
+   }
+#if defined(CONFIG_BCM_HEADSET_SW)
+      /* Create a headset switch */
+      headset_switch.name="h2w";
+      
+      rc = switch_dev_register(&headset_switch);
+      if (rc < 0) {
+         printk(KERN_ERR "HEADSET: Device switch create failed\n");
+         rc = -EFAULT;
+         goto err_dev_destroy;
+      }
+#endif
+   
+#endif
+   ch = gHeadset;
+   if ( ch->hw_cfg.gpio_headset_det < 0 )
+   {
+      /* Missing configuration */
+      rc = -EPERM;
+      printk(KERN_ERR "HEADSET: Detection GPIO not avilable\n");
+      goto err_no_gpio;
+
+   }
+   check_headset_det_gpio( ch );
+
+   return 0;
+
+err_no_gpio:   
+#if CONFIG_SYSFS
+#if defined(CONFIG_BCM_HEADSET_SW)
+   switch_dev_unregister(&headset_switch);
+err_dev_destroy:
+   device_destroy(headset_class, MKDEV( BCM_HEADSET_MAJOR, 0 ));
+#endif
+err_class_destroy:
+   class_destroy( headset_class );
+
+err_unregister_chrdev:
+   unregister_chrdev( BCM_HEADSET_MAJOR, "headset" );
+#endif
+
+err_unregister_platform:
+   platform_driver_unregister( &gPlatform_driver );
+
+   return rc;
+}
+
+/***************************************************************************/
+/**
+*  Destructor for the headset detection driver
+*
+*  @remarks
+*/
+static void __exit headset_exit( void )
+{
+#if CONFIG_SYSFS
+#if defined(CONFIG_BCM_HEADSET_SW)
+      switch_dev_unregister(&headset_switch);
+#endif
+
+   device_destroy( headset_class, MKDEV( BCM_HEADSET_MAJOR, 0 ));
+   class_destroy( headset_class );
+#endif
+
+   unregister_chrdev( BCM_HEADSET_MAJOR, "headset" );
+
+   platform_driver_unregister( &gPlatform_driver );
+}
+
+module_init( headset_init );
+module_exit( headset_exit );
