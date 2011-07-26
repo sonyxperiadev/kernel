e4e0d11c397e92d3370945e9d9029228372c9b06
Albert Su <asua@broadcom.com>
2011-07-26 11:13:50 -0700

    SWMAPYVR-63 - BCM28160 Tablet has vrious incorrect pinmux and hw settings
       * Removed ULP1 from pinmux for 28160
       * Fixed up LED GPIO settings
       * Added battery and headset override configuration for 28160

--- arch/arm/mach-island/hw_cfg/ISLAND_TABLET_28160/leds_gpio_settings.h
+++ arch/arm/mach-island/hw_cfg/ISLAND_TABLET_28160/leds_gpio_settings.h
@@ -1,10 +1,10 @@
 /*****************************************************************************
-* Copyright 2011 Broadcom Corporation.  All rights reserved.
+* Copyright 2009 Broadcom Corporation.  All rights reserved.
 *
 * Unless you and Broadcom execute a separate written software license
 * agreement governing use of this software, this software is licensed to you
 * under the terms of the GNU General Public License version 2, available at
-* http://www.broadcom.com/licenses/GPLv2.php (the "GPL").
+* http://www.broadcom.com/licenses/GPLv2.php (the "GPL"). 
 *
 * Notwithstanding the above, under no circumstances may you combine this
 * software in any way with any other Broadcom software provided under a
@@ -12,16 +12,23 @@
 * consent.
 *****************************************************************************/
 
-#ifndef HEADSET_SETTINGS_H
-#define HEADSET_SETTINGS_H
-
 /*
- * Refer to include/linux/broadcom/headset_cfg.h for details
+ * LED GPIO configurations
  */
 
-#define HW_CFG_HEADSET \
+#ifndef LEDS_GPIO_SETTINGS_H
+#define LEDS_GPIO_SETTINGS_H
+
+#define GPIO_LEDS_SETTINGS \
 { \
-   .gpio_headset_det = 63, \
-   .gpio_mic_det = 123, \
-}
-#endif /* HEADSET_SETTINGS_H */
+    { \
+        .name = "red", \
+        .gpio = 0, \
+    }, \
+    { \
+        .name = "green", \
+        .gpio = 127, \
+    } \
+};
+
+#endif /* LEDS_GPIO_SETTINGS_H */
