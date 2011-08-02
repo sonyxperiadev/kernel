48366f2411f17a2f088e4c4ec489ff617ccc9fdf
Vincent Quach <vincentq@broadcom.com>
2011-08-02 09:39:45 -0700

    SWMAPYVR-31 Removal of hardcoded character device major numbers Removed some device major numbers that are no longer needed

--- include/linux/broadcom/bcm_major.h
+++ include/linux/broadcom/bcm_major.h
@@ -34,7 +34,6 @@
 #define BCM_GE_MAJOR       150
 #define BCM_GEMEMALLOC_MAJOR     151
 
-#define BCM_BSC_MAJOR         199
 #define BCM_CNTIN_MAJOR		  200
 #define BCM_KRIL_MAJOR        201
 #define BCM_LOG_MAJOR	      202
@@ -46,13 +45,9 @@
 #define BCM_ALSA_VOIP_MAJOR   209
 #define BCM_VDEC_MAJOR        210
 #define BCM_VENC_MAJOR        211
-#define BCM_GPS_MAJOR         212
-#define BCM_AMXR_MAJOR        213
-#define BCM_AAA_MAJOR         214
 #define BCM_AACENC_MAJOR	226
 #define BCM_AUDIO_MAJOR	227
 #define BCM_WLAN_DRV_MAJOR    228
-#define BCM_GPIO_MAJOR        229
 #define BCM_MEMALLOC_MAJOR    230	/* used with Hantro driver BCM_VDEC_MAJOR */
 #define BCM_OTP_MAJOR         231
 #define BCM_VC03_MAJOR        232
@@ -61,7 +56,6 @@
 
 #define BCM_PM_MAJOR          235
 #define BCM_LED_MAJOR         236
-#define BCM_HEADSET_MAJOR     237
 #define BCM_RTC_MAJOR         238
 #define BCM_IODUMP_MAJOR      239
 #define BCM_SKEL_MAJOR        240
@@ -77,7 +71,6 @@
 #define BCM_POLYRINGER_MAJOR  250
 #define BCM_DLS_MAJOR         251
 #define BCM_SIM_MAJOR         252
-#define BCM_HALAUDIO_MAJOR    253
 #define BCM_VC02_MAJOR        254
 #define BCM_USB_I2C_MAJOR     255
 
