c76130552e6b375da7b4f1190c69b421744ed170
Pierre <pierre@broadcom.com>
2011-08-02 13:14:20 -0700

    hdmi: adding interface module and export the header for android build to work.  note that the hdmi detection driver is only available in the .36 kernel at this time, this is just to keep the build happy for the android code which is common between kernel versions.  we do not need hdmi support in .35 kernel under android at this time.

---
+++ include/linux/broadcom/hdmi.h
@@ -0,0 +1,71 @@
+/*****************************************************************************
+* Copyright 2011 Broadcom Corporation.  All rights reserved.
+*
+* Unless you and Broadcom execute a separate written software license
+* agreement governing use of this software, this software is licensed to you
+* under the terms of the GNU General Public License version 2, available at
+* http://www.broadcom.com/licenses/GPLv2.php (the "GPL"). 
+*
+* Notwithstanding the above, under no circumstances may you combine this
+* software in any way with any other Broadcom software provided under a
+* license other than the GPL, without Broadcom's express prior written
+* consent.
+*****************************************************************************/
+
+/*
+*
+*****************************************************************************
+*
+*  hdmi.h
+*
+*  PURPOSE:
+*
+*  This file defines the interface to the HDMI hotplug detect driver.
+*
+*  NOTES:
+*
+*****************************************************************************/
+
+
+#if !defined( LINUX_HDMI_H )
+#define LINUX_HDMI_H
+
+/* ---- Include Files ---------------------------------------------------- */
+
+#include <linux/ioctl.h>
+
+/* FIXME - handle hotplug assert pulse from connected state */
+
+typedef enum
+{
+    HDMI_UNPLUGGED = 0,  /* hdmi unplugged */
+    HDMI_CONNECTED,      /* hdmi connected */
+
+} hdmi_state;
+
+#define HDMI_STATE_INIT (hdmi_state)-1
+
+#ifdef __KERNEL__
+typedef enum
+{
+    HDMI_NULL = 0,       /* NULL event */
+    HDMI_REMOVED,        /* hdmi unplugged */
+    HDMI_INSERTED,       /* hdmi inserted */
+
+} hdmi_event;
+#endif
+
+/* ---- Constants and Types ---------------------------------------------- */
+
+#define HDMI_MAGIC   'h'
+
+#define HDMI_CMD_FIRST               0x80
+#define HDMI_CMD_GET_STATE           0x80
+#define HDMI_CMD_LAST                0x80
+
+#define HDMI_IOCTL_GET_STATE _IOR( HDMI_MAGIC, HDMI_CMD_GET_STATE, hdmi_state )
+
+/* ---- Variable Externs ------------------------------------------------- */
+/* ---- Function Prototypes ---------------------------------------------- */
+
+#endif  /* LINUX_HDMI_H */
