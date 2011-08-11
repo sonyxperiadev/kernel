dc8c7453210a75e23b9603c7a77d484692b85380
Sandeepa Prabhu <sprabhu@broadcom.com>
2011-08-11 06:00:31 -0700

    3.0 MIGRATION: Archictecture specific changes are migrated to mps-lmp-3.0
    [Defect / Enhancement / New Feature]
    
    [Problem]
    
    [Solution]
    
    [Reviewers]
    
    [Testing]
        [Test Cases]
    
        [Label/CL# Synced To]
    	mps-lmp-3.0
    
        [Platform]
    	KONA platforms
    
    [BROADCOM INTERNAL NOTES]
    
    Signed-off-by: Sandeepa Prabhu <sprabhu@broadcom.com>

--- arch/arm/kernel/smp_twd.c
+++ arch/arm/kernel/smp_twd.c
@@ -142,3 +142,19 @@ void __cpuinit twd_timer_setup(struct clock_event_device *clk)
 
 	clockevents_register_device(clk);
 }
+
+#ifdef CONFIG_HOTPLUG_CPU
+/*
+ * take a local timer down
+ */
+void twd_timer_stop(void)
+{
+	__raw_writel(0, twd_base + TWD_TIMER_CONTROL);
+}
+#endif
+
+unsigned long twd_get_timer_rate(void)
+{
+	return twd_timer_rate;
+}
+EXPORT_SYMBOL(twd_get_timer_rate);
