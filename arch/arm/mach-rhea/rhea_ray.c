8aff8978b7ba5366569b1483215f1f86c15968dd
Yang Liu <yangliu@broadcom.com>
2011-08-15 20:42:46 -0700

    PMEM: The Bralloc API is broken due to the PMEM change to pass address through kernel command line
    [Defect / Enhancement / New Feature]
    Defect
    [Problem]
    The PMEM change to pass address through command line break the BRALLOC API
    The command line is passsed through u-boot bootargs which is not changed.
    
    [Solution]
    Check if the command line is passing pmem address, if not, still keep the
    allocation through alloc_bootmem_pages
    
    [Reviewers]
    Arun KS, Conrad Smith
    [Testing]
        [Test Cases]
        Verified on camera test
        [Label/CL# Synced To]
        Top of mps-lmp-bsp
        [Platform]
        RheaRay
    [BROADCOM INTERNAL NOTES]

--- arch/arm/mach-rhea/rhea_ray.c
+++ arch/arm/mach-rhea/rhea_ray.c
@@ -559,13 +559,19 @@ static int __init setup_pmem_pages(char *str)
 	if(str)	{
 		pmem_size = memparse((const char *)str, &endp);
 		printk(KERN_INFO "PMEM size is   0x%08x Bytes\n", pmem_size);
-		if (*endp == '@')
+		if (*endp == '@') {
 			pmem_base =  memparse(endp + 1, NULL);
-			printk(KERN_INFO "PMEM starts at 0x%08x\n", (unsigned int)pmem_base);
-		} else	{
-			printk("\"pmem=\" option is not set!!!\n");
-			printk("Unable to determine the memory region for pmem!!!\n");
 		}
+		else {
+			pmem_base = virt_to_phys((void *)alloc_bootmem_pages(pmem_size));
+			if(!pmem_base)
+				printk(KERN_ERR "Failed to allocate the PMEM memory\n");			
+		}
+		printk(KERN_INFO "PMEM starts at 0x%08x\n", (unsigned int)pmem_base);
+	} else	{
+		printk("\"pmem=\" option is not set!!!\n");
+		printk("Unable to determine the memory region for pmem!!!\n");
+	}
 	return 0;
 }
 __setup("pmem=", setup_pmem_pages);
