05dbdb2dcffedd7b03311fed5b6a565f36ac5c4d
zhenz <zhenz@broadcom.com>
2011-08-23 17:30:55 -0700

    Add code for charging on Island
    [Defect / Enhancement / New Feature]
    
    MobC00162109
    
    [Problem]
    battery does not charge
    
    [Solution]
    Add code in PMU driver for charging on Island
    Also, update some modules(core,irq,core,etc) for PMU driver
    
    [Reviewers]
    Lun, Jian, Ismail,Shibaji
    
    [Testing]
        [Test Cases]
          Please note that the VWALL and VBUS are HW tied together for island, which means two interrupts will be generated although we only plug in one charger(either Wall or USB).
          By default USB has high priority, so if you want to test Wall charger, reconfigure MBCCTRL3 to 0x0c before the test
    
        [Label/CL# Synced To]
          Head of mps-lmp-bsp
        [Platform]
          islandStone
    [BROADCOM INTERNAL NOTES]

---
+++ drivers/power/bcmpmu-batt.c
@@ -0,0 +1,294 @@
+/*****************************************************************************
+*  Copyright 2001 - 2008 Broadcom Corporation.  All rights reserved.
+*
+*  Unless you and Broadcom execute a separate written software license
+*  agreement governing use of this software, this software is licensed to you
+*  under the terms of the GNU General Public License version 2, available at
+*  http://www.gnu.org/licenses/old-license/gpl-2.0.html (the "GPL").
+*
+*  Notwithstanding the above, under no circumstances may you combine this
+*  software in any way with any other Broadcom software provided under a
+*  license other than the GPL, without Broadcom's express prior written
+*  consent.
+*
+*****************************************************************************/
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/bug.h>
+#include <linux/device.h>
+#include <linux/delay.h>
+#include <linux/interrupt.h>
+#include <linux/workqueue.h>
+#include <linux/fs.h>
+
+#include <linux/mfd/bcmpmu.h>
+#define BCMPMU_PRINT_ERROR (1U << 0)
+#define BCMPMU_PRINT_INIT (1U << 1)
+#define BCMPMU_PRINT_FLOW (1U << 2)
+#define BCMPMU_PRINT_DATA (1U << 3)
+
+static int debug_mask = BCMPMU_PRINT_ERROR | BCMPMU_PRINT_INIT;
+#define pr_batt(debug_level, args...) \
+	do { \
+		if (debug_mask & BCMPMU_PRINT_##debug_level) { \
+			pr_info(args); \
+		} \
+	} while (0)
+
+struct bcmpmu_batt {
+	struct bcmpmu *bcmpmu;
+	struct power_supply batt;
+	struct bcmpmu_batt_state state;
+	wait_queue_head_t wait;
+	struct mutex lock;
+};
+
+static void bcmpmu_batt_isr(enum bcmpmu_irq irq, void *data)
+{
+	struct bcmpmu_batt *pbatt = data;
+	int ret;
+			
+	if (irq == PMU_IRQ_RTM_DATA_RDY) {
+	}
+	else if ((irq == PMU_IRQ_RTM_IN_CON_MEAS) ||
+		(irq == PMU_IRQ_RTM_UPPER) ||
+		(irq == PMU_IRQ_RTM_IGNORE) ||
+		(irq == PMU_IRQ_RTM_OVERRIDDEN)) {
+	}
+	else {
+	}
+}
+
+static enum power_supply_property bcmpmu_batt_props[] = {
+	POWER_SUPPLY_PROP_TECHNOLOGY,
+	POWER_SUPPLY_PROP_STATUS,
+	POWER_SUPPLY_PROP_PRESENT,
+	POWER_SUPPLY_PROP_CAPACITY,
+	POWER_SUPPLY_PROP_CAPACITY_LEVEL,
+	POWER_SUPPLY_PROP_VOLTAGE_NOW,
+	POWER_SUPPLY_PROP_TEMP,
+	POWER_SUPPLY_PROP_HEALTH,
+};
+
+static int bcmpmu_get_batt_property(struct power_supply *battery,
+		enum power_supply_property property,
+		union power_supply_propval *propval)
+{
+	int ret = 0;
+	struct bcmpmu_batt *pbatt =
+		container_of(battery, struct bcmpmu_batt, batt);
+
+	switch (property) {
+	case POWER_SUPPLY_PROP_STATUS:
+		propval->intval = pbatt->state.status;
+		break;
+
+	case POWER_SUPPLY_PROP_TECHNOLOGY:
+		propval->intval = POWER_SUPPLY_TECHNOLOGY_LION;
+		break;
+
+	case POWER_SUPPLY_PROP_CAPACITY:
+		propval->intval = pbatt->state.capacity;
+		break;
+
+	case POWER_SUPPLY_PROP_CAPACITY_LEVEL:
+		propval->intval = pbatt->state.capacity_lvl;
+		break;
+
+	case POWER_SUPPLY_PROP_VOLTAGE_NOW:
+		propval->intval = pbatt->state.voltage;
+		break;
+
+	case POWER_SUPPLY_PROP_TEMP:
+		propval->intval = pbatt->state.temp;
+		break;
+
+	case POWER_SUPPLY_PROP_HEALTH:
+		propval->intval = pbatt->state.health;
+		break;
+
+	case POWER_SUPPLY_PROP_PRESENT:
+		propval->intval = pbatt->state.present;
+		break;
+	default:
+		ret = -EINVAL;
+		break;
+	}
+
+	return ret;
+}
+
+static int bcmpmu_set_batt_property(struct power_supply *ps,
+		enum power_supply_property property,
+		union power_supply_propval *propval)
+{
+	int ret = 0;
+	struct bcmpmu_batt *pbatt = container_of(ps,
+		struct bcmpmu_batt, batt);
+	switch (property) {
+	case POWER_SUPPLY_PROP_STATUS:
+		pbatt->state.status = propval->intval;
+		break;
+
+	case POWER_SUPPLY_PROP_CAPACITY:
+		pbatt->state.capacity = propval->intval;
+		break;
+
+	case POWER_SUPPLY_PROP_CAPACITY_LEVEL:
+		pbatt->state.capacity_lvl = propval->intval;
+		break;
+
+	case POWER_SUPPLY_PROP_VOLTAGE_NOW:
+		pbatt->state.voltage = propval->intval;
+		break;
+
+	case POWER_SUPPLY_PROP_TEMP:
+		pbatt->state.temp = propval->intval;
+		break;
+
+	case POWER_SUPPLY_PROP_HEALTH:
+		pbatt->state.health = propval->intval;
+		break;
+
+	case POWER_SUPPLY_PROP_PRESENT:
+		pbatt->state.present = propval->intval;
+		break;
+
+	default:
+		ret = -EINVAL;
+		break;
+	}
+}
+#ifdef CONFIG_MFD_BCMPMU_DBG
+static ssize_t
+dbgmsk_show(struct device *dev, struct device_attribute *attr,
+				char *buf)
+{
+	return sprintf(buf, "debug_mask is %x\n", debug_mask);
+}
+
+static ssize_t
+dbgmsk_set(struct device *dev, struct device_attribute *attr,
+				char *buf, size_t count)
+{
+	unsigned long val = simple_strtoul(buf, NULL, 0);
+	if (val > 0xFF || val == 0)
+		return -EINVAL;
+	debug_mask = val;
+	return count;
+}
+
+static DEVICE_ATTR(dbgmsk, 0644, dbgmsk_show, dbgmsk_set);
+static struct attribute *bcmpmu_batt_attrs[] = {
+	&dev_attr_dbgmsk.attr,
+	NULL
+};
+static const struct attribute_group bcmpmu_batt_attr_group = {
+	.attrs = bcmpmu_batt_attrs,
+};
+#endif
+
+static int __devinit bcmpmu_batt_probe(struct platform_device *pdev)
+{
+	int ret;
+
+	struct bcmpmu *bcmpmu = pdev->dev.platform_data;
+	struct bcmpmu_batt *pbatt;
+	
+	printk("bcmpmu_batt: batt_probe called \n") ;
+
+	pbatt = kzalloc(sizeof(struct bcmpmu_batt), GFP_KERNEL);
+	if (pbatt == NULL) {
+		printk("bcmpmu_batt: failed to alloc mem.\n") ;
+		return -ENOMEM;
+	}
+	init_waitqueue_head(&pbatt->wait);
+	mutex_init(&pbatt->lock);
+	pbatt->bcmpmu = bcmpmu;
+	bcmpmu->battinfo = (void *)pbatt;
+	
+	pbatt->batt.properties = bcmpmu_batt_props;
+	pbatt->batt.num_properties = ARRAY_SIZE(bcmpmu_batt_props);
+	pbatt->batt.get_property = bcmpmu_get_batt_property;
+	pbatt->batt.set_property = bcmpmu_set_batt_property;
+	pbatt->batt.name = "battery";
+	pbatt->batt.type = POWER_SUPPLY_TYPE_BATTERY;
+	
+	ret = power_supply_register(&pdev->dev, &pbatt->batt);
+	if (ret)
+		goto err;
+
+	bcmpmu->register_irq(bcmpmu, PMU_IRQ_BATINS, bcmpmu_batt_isr, pbatt);
+	bcmpmu->register_irq(bcmpmu, PMU_IRQ_BATRM, bcmpmu_batt_isr, pbatt);
+	bcmpmu->register_irq(bcmpmu, PMU_IRQ_GBAT_PLUG_IN, bcmpmu_batt_isr, pbatt);
+	bcmpmu->register_irq(bcmpmu, PMU_IRQ_SMPL_INT, bcmpmu_batt_isr, pbatt);
+	bcmpmu->register_irq(bcmpmu, PMU_IRQ_MBTEMPLOW, bcmpmu_batt_isr, pbatt);
+	bcmpmu->register_irq(bcmpmu, PMU_IRQ_MBTEMPHIGH, bcmpmu_batt_isr, pbatt);
+	bcmpmu->register_irq(bcmpmu, PMU_IRQ_MBOV, bcmpmu_batt_isr, pbatt);
+	bcmpmu->register_irq(bcmpmu, PMU_IRQ_MBTEMPLOW, bcmpmu_batt_isr, pbatt);
+	bcmpmu->register_irq(bcmpmu, PMU_IRQ_MBTEMPLOW, bcmpmu_batt_isr, pbatt);
+	bcmpmu->register_irq(bcmpmu, PMU_IRQ_MBOV_DIS, bcmpmu_batt_isr, pbatt);
+	bcmpmu->register_irq(bcmpmu, PMU_IRQ_MBWV_R_10S_WAIT, bcmpmu_batt_isr, pbatt);
+	bcmpmu->register_irq(bcmpmu, PMU_IRQ_BBLOW, bcmpmu_batt_isr, pbatt);
+	bcmpmu->register_irq(bcmpmu, PMU_IRQ_LOWBAT, bcmpmu_batt_isr, pbatt);
+	bcmpmu->register_irq(bcmpmu, PMU_IRQ_VERYLOWBAT, bcmpmu_batt_isr, pbatt);
+
+#ifdef CONFIG_MFD_BCMPMU_DBG
+	sysfs_create_group(&pdev->dev.kobj, &bcmpmu_batt_attr_group);
+#endif
+	return 0;
+
+err:
+	return ret;
+}
+
+static int __devexit bcmpmu_batt_remove(struct platform_device *pdev)
+{
+	struct bcmpmu *bcmpmu = pdev->dev.platform_data;
+	struct bcmpmu_batt *pbatt = bcmpmu->battinfo;
+
+	power_supply_unregister(&pbatt->batt);
+
+	bcmpmu->unregister_irq(bcmpmu, PMU_IRQ_BATINS);
+	bcmpmu->unregister_irq(bcmpmu, PMU_IRQ_BATRM);
+	bcmpmu->unregister_irq(bcmpmu, PMU_IRQ_GBAT_PLUG_IN);
+	bcmpmu->unregister_irq(bcmpmu, PMU_IRQ_SMPL_INT);
+	bcmpmu->unregister_irq(bcmpmu, PMU_IRQ_MBTEMPLOW);
+	bcmpmu->unregister_irq(bcmpmu, PMU_IRQ_MBTEMPHIGH);
+	bcmpmu->unregister_irq(bcmpmu, PMU_IRQ_MBOV);
+	bcmpmu->unregister_irq(bcmpmu, PMU_IRQ_MBTEMPLOW);
+	bcmpmu->unregister_irq(bcmpmu, PMU_IRQ_MBTEMPLOW);
+	bcmpmu->unregister_irq(bcmpmu, PMU_IRQ_MBOV_DIS);
+	bcmpmu->unregister_irq(bcmpmu, PMU_IRQ_MBWV_R_10S_WAIT);
+	bcmpmu->unregister_irq(bcmpmu, PMU_IRQ_BBLOW);
+	bcmpmu->unregister_irq(bcmpmu, PMU_IRQ_LOWBAT);
+	bcmpmu->unregister_irq(bcmpmu, PMU_IRQ_VERYLOWBAT);
+	
+	return 0;
+}
+
+static struct platform_driver bcmpmu_batt_driver = {
+	.driver = {
+		.name = "bcmpmu_batt",
+	},
+	.probe = bcmpmu_batt_probe,
+	.remove = __devexit_p(bcmpmu_batt_remove),
+};
+
+static int __init bcmpmu_batt_init(void)
+{
+	return platform_driver_register(&bcmpmu_batt_driver);
+}
+module_init(bcmpmu_batt_init);
+
+static int __exit bcmpmu_batt_exit(void)
+{
+	platform_driver_unregister(&bcmpmu_batt_driver);
+	return 0;
+}
+module_exit(bcmpmu_batt_exit);
+
+MODULE_DESCRIPTION("BCM PMIC battery driver");
+MODULE_LICENSE("GPL");
