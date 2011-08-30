3a667f388c477723206f5a8af5ca79ac5f6936a1
Zhengran Li <zhengran@broadcom.com>
2011-08-30 16:29:49 -0700

    [islandsonte] add GPIO_171 pinmux setting for HDMI hotplug detection

--- arch/arm/mach-island/pinmux_island_ff.c
+++ arch/arm/mach-island/pinmux_island_ff.c
@@ -43,6 +43,10 @@ static struct __init pin_config board_pin_config[] = {
 
 	/* SIM DATA needs a pull up */
 	PIN_CFG(SIM_DATA, SIM_DATA, 0, OFF, ON, 0, 0, 12MA),
+
+        /* HDMI Hotplug Detect */
+        PIN_CFG(NORFLSH_ADDR_19, GPIO, 0, OFF, OFF, 0, 0, 8MA),
+
 };
 
 /* board level init */
