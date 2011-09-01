841b8b959ac4b5002be5486bdd525973874593d4
Denny Yang <yangd@broadcom.com>
2011-09-01 15:34:13 -0700

    S41: IPC: memory map change to shift IPC area
    [Defect / Enhancement / New Feature]
    MobC00165565
    
    [Problem]
    Clipper can't boot-up with mps-lmp-bsp. IPC memory mismatch
    with CP image.
    
    [Solution]
    IPC 2MB area is shifted into 32MB area so the base address of IPC becomes 0xA1E00000.
    
    [Reviewers]
    Babu Arunachalam, Conrad Smith, Vicent Dang
    
    [Testing]
        [Test Cases]
        Boot-up test w/ Android build and camp test.
    
        [Label/CL# Synced To]
    
        [Platform]
        Rhea S41 LMP, Rhea S41 Android
    
    [BROADCOM INTERNAL NOTES]
    N/A

--- include/linux/broadcom/platform_mconfig_rhea.h
+++ include/linux/broadcom/platform_mconfig_rhea.h
@@ -207,7 +207,10 @@ extern "C" {
 #define AP_SH_BASE                             (DSP_SH_BASE+CP_SH_SIZE)
 #define AP_SH_SIZE                                             0x00020000    // 128k  AP/DSP sharedmem
 
-#define IPC_BASE                               (EXT_RAM_BASE_ADDR+0x01E00000)      // 256kB IPC shared RAM
+/* 256kB IPC shared RAM */
+#define IPC_OFFSET             CONFIG_BCM_IPC_OFFSET
+
+#define IPC_BASE                               (EXT_RAM_BASE_ADDR + IPC_OFFSET)
 #define IPC_SIZE                                               0x00200000
 
 
