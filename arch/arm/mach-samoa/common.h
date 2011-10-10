41f6d321f3ea200df289bb47ac73042b5753f3e1
Joyjit Nath <joyjit@broadcom.com>
2011-10-09 18:07:04 -0700

    [samoa] Remove all Samoa related files
    Signed-off-by: Joyjit Nath <joyjit@broadcom.com>

--- arch/arm/mach-samoa/common.h
+++
@@ -1,6 +0,0 @@
-#ifndef __MACH_SAMOA_COMMON_H
-#define __MACH_SAMOA_COMMON_H
-
-void __init board_add_common_devices(void);
-
-#endif /* __MACH_SAMOA_COMMON_H */
--- arch/arm/mach-samoa/include/mach/pm.h
+++
@@ -1,6 +0,0 @@
-#ifndef __RHEA_PM_H__
-#define __RHEA_PM_H__
-
-#define KONA_MACH_MAX_IDLE_STATE 1
-
-#endif /*__RHEA_PM_H__*/
