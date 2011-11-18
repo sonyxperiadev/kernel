b1f0f27f2daff0a508d2e2e694d4d4f0464bd971
shameer <shameer@broadcom.com>
2011-11-18 17:20:39 +0530

    Rhea PWRMGR : Enabled dummy HW sequencer
    [Defect / Enhancement / New Feature]
    
    Defect
    
    [Problem]
    PMU I2C HS mode is not enabled yet. USe Dummy sequencer to avoid sequencer voltage ramp up delay.
    Delay in voltage ramp up may cause CP to crash if deep sleep is enabled
    
    [Solution]
    
    Use dummy HW sequencer till PMU I2C HS mode is enabled
    
    [Reviewers]
    
    Srinivas
    
    [Testing]
        [Test Cases]
    
    Verified by reading back HW sequencer microcode
        [Label/CL# Synced To]
    
        [Platform]
    
    Rhea
    
    [BROADCOM INTERNAL NOTES]

--- arch/arm/mach-rhea/rhea_pwr_mgr.c
+++ arch/arm/mach-rhea/rhea_pwr_mgr.c
@@ -234,6 +234,78 @@ static const struct rhea_event_table event_table[] = {
 
 };
 
+#ifdef CONFIG_RHEA_PWRMGR_USE_DUMMY_SEQ
+
+static struct i2c_cmd i2c_dummy_seq_cmd[] =
+						{
+							{REG_ADDR,0},		//0 - NOP
+							{JUMP_VOLTAGE,0},	//1 - jump to address based on current voltage request
+							{REG_ADDR,0},		//other:2 - NOP
+							{SET_PC_PINS,0xC0},	//3 - Set PC3/4 pins to 0 to begin transaction (HW has semaphore)
+							{REG_ADDR,0},		//4 - NOP
+							{REG_ADDR,0},	//5 - Set Address for i2cmmhs BSC_CS register
+							{REG_ADDR,0},	//6 - Set Start condition - write 3 to CS register
+							{REG_ADDR,0},	//7 - Wait..
+							{REG_ADDR,0},  		//8 - Clear Start Condition - write 1 to CS
+							{REG_ADDR,0},	//9 - Wait..
+							{REG_ADDR,0},	//10 - PMU client addr  - 8 ..write to FIFO
+							{REG_ADDR,0},	//11 - Wait..
+							{REG_ADDR,0},	//12 - PMU register addr C0 (CSRSTRL1)
+							{REG_ADDR,0},	//13 - Wait..
+							{REG_ADDR,0},		//14 - Data - Write the requested voltage
+							{REG_ADDR,0},	//15 - Wait..
+							{SET_PC_PINS,0xCC},	//16 - Set PC3/4 to 1 to signal End of transaction
+							{END,0},			//17 - End
+							{REG_ADDR,0},		//18 - NOP
+							{REG_ADDR,0},		//19 - NOP
+							{REG_ADDR,0},		//20 - NOP
+							{REG_ADDR,0},		//21 - NOP
+							{REG_ADDR,0},		//22 - NOP
+							{REG_ADDR,0},		//23 - NOP
+							{REG_ADDR,0},		//24 - NOP
+							{REG_ADDR,0},		//25 - NOP
+							{REG_ADDR,0},		//26 - NOP
+							{REG_ADDR,0},		//27 - NOP
+							{REG_ADDR,0},		//28 - NOP
+							{REG_ADDR,0},		//29 - NOP
+							{REG_ADDR,0},		//30 - NOP
+							{REG_ADDR,0},		//31 - NOP
+							{REG_ADDR,0},		//32 - NOP
+							{REG_ADDR,0},		//33 - NOP
+							{REG_ADDR,0},		//34 - NOP
+							{REG_ADDR,0},		//35 - NOP
+							{REG_ADDR,0},		//36 - NOP
+							{REG_ADDR,0},		//37 - NOP
+							{REG_ADDR,0},		//38 - NOP
+							{REG_ADDR,0},		//39 - NOP
+							{REG_ADDR,0},		//40 - NOP
+							{REG_ADDR,0},		//41 - NOP
+							{REG_ADDR,0},		//42 - NOP
+							{REG_ADDR,0},		//43 - NOP
+							{REG_ADDR,0},		//44 - NOP
+							{REG_ADDR,0},		//45 - NOP
+							{REG_ADDR,0},		//46 - NOP
+							{REG_ADDR,0},		//47 - NOP
+							{REG_ADDR,0},		//48 - NOP
+							{REG_ADDR,0},		//49 - NOP
+							{REG_ADDR,0},		//50 - NOP
+							{REG_ADDR,0},		//51 - NOP
+							{REG_ADDR,0},		//set2/zero:52 - NOP
+							{SET_PC_PINS,0x30},	//53 - Set PC1 pins to 0
+							{REG_ADDR,0},		//54 -NOP
+							{END,0},			//55 -END
+							{REG_ADDR,0},		//set1:56 -NOP
+							{SET_PC_PINS,0x31},	//57 - Set PC1 pins to 1
+							{REG_ADDR,0},		//58 -NOP
+							{END,0},			//59 -END
+							{REG_ADDR,0},		//60 - NOP
+							{REG_ADDR,0},		//61 - NOP
+							{REG_ADDR,0},		//62 - NOP
+							{REG_ADDR,0},		//63 - NOP
+
+						  };
+
+#endif /*CONFIG_RHEA_PWRMGR_USE_DUMMY_SEQ*/
 
 struct pwr_mgr_info rhea_pwr_mgr_info = {
 	.num_pi = PI_MGR_PI_ID_MAX,
@@ -255,15 +327,32 @@ int __init rhea_pwr_mgr_init()
 #ifdef CONFIG_RHEA_A0_PM_ASIC_WORKAROUND
 	u32 reg_val = 0;
 #endif
+#ifdef CONFIG_RHEA_PWRMGR_USE_DUMMY_SEQ
+	struct v0x_spec_i2c_cmd_ptr dummy_seq_v0_ptr =
+		{
+			.other_ptr = 2,
+			.set2_val = 1, /*Retention voltage inx*/
+			.set2_ptr = 52,
+			.set1_val = 2,/*wakeup from retention voltage inx*/
+			.set1_ptr = 56,
+			.zerov_ptr = 52, /*Not used for Rhea*/
+		};
+#endif /*CONFIG_RHEA_PWRMGR_USE_DUMMY_SEQ*/
 	struct pm_policy_cfg cfg;
 	cfg.ac = 1;
 	cfg.atl = 0;
-
+#ifdef CONFIG_RHEA_PWRMGR_USE_DUMMY_SEQ
+	rhea_pwr_mgr_info.i2c_cmds = i2c_dummy_seq_cmd ;
+	rhea_pwr_mgr_info.num_i2c_cmds = ARRAY_SIZE(i2c_dummy_seq_cmd);
+	rhea_pwr_mgr_info.i2c_cmd_ptr[VOLT0] = &dummy_seq_v0_ptr;
+#else
 	rhea_pwr_mgr_info.i2c_cmds = pwrmgr_init_param.cmd_buf;
 	rhea_pwr_mgr_info.num_i2c_cmds = pwrmgr_init_param.cmb_buf_size;
+	rhea_pwr_mgr_info.i2c_cmd_ptr[VOLT0] = pwrmgr_init_param.v0ptr;
+#endif /*CONFIG_RHEA_PWRMGR_USE_DUMMY_SEQ*/
 	rhea_pwr_mgr_info.i2c_var_data = pwrmgr_init_param.def_vlt_tbl;
 	rhea_pwr_mgr_info.num_i2c_var_data = pwrmgr_init_param.vlt_tbl_size;
-	rhea_pwr_mgr_info.i2c_cmd_ptr[VOLT0] = pwrmgr_init_param.v0ptr;
+
 #if defined(CONFIG_KONA_PWRMGR_REV2)
 	rhea_pwr_mgr_info.i2c_rd_off = pwrmgr_init_param.i2c_rd_off;
 	rhea_pwr_mgr_info.i2c_rd_slv_addr_off = pwrmgr_init_param.i2c_rd_slv_addr_off;
@@ -317,7 +406,6 @@ int __init rhea_pwr_mgr_init()
 
 	pwr_mgr_set_pc_clkreq_override(PC1,true,1);
 	pwr_mgr_pm_i2c_enable(true);
-
 	/*Init event table*/
 	for(i = 0; i < ARRAY_SIZE(event_table);i++)
 	{
