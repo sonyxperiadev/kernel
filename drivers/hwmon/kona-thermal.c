d9cf37b7f4243964ca1e31febeead8aefc4b089f
rumeek <rumeek@broadcom.com>
2011-11-18 18:34:59 -0800

    Checkpatch updates to thermal mgmt

--- drivers/hwmon/kona-thermal.c
+++ drivers/hwmon/kona-thermal.c
@@ -41,30 +41,29 @@
 #define USE_INTERRUPT
 
 #define therm_debug(format...)              \
-	do {                            \
-	    if (thermal_debug)          	\
-		printk(format); 	\
-	} while(0)
+    do {                            \
+        if (thermal_debug)          	\
+        printk(format); 	\
+    } while (0)
 
-int therm_tbl[] =
-{
+int therm_tbl[] = {
 /* 	x0	x1	x2	x3	x4	x5	x6	x7	x8	x9	xA	xB	xC	xD	xE	xF*/
-/*0x*/	2302,1631,1326,1137,1038,964,905,856,815,779,747,718,693,669,647,627,
-/*1x*/	609,591,575,559,545,531,518,506,494,482,471,461,451,441,432,422,
-/*2x*/	414,405,397,388,381,373,366,358,351,345,338,331,325,318,312,306,
-/*3x*/	300,295,289,283,278,272,267,262,257,252,247,242,237,232,227,223,
-/*4x*/	218,214,209,205,201,196,192,188,184,179,175,171,167,163,159,156,
-/*5x*/	152,148,144,141,137,133,129,126,122,119,115,112,108,105,101,98,
-/*6x*/	95,91,88,84,81,78,75,71,68,65,62,58,55,52,49,46,
-/*7x*/	43,39,36,33,30,27,24,21,18,15,12,9,6,3,0,-3,
-/*8x*/	-6,-9,-12,-15,-18,-21,-24,-27,-30,-33,-36,-39,-41,-44,-47,-50,
-/*9x*/	-53,-56,-59,-62,-65,-68,-71,-74,-77,-80,-83,-86,-89,-92,-95,-98,
-/*Ax*/	-101,-104,-107,-110,-113,-116,-119,-122,-125,-128,-131,-134,-137,-140,-144,-147,
-/*Bx*/	-150,-153,-156,-160,-163,-166,-169,-173,-176,-179,-183,-186,-189,-193,-196,-200,
-/*Cx*/	-203,-207,-210,-214,-218,-221,-225,-229,-232,-236,-240,-244,-248,-252,-256,-260,
-/*Dx*/	-264,-269,-273,-277,-281,-286,-290,-295,-300,-304,-309,-314,-319,-324,-330,-335,
-/*Ex*/	-340,-346,-352,-358,-364,-370,-377,-383,-389,-397,-406,-412,-418,-425,-431,-439,
-/*Fx*/	-447,-455,-464,-473,-483,-495,-507,-520,-536,-554,-574,-599,-630,-673,-742,-750
+/*0x*/	2302, 1631, 1326, 1137, 1038, 964, 905, 856, 815, 779, 747, 718, 693, 669, 647, 627,
+/*1x*/	609, 591, 575, 559, 545, 531, 518, 506, 494, 482, 471, 461, 451, 441, 432, 422,
+/*2x*/	414, 405, 397, 388, 381, 373, 366, 358, 351, 345, 338, 331, 325, 318, 312, 306,
+/*3x*/	300, 295, 289, 283, 278, 272, 267, 262, 257, 252, 247, 242, 237, 232, 227, 223,
+/*4x*/	218, 214, 209, 205, 201, 196, 192, 188, 184, 179, 175, 171, 167, 163, 159, 156, 
+/*5x*/	152, 148, 144, 141, 137, 133, 129, 126, 122, 119, 115, 112, 108, 105, 101, 98, 
+/*6x*/	95, 91, 88, 84, 81, 78, 75, 71, 68, 65, 62, 58, 55, 52, 49, 46, 
+/*7x*/	43, 39, 36, 33, 30, 27, 24, 21, 18, 15, 12, 9, 6, 3, 0, -3, 
+/*8x*/	-6, -9, -12, -15, -18, -21, -24, -27, -30, -33, -36, -39, -41, -44, -47, -50, 
+/*9x*/	-53, -56, -59, -62, -65, -68, -71, -74, -77, -80, -83, -86, -89, -92, -95, -98, 
+/*Ax*/	-101, -104, -107, -110, -113, -116, -119, -122, -125, -128, -131, -134, -137, -140, -144, -147, 
+/*Bx*/	-150, -153, -156, -160, -163, -166, -169, -173, -176, -179, -183, -186, -189, -193, -196, -200, 
+/*Cx*/	-203, -207, -210, -214, -218, -221, -225, -229, -232, -236, -240, -244, -248, -252, -256, -260, 
+/*Dx*/	-264, -269, -273, -277, -281, -286, -290, -295, -300, -304, -309, -314, -319, -324, -330, -335, 
+/*Ex*/	-340, -346, -352, -358, -364, -370, -377, -383, -389, -397, -406, -412, -418, -425, -431, -439, 
+/*Fx*/	-447, -455, -464, -473, -483, -495, -507, -520, -536, -554, -574, -599, -630, -673, -742, -750
 };
 
 struct platform_drvdata {
@@ -76,11 +75,10 @@ struct platform_drvdata {
 };
 
 static int thermal_test_panic(struct notifier_block *this, unsigned long event,
-      void *ptr);
+                              void *ptr);
 
-static struct notifier_block thermal_test_block =
-{
-   .notifier_call = thermal_test_panic,
+static struct notifier_block thermal_test_block = {
+    .notifier_call = thermal_test_panic,
 };
 
 
@@ -92,22 +90,20 @@ ATOMIC_NOTIFIER_HEAD(thermal_notifier_list_4);
 struct atomic_notifier_head therm_notifier[4];
 
 static struct timer_list thermal_timer;
-struct device* test_dev;
-static int thermal_debug = 0;
-int kona_thermal_debug_init(void);
+struct device *test_dev;
+static int thermal_debug;
 
 int convert_adc_to_temp(int raw)
 {
-    return (therm_tbl[raw/4]*100);
+    return therm_tbl[raw/4]*100;
 }
 
 int convert_temp_to_adc(int raw)
 {
     int i;
 
-    for (i=0; i < sizeof(therm_tbl); i++) 
-    {
-        if ((therm_tbl[i] >= raw/10) && (therm_tbl[i+1] <= raw/10)){
+    for (i=0; i < sizeof(therm_tbl); i++) {
+        if ((therm_tbl[i] >= raw/10) && (therm_tbl[i+1] <= raw/10)) {
             therm_debug("new table_offset = 0x%x\n", i+1);
             return ((i+1)*4);
         }
@@ -121,72 +117,71 @@ static ssize_t kona_thermal_get_name( struct device *dev, struct device_attribut
     struct sensor_device_attribute_2 *attr = to_sensor_dev_attr_2(devattr);
     struct therm_data               *board;
 
-	board = pdata->pdev->dev.platform_data;
-	return sprintf(buf, "%s\n", board->sensors[attr->index].thermal_name);
+    board = pdata->pdev->dev.platform_data;
+    return sprintf(buf, "%s\n", board->sensors[attr->index].thermal_name);
 }
 
 static ssize_t kona_thermal_get_threshold( struct device *dev, struct device_attribute *devattr, char *buf)
 {
-        int long raw = 0;
-        struct platform_drvdata *pdata = dev_get_drvdata(dev);
-        struct sensor_device_attribute_2 *attr = to_sensor_dev_attr_2(devattr);
-        struct therm_data               *board;
+    int long raw = 0;
+    struct platform_drvdata *pdata = dev_get_drvdata(dev);
+    struct sensor_device_attribute_2 *attr = to_sensor_dev_attr_2(devattr);
+    struct therm_data               *board;
 
-        board = pdata->pdev->dev.platform_data;
+    board = pdata->pdev->dev.platform_data;
 
-        if (attr->nr == 1) {
-                raw = board->sensors[attr->index].thermal_warning_lvl_1;
+    if (attr->nr == 1) {
+        raw = board->sensors[attr->index].thermal_warning_lvl_1;
+    }
+    else if (attr->nr == 2) {
+        raw = board->sensors[attr->index].thermal_fatal_lvl;
+    }
+    else {
+        if (board->sensors[attr->index].thermal_read == SENSOR_READ_PMU_I2C) {
+            raw = bcm59055_saradc_read_data(board->sensors[attr->index].thermal_sensor_param);
+            therm_debug("temp (deg. Celsius) = %d (raw = %ld (0x%lx) (0x%lx)\n", convert_adc_to_temp(raw),raw, raw, raw/4);
+            return sprintf(buf, "%d\n", convert_adc_to_temp(raw));
         }
-        else if (attr->nr == 2) {
-                raw = board->sensors[attr->index].thermal_fatal_lvl;
-        }
-        else {
-            if (board->sensors[attr->index].thermal_read == SENSOR_READ_PMU_I2C) 
-            {
-                raw = bcm59055_saradc_read_data(board->sensors[attr->index].thermal_sensor_param);
-                therm_debug("temp (deg. Celsius) = %d (raw = %ld (0x%lx) (0x%lx)\n", convert_adc_to_temp(raw),raw, raw, raw/4);
-                return sprintf(buf, "%d\n", convert_adc_to_temp(raw));
-            }
-        }
+    }
 
-        return sprintf(buf, "%ld\n", raw);
+    return sprintf(buf, "%ld\n", raw);
 
 }
 
 static ssize_t kona_thermal_set_threshold( struct device* dev, struct device_attribute *devattr, const char *buf, size_t count)
 {
-        unsigned long raw;
-        struct platform_drvdata *pdata = dev_get_drvdata(dev);
-        struct sensor_device_attribute_2 *attr = to_sensor_dev_attr_2(devattr);
-        struct therm_data               *board;
+    unsigned long raw;
+    struct platform_drvdata *pdata = dev_get_drvdata(dev);
+    struct sensor_device_attribute_2 *attr = to_sensor_dev_attr_2(devattr);
+    struct therm_data               *board;
 
-        board = pdata->pdev->dev.platform_data;
+    board = pdata->pdev->dev.platform_data;
 
-        if (strict_strtol(buf, 10, &raw))
-                return -EINVAL;
+    if (strict_strtol(buf, 10, &raw))
+        return -EINVAL;
 
-        if (attr->nr == 1) {
-                board->sensors[attr->index].thermal_warning_lvl_1 = raw;
-        }
-        else if (attr->nr == 2) {
-                board->sensors[attr->index].thermal_fatal_lvl = raw;
-        }
-        therm_debug("new temp = %ld \n", raw);
+    if (attr->nr == 1) {
+        board->sensors[attr->index].thermal_warning_lvl_1 = raw;
+    }
+    else if (attr->nr == 2) {
+        board->sensors[attr->index].thermal_fatal_lvl = raw;
+    }
+    therm_debug("new temp = %ld \n", raw);
 
-        return count;
+    return count;
 }
 
 static ssize_t kona_thermal_get_alarm( struct device* dev, struct device_attribute *devattr, char *buf)
 {
-        struct platform_drvdata *pdata = dev_get_drvdata(dev);
+    struct platform_drvdata *pdata = dev_get_drvdata(dev);
 
-        if (pdata->alarm_set) {
-                pdata->alarm_set = 0;
+    if (pdata->alarm_set) {
+        pdata->alarm_set = 0;
 
-                return sprintf(buf, "1\n");
-        }
+        return sprintf(buf, "1\n");
+    }
 
-        return sprintf(buf, "0\n");
+    return sprintf(buf, "0\n");
 }
 
 void thermal_interval_wake(unsigned long var)
@@ -338,6 +333,59 @@ static void kona_thermal_wakeup_event( struct work_struct *ws)
     }
 }
 
+struct dentry *dent_thermal_root_dir = NULL;
+static int thermal_test_panic(struct notifier_block *this, unsigned long event,
+      void *ptr)
+{
+    pr_info("thermal_panic test occurred\n");
+
+   return NOTIFY_DONE;
+}
+
+static int set_thermal_notification(void *data, u64 val)
+{
+
+    therm_debug("%s: val: %lld\n", __func__, val);
+    if ((val != 0) && (val != 10)) 
+    {
+        if(val < 10)
+        {
+            kona_thermal_register_notify(val, &thermal_test_block);
+            therm_debug("thermal notify register test (%lld)\n", val-1);
+        }
+        else
+        {
+            kona_thermal_unregister_notify(val-10, &thermal_test_block);
+            therm_debug("thermal notify unregister test (%lld)\n", val-11);
+        }
+    }
+    else
+    {
+        therm_debug("invalid value\n");
+    }
+
+    return 0;
+}
+
+DEFINE_SIMPLE_ATTRIBUTE(set_thermal_test_notify, NULL, set_thermal_notification, "%llu\n");
+
+int kona_thermal_debug_init(void)
+{
+
+	dent_thermal_root_dir = debugfs_create_dir("thermal", 0);
+	if (!dent_thermal_root_dir)
+		return -ENOMEM;
+
+    if(!debugfs_create_u32("debug", S_IWUSR|S_IRUSR, dent_thermal_root_dir, (int*)&thermal_debug))
+		return -ENOMEM;
+
+    if(!debugfs_create_file("test_notify", S_IWUSR|S_IRUSR, dent_thermal_root_dir, NULL, &set_thermal_test_notify))
+		return -ENOMEM;
+    therm_debug("%s:Thermal DEBUGFS initialized\n",__func__);
+
+
+    return 0;
+}
 
 static int kona_thermal_probe(struct platform_device *pdev)
 {
@@ -440,61 +488,6 @@ static int kona_thermal_remove(struct platform_device *pdev)
         return 0;
 }
 
-struct dentry *dent_thermal_root_dir = NULL;
-static int thermal_test_panic(struct notifier_block *this, unsigned long event,
-      void *ptr)
-{
-    pr_info("thermal_panic test occurred\n");
-
-   return NOTIFY_DONE;
-}
-
-static int set_thermal_notification(void *data, u64 val)
-{
-
-    therm_debug("%s: val: %lld\n", __func__, val);
-    if ((val != 0) && (val != 10)) 
-    {
-        if(val < 10)
-        {
-            kona_thermal_register_notify(val, &thermal_test_block);
-            therm_debug("thermal notify register test (%lld)\n", val-1);
-        }
-        else
-        {
-            kona_thermal_unregister_notify(val-10, &thermal_test_block);
-            therm_debug("thermal notify unregister test (%lld)\n", val-11);
-        }
-    }
-    else
-    {
-        therm_debug("invalid value\n");
-    }
-
-    return 0;
-}
-
-DEFINE_SIMPLE_ATTRIBUTE(set_thermal_test_notify, NULL, set_thermal_notification, "%llu\n");
-
-int kona_thermal_debug_init(void)
-{
-
-	dent_thermal_root_dir = debugfs_create_dir("thermal", 0);
-	if (!dent_thermal_root_dir)
-		return -ENOMEM;
-
-    if(!debugfs_create_u32("debug", S_IWUSR|S_IRUSR, dent_thermal_root_dir, (int*)&thermal_debug))
-		return -ENOMEM;
-
-    if(!debugfs_create_file("test_notify", S_IWUSR|S_IRUSR, dent_thermal_root_dir, NULL, &set_thermal_test_notify))
-		return -ENOMEM;
-    therm_debug("%s:Thermal DEBUGFS initialized\n",__func__);
-
-
-    return 0;
-}
-
-//#endif /*  CONFIG_DEBUG_FS  */
 
 #ifdef CONFIG_PM
 static int kona_thermal_resume(struct platform_device *pdev)
--- include/linux/broadcom/kona-thermal.h
+++ include/linux/broadcom/kona-thermal.h
@@ -30,81 +30,72 @@
 #ifndef __KONA_THERMAL_H__
 #define __KONA_THERMAL_H__
 
-typedef enum
-{
-	SENSOR_BB_TMON,  		/*Kona TMON for baseband temp monitoring	 	*/
-	SENSOR_BB_MULTICORE,	/*allow for multi-core					 */
-	SENSOR_PMU,				
+typedef enum {
+	SENSOR_BB_TMON,			/*Kona TMON for baseband temp monitoring */
+	SENSOR_BB_MULTICORE,	/*allow for multi-core*/
+	SENSOR_PMU,
 	SENSOR_BATTERY,
-    SENSOR_CRYSTAL,		    /* 32KHz, 26Mhz 					*/
-    SENSOR_PA,
-    SENSOR_OTHER		    /* allow for growth 					*/
+	SENSOR_CRYSTAL,			/* 32KHz, 26Mhz */
+	SENSOR_PA,
+	SENSOR_OTHER			/* allow for growth */
 }sensor_type;
 
-typedef enum
-{
-	SENSOR_READ_DIRECT,  	/* Direct register access */		
+typedef enum {
+	SENSOR_READ_DIRECT,		/* Direct register access */
 	SENSOR_READ_PMU_I2C,	/* I2C access 		*/
-	SENSOR_READ_SPI		/* SPI access		*/
+	SENSOR_READ_SPI			/* SPI access		*/
 }sensor_read_type;
 
-struct temp_threshold
-{
-	unsigned	 temp_low;		/*undertemp  threshold*/
-	unsigned	 temp_high; 		/*overtemp  threshold   */
+struct temp_threshold {
+	unsigned	temp_low;	/*undertemp  threshold*/
+	unsigned	temp_high;	/*overtemp  threshold */
 };
 
-typedef enum
-{
-	WARNING_1, 	/* current enum allows two warning and one fatal alarm */
-	WARNING_2,  	/* threshold but this can be expanded , if necessary	 */
-	FATAL	
+typedef enum {
+	WARNING_1,	/* current enum allows two warning and one fatal alarm */
+	WARNING_2,	/* threshold but this can be expanded , if necessary */
+	FATAL
 }threshold_type;
 
-typedef enum
-{
-	LOW,  		/* this is used for setting the undertemp threshold 	*/
-	HIGH		/* this is used for setting the overtemp threshold 	*/
+typedef enum {
+	LOW,	/* this is used for setting the undertemp threshold 	*/
+	HIGH	/* this is used for setting the overtemp threshold 	*/
 }temperature_type;
 
-typedef enum 
-{
+typedef enum  {
 	THERM_ACTION_NONE,
 	THERM_ACTION_NOTIFY,
 	THERM_ACTION_NOTIFY_SHUTDOWN,
 	THERM_ACTION_SHUTDOWN
 } therm_action_type;
 
-typedef enum  
-{
-	SENSOR_PERIODIC_READ,  	/*Interval located in separate field 		*/
-	SENSOR_INTERRUPT, 	 	/*Interrupt detection of temperature violation 	*/
+typedef enum   {
+	SENSOR_PERIODIC_READ,	/*Interval located in separate field 		*/
+	SENSOR_INTERRUPT,	/*Interrupt detection of temperature violation 	*/
 	SENSOR_DRX_SYNC		/* synchronize sensor reading to DRX cycles 	*/
 } sensor_control_type;
 
-struct thermal_sensor_config
-{
-   unsigned             thermal_id;
-   char*                thermal_name;
-   sensor_type          thermal_type;
-   unsigned             thermal_mc;
-   sensor_read_type     thermal_read;
-   unsigned        		thermal_location;
-   int long        		thermal_warning_lvl_1;
-   int long        		thermal_warning_lvl_2;
-   int long        		thermal_fatal_lvl;
-   therm_action_type    thermal_warning_action;
-   therm_action_type    thermal_fatal_action;
-   unsigned             thermal_sensor_param;
-   sensor_control_type  thermal_control;
-   unsigned*            convert_callback;
+struct thermal_sensor_config {
+   unsigned				thermal_id;
+   char					*thermal_name;
+   sensor_type			thermal_type;
+   unsigned				thermal_mc;
+   sensor_read_type		thermal_read;
+   unsigned				thermal_location;
+   int long				thermal_warning_lvl_1;
+   int long				thermal_warning_lvl_2;
+   int long				thermal_fatal_lvl;
+   therm_action_type	thermal_warning_action;
+   therm_action_type	thermal_fatal_action;
+   unsigned				thermal_sensor_param;
+   sensor_control_type	thermal_control;
+   unsigned				*convert_callback;
 };
 typedef struct thermal_sensor_config THERMAL_SENSOR_CONFIG_t;
 
-struct therm_data
-{
+struct therm_data {
 	unsigned 		        flags;
-    int                     thermal_update_interval;
+	int                     thermal_update_interval;
 	unsigned                num_sensors;
 	struct thermal_sensor_config *sensors;
 };
