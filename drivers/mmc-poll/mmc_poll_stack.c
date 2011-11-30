244f16d0636dd1b4c0bca338b8ea2a066d812abd
Denny Yang <yangd@broadcom.com>
2011-11-30 15:21:32 -0800

    apanic: detect eMMC device configuration through platform hw config
    [Defect / Enhancement / New Feature]
    MobC00182644
    
    [Problem]
    Clipper HW couldn't write panic data into eMMC device. Device initialization
    has been failed because it's using eMMC1.
    
    [Solution]
    Check eMMC devices of platform HW configuration and use as per the information
    
    [Reviewers]
    Conrad Smith, Xiangdong Lou, Kernel team
    
    [Testing]
        [Test Cases]
        Force panic and check apanic data in kpanic partition
    
        [Label/CL# Synced To]
        [Platform]
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: I3cc8627c2659da70edc06ddceb5e586f7a2c2953

--- drivers/mmc-poll/mmc_poll_stack.c
+++ drivers/mmc-poll/mmc_poll_stack.c
@@ -26,7 +26,7 @@
 #include <linux/kernel.h>
 #include <linux/mmc-poll/mmc_poll.h>
 #include <linux/mmc-poll/mmc_poll_stack.h>
-
+#include <mach/sdio_platform.h>
 #include "kona_mmc.h"
 
 /* #define __MMC_POLL_INIT_DEBUG */
@@ -37,39 +37,65 @@
 #define debug_print(a,b...)
 #endif
 
-int mmc_poll_stack_init (struct mmc **mmc, int *dev_num) 
+static int mmc_poll_panic_device[SDIO_DEV_TYPE_MAX+1];
+
+int mmc_poll_stack_init(void **mmc_p, int *dev_num)
 {
 	int status;
+	int kona_dev_num, log_dev_num;
+	struct mmc **mmc = (struct mmc **)mmc_p;
 
 	if ((mmc == NULL) || (dev_num == NULL))
 		return -1;
 
 	mmc_initialize();
 
-	/* For eMMC initilaize the instance number 2 and for SD Card use 1 */
-	status = kona_mmc_init (2);
-	if (status  < 0) {
-		printk("rhearay_mmc_init failed \r\n");
+	for (log_dev_num = 0, kona_dev_num = 2;
+	     kona_dev_num <= SDIO_DEV_TYPE_MAX;
+	     log_dev_num++, kona_dev_num++) {
+		if (!mmc_poll_panic_device[kona_dev_num-1])
+			continue;
+
+		/* e.g) eMMC instance number 2 and for SD Card use 1 */
+		status = kona_mmc_init(kona_dev_num);
+		if (status  < 0) {
+			pr_err("kona_mmc_init failed %d\n", kona_dev_num);
+			return -1;
+		}
+
+		pr_debug("kona_mmc_init done %d\n", kona_dev_num);
+
+		/* Use 0 for first device registered via kona_mmc_init etc */
+		*mmc = find_mmc_device(log_dev_num);
+		if (*mmc == NULL) {
+			pr_err("No mmc device found\n");
+			return -1;
+		}
+
+		if (mmc_init(*mmc) < 0) {
+			pr_err("mmc init failed kona_dev:%d\n", kona_dev_num);
+			continue;
+		}
+		pr_debug("mmc_init done kona_dev:%d\n", kona_dev_num);
+
+		break;
+	}
+
+	if (kona_dev_num > SDIO_DEV_TYPE_MAX) {
+		pr_err("No mmc device available\n");
 		return -1;
 	}
-	debug_print("kona_mmc_init done \r\n");
 
 	/* Use 0 for the first device registered via kona_mmc_init etc */
-	*mmc = find_mmc_device (0);
-	if (*mmc == NULL) {
-		printk("No mmc device found\r\n");
-		return -1;
-	}
+	*dev_num = log_dev_num;
 
-	if (mmc_init(*mmc) < 0) {
-		printk("mmc init failed \r\n");
-		return -1;
-	}
-	debug_print("mmc_init done \r\n");
-
-	/* Use 0 for the first device registered via kona_mmc_init etc */
-	*dev_num = 0;
 	return 0;
+}
+
+void mmc_poll_stack_device(unsigned dev_num)
+{
+	if (dev_num < SDIO_DEV_TYPE_MAX)
+		mmc_poll_panic_device[dev_num] = 1;
 }
 
 static void print_mmcinfo(struct mmc *mmc)
