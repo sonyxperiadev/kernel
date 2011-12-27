5c7fe71e39fc3c340b83fb253ef851195d27b3a5
Jason Shim <jshim@broadcom.com>
2011-12-27 13:30:47 -0800

    enable saving ap dump information to emmc. run checkpatch.pl
    [Defect / Enhancement / New Feature]
    MobC00190411
    
    [Problem]
    ap dump is not set to any port.
    
    [Solution]
    enable saving to emmc and set emmc to default ap dump port
    
    [Reviewers]
    Jack Yu
    
    [Testing]
        [Test Cases]
    trigger ap panic by running, echo c> /proc/sysrq-trigger
    after reboot, run dd if=/dev/block/mmcblk0p27 of=/mnt/sdcard/apanic.txt
    verify apanic information.
    Verify ap dump port is flash by default.
    Note: after crash, user has to reboot a device and run dd if=/dev/block/mmcblk0p27 of=/mnt/sdcard/apanic.txt to save ap panic information to sd card.
        [Label/CL# Synced To]
        HEAD of sdb-android-ics
        [Platform]
        android_rhearay_edn2x

--- drivers/char/broadcom/fuse_log/config.c
+++ drivers/char/broadcom/fuse_log/config.c
@@ -1,16 +1,17 @@
-//
 /****************************************************************************
 *
 *     Copyright (c) 2009 Broadcom Corporation
 *
-*   Unless you and Broadcom execute a separate written software license 
-*   agreement governing use of this software, this software is licensed to you 
-*   under the terms of the GNU General Public License version 2, available 
-*    at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html (the "GPL"). 
+*   Unless you and Broadcom execute a separate written software license
+*   agreement governing use of this software, this software is licensed
+*   to you under the terms of the GNU General Public License version 2,
+*   available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*   (the "GPL").
 *
-*   Notwithstanding the above, under no circumstances may you combine this 
-*   software in any way with any other Broadcom software provided under a license 
-*   other than the GPL, without Broadcom's express prior written consent.
+*   Notwithstanding the above, under no circumstances may you combine
+    this software in any way with any other Broadcom software provided
+*   under a license other than the GPL, without Broadcom's express
+*   prior written consent.
 *
 ****************************************************************************/
 
@@ -19,90 +20,100 @@
 #include <linux/fs.h>
 #include <linux/proc_fs.h>
 #include <linux/workqueue.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 #include <linux/errno.h>
 
 #include "config.h"
 
-static struct delayed_work g_load_config_wq ;			///< work queue for reading config file
-static BCMLOG_Config_t g_config ;						///< log configuration
-static struct proc_dir_entry *g_proc_dir_entry = NULL ;	///< procfs file
+/* work queue for reading config file */
+static struct delayed_work g_load_config_wq;
+/* log configuration */
+static BCMLOG_Config_t g_config;
+/* procfs file */
+static struct proc_dir_entry *g_proc_dir_entry = { 0 };
 
 /**
  *	Set configuration defaults
  **/
-static void SetConfigDefaults( void )
+static void SetConfigDefaults(void)
 {
-	g_config.ap_crashlog_dev  = BCMLOG_OUTDEV_NONE ;
-#ifdef CONFIG_STM_DEFAULT
-	g_config.runlog_dev       = BCMLOG_OUTDEV_STM ;	        // run-time log to STM
-	g_config.cp_crashlog_dev  = BCMLOG_OUTDEV_STM ;
+#ifdef CONFIG_APANIC_ON_MMC
+	g_config.ap_crashlog_dev = BCMLOG_OUTDEV_PANIC;
 #else
-	g_config.runlog_dev       = BCMLOG_OUTDEV_STM ;
-	g_config.cp_crashlog_dev  = BCMLOG_OUTDEV_STM ; //eventually set to BCMLOG_OUTDEV_PANIC once its working
+	g_config.ap_crashlog_dev = BCMLOG_OUTDEV_NONE;
+#endif
+#ifdef CONFIG_STM_DEFAULT
+	/* run-time log to STM */
+	g_config.runlog_dev = BCMLOG_OUTDEV_STM;
+	g_config.cp_crashlog_dev = BCMLOG_OUTDEV_STM;
+#else
+	g_config.runlog_dev = BCMLOG_OUTDEV_STM;
+	/* eventually set to BCMLOG_OUTDEV_PANIC once its working */
+	g_config.cp_crashlog_dev = BCMLOG_OUTDEV_STM;
 #endif
 }
 
 /**
  *	'safe' strncpy
  **/
-static void safe_strncat( char *dst, const char *src, int len )
+static void safe_strncat(char *dst, const char *src, int len)
 {
-	int l_dst = strlen( dst ) ;
-	int l_src = strlen( src ) ;
-	if( l_dst + l_src < len )
-	{
-		strncat( dst, src, len - l_dst - l_src ) ;
-	}
+	int l_dst = strlen(dst);
+	int l_src = strlen(src);
+	if (l_dst + l_src < len)
+		strncat(dst, src, len - l_dst - l_src);
 }
 
 /**
  *	build device status string and append to 'buf'
  **/
-static void bld_device_status_str( char *buf, int len, char *label, int device )
+static void bld_device_status_str(char *buf, int len, char *label, int device)
 {
-	safe_strncat( buf, label, len ) ;
+	safe_strncat(buf, label, len);
 
-	switch( device )
-	{
+	switch (device) {
 	case BCMLOG_OUTDEV_NONE:
-		safe_strncat( buf, "-> disabled\n", len ) ;
-		break ;
+		safe_strncat(buf, "-> disabled\n", len);
+		break;
 	case BCMLOG_OUTDEV_PANIC:
-		safe_strncat( buf, "-> flash\n", len ) ;
-		break ;
+		safe_strncat(buf, "-> flash\n", len);
+		break;
 	case BCMLOG_OUTDEV_RNDIS:
-		safe_strncat( buf, "-> RNDIS\n", len ) ;
-		break ;
+		safe_strncat(buf, "-> RNDIS\n", len);
+		break;
 	case BCMLOG_OUTDEV_SDCARD:
-		safe_strncat( buf, "-> SD card\n", len ) ;
-		break ;
+		safe_strncat(buf, "-> SD card\n", len);
+		break;
 	case BCMLOG_OUTDEV_UART:
-		safe_strncat( buf, "-> UART\n", len ) ;
-		break ;
+		safe_strncat(buf, "-> UART\n", len);
+		break;
 	case BCMLOG_OUTDEV_ACM:
-		safe_strncat( buf, "-> ACM\n", len ) ;
-		break ;
+		safe_strncat(buf, "-> ACM\n", len);
+		break;
 	case BCMLOG_OUTDEV_STM:
-		safe_strncat( buf, "-> STM\n", len ) ;
-		break ;
+		safe_strncat(buf, "-> STM\n", len);
+		break;
 	default:
-		safe_strncat( buf, "-> ERROR\n", len ) ;
-		break ;
+		safe_strncat(buf, "-> ERROR\n", len);
+		break;
 	}
 }
 
 /**
  *	proc read handler
  **/
-static int proc_read(char *page, char **start, off_t offset, int count, int *eof, void *data)
+static int proc_read(char *page, char **start, off_t offset, int count,
+		     int *eof, void *data)
 {
-	*page = 0 ;
-	bld_device_status_str( page, count, "  BMTT logging", g_config.runlog_dev ) ;
-	bld_device_status_str( page, count, "  AP crash dump", g_config.ap_crashlog_dev ) ;
-	bld_device_status_str( page, count, "  CP crash dump", g_config.cp_crashlog_dev ) ;
-	*eof = 1 ;
-	return 1+strlen( page ) ;
+	*page = 0;
+	bld_device_status_str(page, count, "  BMTT logging",
+			      g_config.runlog_dev);
+	bld_device_status_str(page, count, "  AP crash dump",
+			      g_config.ap_crashlog_dev);
+	bld_device_status_str(page, count, "  CP crash dump",
+			      g_config.cp_crashlog_dev);
+	*eof = 1;
+	return 1 + strlen(page);
 }
 
 /**
@@ -126,105 +137,103 @@ static int proc_read(char *page, char **start, off_t offset, int count, int *eof
  *		p - CP crash dump -> ACM
  *		s -  both BMTT and CP crash dump -> STM
  **/
-static ssize_t proc_write(struct file *file, const char *buffer, unsigned long count, void *data)
-{	
-	if( count > 0 )
-	{
-		switch( *buffer )
-		{
-		case 'a': 	
-			g_config.runlog_dev = BCMLOG_OUTDEV_RNDIS ;
-			break ;
-		case 'b': 	
-			g_config.runlog_dev = BCMLOG_OUTDEV_ACM ;
-			break ;
-		case 'c': 
-			g_config.runlog_dev = BCMLOG_OUTDEV_UART ;
-			break ;
-		case 'd':	
-			g_config.runlog_dev = BCMLOG_OUTDEV_SDCARD ;
-			break ;
-		case 'e':	
-			g_config.ap_crashlog_dev = BCMLOG_OUTDEV_PANIC ;
-			break ;
-		case 'f': 
-			g_config.ap_crashlog_dev = BCMLOG_OUTDEV_SDCARD ;
-			break ;
-		case 'g': 	
-			g_config.ap_crashlog_dev = BCMLOG_OUTDEV_NONE ;
-			break ;
-		case 'h': 
-			BCMLOG_SaveConfig( 1 ) ;
-			break ;
-		case 's':      
+static ssize_t proc_write(struct file *file, const char *buffer,
+			  unsigned long count, void *data)
+{
+	if (count > 0) {
+		switch (*buffer) {
+		case 'a':
+			g_config.runlog_dev = BCMLOG_OUTDEV_RNDIS;
+			break;
+		case 'b':
+			g_config.runlog_dev = BCMLOG_OUTDEV_ACM;
+			break;
+		case 'c':
+			g_config.runlog_dev = BCMLOG_OUTDEV_UART;
+			break;
+		case 'd':
+			g_config.runlog_dev = BCMLOG_OUTDEV_SDCARD;
+			break;
+		case 'e':
+			g_config.ap_crashlog_dev = BCMLOG_OUTDEV_PANIC;
+			break;
+		case 'f':
+			g_config.ap_crashlog_dev = BCMLOG_OUTDEV_SDCARD;
+			break;
+		case 'g':
+			g_config.ap_crashlog_dev = BCMLOG_OUTDEV_NONE;
+			break;
+		case 'h':
+			BCMLOG_SaveConfig(1);
+			break;
+		case 's':
 			g_config.runlog_dev = BCMLOG_OUTDEV_STM;
 			g_config.cp_crashlog_dev = BCMLOG_OUTDEV_STM;
-			break ;
-		case 'i': 
-			SetConfigDefaults( ) ;
-			BCMLOG_SaveConfig( 0 ) ;
-			break ;
+			break;
+		case 'i':
+			SetConfigDefaults();
+			BCMLOG_SaveConfig(0);
+			break;
 		case 'j':
-			g_config.cp_crashlog_dev = BCMLOG_OUTDEV_PANIC ;
-			break ;
+			g_config.cp_crashlog_dev = BCMLOG_OUTDEV_PANIC;
+			break;
 		case 'k':
-			g_config.cp_crashlog_dev = BCMLOG_OUTDEV_SDCARD ;
-			break ;
+			g_config.cp_crashlog_dev = BCMLOG_OUTDEV_SDCARD;
+			break;
 		case 'l':
-			g_config.cp_crashlog_dev = BCMLOG_OUTDEV_NONE ;
-			break ;
+			g_config.cp_crashlog_dev = BCMLOG_OUTDEV_NONE;
+			break;
 		case 'm':
-			g_config.cp_crashlog_dev = BCMLOG_OUTDEV_RNDIS ;
-			break ;
+			g_config.cp_crashlog_dev = BCMLOG_OUTDEV_RNDIS;
+			break;
 		case 'n':
 			g_config.runlog_dev = BCMLOG_OUTDEV_STM;
-			break ;
+			break;
 		case 'o':
 			g_config.cp_crashlog_dev = BCMLOG_OUTDEV_STM;
-			break ;
+			break;
 		case 'p':
 			g_config.cp_crashlog_dev = BCMLOG_OUTDEV_ACM;
-			break ;
+			break;
 
 		}
 	}
 
-	return count ;
+	return count;
 }
 
 /**
  *	Load configuration from persistent storage
  **/
-static int LoadConfigFromPersistentStorage( void )
+static int LoadConfigFromPersistentStorage(void)
 {
-	mm_segment_t oldfs ;
-	struct file* config_file ;
-	int rc = 0 ;
+	mm_segment_t oldfs;
+	struct file *config_file;
+	int rc = 0;
 
-	oldfs = get_fs( ) ;
-	set_fs (KERNEL_DS);
+	oldfs = get_fs();
+	set_fs(KERNEL_DS);
 
-	config_file = filp_open( BCMLOG_CONFIG_PS_FILE, O_RDONLY, 0 ); 
+	config_file = filp_open(BCMLOG_CONFIG_PS_FILE, O_RDONLY, 0);
 
-	if( IS_ERR( config_file ) )
-	{
-		rc = -1 ;
+	if (IS_ERR(config_file)) {
+		rc = -1;
 	}
 
-	else
-	{
-		if( sizeof(BCMLOG_Config_t)!= config_file->f_op->read( config_file, (void*)&g_config, 
-				sizeof(BCMLOG_Config_t), &config_file->f_pos ) )
-		{
-			rc = -1 ;
+	else {
+		if (sizeof(BCMLOG_Config_t) !=
+		    config_file->f_op->read(config_file, (void *)&g_config,
+					    sizeof(BCMLOG_Config_t),
+					    &config_file->f_pos)) {
+			rc = -1;
 		}
 
-		filp_close( config_file ,NULL );
+		filp_close(config_file, NULL);
 	}
-		
-	set_fs( oldfs ) ;
 
-	return rc ;
+	set_fs(oldfs);
+
+	return rc;
 }
 
 /**
@@ -233,23 +242,23 @@ static int LoadConfigFromPersistentStorage( void )
  *	configuration is successfully loaded, up to predetermined
  *	number of attempts.  Delay is necessary to wait until
  *	file system is available.
- *   
+ *
  *	@param	(in)	ptr to work structure
  *	@note
  *		Function prototype as required by INIT_DELAYED_WORK macro.
  *	@note
- *		There is a one-second delay between each attemt.  It was 
+ *		There is a one-second delay between each attemt.  It was
  *		determined emperically that 5 - 6 seconds is about
- *		the time required for FS to come up, so we'll give 
+ *		the time required for FS to come up, so we'll give
  *		it up to 10 seconds.
  **/
-static void try_load_config( struct work_struct *work )
+static void try_load_config(struct work_struct *work)
 {
-	static int tries = 10 ;
+	static int tries = 10;
 
-	if( --tries > 0 )
-		if( LoadConfigFromPersistentStorage( ) < 0 )
-			schedule_delayed_work( &g_load_config_wq, 1*HZ ) ;
+	if (--tries > 0)
+		if (LoadConfigFromPersistentStorage() < 0)
+			schedule_delayed_work(&g_load_config_wq, 1 * HZ);
 }
 
 /**
@@ -258,34 +267,39 @@ static void try_load_config( struct work_struct *work )
  *				configuration
  *	@return		0 on success, -1 on error
  **/
-int BCMLOG_SaveConfig( int saveFlag )
+int BCMLOG_SaveConfig(int saveFlag)
 {
-	mm_segment_t oldfs ;
-	struct file* config_file ;
-	int rc = 0 ;
+	mm_segment_t oldfs;
+	struct file *config_file;
+	int rc = 0;
 
-	oldfs = get_fs( ) ;
-	set_fs (KERNEL_DS);
+	oldfs = get_fs();
+	set_fs(KERNEL_DS);
 
-	config_file = filp_open( BCMLOG_CONFIG_PS_FILE, O_WRONLY|O_TRUNC|O_CREAT, 0666); 
+	config_file =
+	    filp_open(BCMLOG_CONFIG_PS_FILE, O_WRONLY | O_TRUNC | O_CREAT,
+		      0666);
 
-	if( IS_ERR( config_file ) )
-		rc = -1 ;
+	if (IS_ERR(config_file))
+		rc = -1;
 
-	else
-	{
-		if( saveFlag )
-			if( sizeof(BCMLOG_Config_t) != config_file->f_op->write( config_file, (void*)&g_config, 
-				sizeof(BCMLOG_Config_t), &config_file->f_pos ) )
-				rc = -1 ;
+	else {
+		if (saveFlag)
+			if (sizeof(BCMLOG_Config_t) !=
+			    config_file->f_op->write(config_file,
+						     (void *)&g_config,
+						     sizeof(BCMLOG_Config_t),
+						     &config_file->f_pos))
+				rc = -1;
 
-		// if !saveFlag the file is truncated to zero bytes, invalidating the configuration
-		filp_close( config_file ,NULL );
+		/* if !saveFlag the file is truncated to zero bytes,
+		* invalidating the configuration */
+		filp_close(config_file, NULL);
 	}
-		
-	set_fs( oldfs ) ;
 
-	return rc ;
+	set_fs(oldfs);
+
+	return rc;
 }
 
 /**
@@ -293,20 +307,19 @@ int BCMLOG_SaveConfig( int saveFlag )
  *	@param		inLogId (in) log id
  *	@param		inEnable (in) nonzero to enable; zero to disable log ID
  **/
-void BCMLOG_EnableLogId( unsigned short inLogId, int inEnable )
+void BCMLOG_EnableLogId(unsigned short inLogId, int inEnable)
 {
-	unsigned long index ;
-	unsigned long bit ;
-	
-	if( inLogId < BCMLOG_MAX_LOG_ID )
-	{
-		index = inLogId / BITS_PER_LONG ;
-		bit   = inLogId % BITS_PER_LONG ;
-						
-		if( inEnable )
-			g_config.id_enable[index] |= ( 1 << bit ) ;
+	unsigned long index;
+	unsigned long bit;
+
+	if (inLogId < BCMLOG_MAX_LOG_ID) {
+		index = inLogId / BITS_PER_LONG;
+		bit = inLogId % BITS_PER_LONG;
+
+		if (inEnable)
+			g_config.id_enable[index] |= (1 << bit);
 		else
-			g_config.id_enable[index] &= ~( 1 << bit ) ;
+			g_config.id_enable[index] &= ~(1 << bit);
 	}
 }
 
@@ -315,82 +328,82 @@ void BCMLOG_EnableLogId( unsigned short inLogId, int inEnable )
  *	@param		inLogId (in) log id
  *	@return		1 if enabled; 0 if not enabled or invalid ID
  **/
-int BCMLOG_LogIdIsEnabled( unsigned short inLogId )
+int BCMLOG_LogIdIsEnabled(unsigned short inLogId)
 {
 
-	unsigned long index ;
-	unsigned long bit ;
-	
-	if( inLogId >= BCMLOG_MAX_LOG_ID )
-	{
-		return 0 ;
-	}
-	
-	index = inLogId / BITS_PER_LONG ;
-	bit   = inLogId % BITS_PER_LONG ;
-					
-	return g_config.id_enable[index] & ( 1 << bit ) ? 1 : 0 ;
+	unsigned long index;
+	unsigned long bit;
+
+	if (inLogId >= BCMLOG_MAX_LOG_ID)
+		return 0;
+
+	index = inLogId / BITS_PER_LONG;
+	bit = inLogId % BITS_PER_LONG;
+
+	return g_config.id_enable[index] & (1 << bit) ? 1 : 0;
 }
 
 /**
  *	Initialize logging configuration.  Schedules a work thread to
  *	load the configuration file once the file system is readable.
  **/
-void BCMLOG_InitConfig( void ) 
+void BCMLOG_InitConfig(void)
 {
 	/*
-	 *	disable all AP logging (CP logging is handled by CP) [MobC00126731]
+	 *      disable all AP logging (CP logging is
+	 *      handled by CP) [MobC00126731]
 	 */
-	memset( &g_config, 0x00, sizeof( g_config ) ) ;
-	
-	/*
-	 *	set default configuration
-	 */
-	SetConfigDefaults( ) ;
-	
-	/*
-	 *	create the procfs entry
-	 */
-	g_proc_dir_entry = create_proc_entry( BCMLOG_CONFIG_PROC_FILE, S_IRWXU | S_IRWXG | S_IRWXO, NULL );
+	memset(&g_config, 0x00, sizeof(g_config));
 
-	if (g_proc_dir_entry == NULL) 
-	{
-		remove_proc_entry( BCMLOG_CONFIG_PROC_FILE, NULL); 
+	/*
+	 *      set default configuration
+	 */
+	SetConfigDefaults();
+
+	/*
+	 *      create the procfs entry
+	 */
+	g_proc_dir_entry =
+	    create_proc_entry(BCMLOG_CONFIG_PROC_FILE,
+			      S_IRWXU | S_IRWXG | S_IRWXO, NULL);
+
+	if (g_proc_dir_entry == NULL) {
+		remove_proc_entry(BCMLOG_CONFIG_PROC_FILE, NULL);
 	}
 
-	else
-	{
-		g_proc_dir_entry->read_proc  = proc_read;
+	else {
+		g_proc_dir_entry->read_proc = proc_read;
 		g_proc_dir_entry->write_proc = proc_write;
 	}
 
 	/*
-	 *	start a thread to attempt to load configuration when filesystem ready
+	 *      start a thread to attempt to load configuration
+	 *      when filesystem ready
 	 */
-	INIT_DELAYED_WORK( &g_load_config_wq, try_load_config ) ;
-	
-	schedule_delayed_work( &g_load_config_wq, 1*HZ ) ;
+	INIT_DELAYED_WORK(&g_load_config_wq, try_load_config);
+
+	schedule_delayed_work(&g_load_config_wq, 1 * HZ);
 }
 
-int BCMLOG_GetRunlogDevice( void ) 
+int BCMLOG_GetRunlogDevice(void)
 {
-	return g_config.runlog_dev ;
+	return g_config.runlog_dev;
 }
 
-int BCMLOG_GetCpCrashLogDevice( void )
+int BCMLOG_GetCpCrashLogDevice(void)
 {
-	return g_config.cp_crashlog_dev ;
+	return g_config.cp_crashlog_dev;
 }
 
-int BCMLOG_GetApCrashLogDevice( void )
+int BCMLOG_GetApCrashLogDevice(void)
 {
-	return g_config.ap_crashlog_dev ;
+	return g_config.ap_crashlog_dev;
 }
-
 
 int BCMLOG_IsUSBLog(void)
 {
-	if  (BCMLOG_OUTDEV_RNDIS == g_config.runlog_dev ||BCMLOG_OUTDEV_ACM==g_config.runlog_dev)
+	if (BCMLOG_OUTDEV_RNDIS == g_config.runlog_dev
+	    || BCMLOG_OUTDEV_ACM == g_config.runlog_dev)
 		return 1;
 	else
 		return 0;
