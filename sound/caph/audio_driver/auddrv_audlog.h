1112cf8e029806bc3146ebc3983c7d49614ad1a3
yezhang <yezhang@broadcom.com>
2012-01-12 13:26:54 -0800

    Audio : Fix check patch error
    [Defect / Enhancement / New Feature]
    See above
    
    [Problem]
    See above
    
    [Solution]
    Fix it
    
    [Reviewers]
    Audio Group, Kernel group
    
    [Testing]
        [Test Cases]
    voice call, audio playback, channel switch, logging capture in voice call all work fine
    
        [Label/CL# Synced To]
    head of sdb-rhea-android-ics
    
        [Platform]
    lmp_rhearay_edn2x
    
    [BROADCOM INTERNAL NOTES]

--- sound/caph/audio_driver/auddrv_audlog.h
+++ sound/caph/audio_driver/auddrv_audlog.h
@@ -1,28 +1,22 @@
-/************************************************************************************************/
-/*                                                                                              */
-/*  Copyright 2011  Broadcom Corporation                                                        */
-/*                                                                                              */
-/*     Unless you and Broadcom execute a separate written software license agreement governing  */
-/*     use of this software, this software is licensed to you under the terms of the GNU        */
-/*     General Public License version 2 (the GPL), available at                                 */
-/*                                                                                              */
-/*          http://www.broadcom.com/licenses/GPLv2.php                                          */
-/*                                                                                              */
-/*     with the following added to such license:                                                */
-/*                                                                                              */
-/*     As a special exception, the copyright holders of this software give you permission to    */
-/*     link this software with independent modules, and to copy and distribute the resulting    */
-/*     executable under terms of your choice, provided that you also meet, for each linked      */
-/*     independent module, the terms and conditions of the license of that module.              */
-/*     An independent module is a module which is not derived from this software.  The special  */
-/*     exception does not apply to any modifications of the software.                           */
-/*                                                                                              */
-/*     Notwithstanding the above, under no circumstances may you combine this software in any   */
-/*     way with any other Broadcom software provided under a license other than the GPL,        */
-/*     without Broadcom's express prior written consent.                                        */
-/*                                                                                              */
-/************************************************************************************************/
+/****************************************************************************
+Copyright 2009 - 2011  Broadcom Corporation
+ Unless you and Broadcom execute a separate written software license agreement
+ governing use of this software, this software is licensed to you under the
+ terms of the GNU General Public License version 2 (the GPL), available at
+    http://www.broadcom.com/licenses/GPLv2.php
 
+ with the following added to such license:
+ As a special exception, the copyright holders of this software give you
+ permission to link this software with independent modules, and to copy and
+ distribute the resulting executable under terms of your choice, provided
+ that you also meet, for each linked independent module, the terms and
+ conditions of the license of that module.
+ An independent module is a module which is not derived from this software.
+ The special exception does not apply to any modifications of the software.
+ Notwithstanding the above, under no circumstances may you combine this software
+ in any way with any other Broadcom software provided under a license other than
+ the GPL, without Broadcom's express prior written consent.
+***************************************************************************/
 /**
 *
 *  @file  auddrv_audlog.h
@@ -35,7 +29,6 @@
 * @brief    This group defines the common APIs for the audio logging driver
 *
 *****************************************************************************/
-
 
 #ifndef	__AUDDRV_AUDLOG_H__
 #define	__AUDDRV_AUDLOG_H__
@@ -59,66 +52,61 @@
 /**
 	Log message consumer type
 **/
-typedef enum
-{
+enum __Media_t {
 	PCM_TYPE,
 	AMR_WB_TYPE,
 	AMR_NB_TYPE,
-    AAC_TYPE
-}Media_t;
+	AAC_TYPE
+};
+#define Media_t enum __Media_t
 
-
-typedef enum
-{
+enum __CAPTURE_POINT_t {
 	AUD_LOG_NONE,
-	AUD_LOG_PCMOUT,          // CAPH output.
-	AUD_LOG_PCMIN,           // CAPH input.
-	AUD_LOG_VOCODER_DL,      // VoIP DL.
-	AUD_LOG_VOCODER_UL,      // VoIP UL.
-	AUD_LOG_VOICEIN          // voice recording via DSP.
-} CAPTURE_POINT_t;
-
+	AUD_LOG_PCMOUT,		/* CAPH output. */
+	AUD_LOG_PCMIN,		/* CAPH input. */
+	AUD_LOG_VOCODER_DL,	/* VoIP DL. */
+	AUD_LOG_VOCODER_UL,	/* VoIP UL. */
+	AUD_LOG_VOICEIN		/* voice recording via DSP. */
+};
+#define CAPTURE_POINT_t enum __CAPTURE_POINT_t
 
 /**
 	Log message consumer type
 **/
-typedef enum
-{
-	LOG_TO_PC = 0,		//< Log message to PC/MTT
-	LOG_TO_FLASH,		//< Save log message to local flash
-} AUDLOG_DEST_en_t;
+enum __AUDLOG_DEST_en_t {
+	LOG_TO_PC = 0,		/* Log message to PC/MTT */
+	LOG_TO_FLASH,		/* Save log message to local flash */
+};
+#define AUDLOG_DEST_en_t enum __AUDLOG_DEST_en_t
 
-
-typedef enum
-{
+enum __AUDLOG_MUSIC_STREAM_INDEX_t {
 	AUDIO_LOG_PATH_1 = 17,
 	AUDIO_LOG_PATH_2 = 18,
 	AUDIO_LOG_PATH_3 = 19,
 	AUDIO_LOG_PATH_4 = 20,
-} AUDLOG_MUSIC_STREAM_INDEX_t;
+};
+#define AUDLOG_MUSIC_STREAM_INDEX_t enum __AUDLOG_MUSIC_STREAM_INDEX_t
 
-
-typedef struct AUDIOLOG_HEADER_t
-{
-	UInt32  magicID;                        //unique ID for audio header: 0xA0D10106
-    UInt32  logPointID;                     //in case need to support multi logging points simultaneously.
-	Media_t audioFormat;                    // PCM, AMR, AAC etc.
-	AUDIO_SAMPLING_RATE_t	samplingRate;   // 8000 ¨C 48000 Hz
-	AUDIO_NUM_OF_CHANNEL_t  stereoType;     // mono/stereo
-	AUDIO_BITS_PER_SAMPLE_t  bitsPerSample; // 8/16/24/32
-} AUDIOLOG_HEADER_t;
+struct _AUDIOLOG_HEADER_t {
+	UInt32 magicID;		/* unique ID for audio header: 0xA0D10106 */
+	UInt32 logPointID;	/* support multiLoggingPoints simultaneously. */
+	Media_t audioFormat;	/* PCM, AMR, AAC etc. */
+	AUDIO_SAMPLING_RATE_t samplingRate; /* 8000, 48000 Hz */
+	AUDIO_NUM_OF_CHANNEL_t stereoType;	/* mono/stereo */
+	AUDIO_BITS_PER_SAMPLE_t bitsPerSample;	/* 8/16/24/32 */
+};
+#define AUDIOLOG_HEADER_t struct _AUDIOLOG_HEADER_t
 
 /**
 	Audvoc driver call back for logging message save to file system
 **/
-typedef struct audlog_cb_info_t
-{
+struct _AUDLOG_CB_INFO {
 	spinlock_t audio_log_lock;
 	Boolean capture_ready;
-	UInt16 *p_LogRead;	    //< shared memory read pointer
-    UInt32 size_to_read;	//< read size
-    CAPTURE_POINT_t capture_point;	//< the capture point value in the stream
-    UInt32 stream_index;	//< the stream number
+	UInt16 *p_LogRead;	/* shared memory read pointer */
+	UInt32 size_to_read;	/* read size */
+	CAPTURE_POINT_t capture_point;	/* capture point value in stream */
+	UInt32 stream_index;	/* the stream number */
 	AUDLOG_DEST_en_t consumer;
 	void *pPrivate;
 	AUDIOLOG_HEADER_t audlog_header;
@@ -126,92 +114,92 @@ typedef struct audlog_cb_info_t
 	int dma_count;
 	int read_count;
 
-} AUDLOG_CB_INFO;
+};
+#define AUDLOG_CB_INFO struct _AUDLOG_CB_INFO
 
-typedef void (*AUDLOG_CB_FUNC)( AUDLOG_CB_INFO *);
-
+typedef void (*AUDLOG_CB_FUNC) (AUDLOG_CB_INFO *);
 
 /**
 	Log data source defintion with DSP sharedmem interface
 **/
-typedef struct audvoc_log_t
-{
-	AUDLOG_CB_FUNC	LogMessageCallback;	//< call back function to save stream frame to flash
-	AUDLOG_DEST_en_t	log_consumer[4];	//< log message consumer 0 : MTT  1 : file system
-} AUDLOG_INFO;
+struct _AUDLOG_INFO {
+	AUDLOG_CB_FUNC LogMessageCallback; /* save stream frame to flash */
+	AUDLOG_DEST_en_t log_consumer[4]; /* 0:MTT; 1:file sys */
+};
+#define AUDLOG_INFO struct _AUDLOG_INFO
 
+/**
+ * buffer: buffer pointer
+ * current_ptr : current position of buffer
+ * length : buffer length
+ * bits_per_sample : number of bits per audio sample
+ * flag : flag = 0, buffer this block, flag = 1, start to play immediately.
+ * buffer_type : 0: buffer mode one buffer, 1: stream mode, use queues.
+ * buffer_serial_index : buffer serial ID
+ */
+struct _AUDVOC_BUFFER_INFO {
+	UInt16 *buffer;
+	UInt16 *current_ptr;
+	Int32 length;
+	UInt32 bits_per_sample;
+	Int32 flag;
+	Int32 buffer_type;
+	UInt32 buffer_serial_index;
+};
+#define AUDVOC_BUFFER_INFO struct _AUDVOC_BUFFER_INFO
 
-typedef struct audvoc_audio_buffer_t
-{
-	UInt16	*buffer;			///< buffer pointer
-	UInt16	*current_ptr;		///< current position of buffer
-	Int32	length;				///< buffer length
-	UInt32	bits_per_sample;	///< number of bits per audio sample
-	Int32	flag;				///< flag = 0, buffer this block, flag = 1, start to play immediately.
-	Int32	buffer_type;		///< buffer_type = 0,	buffer mode one buffer, buffer_type = 1, stream mode, use queues.
-	UInt32	buffer_serial_index; ///< buffer serial ID
-} AUDVOC_BUFFER_INFO;
+struct _AUDIO_PLAY_MSG_t {
+	UInt32 type;
+	AUDVOC_BUFFER_INFO block_info;
+};
+#define AUDIO_PLAY_MSG_t struct _AUDIO_PLAY_MSG_t
 
-typedef struct
-{
-	UInt32				type;
-	AUDVOC_BUFFER_INFO	block_info;
-} AUDIO_PLAY_MSG_t;
+struct _LOG_FRAME_t {
+	UInt16 stream_index;
+	UInt16 log_capture_control;
+	UInt16 log_msg[LOG_FRAME_SIZE / 2];	/* VR_Lin_PCM_t log_msg; */
+};
+#define LOG_FRAME_t struct _LOG_FRAME_t
 
-typedef	struct
-{
-	UInt16	stream_index;
-	UInt16	log_capture_control;
-	UInt16	log_msg[LOG_FRAME_SIZE/2];	// VR_Lin_PCM_t log_msg;
-} LOG_FRAME_t;
-
-typedef struct
-{
-	UInt16*	p_LogMsg;
-	UInt32	i_LogMsgSize;
-} LOG_MSG_SAVE_t;
-
+struct _LOG_MSG_SAVE_t {
+	UInt16 *p_LogMsg;
+	UInt32 i_LogMsgSize;
+};
+#define LOG_MSG_SAVE_t struct _LOG_MSG_SAVE_t
 
 extern wait_queue_head_t bcmlogreadq;
 extern int *bcmlog_stream_area;
+extern int audio_data_arrived;
 
+/* Initialize driver internal variables and task queue. */
+Result_t AUDDRV_AudLog_Init(void);
 
-//Initialize driver internal variables and task queue.
-Result_t AUDDRV_AudLog_Init( void );
+/* Shut down driver internal variables and task queue. */
+Result_t AUDDRV_AudLog_Shutdown(void);
 
-//Shut down driver internal variables and task queue.
-Result_t AUDDRV_AudLog_Shutdown( void );
+/* when driver finished the data in the buffer, driver
+   generates this callback to let client use the buffer.*/
 
-//when driver finished the data in the buffer, driver generates this callback to let client use the buffer.
+Result_t AUDDRV_AudLog_SetBufDoneCB(AUDLOG_CB_FUNC bufDone_cb);
 
-Result_t AUDDRV_AudLog_SetBufDoneCB (AUDLOG_CB_FUNC bufDone_cb);
+/* Read audio data from driver. */
+UInt32 AUDDRV_AudLog_Read(UInt8 *pBuf, UInt32 nSize);
 
+Result_t AUDDRV_AudLog_Start(UInt32 log_stream,
+			     UInt32 log_capture_point,
+			     AUDLOG_DEST_en_t log_consumer, char *filename);
 
-//Read audio data from driver.
-UInt32 AUDDRV_AudLog_Read(
-                    UInt8*     pBuf,
-                    UInt32	   nSize
-					);
+Result_t AUDDRV_AudLog_Pause(void);
 
-Result_t AUDDRV_AudLog_Start (
-							  UInt32 log_stream,
-							  UInt32 log_capture_point,
-							  AUDLOG_DEST_en_t log_consumer,
-							  char *filename
-							  );
+Result_t AUDDRV_AudLog_Resume(void);
 
-Result_t AUDDRV_AudLog_Pause ( void );
+Result_t AUDDRV_AudLog_Stop(UInt32 log_stream);
 
-Result_t AUDDRV_AudLog_Resume( void );
-
-Result_t AUDDRV_AudLog_Stop( UInt32 log_stream );
-
-Result_t AUDDRV_AudLog_StartRetrieveFile( char *filename );
-Result_t AUDDRV_AudLog_StopRetrieveFile( void );
+Result_t AUDDRV_AudLog_StartRetrieveFile(char *filename);
+Result_t AUDDRV_AudLog_StopRetrieveFile(void);
 
 void AUDLOG_ProcessLogChannel(UInt16 audio_stream_buffer_idx);
 
-
 /** @} */
 
-#endif // __AUDDRV_AUDLOG_H__
+#endif /* __AUDDRV_AUDLOG_H__ */
--- sound/caph/audio_driver/audio_ddriver.h
+++ sound/caph/audio_driver/audio_ddriver.h
@@ -1,28 +1,22 @@
-/************************************************************************************************/
-/*                                                                                              */
-/*  Copyright 2011  Broadcom Corporation                                                        */
-/*                                                                                              */
-/*     Unless you and Broadcom execute a separate written software license agreement governing  */
-/*     use of this software, this software is licensed to you under the terms of the GNU        */
-/*     General Public License version 2 (the GPL), available at                                 */
-/*                                                                                              */
-/*          http://www.broadcom.com/licenses/GPLv2.php                                          */
-/*                                                                                              */
-/*     with the following added to such license:                                                */
-/*                                                                                              */
-/*     As a special exception, the copyright holders of this software give you permission to    */
-/*     link this software with independent modules, and to copy and distribute the resulting    */
-/*     executable under terms of your choice, provided that you also meet, for each linked      */
-/*     independent module, the terms and conditions of the license of that module.              */
-/*     An independent module is a module which is not derived from this software.  The special  */
-/*     exception does not apply to any modifications of the software.                           */
-/*                                                                                              */
-/*     Notwithstanding the above, under no circumstances may you combine this software in any   */
-/*     way with any other Broadcom software provided under a license other than the GPL,        */
-/*     without Broadcom's express prior written consent.                                        */
-/*                                                                                              */
-/************************************************************************************************/
+/****************************************************************************
+Copyright 2009 - 2011  Broadcom Corporation
+ Unless you and Broadcom execute a separate written software license agreement
+ governing use of this software, this software is licensed to you under the
+ terms of the GNU General Public License version 2 (the GPL), available at
+    http://www.broadcom.com/licenses/GPLv2.php
 
+ with the following added to such license:
+ As a special exception, the copyright holders of this software give you
+ permission to link this software with independent modules, and to copy and
+ distribute the resulting executable under terms of your choice, provided
+ that you also meet, for each linked independent module, the terms and
+ conditions of the license of that module.
+ An independent module is a module which is not derived from this software.
+ The special exception does not apply to any modifications of the software.
+ Notwithstanding the above, under no circumstances may you combine this software
+ in any way with any other Broadcom software provided under a license other than
+ the GPL, without Broadcom's express prior written consent.
+***************************************************************************/
 /**
 *
 * @file   audio_ddriver.h
@@ -33,115 +27,109 @@
 #define __AUDIO_DDRIVER_H__
 
 /**
- * @addtogroup Audio_ddriver 
+ * @addtogroup Audio_ddriver
  * @{
  */
 
-typedef enum AUDIO_DRIVER_TYPE_t
-{
+enum __AUDIO_DRIVER_TYPE_t {
 	AUDIO_DRIVER_PLAY_VOICE = 0,
-    AUDIO_DRIVER_PLAY_AUDIO,
-    AUDIO_DRIVER_PLAY_RINGER,
-    AUDIO_DRIVER_PLAY_BT,
-    AUDIO_DRIVER_CAPT_VOICE,
-    AUDIO_DRIVER_CAPT_HQ,
-    AUDIO_DRIVER_CAPT_FM,
-    AUDIO_DRIVER_CAPT_BT,
-    AUDIO_DRIVER_VOIP,
-    AUDIO_DRIVER_VOIF
-} AUDIO_DRIVER_TYPE_t;
+	AUDIO_DRIVER_PLAY_AUDIO,
+	AUDIO_DRIVER_PLAY_RINGER,
+	AUDIO_DRIVER_PLAY_BT,
+	AUDIO_DRIVER_CAPT_VOICE,
+	AUDIO_DRIVER_CAPT_HQ,
+	AUDIO_DRIVER_CAPT_FM,
+	AUDIO_DRIVER_CAPT_BT,
+	AUDIO_DRIVER_VOIP,
+	AUDIO_DRIVER_VOIF
+};
+#define AUDIO_DRIVER_TYPE_t enum __AUDIO_DRIVER_TYPE_t
 
-typedef enum AUDIO_DRIVER_CTRL_t
-{
-	AUDIO_DRIVER_START = 0,   
-    AUDIO_DRIVER_STOP,           
-    AUDIO_DRIVER_PAUSE,        
-    AUDIO_DRIVER_RESUME,    
-    AUDIO_DRIVER_FLUSH,       
-    AUDIO_DRIVER_CONFIG,    
-    AUDIO_DRIVER_SET_CB,     
-    AUDIO_DRIVER_SET_VOICE_CAPT_TYPE,  
-    AUDIO_DRIVER_SET_INT_PERIOD,              
-    AUDIO_DRIVER_SET_BUF_PARAMS,
-    AUDIO_DRIVER_GET_DRV_TYPE,
-    AUDIO_DRIVER_ALLOC_BUFFER,                 
-    AUDIO_DRIVER_FREE_BUFFER,
-    AUDIO_DRIVER_SET_VOIP_DL_CB,
-    AUDIO_DRIVER_SET_VOIP_UL_CB,
-    AUDIO_DRIVER_SET_VOIF_CB
-} AUDIO_DRIVER_CTRL_t;
+enum __AUDIO_DRIVER_CTRL_t {
+	AUDIO_DRIVER_START = 0,
+	AUDIO_DRIVER_STOP,
+	AUDIO_DRIVER_PAUSE,
+	AUDIO_DRIVER_RESUME,
+	AUDIO_DRIVER_FLUSH,
+	AUDIO_DRIVER_CONFIG,
+	AUDIO_DRIVER_SET_CB,
+	AUDIO_DRIVER_SET_VOICE_CAPT_TYPE,
+	AUDIO_DRIVER_SET_INT_PERIOD,
+	AUDIO_DRIVER_SET_BUF_PARAMS,
+	AUDIO_DRIVER_GET_DRV_TYPE,
+	AUDIO_DRIVER_ALLOC_BUFFER,
+	AUDIO_DRIVER_FREE_BUFFER,
+	AUDIO_DRIVER_SET_VOIP_DL_CB,
+	AUDIO_DRIVER_SET_VOIP_UL_CB,
+	AUDIO_DRIVER_SET_VOIF_CB
+};
+#define AUDIO_DRIVER_CTRL_t enum __AUDIO_DRIVER_CTRL_t
 
-typedef struct AUDIO_DRIVER_CONFIG_t
-{
-	AUDIO_SAMPLING_RATE_t       sample_rate;
-	AUDIO_NUM_OF_CHANNEL_t      num_channel;
-	AUDIO_BITS_PER_SAMPLE_t     bits_per_sample;
-	UInt32 						instanceId; //ARM2SP1/ARM2SP2 mapped to PCMOUT1/PCMOUT2
-	UInt32 						arm2sp_mixMode;
-} AUDIO_DRIVER_CONFIG_t;
+struct _AUDIO_DRIVER_CONFIG_t {
+	AUDIO_SAMPLING_RATE_t sample_rate;
+	AUDIO_NUM_OF_CHANNEL_t num_channel;
+	AUDIO_BITS_PER_SAMPLE_t bits_per_sample;
+	UInt32 instanceId;	/* ARM2SP1/ARM2SP2 mapped to PCMOUT1/PCMOUT2 */
+	UInt32 arm2sp_mixMode;
+};
+#define AUDIO_DRIVER_CONFIG_t struct _AUDIO_DRIVER_CONFIG_t
 
-typedef struct AUDIO_DRIVER_BUFFER_t
-{
-    UInt32    buf_size;
-    UInt32    phy_addr;
-    UInt8*    pBuf;
-    
-} AUDIO_DRIVER_BUFFER_t;
+struct _AUDIO_DRIVER_BUFFER_t {
+	UInt32 buf_size;
+	UInt32 phy_addr;
+	UInt8 *pBuf;
+};
+#define AUDIO_DRIVER_BUFFER_t struct _AUDIO_DRIVER_BUFFER_t
 
-typedef void*  AUDIO_DRIVER_HANDLE_t;
+#define AUDIO_DRIVER_HANDLE_t void *
 
+typedef void (*AUDIO_DRIVER_InterruptPeriodCB_t) (void *p);
 
-typedef void (*AUDIO_DRIVER_InterruptPeriodCB_t)( void * p);
+typedef void (*AUDIO_DRIVER_VoipCB_t) (void *p, u8 * pBuf, u32 nsize);
 
-typedef void (*AUDIO_DRIVER_VoipCB_t)( void *p, u8* pBuf, u32 nsize);
+struct _AUDIO_DRIVER_CallBackParams_t {
+	AUDIO_DRIVER_InterruptPeriodCB_t pfCallBack;
+	void *pPrivateData;
+	AUDIO_DRIVER_VoipCB_t voipULCallback;
+	AUDIO_DRIVER_VoipCB_t voipDLCallback;
+};
+#define AUDIO_DRIVER_CallBackParams_t struct _AUDIO_DRIVER_CallBackParams_t
 
-
-typedef struct AUDIO_DRIVER_CallBackParams_t
-{
-	AUDIO_DRIVER_InterruptPeriodCB_t	pfCallBack;
-	void	*pPrivateData;
-	AUDIO_DRIVER_VoipCB_t				voipULCallback;
-	AUDIO_DRIVER_VoipCB_t				voipDLCallback;
-
-}AUDIO_DRIVER_CallBackParams_t;
-
-typedef struct voip_data
-{
+struct _voip_data_t {
 	UInt32 codec_type;
 	UInt32 bitrate_index;
 	UInt32 mic;
-	UInt32 spk; 	
-	UInt8  isVoLTE;
-}voip_data_t;
+	UInt32 spk;
+	UInt8 isVoLTE;
+};
+#define voip_data_t struct _voip_data_t
 
 /* ARM2SP declarations */
 
-typedef enum
-{
+enum __VORENDER_ARM2SP_INSTANCE_e {
 	VORENDER_ARM2SP_INSTANCE1,
 	VORENDER_ARM2SP_INSTANCE2,
 	VORENDER_ARM2SP_INSTANCE_TOTAL
-} VORENDER_ARM2SP_INSTANCE_e;
+};
+#define VORENDER_ARM2SP_INSTANCE_e enum __VORENDER_ARM2SP_INSTANCE_e
 
 /* Voice capture declarations */
 
-typedef enum VOCAPTURE_RECORD_MODE_t
-{
+enum __VOCAPTURE_RECORD_MODE_t {
 	VOCAPTURE_RECORD_NONE,
-	VOCAPTURE_RECORD_UL,		
+	VOCAPTURE_RECORD_UL,
 	VOCAPTURE_RECORD_DL,
 	VOCAPTURE_RECORD_BOTH
-} VOCAPTURE_RECORD_MODE_t;
-						
+};
+#define VOCAPTURE_RECORD_MODE_t enum __VOCAPTURE_RECORD_MODE_t
 
-typedef enum VOCAPTURE_VOICE_MIX_MODE_t
-{
+enum __VOCAPTURE_VOICE_MIX_MODE_t {
 	VOCAPTURE_VOICE_MIX_NONE,
 	VOCAPTURE_VOICE_MIX_DL,
 	VOCAPTURE_VOICE_MIX_UL,
 	VOCAPTURE_VOICE_MIX_BOTH
-} VOCAPTURE_VOICE_MIX_MODE_t;
-
+};
+#define VOCAPTURE_VOICE_MIX_MODE_t enum __VOCAPTURE_VOICE_MIX_MODE_t
 
 /**
 *  @brief  This function is used to open the audio data driver
@@ -150,8 +138,8 @@ typedef enum VOCAPTURE_VOICE_MIX_MODE_t
 *
 *  @return AUDIO_DRIVER_HANDLE_t
 *
-****************************************************************************/
-AUDIO_DRIVER_HANDLE_t  AUDIO_DRIVER_Open(AUDIO_DRIVER_TYPE_t drv_type);
+ *************************************************************************/
+AUDIO_DRIVER_HANDLE_t AUDIO_DRIVER_Open(AUDIO_DRIVER_TYPE_t drv_type);
 
 /**
 *  @brief  This function is used to close the audio data driver
@@ -160,7 +148,7 @@ AUDIO_DRIVER_HANDLE_t  AUDIO_DRIVER_Open(AUDIO_DRIVER_TYPE_t drv_type);
 *
 *  @return none
 *
-****************************************************************************/
+ *************************************************************************/
 void AUDIO_DRIVER_Close(AUDIO_DRIVER_HANDLE_t drv_handle);
 
 /**
@@ -172,10 +160,9 @@ void AUDIO_DRIVER_Close(AUDIO_DRIVER_HANDLE_t drv_handle);
 *
 *  @return none
 *
-****************************************************************************/
+ *************************************************************************/
 void AUDIO_DRIVER_Read(AUDIO_DRIVER_HANDLE_t drv_handle,
-                  UInt8* pBuf,
-                  UInt32 nSize);
+		       UInt8 *pBuf, UInt32 nSize);
 
 /**
 *  @brief  This function is used to write the data to the driver
@@ -186,10 +173,9 @@ void AUDIO_DRIVER_Read(AUDIO_DRIVER_HANDLE_t drv_handle,
 *
 *  @return none
 *
-****************************************************************************/
+ *************************************************************************/
 void AUDIO_DRIVER_Write(AUDIO_DRIVER_HANDLE_t drv_handle,
-                   UInt8* pBuf,
-                   UInt32 nBufSize);
+			UInt8 *pBuf, UInt32 nBufSize);
 
 /**
 *  @brief  This function is used to send a control command to the driver
@@ -200,11 +186,9 @@ void AUDIO_DRIVER_Write(AUDIO_DRIVER_HANDLE_t drv_handle,
 *
 *  @return none
 *
-****************************************************************************/
+ *************************************************************************/
 void AUDIO_DRIVER_Ctrl(AUDIO_DRIVER_HANDLE_t drv_handle,
-                       AUDIO_DRIVER_CTRL_t ctrl_cmd,
-                       void* pCtrlStruct);
-
+		       AUDIO_DRIVER_CTRL_t ctrl_cmd, void *pCtrlStruct);
 
 /**
 *  @brief  This function is used to update the buffer indexes
@@ -217,72 +201,67 @@ void AUDIO_DRIVER_Ctrl(AUDIO_DRIVER_HANDLE_t drv_handle,
 *
 *  @return none
 *
-****************************************************************************/
-void AUDIO_DRIVER_UpdateBuffer (AUDIO_DRIVER_HANDLE_t drv_handle,
-                                UInt8* pBuf,
-                                UInt32 nBufSize,
-                                UInt32 nCurrentIndex,
-                                UInt32 nSize);
-
+ *************************************************************************/
+void AUDIO_DRIVER_UpdateBuffer(AUDIO_DRIVER_HANDLE_t drv_handle,
+			       UInt8 *pBuf,
+			       UInt32 nBufSize,
+			       UInt32 nCurrentIndex, UInt32 nSize);
 
 UInt32 StreamIdOfDriver(AUDIO_DRIVER_HANDLE_t h);
 
-// ==========================================================================
-//
-// Function Name: ARM2SP_Render_Request
-//
-// Description: Callback for ARM2SP1 playback
-//
-// =========================================================================
-
+/**
+ *
+ * Function Name: ARM2SP_Render_Request
+ *
+ * Description: Callback for ARM2SP1 playback
+ *
+ *************************************************************************/
 void ARM2SP_Render_Request(UInt16 buf_index);
 
-// ==========================================================================
-//
-// Function Name: ARM2SP2_Render_Request
-//
-// Description: Callback for ARM2SP2 playback
-//
-// =========================================================================
-
+/**
+ *
+ * Function Name: ARM2SP2_Render_Request
+ *
+ * Description: Callback for ARM2SP2 playback
+ *
+ *************************************************************************/
 void ARM2SP2_Render_Request(UInt16 buf_index);
 
-// ===================================================================
-//
-// Function Name: VPU_Capture_Request
-//
-// Description: Send a VPU capture request for voice capture driver to copy
-// data from DSP shared memory.
-//
-// ====================================================================
+/**
+ *
+ * Function Name: VPU_Capture_Request
+ *
+ * Description: Send a VPU capture request for voice capture driver to copy
+ * data from DSP shared memory.
+ *
+ *************************************************************************/
 void VPU_Capture_Request(UInt16 buf_index);
 
-//*********************************************************************
 /**
 * Prototype of voif callback function.
 *
 *   @param  ulData: The data pointer of the UL data. Input of VOIF processing
-*   @param  dlData: The data pointer of the DL data. Input of VOIF processing and output VOIF processing.
+*   @param  dlData: The data pointer of the DL data. Input of VOIF processing
+*	 and output VOIF processing.
 *   @param  sampleCout: the number of samples, 16 bit per sample
 *   @param  isCall16K: indicates whether voice call is WB or NB
 *   @return void
-*   @note   The user code use the callback function to get UL/DL data and write back processed DL.
-**************************************************************************/
-typedef void (*VOIF_CB) (Int16 * ulData, Int16 *dlData, UInt32 sampleCount, UInt8 isCall16K);
+*   @note   The user code use the callback function to get UL/DL data and
+*	write back processed DL.
+ *************************************************************************/
+typedef void (*VOIF_CB) (Int16 *ulData, Int16 *dlData, UInt32 sampleCount,
+			 UInt8 isCall16K);
 
-
-//*********************************************************************
 /**
 * VOIF processing interrupt handler.
 *
 *   @param  bufferIndex: The index of the ping-pong buffers to hold the DL/DL data
 *   @return void
 *   @note   Not for application programming.
-**************************************************************************/
-// voif interrupt handler, called by hw interrupt.
-void VOIF_Buffer_Request (UInt32 bufferIndex, UInt32 samplingRate);
+ *************************************************************************/
+void VOIF_Buffer_Request(UInt32 bufferIndex, UInt32 samplingRate);
 
 void AP_ProcessStatusMainAMRDone(UInt16 codecType);
 void VOIP_ProcessVOIPDLDone(void);
 
-#endif //#define __AUDIO_DDRIVER_H__
+#endif /* #define __AUDIO_DDRIVER_H__ */
