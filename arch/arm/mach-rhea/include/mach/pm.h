e33f1ef3b8910f76ea1a573af2ba20968c4c4a9f
Sajith <sajith@broadcom.com>
2012-01-17 03:27:19 -0800

    PM: GPIO code to debug dormant mode
    [Defect / Enhancement / New Feature]
    MobC00193714
    
    [Problem]
    GPIO code to debug dormant mode
    
    [Solution]
    Use GPIO to determine if crash due to dormant
    mode occurred after returning to non-secure code
    path.
    
    [Reviewers]
    Shameer, Prasanth
    
    [Testing]
        [Test Cases]
        1. Run the following commands on rhearay edn2x to
           configure the debug gpio.
        busybox devmem 0x35004FF0 32 0x00a5a501
        busybox devmem 0x35004F80 32 0x00000000
        busybox devmem 0x3500483C 32 0x00000004
        busybox devmem 0x35003520 32 0x00A5A501
        busybox devmem 0x35003100 32 0x00000000
    
        echo "d g s 0" > /sys/module/rhea_pm_dbg/parameters/debug
        echo "d g e" > /sys/module/rhea_pm_dbg/parameters/debug
        echo "d g d" > /sys/module/rhea_pm_dbg/parameters/debug
    
        2. Monitor GPIO0 on oscilloscope. GPIO0 will be:
           a. LOW on dormant entry.
           b. HIGH on dormant exit.
    
        [Label/CL# Synced To]
        HEAD
    
        [Platform]
        Rhea
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: Ie9e9ea133f458ef1b6961391db430e0fc1468614
    Signed-off-by: Sajith <sajith@broadcom.com>

--- arch/arm/mach-rhea/include/mach/pm.h
+++ arch/arm/mach-rhea/include/mach/pm.h
@@ -3,6 +3,24 @@
 
 #define KONA_MACH_MAX_IDLE_STATE 1
 
+#ifndef __ASSEMBLY__
+/*
+ * Any change in this structure should reflect in the definition
+ * in the asm file (arch/arm/mach-rhea/dm_pwr_policy_top.S).
+ */
+struct dormant_gpio_data {
+	s32 enable;
+	u32 gpio_set_p;
+	u32 gpio_set_v;
+	u32 gpio_clr_p;
+	u32 gpio_clr_v;
+	u32 gpio_bit;
+};
+
 extern u32 dormant_start(void);
+
+/* Variables exported by asm code */
+extern struct dormant_gpio_data dormant_gpio_data;
+#endif /* __ASSEMBLY__ */
 
 #endif /*__RHEA_PM_H__*/
