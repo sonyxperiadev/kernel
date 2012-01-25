eb1fe57fa8f321e9fe6ba62b3180ca7e60112c9f
kamrun <kamrun@broadcom.com>
2012-01-25 10:24:49 -0800

    Revert "PM: GPIO code to debug dormant mode"
    This reverts commit e33f1ef3b8910f76ea1a573af2ba20968c4c4a9f.

--- arch/arm/mach-rhea/rhea_pm_dbg.c
+++ arch/arm/mach-rhea/rhea_pm_dbg.c
@@ -13,8 +13,6 @@
 #include <mach/io_map.h>
 #include <mach/rdb/brcm_rdb_csr.h>
 #include <mach/rdb/brcm_rdb_hsotg_ctrl.h>
-#include <mach/rdb/brcm_rdb_gpio.h>
-#include <mach/pm.h>
 
 /*****************************************************************************
  *                        SLEEP STATE DEBUG INTERFACE                        *
@@ -44,7 +42,6 @@ module_param_named(debug, debug, debug, S_IRUGO | S_IWUSR | S_IWGRP);
 /* List of supported commands */
 enum {
 	CMD_SHOW_HELP = 'h',
-	CMD_DORMANT = 'd',
 };
 
 static void cmd_show_usage(void)
@@ -52,72 +49,9 @@ static void cmd_show_usage(void)
 	const char usage[] = "Usage:\n"
 	  "echo 'cmd string' > /sys/module/rhea_pm/parameters/debug\n"
 	  "'cmd string' is constructed as follows:\n"
-	  "set dormant gpio d g s <gpio number>\n"
-	  "enable dormant gpio d g e\n"
-	  "disable dormant gpio d g c\n"
-	  "display dormant gpio data d g d\n"
 	  "\n";
 
 	pr_info("%s", usage);
-}
-
-#define GPIO_GPORS_BASE_PHYS    (GPIO2_BASE_ADDR + GPIO_GPORS0_OFFSET)
-#define GPIO_GPORS_BASE_VIRT    (KONA_GPIO2_VA + GPIO_GPORS0_OFFSET)
-#define GPIO_GPORC_BASE_PHYS    (GPIO2_BASE_ADDR + GPIO_GPORC0_OFFSET)
-#define GPIO_GPORC_BASE_VIRT    (KONA_GPIO2_VA + GPIO_GPORC0_OFFSET)
-
-static void cmd_dormant_gpio(const char *p)
-{
-	u32 gpio;
-	u32 reg;
-
-	switch (*p) {
-	case 'e':
-		pr_info("%s: enabling dormant gpio\n", __func__);
-		dormant_gpio_data.enable = 1;
-		break;
-	case 'c':
-		pr_info("%s: disabling dormant gpio\n", __func__);
-		dormant_gpio_data.enable = 0;
-		break;
-	case 's':
-		p++;
-		while (*p == ' ' || *p == '\t')
-			p++;
-
-		sscanf(p, "%d", &gpio);
-		pr_info("%s: setting dormant gpio to %d\n", __func__, gpio);
-		reg = gpio / 32;
-		dormant_gpio_data.gpio_bit = 1 << (gpio % 32);
-		dormant_gpio_data.gpio_set_p = GPIO_GPORS_BASE_PHYS + reg * 4;
-		dormant_gpio_data.gpio_set_v = GPIO_GPORS_BASE_VIRT + reg * 4;
-		dormant_gpio_data.gpio_clr_p = GPIO_GPORC_BASE_PHYS + reg * 4;
-		dormant_gpio_data.gpio_clr_v = GPIO_GPORC_BASE_VIRT + reg * 4;
-		break;
-	case 'd':
-		pr_info("enable       : %d\n", dormant_gpio_data.enable);
-		pr_info("gpio set phys: %x\n", dormant_gpio_data.gpio_set_p);
-		pr_info("gpio set virt: %x\n", dormant_gpio_data.gpio_set_v);
-		pr_info("gpio clr phys: %x\n", dormant_gpio_data.gpio_clr_p);
-		pr_info("gpio clr virt: %x\n", dormant_gpio_data.gpio_clr_v);
-		pr_info("gpio bit     : %x\n", dormant_gpio_data.gpio_bit);
-		break;
-	}
-}
-
-static void cmd_dormant(const char *p)
-{
-	char cmd = *p;
-
-	p++;
-	while (*p == ' ' || *p == '\t')
-		p++;
-
-	switch (cmd) {
-	case 'g': /* Handle dormant gpio related commands */
-		cmd_dormant_gpio(p);
-		break;
-	}
 }
 
 static int param_set_debug(const char *val, const struct kernel_param *kp)
@@ -136,9 +70,6 @@ static int param_set_debug(const char *val, const struct kernel_param *kp)
 		p++;
 
 	switch (val[0]) {
-	case CMD_DORMANT:
-		cmd_dormant(p);
-		break;
 	case CMD_SHOW_HELP: /* Fall-through */
 	default:
 		cmd_show_usage();
@@ -205,7 +136,6 @@ void instrument_idle_exit(void)
 int __init rhea_pmdbg_init(void)
 {
 	snapshot_table_register(snapshot, ARRAY_SIZE(snapshot));
-	dormant_gpio_data.enable = 0;
 	return 0;
 }
 
