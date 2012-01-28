5ab9aeef51490f12e577bba4b76b9c779abf23e9
shriram choudhary <shriramc@broadcom.com>
2012-01-27 16:33:06 -0800

    Added BCM4334 WLAN driver
    Change-Id: I81e4ba8205bd1b3b8245920e0436656c1db488aa

--- drivers/mmc/core/sdio.c
+++ drivers/mmc/core/sdio.c
@@ -115,7 +115,9 @@ static int sdio_read_cccr(struct mmc_card *card)
 
 	cccr_vsn = data & 0x0f;
 
-	if (cccr_vsn > SDIO_CCCR_REV_1_20) {
+//	if (cccr_vsn > SDIO_CCCR_REV_1_20) {
+	if (cccr_vsn > SDIO_CCCR_REV_3_00) {
+
 		printk(KERN_ERR "%s: unrecognised CCCR structure version %d\n",
 			mmc_hostname(card->host), cccr_vsn);
 		return -EINVAL;
