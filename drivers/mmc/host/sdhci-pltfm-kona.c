7e02b6f027f4941b878b9382d0ad6b7c4c8d952d
Sundar Jayakumar Dev <sundarjdev@broadcom.com>
2012-02-06 01:05:02 -0800

    Set SD GPIO card detect debounce value to max (128ms)
    [Defect / Enhancement / New Feature]
    MobC00194642
    
    [Problem]
    Debounce value for SD Card detet GPIO is currently set to default value of 1ms
    
    [Solution]
    Set SD GPIO card detect debounce value to max (128ms)
    
    [Reviewers]
    Naga Chandra P and Vikram M P
    [Testing]
        [Test Cases]
    1. SD Card insertion and removal
        [Label/CL# Synced To]
    HEAD of sdb-rhea-android-ics
        [Platform]
    Rheabherri EDN40
    [BROADCOM INTERNAL NOTES]
    
    Signed-off-by: Sundar Jayakumar Dev <sundarjdev@broadcom.com>

--- drivers/mmc/host/sdhci-pltfm-kona.c
+++ drivers/mmc/host/sdhci-pltfm-kona.c
@@ -69,6 +69,8 @@
 #define PROC_GLOBAL_PARENT_DIR  "sdhci-pltfm"
 #define PROC_ENTRY_CARD_CTRL    "cardCtrl"
 
+#define SD_DETECT_GPIO_DEBOUNCE_128MS	128000
+
 struct procfs {
 	char name[MAX_PROC_NAME_SIZE];
 	struct proc_dir_entry *parent;
@@ -575,6 +577,13 @@ static int __devinit sdhci_pltfm_probe(struct platform_device *pdev)
 		if (ret) {
 			dev_err(&pdev->dev, "Unable to request card detection irq=%d for gpio=%d\n",
 			gpio_to_irq(dev->cd_gpio), dev->cd_gpio);
+			goto err_free_cd_gpio;
+		}
+
+		/* Set debounce for SD Card detect to maximum value */
+		ret = gpio_set_debounce(dev->cd_gpio, SD_DETECT_GPIO_DEBOUNCE_128MS);
+		if (ret < 0) {
+			dev_err(&pdev->dev, "%s: gpio set debounce failed\n", __func__);
 			goto err_free_cd_gpio;
 		}
 
