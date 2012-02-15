038eb68c6e7de86969de101ec1c8da7bb5c90f82
swamins <swamins@broadcom.com>
2012-02-14 19:42:45 -0800

    Build: Do some clean up with compile flags
    [Defect / Enhancement / New Feature]
    Add Support for hierarchical compile option for Samsung projects.
    
    [Problem]
    Samsung changed the name for common flag as
    CONFIG_MACH_RHEA_SS_COMMON=y
    
    So top level, we need to use CONFIG_MACH_RHEA_SS_COMMON and the project level we use one of these:
    
    CONFIG_MACH_RHEA_SS -> for the first cooper model
    CONFIG_MACH_RHEA_SS_AMAZING -> for Amazing
    CONFIG_MACH_RHEA_SS_LUCAS -> for Lucas
    
    [Solution]
    Modified accordingly
    
    [Reviewers]
    Yan, Francis, Rizavan
    
    [Testing]
        [Test Cases]
    Verified build is ok
    
        [Label/CL# Synced To]
    Head
    
        [Platform]
    Amazing
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: I3eeab6bd6e536cc7cf31176cc2de7c6e09c3fce5

--- drivers/video/broadcom/rhea_fb.c
+++ drivers/video/broadcom/rhea_fb.c
@@ -77,8 +77,8 @@ struct rhea_fb {
 	struct completion thread_sem;
 	struct mutex update_sem;
 	struct completion prev_buf_done_sem;
-#if !defined(CONFIG_MACH_RHEA_RAY_EDN1X) && !defined(CONFIG_MACH_RHEA_BERRI) && !defined(CONFIG_MACH_RHEA_RAY_EDN2X) && !defined(CONFIG_MACH_RHEA_SS)  \
-	&& !defined(CONFIG_MACH_RHEA_SS_AMAZING) && !defined(CONFIG_MACH_RHEA_RAY_DEMO) && !defined(CONFIG_MACH_RHEA_BERRI_EDN40)
+#if !defined(CONFIG_MACH_RHEA_RAY_EDN1X) && !defined(CONFIG_MACH_RHEA_BERRI) && !defined(CONFIG_MACH_RHEA_RAY_EDN2X) && !defined(CONFIG_MACH_RHEA_SS_COMMON)  \
+	&& !defined(CONFIG_MACH_RHEA_RAY_DEMO) && !defined(CONFIG_MACH_RHEA_BERRI_EDN40)
 	struct completion refresh_wait_sem;
 #endif
 	atomic_t buff_idx;
@@ -488,8 +488,8 @@ static void rhea_fb_late_resume(struct early_suspend *h)
 }
 #endif
 
-#if !defined(CONFIG_MACH_RHEA_RAY_EDN1X) && !defined(CONFIG_MACH_RHEA_BERRI) && !defined(CONFIG_MACH_RHEA_RAY_EDN2X) && !defined(CONFIG_MACH_RHEA_SS) \
-	&& !defined(CONFIG_MACH_RHEA_SS_AMAZING) && !defined(CONFIG_MACH_RHEA_RAY_DEMO) && !defined(CONFIG_MACH_RHEA_BERRI_EDN40)
+#if !defined(CONFIG_MACH_RHEA_RAY_EDN1X) && !defined(CONFIG_MACH_RHEA_BERRI) && !defined(CONFIG_MACH_RHEA_RAY_EDN2X) && !defined(CONFIG_MACH_RHEA_SS_COMMON) \
+	&& !defined(CONFIG_MACH_RHEA_RAY_DEMO) && !defined(CONFIG_MACH_RHEA_BERRI_EDN40)
 static int rhea_refresh_thread(void *arg)
 {
 	struct rhea_fb *fb = arg;
@@ -528,7 +528,7 @@ static struct notifier_block vt_notifier_block = {
 	.notifier_call = vt_notifier_call,
 };
 
-#endif /* !CONFIG_MACH_RHEA_RAY_EDN1X  && !CONFIG_MACH_RHEA_RAY_EDN2X && !CONFIG_MACH_RHEA_SS && !CONFIG_MACH_RHEA_SS_AMAZING */
+#endif /* !CONFIG_MACH_RHEA_RAY_EDN1X  && !CONFIG_MACH_RHEA_RAY_EDN2X && !CONFIG_MACH_RHEA_SS_COMMON */
 
 static struct fb_ops rhea_fb_ops = {
 	.owner          = THIS_MODULE,
@@ -593,8 +593,8 @@ static int rhea_fb_probe(struct platform_device *pdev)
 	atomic_set(&fb->is_graphics_started, 0);
 	init_completion(&fb->thread_sem);
 
-#if !defined(CONFIG_MACH_RHEA_RAY_EDN1X) && !defined(CONFIG_MACH_RHEA_BERRI) && !defined(CONFIG_MACH_RHEA_RAY_EDN2X) && !defined(CONFIG_MACH_RHEA_SS) \
-	&& !defined(CONFIG_MACH_RHEA_SS_AMAZING) && !defined(CONFIG_MACH_RHEA_RAY_DEMO) && !defined(CONFIG_MACH_RHEA_BERRI_EDN40)
+#if !defined(CONFIG_MACH_RHEA_RAY_EDN1X) && !defined(CONFIG_MACH_RHEA_BERRI) && !defined(CONFIG_MACH_RHEA_RAY_EDN2X) && !defined(CONFIG_MACH_RHEA_SS_COMMON) \
+	&& !defined(CONFIG_MACH_RHEA_RAY_DEMO) && !defined(CONFIG_MACH_RHEA_BERRI_EDN40)
 
 	sema_init(&fb->refresh_wait_sem, 0);
 
@@ -734,8 +734,8 @@ static int rhea_fb_probe(struct platform_device *pdev)
 	atomic_set(&fb->is_fb_registered, 1);
 	rheafb_info("RHEA Framebuffer probe successfull\n");
 
-#if !defined(CONFIG_MACH_RHEA_RAY_EDN1X) && !defined(CONFIG_MACH_RHEA_BERRI) && !defined(CONFIG_MACH_RHEA_RAY_EDN2X) && !defined(CONFIG_MACH_RHEA_SS) \
-	&& !defined(CONFIG_MACH_RHEA_SS_AMAZING) && !defined(CONFIG_MACH_RHEA_RAY_DEMO) && !defined(CONFIG_MACH_RHEA_BERRI_EDN40) && !defined(CONFIG_MACH_RHEA_STONE)
+#if !defined(CONFIG_MACH_RHEA_RAY_EDN1X) && !defined(CONFIG_MACH_RHEA_BERRI) && !defined(CONFIG_MACH_RHEA_RAY_EDN2X) && !defined(CONFIG_MACH_RHEA_SS_COMMON) \
+	&& !defined(CONFIG_MACH_RHEA_RAY_DEMO) && !defined(CONFIG_MACH_RHEA_BERRI_EDN40) && !defined(CONFIG_MACH_RHEA_STONE)
 	register_vt_notifier(&vt_notifier_block);
 #endif
 
@@ -784,8 +784,8 @@ err_set_var_failed:
 
 err_enable_display_failed:
 err_fbmem_alloc_failed:
-#if !defined(CONFIG_MACH_RHEA_RAY_EDN1X) && !defined(CONFIG_MACH_RHEA_BERRI) && !defined(CONFIG_MACH_RHEA_RAY_EDN2X) && !defined(CONFIG_MACH_RHEA_SS) \
-	&& !defined(CONFIG_MACH_RHEA_SS_AMAZING) && !defined(CONFIG_MACH_RHEA_RAY_DEMO) && !defined(CONFIG_MACH_RHEA_BERRI_EDN40)
+#if !defined(CONFIG_MACH_RHEA_RAY_EDN1X) && !defined(CONFIG_MACH_RHEA_BERRI) && !defined(CONFIG_MACH_RHEA_RAY_EDN2X) && !defined(CONFIG_MACH_RHEA_SS_COMMON) \
+	&& !defined(CONFIG_MACH_RHEA_RAY_DEMO) && !defined(CONFIG_MACH_RHEA_BERRI_EDN40)
 thread_create_failed:
 #endif
 	if (pi_mgr_dfs_request_remove(&fb->dfs_node))
