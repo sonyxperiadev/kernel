383b564443bc0105a08ebe69a6111a9ac84e42ae
Raju Biswas <Raju.Biswas@broadcom.com>
2012-03-02 15:45:02 +0530

    "Audio: no default log level"
    [Defect / Enhancement / New Feature]
    MobC00197816
    
    [Problem]
    See above
    
    [Solution]
    See above
    
    [Reviewers]
    blr-audio,us-audio
    
    [Testing]
        [Test Cases]
    music playback,music capture,voice call - MO,MT,
    rapid touchtones and vibra
    voice call(MO + MT - both empty calls and receive calls) and music and
    press keys in dial pad.
    voice call(MO + MT - both empty calls and receive calls) and voice
    record and press keys in dial pad.
    FM and voice call(MO + MT - both empty calls and receive calls) and
    press kets in dial pad
        [Label/CL# Synced To]
    sdb-android-rhea-ics
    
        [Platform]
    android-rheastone-edn00
    
    [BROADCOM INTERNAL NOTES]

--- sound/caph/caph_module.c
+++ sound/caph/caph_module.c
@@ -70,7 +70,7 @@ MODULE_DESCRIPTION("Broadcom CAPH sound interface");
 MODULE_LICENSE("GPL");
 
 /* global */
-int gAudioDebugLevel = 2; /*Default audio logging is set to 2:ALSA Interface enabled */
+int gAudioDebugLevel; /* No default audio logging */
 module_param(gAudioDebugLevel, int, 0);
 
 static brcm_alsa_chip_t *sgpCaph_chip;
