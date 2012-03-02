98be0cebe2a72c7ff2cb368a7c9f16f0cf8a316b
Praveen Chidambaram <pchidamb@codeaurora.org>
2012-03-02 10:01:08 -0700

    msm: spm: Update PMIC DLY values in SPM
    In a rare possibility, PMIC8921 could get a command but may delay in
    setting the voltage during power collapse restore and in such a case.
    the Krait SPM would wait for 303 us and may power on the Krait assuming
    that the PMIC voltage has been set. If the voltage is not stabilized by
    then, Krait could possibly end up in a unresponsive state. Increase the
    delay in SPM sequence in waiting for the voltage to stabilize. This
    patch sets the delay to 607 us.
    
    Change-Id: I9c064ce60856b576f123587364f6d64a05de4023
    Signed-off-by: Praveen Chidambaram <pchidamb@codeaurora.org>

--- arch/arm/mach-msm/board-8960.c
+++ arch/arm/mach-msm/board-8960.c
@@ -1158,7 +1158,7 @@ static struct msm_spm_platform_data msm_spm_data[] __initdata = {
 		.reg_init_values[MSM_SPM_REG_SAW2_AVS_HYSTERESIS] = 0x00,
 #endif
 		.reg_init_values[MSM_SPM_REG_SAW2_SPM_CTL] = 0x01,
-		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DLY] = 0x02020202,
+		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DLY] = 0x02020204,
 		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_0] = 0x0060009C,
 		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_1] = 0x0000001C,
 		.vctl_timeout_us = 50,
@@ -1175,7 +1175,7 @@ static struct msm_spm_platform_data msm_spm_data[] __initdata = {
 		.reg_init_values[MSM_SPM_REG_SAW2_AVS_HYSTERESIS] = 0x00,
 #endif
 		.reg_init_values[MSM_SPM_REG_SAW2_SPM_CTL] = 0x01,
-		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DLY] = 0x02020202,
+		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DLY] = 0x02020204,
 		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_0] = 0x0060009C,
 		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_1] = 0x0000001C,
 		.vctl_timeout_us = 50,
@@ -1225,7 +1225,7 @@ static struct msm_spm_platform_data msm_spm_l2_data[] __initdata = {
 		.reg_base_addr = MSM_SAW_L2_BASE,
 		.reg_init_values[MSM_SPM_REG_SAW2_SECURE] = 0x00,
 		.reg_init_values[MSM_SPM_REG_SAW2_SPM_CTL] = 0x00,
-		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DLY] = 0x02020202,
+		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DLY] = 0x02020204,
 		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_0] = 0x00A000AE,
 		.reg_init_values[MSM_SPM_REG_SAW2_PMIC_DATA_1] = 0x00A00020,
 		.modes = msm_spm_l2_seq_list,
