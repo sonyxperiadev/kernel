786e7a82a2d8036c2baa77c2813dc0e4b3a28c1d
Sajith P V <sajith@broadcom.com>
2012-03-03 09:25:21 -0800

    PM: Remove JIRA2301 Workaround for Rhea B1
    [Defect / Enhancement / New Feature]
    Enhancement
    
    [Problem]
    Remove JIRA2301 Workaround for Rhea B1
    
    [Solution]
    Remove JIRA2301 Workaround for Rhea B1
    
    [Reviewers]
    Shameer, Vikas
    
    [Testing]
        [Test Cases]
        Ensure sleep pattern is fine
    
        [Label/CL# Synced To]
        HEAD
    
        [Platform]
        Rhea
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: I0fc5e548b7134159666c4e9980ecb59ff49e58b9
    Signed-off-by: Sajith P V <sajith@broadcom.com>

--- arch/arm/mach-rhea/pm_params.c
+++ arch/arm/mach-rhea/pm_params.c
@@ -316,7 +316,7 @@ static void rhea_pm_init_wa_flgs(void)
 #endif
 
 #ifdef CONFIG_RHEA_WA_HWJIRA_2301
-	JIRA_WA_FLG_NAME(2301) = chip_rev <= RHEA_CHIP_REV_B1;
+	JIRA_WA_FLG_NAME(2301) = chip_rev < RHEA_CHIP_REV_B1;
 #endif
 
 #ifdef CONFIG_RHEA_WA_HWJIRA_2489
