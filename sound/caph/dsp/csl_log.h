13b38f9ea1a781b259e17dbf3cc9e0c7e4f3686e
kochanov <kochanov@broadcom.com>
2012-03-07 19:08:25 -0800

    DSP: DSP CSL checkpatch cleanup
    [Defect]
    MobC000201230
    
    [Problem]
    There is a lot of checkpatch errors and warning in DSP CSL.
    
    [Solution]
    Clean up the code.
    
    [Reviewers]
    
    [Testing]
        [Test Cases]
    2G voice call.
    
        [Label/CL# Synced To]
    Head of sdb-rhea-android-ics 03/07
    
        [Platform]
    RheaRhay-edn2x
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: I4162a92fb62897a1c89710df0ee73b4ee10c52f6

--- sound/caph/dsp/csl_log.h
+++ sound/caph/dsp/csl_log.h
@@ -1,12 +1,30 @@
-//*********************************************************************
-//
-// (c)1999-2011 Broadcom Corporation
-//
-// Unless you and Broadcom execute a separate written software license agreement governing use of this software,
-// this software is licensed to you under the terms of the GNU General Public License version 2,
-// available at http://www.broadcom.com/licenses/GPLv2.php (the "GPL").
-//
-//*********************************************************************
+/******************************************************************************
+*
+* Copyright 2009 - 2012  Broadcom Corporation
+*
+*  Unless you and Broadcom execute a separate written software license
+*  agreement governing use of this software, this software is licensed to you
+*  under the terms of the GNU General Public License version 2 (the GPL),
+*  available at
+*
+*      http://www.broadcom.com/licenses/GPLv2.php
+*
+*  with the following added to such license:
+*
+*  As a special exception, the copyright holders of this software give you
+*  permission to link this software with independent modules, and to copy and
+*  distribute the resulting executable under terms of your choice, provided
+*  that you also meet, for each linked independent module, the terms and
+*  conditions of the license of that module.
+*  An independent module is a module which is not derived from this software.
+*  The special exception does not apply to any modifications of the software.
+*
+*  Notwithstanding the above, under no circumstances may you combine this
+*  software in any way with any other Broadcom software provided under a
+*  license other than the GPL, without Broadcom's express prior written
+*  consent.
+*
+******************************************************************************/
 /**
 *
 *   @file   csl_log.h
@@ -17,8 +35,11 @@
 #ifndef _CSL_LOG_H_
 #define _CSL_LOG_H_
 
-// ---- Include Files -------------------------------------------------------
+/* ---- Include Files ----------------------------------------------------- */
 #include "mobcom_types.h"
+#include "shared.h"
+
+extern AP_SharedMem_t	*vp_shared_mem;
 
 /**
  * @addtogroup CSL audio logging interface
@@ -30,45 +51,43 @@
 #define	LOG_WB_SIZE				642
 
 
-// ---- Function Declarations -----------------------------------------
-//*********************************************************************
+/* ---- Function Declarations ----------------------------------------- */
+/*********************************************************************/
 /**
 *
 *   CSL_LOG_Start starts audio logging for specified stream.
-* 
+*
 *   @param    streamNumber	(in)	stream number 1:4
 *   @param    controlInfo	(in)	control information
-*   @return   Result_t			RESULT_ERROR or RESULT_OK
 *
 **********************************************************************/
 Result_t CSL_LOG_Start(UInt16 streamNumber, UInt16 controlInfo);
 
-//*********************************************************************
+/*********************************************************************/
 /**
 *
 *   CSL_LOG_Start stops audio logging for specified stream.
-* 
+*
 *   @param    streamNumber	(in)	stream number 1:4
-*   @param    flag		(out)	shut down information   
-*   @return   Result_t			RESULT_ERROR or RESULT_OK
 *
 **********************************************************************/
 Result_t CSL_LOG_Stop(UInt16 streamNumber, UInt8 *flag);
 
-//*********************************************************************
+/*********************************************************************/
 /**
 *
 *   CSL_LOG_Read reads data from shared memory for audio logging.
-* 
+*
 *   @param    streamNumber	(in)	stream number 1:4
-*   @param    readIndex		(in)	index of ping-pong buffer 
+*   @param    readIndex		(in)	index of ping-pong buffer
 *   @param    outBuf		(out)	output buffer
 *   @param    controlInfo	(out)	logging control information
 *   @return   UInt32				number of bytes read to the buffer
 *
 **********************************************************************/
-UInt32 CSL_LOG_Read(UInt16 streamNumber, UInt16 readIndex, UInt8 *outBuf, UInt16 *controlInfo);
+UInt32 CSL_LOG_Read(UInt16 streamNumber, UInt16 readIndex, UInt8 *outBuf,
+		    UInt16 *controlInfo);
 
 /** @} */
 
-#endif //_CSL_LOG_H_
+#endif /* _CSL_LOG_H_ */
