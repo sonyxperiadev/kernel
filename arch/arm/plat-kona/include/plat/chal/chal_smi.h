cba5758479ef2a9b613c06148bc23c9cb49039ed
Denny Yang <yangd@broadcom.com>
2012-03-08 10:16:51 -0800

    plat-kona: chal: execute Lindent to reduce checkpatch errors & warnings
    There was a decision to run Lindent for broadcom owned kernel files
    and modified open source by broadcom. It will reduce checkpatch errors
    and warnings. Targetting kernel coding style to be 0 errors and 0 warnings.
    Lindeted files may have checkpatch warning & errors. Ignore it at this time.

--- arch/arm/plat-kona/include/plat/chal/chal_smi.h
+++ arch/arm/plat-kona/include/plat/chal/chal_smi.h
@@ -25,7 +25,6 @@
 #include "chal_common.h"
 */
 
-
 #ifndef __CHAL_SMI_H__
 #define __CHAL_SMI_H__
 
@@ -37,7 +36,6 @@ extern "C" {
  * @addtogroup cHAL_Interface 
  * @{
  */
-
 
 /**
 *
@@ -54,110 +52,105 @@ extern "C" {
 #define CHAL_SMI_RD_HOLD_MAX    63
 #define CHAL_SMI_RD_SETUP_MAX   63
 
-
 /**
 *
 *  SMI CS Register - Status
 *
 *****************************************************************************/
-#define CHAL_SMI_CS_TRANSFER_DONE	       0x00000002  ///< 01   DONE               
-#define CHAL_SMI_CS_TRANSFER_ACTIVE        0x00000004  ///< 02   TR pending               
-#define CHAL_SMI_CS_SETUP_ERR              0x00002000  ///< 13   SETUP ERR           
-#define CHAL_SMI_CS_EXT_DREQ_RECEIVED      0x00008000  ///< 15   DREQ received            
-#define CHAL_SMI_CS_AXI_FIFO_ERR           0x02000000  ///< 25   AXI ERR               
-#define CHAL_SMI_CS_TX_FIFO_NEED_WR        0x04000000  ///< 26   TX needs WR            
-#define CHAL_SMI_CS_RX_FIFO_NEED_RD        0x08000000  ///< 27   RX needs RD            
-#define CHAL_SMI_CS_TX_FIFO_HAS_SPACE      0x10000000  ///< 28   TX has space            
-#define CHAL_SMI_CS_RX_FIFO_HAS_DATA       0x20000000  ///< 29   RX has data            
-#define CHAL_SMI_CS_TX_FIFO_EMPTY          0x40000000  ///< 30   TX empty            
-#define CHAL_SMI_CS_RX_FIFO_FULL           0x80000000  ///< 31   RX Full             
+#define CHAL_SMI_CS_TRANSFER_DONE	       0x00000002	///< 01   DONE
+#define CHAL_SMI_CS_TRANSFER_ACTIVE        0x00000004	///< 02   TR pending
+#define CHAL_SMI_CS_SETUP_ERR              0x00002000	///< 13   SETUP ERR
+#define CHAL_SMI_CS_EXT_DREQ_RECEIVED      0x00008000	///< 15   DREQ received
+#define CHAL_SMI_CS_AXI_FIFO_ERR           0x02000000	///< 25   AXI ERR
+#define CHAL_SMI_CS_TX_FIFO_NEED_WR        0x04000000	///< 26   TX needs WR
+#define CHAL_SMI_CS_RX_FIFO_NEED_RD        0x08000000	///< 27   RX needs RD
+#define CHAL_SMI_CS_TX_FIFO_HAS_SPACE      0x10000000	///< 28   TX has space
+#define CHAL_SMI_CS_RX_FIFO_HAS_DATA       0x20000000	///< 29   RX has data
+#define CHAL_SMI_CS_TX_FIFO_EMPTY          0x40000000	///< 30   TX empty
+#define CHAL_SMI_CS_RX_FIFO_FULL           0x80000000	///< 31   RX Full
 
 /**
 *
 *  SMI CS Register - Int Enable 
 *
 *****************************************************************************/
-#define CHAL_SMI_CS_INT_DONE               0x00000200  ///< 09   INT on DONE            
-#define CHAL_SMI_CS_INT_TX                 0x00000400  ///< 10   INT on TX            
-#define CHAL_SMI_CS_INT_RX                 0x00000800  ///< 11   INT on RX             
-#define CHAL_SMI_CS_INT_MASK               0x00000E00  ///<      INT MASK
-
+#define CHAL_SMI_CS_INT_DONE               0x00000200	///< 09   INT on DONE
+#define CHAL_SMI_CS_INT_TX                 0x00000400	///< 10   INT on TX
+#define CHAL_SMI_CS_INT_RX                 0x00000800	///< 11   INT on RX
+#define CHAL_SMI_CS_INT_MASK               0x00000E00	///<      INT MASK
 
 /**
 *
 *  SMI DCS Status ( Direct Access )
 *
 *****************************************************************************/
-#define CHAL_SMI_DIR_DONE                  0x00000004  ///< 02   DIR DONE             
-
-
+#define CHAL_SMI_DIR_DONE                  0x00000004	///< 02   DIR DONE
 
 /**
 *
 *  SMI Controller Input Color Modes
 *
 *****************************************************************************/
-typedef enum {
-    CHAL_SMI_CM_16B_RGB565  = 0,
-    CHAL_SMI_CM_32B_RGB888  = 1,
-    CHAL_SMI_CM_NONE        = 2
-} CHAL_SMI_COL_MODE_T;   
+	typedef enum {
+		CHAL_SMI_CM_16B_RGB565 = 0,
+		CHAL_SMI_CM_32B_RGB888 = 1,
+		CHAL_SMI_CM_NONE = 2
+	} CHAL_SMI_COL_MODE_T;
 
 /**
 *
 *  SMI Controller Bus Width
 *
 *****************************************************************************/
-typedef enum {
-    CHAL_SMI_BUSW_08B       = 0,
-    CHAL_SMI_BUSW_16B       = 1,
-    CHAL_SMI_BUSW_18B       = 2,
-    CHAL_SMI_BUSW_09B       = 3 
-} CHAL_SMI_BUS_WIDTH_T;   
+	typedef enum {
+		CHAL_SMI_BUSW_08B = 0,
+		CHAL_SMI_BUSW_16B = 1,
+		CHAL_SMI_BUSW_18B = 2,
+		CHAL_SMI_BUSW_09B = 3
+	} CHAL_SMI_BUS_WIDTH_T;
 
 /**
 *
 *  SMI Controller Mode
 *
 *****************************************************************************/
-typedef struct {
-    cBool               m68;
-    cBool               swap;
-    cBool               setupFirstTrasferOnly;  ///< Setup time for first transfer only
-    CHAL_SMI_COL_MODE_T inPixelMode;
-} CHAL_SMI_MODE_T, *pCHAL_SMI_MODE;
+	typedef struct {
+		cBool m68;
+		cBool swap;
+		cBool setupFirstTrasferOnly;	///< Setup time for first transfer only
+		CHAL_SMI_COL_MODE_T inPixelMode;
+	} CHAL_SMI_MODE_T, *pCHAL_SMI_MODE;
 
 /**
 *
 *  SMI Controller RD/WR Timing
 *
 *****************************************************************************/
-typedef struct {
-    CHAL_SMI_BUS_WIDTH_T busWidth;  ///< SMI bus width
-    cBool                paceAll;   ///< PACE TIME applies to all transfers
-    cUInt32              setup;     ///< setup
-    cUInt32              hold;      ///< hold
-    cUInt32              pace;      ///< pace
-    cUInt32              strobe;    ///< strobe
-} CHAL_SMI_TIMING_T, *pCHAL_SMI_TIMING;
+	typedef struct {
+		CHAL_SMI_BUS_WIDTH_T busWidth;	///< SMI bus width
+		cBool paceAll;	///< PACE TIME applies to all transfers
+		cUInt32 setup;	///< setup
+		cUInt32 hold;	///< hold
+		cUInt32 pace;	///< pace
+		cUInt32 strobe;	///< strobe
+	} CHAL_SMI_TIMING_T, *pCHAL_SMI_TIMING;
 
 /**
 *
 *  SMI FIFO Access Mode
 *
 *****************************************************************************/
-typedef struct
-{
-    cUInt32     bank;               ///< SMI bank
-    cUInt32     addr;               ///< SMI Address to drive
-    cUInt32     smilL;              ///< SMI Transfer Length
-    cUInt32     isPixelFormat;      ///< Direct | Pixel   mode
-    cUInt32     isDma;              ///< DMA    | Non-DMA mode
-    cUInt32     isWrite;            ///< Write  | Read
-    cUInt32     isTE;               ///< TE synced
+	typedef struct {
+		cUInt32 bank;	///< SMI bank
+		cUInt32 addr;	///< SMI Address to drive
+		cUInt32 smilL;	///< SMI Transfer Length
+		cUInt32 isPixelFormat;	///< Direct | Pixel   mode
+		cUInt32 isDma;	///< DMA    | Non-DMA mode
+		cUInt32 isWrite;	///< Write  | Read
+		cUInt32 isTE;	///< TE synced
 //  cUInt32     isPvMode;
 //  cUInt32     padCount;
-} CHAL_FIFO_ACC_t, *pCHAL_FIFO_ACC;
+	} CHAL_FIFO_ACC_t, *pCHAL_FIFO_ACC;
 
 /**
 *
@@ -172,11 +165,9 @@ typedef struct
 *  @note     
 *
 *****************************************************************************/
-cUInt32 chal_smi_calc_smil_len (
-    cUInt32              bytes,        
-    CHAL_SMI_COL_MODE_T  in_col_mode,
-    CHAL_SMI_BUS_WIDTH_T bus_width 
-    );
+	cUInt32 chal_smi_calc_smil_len(cUInt32 bytes,
+				       CHAL_SMI_COL_MODE_T in_col_mode,
+				       CHAL_SMI_BUS_WIDTH_T bus_width);
 
 /**
 *
@@ -189,7 +180,7 @@ cUInt32 chal_smi_calc_smil_len (
 *  @note     
 *
 *****************************************************************************/
-CHAL_HANDLE chal_smi_init ( cUInt32 baseAddr );
+	CHAL_HANDLE chal_smi_init(cUInt32 baseAddr);
 
 /**
 *
@@ -204,11 +195,8 @@ CHAL_HANDLE chal_smi_init ( cUInt32 baseAddr );
 *  @note     
 *
 *****************************************************************************/
-void chal_smi_setup_mode ( 
-    CHAL_HANDLE         handle, 
-    cUInt32             bank, 
-    pCHAL_SMI_MODE      mode 
-    );
+	void chal_smi_setup_mode(CHAL_HANDLE handle,
+				 cUInt32 bank, pCHAL_SMI_MODE mode);
 
 /**
 *
@@ -223,11 +211,8 @@ void chal_smi_setup_mode (
 *  @note     
 *
 *****************************************************************************/
-void chal_smi_setup_rd_timing ( 
-    CHAL_HANDLE         handle, 
-    cUInt32             bank, 
-    pCHAL_SMI_TIMING    timing 
-    );
+	void chal_smi_setup_rd_timing(CHAL_HANDLE handle,
+				      cUInt32 bank, pCHAL_SMI_TIMING timing);
 
 /**
 *
@@ -243,11 +228,8 @@ void chal_smi_setup_rd_timing (
 *            Use RDB mask(s) for SMI_CS register's INT defs.
 *
 *****************************************************************************/
-void chal_smi_setup_int ( 
-    CHAL_HANDLE  handle, 
-    cUInt32      intMask, 
-    cUInt32      intVal 
-    );
+	void chal_smi_setup_int(CHAL_HANDLE handle,
+				cUInt32 intMask, cUInt32 intVal);
 
 /**
 *
@@ -262,12 +244,8 @@ void chal_smi_setup_int (
 *  @note     
 *
 *****************************************************************************/
-void chal_smi_setup_wr_timing ( 
-    CHAL_HANDLE         handle, 
-    cUInt32             bank, 
-    pCHAL_SMI_TIMING    timing 
-    );
-
+	void chal_smi_setup_wr_timing(CHAL_HANDLE handle,
+				      cUInt32 bank, pCHAL_SMI_TIMING timing);
 
 /**
 *
@@ -283,12 +261,8 @@ void chal_smi_setup_wr_timing (
 *  @note     
 *
 *****************************************************************************/
-void chal_smi_dir_write_acc ( 
-    CHAL_HANDLE handle,
-    cUInt8      bank, 
-    cUInt8      addr, 
-    cUInt32     data 
-    ); 
+	void chal_smi_dir_write_acc(CHAL_HANDLE handle,
+				    cUInt8 bank, cUInt8 addr, cUInt32 data);
 
 /**
 *
@@ -303,7 +277,8 @@ void chal_smi_dir_write_acc (
 *  @note     
 *
 *****************************************************************************/
-void chal_smi_dir_read_acc ( CHAL_HANDLE handle, cUInt8 bank, cUInt8 addr ); 
+	void chal_smi_dir_read_acc(CHAL_HANDLE handle, cUInt8 bank,
+				   cUInt8 addr);
 
 /**
 *
@@ -317,8 +292,7 @@ void chal_smi_dir_read_acc ( CHAL_HANDLE handle, cUInt8 bank, cUInt8 addr );
 *  @note     
 *
 *****************************************************************************/
-cUInt32 chal_smi_dir_read_data ( CHAL_HANDLE handle );
-
+	cUInt32 chal_smi_dir_read_data(CHAL_HANDLE handle);
 
 /**
 *
@@ -331,7 +305,7 @@ cUInt32 chal_smi_dir_read_data ( CHAL_HANDLE handle );
 *  @note     Clears FIFO, DMA/TE/INT, DONE Flag. Disable PROG Mode. 
 *
 *****************************************************************************/
-void chal_smi_fifo_done ( CHAL_HANDLE handle );
+	void chal_smi_fifo_done(CHAL_HANDLE handle);
 
 /**
 *
@@ -345,7 +319,7 @@ void chal_smi_fifo_done ( CHAL_HANDLE handle );
 *  @note     
 *
 *****************************************************************************/
-void chal_smi_fifo_acc ( CHAL_HANDLE handle, pCHAL_FIFO_ACC fifoMode );   
+	void chal_smi_fifo_acc(CHAL_HANDLE handle, pCHAL_FIFO_ACC fifoMode);
 
 /**
 *
@@ -359,7 +333,7 @@ void chal_smi_fifo_acc ( CHAL_HANDLE handle, pCHAL_FIFO_ACC fifoMode );
 *  @note     
 *
 *****************************************************************************/
-void chal_smi_fifo_wr ( CHAL_HANDLE handle, cUInt32 data );
+	void chal_smi_fifo_wr(CHAL_HANDLE handle, cUInt32 data);
 
 /**
 *
@@ -372,7 +346,7 @@ void chal_smi_fifo_wr ( CHAL_HANDLE handle, cUInt32 data );
 *  @note     
 *
 *****************************************************************************/
-cUInt32 chal_smi_fifo_rd ( CHAL_HANDLE handle );
+	cUInt32 chal_smi_fifo_rd(CHAL_HANDLE handle);
 
 /**
 *
@@ -385,8 +359,7 @@ cUInt32 chal_smi_fifo_rd ( CHAL_HANDLE handle );
 *  @note     
 *
 *****************************************************************************/
-cUInt32 chal_smi_get_dma_addr ( CHAL_HANDLE handle );
-
+	cUInt32 chal_smi_get_dma_addr(CHAL_HANDLE handle);
 
 /**
 *
@@ -399,7 +372,7 @@ cUInt32 chal_smi_get_dma_addr ( CHAL_HANDLE handle );
 *  @note     
 *
 *****************************************************************************/
-void chal_smi_set_dma ( CHAL_HANDLE handle, cBool enable );
+	void chal_smi_set_dma(CHAL_HANDLE handle, cBool enable);
 
 /**
 *
@@ -412,8 +385,8 @@ void chal_smi_set_dma ( CHAL_HANDLE handle, cBool enable );
 *
 *  @note     Common to Z80/M68 & DBI modes
 *
-*****************************************************************************/                           
-cVoid  chal_smi_set_te_enable ( CHAL_HANDLE handle, cBool enable );
+*****************************************************************************/
+	cVoid chal_smi_set_te_enable(CHAL_HANDLE handle, cBool enable);
 
 /**
 *
@@ -425,9 +398,8 @@ cVoid  chal_smi_set_te_enable ( CHAL_HANDLE handle, cBool enable );
 *
 *  @note     
 *
-*****************************************************************************/                           
-cUInt32 chal_smi_rd_status_fifo ( CHAL_HANDLE handle );
-
+*****************************************************************************/
+	cUInt32 chal_smi_rd_status_fifo(CHAL_HANDLE handle);
 
 /**
 *
@@ -439,15 +411,12 @@ cUInt32 chal_smi_rd_status_fifo ( CHAL_HANDLE handle );
 *
 *  @note     
 *
-*****************************************************************************/                           
-cUInt32 chal_smi_rd_status_dir ( CHAL_HANDLE handle );
+*****************************************************************************/
+	cUInt32 chal_smi_rd_status_dir(CHAL_HANDLE handle);
 
 /** @} */
 
 #ifdef __cplusplus
 }
 #endif
-
-#endif // __CHAL_SMI_H__
-
-
+#endif				// __CHAL_SMI_H__
