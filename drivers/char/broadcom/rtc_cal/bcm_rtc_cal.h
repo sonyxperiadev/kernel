15ebb29d6d6b10cef4497e1f0ae094f3f88ebd06
kyoungoh <kyoungoh@broadcom.com>
2012-03-08 09:46:55 -0800

    RTC_CAL : RTC cal code clean up/configuration update
    [Defect]
        MobC00201540: RTC cal code clean up/configuration update
    
    [Problem]
        RTC cal checkpatch issues, update for build configuration
    
    [Solution]
        Fix/clean up for checkpatch, modified build configuration
    
    [Reviewers]
        Chris Muller, Shameer Puthalan
    
    [Testing]
        build/sanity
    
    [Label]
        Sync to head
    
    Change-Id: I61fb56e2318b41b87bdcfaa9e9bac2fc02754316
    Signed-off-by: kyoungoh <kyoungoh@broadcom.com>

--- drivers/char/broadcom/rtc_cal/bcm_rtc_cal.h
+++ drivers/char/broadcom/rtc_cal/bcm_rtc_cal.h
@@ -1,7 +1,7 @@
 /*******************************************************************************
 * Copyright 2012 Broadcom Corporation.  All rights reserved.
 *
-* 	@file	 rtcbcm_rtc_cal.h
+* @file rtcbcm_rtc_cal.h
 *
 * Unless you and Broadcom execute a separate written software license agreement
 * governing use of this software, this software is licensed to you under the
@@ -17,61 +17,61 @@
 #define _BCM_RTC_CAL_H_
 
 
-#define RTC_CAL_QOS_DEFAULT_VALUE 	0xFFFFFFFF
-#define RTC_CAL_DFS_MIN_VALUE 	0
-#define RTC_OPP_UNSUPPORTED		0xFFFF
+#define RTC_CAL_QOS_DEFAULT_VALUE  0xFFFFFFFF
+#define RTC_CAL_DFS_MIN_VALUE  0
+#define RTC_OPP_UNSUPPORTED  0xFFFF
 
-#define RTC_CAL_ACTIVE_STATE_INX		0
-
-/**
-Real Time Clock Time structure
-**/
-typedef struct{
-	UInt8  Sec;		///< 0-59 seconds
-	UInt8  Min;		///< 0-59 minutes
-	UInt8  Hour;	///< 0-23 hours
-	UInt8  Week;	///< 0-6==sun-sat week
-	UInt8  Day;		///< 1-31 day
-	UInt8  Month;	///< 1-12 Month
-	UInt16 Year;	///< (RTC_YEARBASE) - (RTC_YEARBASE + 99)
-} RTCTime_t;
+#define RTC_CAL_ACTIVE_STATE_INX  0
 
 /**
 Real Time Clock Time structure
 **/
+struct RTCTime_t {
+	UInt8  Sec;
+	UInt8  Min;
+	UInt8  Hour;
+	UInt8  Week;
+	UInt8  Day;
+	UInt8  Month;
+	UInt16 Year;
+};
 
-typedef struct{
-	u8  sec;		///< 0-59 seconds
-	u8  min;		///< 0-59 minutes
-	u8  hour;	///< 0-23 hours
-	u8  week;	///< 0-6==sun-sat week
-	u8  mday;		///< 1-31 day
-	u8  mon;	///< 1-12 Month
-	u16 year;	///< (RTC_YEARBASE) - (RTC_YEARBASE + 99)
-} rtctime_t;
+/**
+Real Time Clock Time structure
+**/
+
+struct rtctime_t {
+	u8  sec;
+	u8  min;
+	u8  hour;
+	u8  week;
+	u8  mday;
+	u8  mon;
+	u16 year;
+};
 
 /**
 RTC Time Calibration structure
 **/
-typedef struct{
+struct rtc_cal_TC_t {
 
 	unsigned long time_next;
 
 	u64 slippage_acc;
 
-	unsigned long time_hw_time; 
-	unsigned long time_hz_acc; 
+	unsigned long time_hw_time;
+	unsigned long time_hz_acc;
 	u64 time_err_acc;
-	bool flag_err_acc; 
+	bool flag_err_acc;
 
-	unsigned long time_hz_curr; 
-	unsigned long time_hz_prev; 
-	unsigned long time_hz_interval; 
+	unsigned long time_hz_curr;
+	unsigned long time_hz_prev;
+	unsigned long time_hz_interval;
 	u64 time_err_curr;
-	bool flag_err_curr; 
+	bool flag_err_curr;
 
-	unsigned long time_ratio_curr; 
-	bool flag_ratio_curr; 
+	unsigned long time_ratio_curr;
+	bool flag_ratio_curr;
 	unsigned long time_hz_delt;
 	unsigned long slippage_ms;
 	bool slippage_flag;
@@ -81,10 +81,9 @@ typedef struct{
 	struct rtc_time tm_read;
 	struct rtc_time tm_write;
 
-} rtc_cal_TC_t;
+};
 
-struct rtccal_pdata 
-{
+struct rtccal_pdata {
 	const char *device;
 	unsigned long		nom_ratio;
 	unsigned long		min_ratio;
@@ -98,15 +97,12 @@ struct rtccal_pdata
 	unsigned long		method;
 };
 
-typedef unsigned char timer_id_type;
-
-typedef struct{
+struct rtc_cal_info_t {
 	unsigned long ratio;
 	unsigned int temp;
-} rtc_cal_info_t;
+};
 
-typedef enum
-{
+enum rtc_cal_msg_id {
 	RTCCAL_ID_SETTIME,
 	RTCCAL_ID_CALEVENT,
 	RTCCAL_ID_CONFIG,
@@ -116,34 +112,31 @@ typedef enum
 	RTCCAL_ID_TIMECORR,
 	RTCCAL_ID_GETTIME,
 	RTCCAL_ID_MAX
-} rtc_cal_msg_id_t;
+};
 
-typedef enum
-{
-	RTCCAL_PARM_RATIO_NORM = 0, // clock ratio
-	RTCCAL_PARM_RATIO_MIN = 1, // clock ratio
-	RTCCAL_PARM_RATIO_MAX = 2, // clock ratio
-	RTCCAL_PARM_RANGE = 3, // in ppm
-	RTCCAL_PARM_THRESHOLD = 4, // in ppb
-	RTCCAL_PARM_TIME_MEASURE = 5, // in milisecond
-	RTCCAL_PARM_TIME_CALC = 6, // in milisecond
-	RTCCAL_PARM_SLIPPAGE = 7,	// in milisecond
-	RTCCAL_PARM_PERIODICUPDATE = 8,  // periodic update in mimnute
-	RTCCAL_PARM_MAX		// reserved for all other indexes
-} RTCCAL_Parm_t;
+enum RTCCAL_Parm {
+	RTCCAL_PARM_RATIO_NORM = 0,
+	RTCCAL_PARM_RATIO_MIN = 1,
+	RTCCAL_PARM_RATIO_MAX = 2,
+	RTCCAL_PARM_RANGE = 3,
+	RTCCAL_PARM_THRESHOLD = 4,
+	RTCCAL_PARM_TIME_MEASURE = 5,
+	RTCCAL_PARM_TIME_CALC = 6,
+	RTCCAL_PARM_SLIPPAGE = 7,
+	RTCCAL_PARM_PERIODICUPDATE = 8,
+	RTCCAL_PARM_MAX
+};
 
-typedef struct
-{
-	rtc_cal_msg_id_t id;
-	rtctime_t time;
+struct rtc_cal_msg_t {
+	unsigned char id;
+	struct rtctime_t time;
 	unsigned long val1;
 	unsigned long val2;
 	unsigned long val3;
 	unsigned long val4;
-} rtc_cal_msg_t;
+};
 
-enum
-{
+enum {
 	RTC_CAL_ID_MM,
 	RTC_CAL_ID_HUB_SWITCHABLE,
 	RTC_CAL_ID_HUB_AON,
@@ -153,22 +146,23 @@ enum
 	RTC_CAL_ID_MAX
 };
 
+extern uint32_t __div64_32(uint64_t *dividend, uint32_t divisor);
+
+void bcm_rtc_cal_ratio(unsigned long val1, unsigned long val2);
 
 
-//***************************************************************************************
 /**
 	This function is used to get RTCTime from PMU for startup
-	
+
 	@param		void
 	@note
 
 **/
 void RTCCal_GetRTCTime(void);
 
-//***************************************************************************************
 /**
 	This function is used to init task queue
-	
+
 	@note
 	@return		Boolean
 
@@ -178,10 +172,9 @@ bool RTCCal_Init(
 	);
 
 
-//***************************************************************************************
 /**
 	This function is used to run RTC
-	
+
 	@param		void
 	@note
 
@@ -190,7 +183,6 @@ void RTCCal_Run(
 	void
 	);
 
-//***************************************************************************************
 /**
 	Function used to update current time
 
@@ -201,12 +193,11 @@ void RTCCal_UpdateTime(
 	unsigned long	time
 	);
 
-//***************************************************************************************
 /**
 	Function used to get time from RTC CAL for upper layer usage
 
 	@param		rtc_ptr  in RTCTime_t pointer
 	@note
 **/
-void RTCCal_GetTime(rtctime_t *rtc_ptr);
-#endif // _RTC_CAL_H_
+void RTCCal_GetTime(struct rtctime_t *rtc_ptr);
+#endif /* _BCM_RTC_CAL_H_ */
