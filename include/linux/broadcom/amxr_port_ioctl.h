6cbf064a30c4e272bae4584ed37c2a1edc6424cd
Denny Yang <yangd@broadcom.com>
2012-03-13 16:36:56 -0700

    Lindent: execute Lindent to reduce checkpatch errors & warnings
    There was a decision to run Lindent for broadcom owned kernel files
    and modified lines by brcm in open source. It will reduce checkpatch
    errors and warnings. Targetting kernel coding style to be 0 errors
    and 0 warnings. Lindeted files may have checkpatch warnings & errors.
    Ignore it at this time.
    
     include/linux/broadcom/amxr_port_ioctl.h |   21 ++++++------------
     include/linux/broadcom/atc_kernel.h      |   34 +++++++++++------------------
     2 files changed, 20 insertions(+), 35 deletions(-)

--- include/linux/broadcom/amxr_port_ioctl.h
+++ include/linux/broadcom/amxr_port_ioctl.h
@@ -32,8 +32,7 @@
 #define AMXR_PORT_MAGIC			'N'
 
 /* IOCTL commands */
-enum amxr_port_cmd_e
-{
+enum amxr_port_cmd_e {
 	AMXR_PORT_CMD_CREATE_PORT = 0x30,	/* Start at 0x30 arbitrarily */
 	AMXR_PORT_CMD_SET_PORT_DST_FREQ,
 	AMXR_PORT_CMD_SET_PORT_SRC_FREQ,
@@ -41,12 +40,11 @@ enum amxr_port_cmd_e
 	AMXR_PORT_CMD_SET_PORT_SRC_CHANS,
 	AMXR_PORT_CMD_GET_MSG,
 	AMXR_PORT_CMD_RELEASE,
-	AMXR_PORT_CMD_LAST			/* Do no delete */
+	AMXR_PORT_CMD_LAST	/* Do no delete */
 };
 
 /* IOCTL Data structures */
-struct amxr_port_ioctl_createport
-{
+struct amxr_port_ioctl_createport {
 	char name[32];		/**< (i) Name string */
 	AMXR_PORT_CB cb;	/**< (i) Callbacks */
 	int dst_hz;		/**< (i) Current destination sampling frequency in Hz */
@@ -57,23 +55,19 @@ struct amxr_port_ioctl_createport
 	int src_bytes;		/**< (i) Source period size in bytes */
 };
 
-
-struct amxr_port_ioctl_setportfreq
-{
+struct amxr_port_ioctl_setportfreq {
 	int hz;			/**< (i) Sampling frequency in Hz */
 	int bytes;		/**< (i) Period size in bytes */
 };
 
-struct amxr_port_ioctl_setportchans
-{
+struct amxr_port_ioctl_setportchans {
 	int chans;		/**< (i) Number of channels: 1 for mono, 2 for stereo, etc */
 	int bytes;		/**< (i) Period size in bytes */
 };
 
 /* Message definitions */
 
-enum amxr_port_msgid
-{
+enum amxr_port_msgid {
 	AMXR_PORT_SRCDONE_MSG = 100,
 	AMXR_PORT_DSTDONE_MSG,
 	AMXR_PORT_DSTCNXSREMOVED_MSG,
@@ -83,8 +77,7 @@ enum amxr_port_msgid
  * from kernel to user ports. This structure should be as
  * small as possible to limit the amount of memory copies.
  */
-struct amxr_port_msg
-{
+struct amxr_port_msg {
 	int msgid;		/* Message ID */
 	long buf_pgoff;		/* Buffer page offset */
 	int bytes;		/* Size of buffer in bytes */
