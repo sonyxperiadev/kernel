949de4863d66f9db720ac1474d0f681bdc73033d
Tim Kryger <tkryger@broadcom.com>
2012-03-14 11:10:19 -0700

    Clean up Kona Secure Monitor Call functions
    Change-Id: I3406abad318e8cb6cfb989d7163a65305820d516
    Signed-off-by: Tim Kryger <tkryger@broadcom.com>

--- arch/arm/mach-hawaii/include/mach/sec_api.h
+++ arch/arm/mach-hawaii/include/mach/sec_api.h
@@ -1,18 +1,16 @@
 #ifndef SEC_API_H
 #define SEC_API_H
 
-
-#define FLAGS     (SEC_ROM_ICACHE_ENABLE_MASK | SEC_ROM_DCACHE_ENABLE_MASK | \
-                   SEC_ROM_IRQ_ENABLE_MASK | SEC_ROM_FIQ_ENABLE_MASK)
+#define FLAGS	(SEC_ROM_ICACHE_ENABLE_MASK | SEC_ROM_DCACHE_ENABLE_MASK | \
+			SEC_ROM_IRQ_ENABLE_MASK | SEC_ROM_FIQ_ENABLE_MASK)
 
 /*!
  * Definitions for IRQ & FIQ Mask for ARM
  */
 
-#define FIQ_IRQ_MASK                            		  0xC0
-#define FIQ_MASK                                		  0x40
-#define IRQ_MASK                                		  0x80
-
+#define FIQ_IRQ_MASK						0xC0
+#define FIQ_MASK						0x40
+#define IRQ_MASK						0x80
 
 /*!
  * Secure Mode FLAGs
@@ -33,7 +31,6 @@
 /* When When set, enables Unified L2 cache within the secure mode */
 #define SEC_ROM_UL2_CACHE_ENABLE_MASK                     0x00000010
 
-
 /* Following are BRCM ROM Secure Service API */
 #define SSAPI_DORMANT_ENTRY_SERV                          0x01000000
 #define SSAPI_PUBLIC_OTP_SERV                             0x01000001
@@ -41,7 +38,6 @@
 #define SSAPI_DISABLE_L2_CACHE                            0x01000003
 #define SSAPI_WRITE_SCU_STATUS                            0x01000004
 #define SSAPI_WRITE_PWR_GATE                              0x01000005
-
 
 extern unsigned secure_api_call(unsigned appl_id, unsigned flags, ...);
 #endif /* SEC_API_H */
