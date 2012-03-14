cc22133ee096e3b8e9d251d57abf52c8d28c0fce
Denny Yang <yangd@broadcom.com>
2012-03-14 06:10:02 -0700

    Revert "checkpatch: executed Lindent to reduce checkpatch errors & warnings"
    This reverts commit 5177d7957e30b4916d929646b63c05e2b89a7551.

--- arch/arm/include/asm/idmap.h
+++ arch/arm/include/asm/idmap.h
@@ -4,12 +4,12 @@
 #include <asm/page.h>
 #include <asm/sections.h>
 
-void identity_mapping_add(pgd_t * pgd, unsigned long addr, unsigned long end);
-void identity_mapping_del(pgd_t * pgd, unsigned long addr, unsigned long end);
+void identity_mapping_add(pgd_t *pgd, unsigned long addr, unsigned long end);
+void identity_mapping_del(pgd_t *pgd, unsigned long addr, unsigned long end);
 
 /* Page reserved below swapper. */
 #define RESERVE_STACK_PAGE	(unsigned long)swapper_pg_dir
 
-void setup_mm_for_reboot(char mode, pgd_t * pgd);
+void setup_mm_for_reboot(char mode, pgd_t *pgd);
 
-#endif /* _ARM_IDMAP_H */
+#endif	/* _ARM_IDMAP_H */
