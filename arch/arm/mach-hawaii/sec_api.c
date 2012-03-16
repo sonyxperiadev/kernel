e7ef07133cec417dd76fc9617039a36c8850c24a
Tim Kryger <tkryger@broadcom.com>
2012-03-16 11:05:38 -0700

    Force all Secure Monitor Calls to run on Core 0
    Due to a limitation of the secure monitor, we must use the SMP
    infrastructure to forward all secure monitor calls to Core 0.
    
    Change-Id: If1dfd02941b692a6f5b7ec2bc670050daec43217
    Signed-off-by: Tim Kryger <tkryger@broadcom.com>

--- arch/arm/mach-hawaii/sec_api.c
+++ arch/arm/mach-hawaii/sec_api.c
@@ -1,35 +1,56 @@
 #include <stdarg.h>
 #include <mach/sec_api.h>
+#include <linux/smp.h>
 
 extern unsigned hw_sec_rom_pub_bridge(unsigned appl_id, unsigned flags,
 				      va_list);
 
-static unsigned read_cpsr()
+struct sec_api_data {
+	va_list argp;
+	unsigned return_value;
+	unsigned appl_id;
+	unsigned flags;
+};
+
+static unsigned read_cpsr(void)
 {
 	unsigned cpsr;
 	asm volatile ("mrs  %0,cpsr" : "=r" (cpsr));
 	return cpsr;
 }
 
-unsigned secure_api_call(unsigned appl_id, unsigned flags, ...)
+/* This function exclusively runs on Core 0 with preemption disabled */
+static void secure_api_call_shim(void *info)
 {
-	va_list ap;
-	unsigned return_value;
-	unsigned pub_cpsr;
 
-	pub_cpsr = read_cpsr();
+	struct sec_api_data *data = (struct sec_api_data *)info;
+	unsigned pub_cpsr = read_cpsr();
+
+	printk(KERN_NOTICE "About to trap into Secure Monitor on CPU %d\n",
+	       raw_smp_processor_id());
 
 	/* FIQ won't be enabled in secure mode if FIQ is currently disabled */
 	if (pub_cpsr & FIQ_MASK)
-		flags &= ~SEC_ROM_FIQ_ENABLE_MASK;
+		data->flags &= ~SEC_ROM_FIQ_ENABLE_MASK;
 
 	/* IRQ won't be enabled in secure mode if IRQ is currently disabled */
 	if (pub_cpsr & IRQ_MASK)
-		flags &= ~SEC_ROM_IRQ_ENABLE_MASK;
+		data->flags &= ~SEC_ROM_IRQ_ENABLE_MASK;
 
-	va_start(ap, flags);
-	return_value = hw_sec_rom_pub_bridge(appl_id, flags, ap);
-	va_end(ap);
+	data->return_value =
+	    hw_sec_rom_pub_bridge(data->appl_id, data->flags, data->argp);
+}
 
-	return return_value;
+unsigned secure_api_call(unsigned appl_id, unsigned flags, ...)
+{
+	struct sec_api_data data;
+
+	data.appl_id = appl_id;
+	data.flags = flags;
+
+	va_start(data.argp, flags);
+	smp_call_function_single(0, secure_api_call_shim, (void *)&data, 1);
+	va_end(data.argp);
+
+	return data.return_value;
 }
