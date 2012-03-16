ffe59ed93f6748650c302021f1dc36a71bbeea43
Stephen Boyd <sboyd@codeaurora.org>
2012-03-16 00:14:42 -0700

    msm: pil: Synchronize request_firmware() with suspend
    Calling request_firmware() during suspend/resume operations is
    buggy and error prone. Therefore a check exists in
    request_firmware() to see if a suspend/resume operation is in
    progress. In testing we've seen that a wakeup interrupt can come
    in during suspend and trigger subsystem restart to call
    pil_force_boot() before userspace has fully thawed. Since this is
    not allowed the request_firmware() call from pil_force_boot()
    fails with -EBUSY and the processor is never rebooted.
    
    Register for PM notifiers to avoid calling request_firmware()
    when suspend/resume is in progress. Within the notifier
    synchronize any request firmware calls with suspend using a
    rwsem. In the case where pil is called before suspend, the
    suspend notifier should block on the down_write() during
    PM_SUSPEND_PREPARE. When suspend is initiated before pil any pil
    operations should block on the down_read() until suspend
    finishes. Doing this should fix races between suspend/resume and
    request_firmware() within PIL.
    
    Change-Id: Id12d128a3729680b36845de7228bf3919c32a672
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- arch/arm/mach-msm/peripheral-loader.c
+++ arch/arm/mach-msm/peripheral-loader.c
@@ -20,6 +20,8 @@
 #include <linux/mutex.h>
 #include <linux/memblock.h>
 #include <linux/slab.h>
+#include <linux/suspend.h>
+#include <linux/rwsem.h>
 
 #include <mach/socinfo.h>
 
@@ -156,6 +158,9 @@ static int segment_is_loadable(const struct elf32_phdr *p)
 	return (p->p_type & PT_LOAD) && !segment_is_hash(p->p_flags);
 }
 
+/* Sychronize request_firmware() with suspend */
+static DECLARE_RWSEM(pil_pm_rwsem);
+
 static int load_image(struct pil_device *pil)
 {
 	int i, ret;
@@ -164,6 +169,7 @@ static int load_image(struct pil_device *pil)
 	const struct elf32_phdr *phdr;
 	const struct firmware *fw;
 
+	down_read(&pil_pm_rwsem);
 	snprintf(fw_name, sizeof(fw_name), "%s.mdt", pil->desc->name);
 	ret = request_firmware(&fw, fw_name, pil->desc->dev);
 	if (ret) {
@@ -225,6 +231,7 @@ static int load_image(struct pil_device *pil)
 release_fw:
 	release_firmware(fw);
 out:
+	up_read(&pil_pm_rwsem);
 	return ret;
 }
 
@@ -404,7 +411,6 @@ static int msm_pil_debugfs_init(void)
 
 	return 0;
 }
-arch_initcall(msm_pil_debugfs_init);
 
 static int msm_pil_debugfs_add(struct pil_device *pil)
 {
@@ -417,6 +423,7 @@ static int msm_pil_debugfs_add(struct pil_device *pil)
 	return 0;
 }
 #else
+static int msm_pil_debugfs_init(void) { return 0; }
 static int msm_pil_debugfs_add(struct pil_device *pil) { return 0; }
 #endif
 
@@ -450,6 +457,33 @@ int msm_pil_register(struct pil_desc *desc)
 	return msm_pil_debugfs_add(pil);
 }
 EXPORT_SYMBOL(msm_pil_register);
+
+static int pil_pm_notify(struct notifier_block *b, unsigned long event, void *p)
+{
+	switch (event) {
+	case PM_SUSPEND_PREPARE:
+		down_write(&pil_pm_rwsem);
+		break;
+	case PM_POST_SUSPEND:
+		up_write(&pil_pm_rwsem);
+		break;
+	}
+	return NOTIFY_DONE;
+}
+
+static struct notifier_block pil_pm_notifier = {
+	.notifier_call = pil_pm_notify,
+};
+
+static int __init msm_pil_init(void)
+{
+	int ret = msm_pil_debugfs_init();
+	if (ret)
+		return ret;
+	register_pm_notifier(&pil_pm_notifier);
+	return ret;
+}
+arch_initcall(msm_pil_init);
 
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("Load peripheral images and bring peripherals out of reset");
