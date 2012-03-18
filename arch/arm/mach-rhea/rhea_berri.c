71fb19019efd78911e0f082a94f34cad054b47b7
shriram choudhary <shriramc@broadcom.com>
2012-03-17 20:38:50 -0700

    Fixed the WLAN  initialization Bug
    Change-Id: Icfa7a4c9f606537e05ac66cffe38bc64dc84ae2a

--- arch/arm/mach-rhea/rhea_berri.c
+++ arch/arm/mach-rhea/rhea_berri.c
@@ -1641,6 +1641,13 @@ static void __init rhea_berri_add_i2c_devices(void)
 
 static int __init rhea_berri_add_lateInit_devices(void)
 {
+#ifdef CONFIG_BRCM_UNIFIED_DHD_SUPPORT
+		
+			printk(KERN_ERR "Calling WLAN_INIT!\n");
+		
+			 rhea_wlan_init();
+				printk(KERN_ERR "DONE WLAN_INIT!\n");
+#endif
 	board_add_sdio_devices();
 	return 0;
 }
