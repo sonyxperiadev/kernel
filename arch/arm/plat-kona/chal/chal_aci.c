0fee7d6a8b2f3d830bdb9fd8b5e3579050160b1d
Shankar B <bshankar@broadcom.com>
2012-03-19 06:19:00 -0700

    drivers/input/misc/kona_headset.c [Defect] MobC00204151
    [Problem]
    Switching OFF the MIC BIAS of Accessory Detection was turning OFF the MIC BIAS
    of the on board MIC as well
    
    [Solution]
    Permanant solution needs to be discussed with ASIC team.
    As a temporary fix I'm not switching OFF the MIC BIAS at all to enable the
    release. Ideally we need to switch of MIC BIAS when not in use. But now that
    its coupled with On Board MIC BIAS as well, need to discuss with Audio/ASIC
    team for a proper solution.
    
    THIS COMMIT IS PURELY TEMPORARY AND WILL CAUSE LEAKAGE ON ARXAVDD2P9
    nullifying the fix for MobC00190435 8aa5fa4bdc08a7642d193d02b1324ff291cd0c1e
    
    Since there was another commit 0c86140a460c718ac49e901acedc8dfc8bd0112b done
    on top of 8aa5fa4bdc08a7642d193d02b1324ff291cd0c1e, instead of reverting I'm
    doing this commit to enable the release.
    
    [Reviewers]
    
    [Testing]
        [Test Cases]
        Audio play back using on board speaker
        Plugged in the HS while play back is ON an was able to listen to the music
        on HS
        Switched back and forth between on board speaker and HS and it works good
    
        MO call using on board MIC & speaker works OK (both UL audio and DL audio
        works good)
    
        MT call using on board MIC & speaker works OK (both UL audio and DL audio
        works good)
    
        When call in progress plugged the HS and tested that voice routing happens
        OK.
    
        [Label/CL# Synced To]
    
        [Platform]
        Rheaberri
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: Ibec69bab0bfc7d6456f903b1607d20d052144f5f

--- arch/arm/plat-kona/chal/chal_aci.c
+++ arch/arm/plat-kona/chal/chal_aci.c
@@ -618,11 +618,12 @@ static void chal_aci_block_ctrl_arg( CHAL_HANDLE handle, CHAL_ACI_block_action_t
                 BRCM_WRITE_REG_FIELD( KONA_AUXMIC_VA,    AUXMIC_AUXEN,    MICAUX_EN,              1 );
             }
 
+#if 0
 	    BRCM_WRITE_REG_FIELD( KONA_ACI_VA,  ACI_SLEEP_CTRL, WEAK_SLEEP_EN, 1);
 	    BRCM_WRITE_REG_FIELD( KONA_ACI_VA,  ACI_MIC_BIAS, MIC_AUX_BIAS_GND, 0);
 	    BRCM_WRITE_REG_FIELD( KONA_ACI_VA,    ACI_ACI_CTRL,   ACI_BIAS_PWRDN,     ACI_BIAS_PWRDN_POWERUP);        // Power Up
 	    BRCM_WRITE_REG_FIELD( KONA_ACI_VA,  ACI_ADC_CTRL, AUDIORX_BIAS_PWRUP, AUDIORX_BIAS_PWRUP_POWERUP);
-
+#endif
             break;
         case CHAL_ACI_MIC_BIAS_DISCONTINUOUS:
             // Set Bias Voltage
@@ -655,29 +656,28 @@ static void chal_aci_block_ctrl_arg( CHAL_HANDLE handle, CHAL_ACI_block_action_t
             BRCM_WRITE_REG_FIELD( KONA_ACI_VA,        ACI_MIC_BIAS,       MIC_BIAS,               ACI_MIC_BIAS_MIC_BIAS_CMD_PERIODIC_MEASUREMENT_EN);
             /* Powerup generic detection block (including bias) */
             BRCM_WRITE_REG_FIELD( KONA_AUXMIC_VA,     AUXMIC_F_PWRDWN,    FORCE_PWR_DWN,          0 );
-
+#if 0
 	    BRCM_WRITE_REG_FIELD( KONA_ACI_VA,  ACI_SLEEP_CTRL, WEAK_SLEEP_EN, 1);
 	    BRCM_WRITE_REG_FIELD( KONA_ACI_VA,  ACI_MIC_BIAS, MIC_AUX_BIAS_GND, 0);
 	    BRCM_WRITE_REG_FIELD( KONA_ACI_VA,    ACI_ACI_CTRL,   ACI_BIAS_PWRDN,     ACI_BIAS_PWRDN_POWERUP);        // Power Up
 	    BRCM_WRITE_REG_FIELD( KONA_ACI_VA,  ACI_ADC_CTRL, AUDIORX_BIAS_PWRUP, AUDIORX_BIAS_PWRUP_POWERUP);
-
+#endif
             break;
         case CHAL_ACI_MIC_BIAS_OFF:
-#if 0
             /* MIC Bias can not turned full off in this state. MIC is also the supply for other internal blocks */
             /* Powerup generic detection block (including bias) */
             BRCM_WRITE_REG_FIELD( KONA_AUXMIC_VA,     AUXMIC_F_PWRDWN,    FORCE_PWR_DWN,          0 );
             /* Configure Continuous measurement mode */
             BRCM_WRITE_REG_FIELD( KONA_AUXMIC_VA,     AUXMIC_CMC,         CONT_MSR_CTRL,          1 );
             BRCM_WRITE_REG_FIELD( KONA_ACI_VA,        ACI_MIC_BIAS,       MIC_BIAS,               ACI_MIC_BIAS_MIC_BIAS_CMD_PERIODIC_MEASUREMENT_DIS);
-#endif
+#if 0
 	    BRCM_WRITE_REG_FIELD( KONA_AUXMIC_VA, AUXMIC_F_PWRDWN, FORCE_PWR_DWN, 1 );
 	    BRCM_WRITE_REG_FIELD( KONA_AUXMIC_VA, AUXMIC_AUXEN,    MICAUX_EN,    0 );
 	    BRCM_WRITE_REG_FIELD( KONA_ACI_VA,  ACI_SLEEP_CTRL, WEAK_SLEEP_EN,   0);
 	    BRCM_WRITE_REG_FIELD( KONA_ACI_VA,  ACI_MIC_BIAS,	    MIC_AUX_BIAS_GND, 1);
 	    BRCM_WRITE_REG_FIELD( KONA_ACI_VA,  ACI_ADC_CTRL, AUDIORX_BIAS_PWRUP, AUDIORX_BIAS_PWRUP_POWERDOWN);
 	    BRCM_WRITE_REG_FIELD( KONA_ACI_VA,  ACI_ACI_CTRL, ACI_BIAS_PWRDN, ACI_BIAS_PWRDN_POWERDOWN);
-
+#endif
             break;
         case CHAL_ACI_MIC_BIAS_GND:
             /* MIC Bias can not turned full off in this state. MIC is also the supply for other internal blocks */
