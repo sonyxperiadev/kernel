bd26a28953bd2db823f83b7fe7bb457c166d4875
Jason Shim <jshim@broadcom.com>
2012-03-19 13:43:45 -0700

    build error fix caused by 727c623b69ee44eb8b86daff911d24d963ba1954
    [Defect / Enhancement / New Feature]
    MobC00204404
    
    [Problem]
    build error fix caused by 727c623b69ee44eb8b86daff911d24d963ba1954. git mergetool intruduced a build error.
    
    [Solution]
    Fix in this commit
    
    [Reviewers]
    Jack Yu
    
    [Testing]
        [Test Cases]
    
    Change-Id: Ib0ed0ac561868d445efe7e9b2239db26b3ace255

--- arch/arm/mach-rhea/common.c
+++ arch/arm/mach-rhea/common.c
@@ -110,8 +110,8 @@ EXPORT_SYMBOL(etm_on);
 	.regshift   = 2,				\
 	.iotype	    = UPIO_DWAPB,			\
 	.type	    = PORT_16550A,			\
-	.flags	    = UPF_BOOT_AUTOCONF | UPF_FIXED_TYPE | UPF_SKIP_TEST \
-					| UPF_LOW_LATENCY, \
+	.flags	    = UPF_BOOT_AUTOCONF | UPF_FIXED_TYPE | UPF_SKIP_TEST | \
+						UPF_LOW_LATENCY, \
 	.private_data = (void __iomem *)((KONA_##name##_VA) + \
 						UARTB_USR_OFFSET), \
 	.clk_name = clk,	\
