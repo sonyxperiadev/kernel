489d1ba6f9d0d59242997840259421fa5e29225f
Ravishangar Kalyanam <rkalya@codeaurora.org>
2012-03-26 12:00:45 -0700

    msm_fb: display: Wait for external vsync before turning off DTV
    Wait for external vsync interrupt before turning off the timing generator
    for DTV display
    
    CRs-Fixed: 341400
    Change-Id: Ibd380f8e20a7ebb250669fcabf4233e9e526d22f
    Signed-off-by: Ravishangar Kalyanam <rkalya@codeaurora.org>

--- drivers/video/msm/mdp4.h
+++ drivers/video/msm/mdp4.h
@@ -425,6 +425,8 @@ int mdp4_overlay_dtv_set(struct msm_fb_data_type *mfd,
 			struct mdp4_overlay_pipe *pipe);
 int mdp4_overlay_dtv_unset(struct msm_fb_data_type *mfd,
 			struct mdp4_overlay_pipe *pipe);
+void mdp4_dma_e_done_dtv(void);
+void mdp4_overlay_dtv_wait4vsync(void);
 #else
 static inline void mdp4_overlay_dtv_start(void)
 {
@@ -449,6 +451,14 @@ static inline int mdp4_overlay_dtv_unset(struct msm_fb_data_type *mfd,
 			struct mdp4_overlay_pipe *pipe)
 {
 	return 0;
+}
+static inline void mdp4_dma_e_done_dtv(void)
+{
+    /* empty */
+}
+static inline void mdp4_overlay_dtv_wait4vsync(void)
+{
+    /* empty */
 }
 #endif
 
