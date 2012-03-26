a027bf8fa4d3cbf084b3ec7d536e4cab0f7e7620
Ravishangar Kalyanam <rkalya@codeaurora.org>
2012-03-26 13:31:28 -0700

    msm_fb: display: Call footswitch control from overlay dtv on/off
    Instead of dtv on and off, call footswitch control during
    overlay dtv timing generator on and off calls
    
    Change-Id: I186955d9c6b07608e77b599c23d9a6593faba6e3
    Signed-off-by: Ravishangar Kalyanam <rkalya@codeaurora.org>

--- drivers/video/msm/mdp4_overlay_dtv.c
+++ drivers/video/msm/mdp4_overlay_dtv.c
@@ -229,6 +229,7 @@ int mdp4_dtv_on(struct platform_device *pdev)
 	if (mfd->key != MFD_KEY)
 		return -EINVAL;
 
+	mdp_footswitch_ctrl(TRUE);
 	mdp4_overlay_panel_mode(MDP4_MIXER1, MDP4_PANEL_DTV);
 	if (dtv_pipe != NULL)
 		ret = mdp4_dtv_start(mfd);
@@ -259,6 +260,7 @@ int mdp4_dtv_off(struct platform_device *pdev)
 	mdp4_overlay_panel_mode_unset(MDP4_MIXER1, MDP4_PANEL_DTV);
 
 	ret = panel_next_off(pdev);
+	mdp_footswitch_ctrl(FALSE);
 
 	dev_info(&pdev->dev, "mdp4_overlay_dtv: off");
 	return ret;
