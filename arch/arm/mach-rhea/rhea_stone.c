ff093be143572e3f8eab28219d15943338b4be52
kendai <kendai@broadcom.com>
2012-03-28 11:29:16 -0700

    NFC: change registered NFC I2C address
    [Defect / Enhancement / New Feature]
    MobC00205187
    
    [Problem]
    GPS and NFC use same default address
    
    [Solution]
    Both GPS and NFC change default address right after power-on,
    NFC register a fake address to I2C bus driver during initialization, so do not block GPS
    
    [Reviewers]
    
    [Testing]
        [Test Cases]
    
        [Label/CL# Synced To]
    
        [Platform]
    
    [BROADCOM INTERNAL NOTES]

--- arch/arm/mach-rhea/rhea_stone.c
+++ arch/arm/mach-rhea/rhea_stone.c
@@ -338,7 +338,7 @@ static int bcmi2cnfc_gpio_clear(void *this)
 
 static struct i2c_board_info __initdata bcmi2cnfc[] = {
 	{
-	 I2C_BOARD_INFO("bcmi2cnfc", 0x1FA),
+	 I2C_BOARD_INFO("bcmi2cnfc", 0x1F0),
 	 .flags = I2C_CLIENT_TEN,
 	 .platform_data = (void *)&bcmi2cnfc_pdata,
 	 .irq = gpio_to_irq(4),
