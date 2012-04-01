118feb9ce7751451647f4d1db3176da295c33d88
Zhoulu Luo <zluo@codeaurora.org>
2012-03-31 19:49:58 -0700

    Revert "power: pm8921-bms: calibrate xoadc more often"
    This reverts commit e8221f4a6228e0fa8b7d3ba9a1bf2a45b7e20ebb.
    
    Change-Id: I33abfe7f0b2fd790ededd57b97a4816eb047ccd5
    Signed-off-by: Zhoulu Luo <zluo@codeaurora.org>

--- include/linux/mfd/pm8xxx/pm8921-bms.h
+++ include/linux/mfd/pm8xxx/pm8921-bms.h
@@ -166,8 +166,6 @@ void pm8921_bms_charging_began(void);
  *				track of chargecycles
  */
 void pm8921_bms_charging_end(int is_battery_full);
-
-void pm8921_bms_calibrate_hkadc(void);
 #else
 static inline int pm8921_bms_get_vsense_avg(int *result)
 {
@@ -189,9 +187,6 @@ static inline void pm8921_bms_charging_began(void)
 {
 }
 static inline void pm8921_bms_charging_end(int is_battery_full)
-{
-}
-static inline void pm8921_bms_calibrate_hkadc(void)
 {
 }
 #endif
