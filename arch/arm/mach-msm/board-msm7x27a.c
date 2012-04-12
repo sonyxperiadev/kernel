28fa519a5750eb728b33e969f95dabba15677b24
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2012-04-12 11:33:14 +0530

    msm_fb: Make buffers aligned to 4K page boundary
    This change makes the frame buffers aligned to the page
    boundary. Improves GPU performance since accessing the
    frame buffer from page boundary is faster.
    
    Change-Id: I17879437dd7cbb9a1758d82321c6e68dbd88b9e0
    CRs-Fixed: 325134
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- arch/arm/mach-msm/board-msm7x27a.c
+++ arch/arm/mach-msm/board-msm7x27a.c
@@ -159,10 +159,10 @@ static struct msm_i2c_platform_data msm_gsbi1_qup_i2c_pdata = {
 
 
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
-#define MSM_FB_SIZE		0x260000
+#define MSM_FB_SIZE		0x261000
 #define MSM7x25A_MSM_FB_SIZE	0xE1000
 #else
-#define MSM_FB_SIZE		0x195000
+#define MSM_FB_SIZE		0x196000
 #define MSM7x25A_MSM_FB_SIZE	0x96000
 
 #endif
--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -1011,6 +1011,7 @@ static int msm_fb_register(struct msm_fb_data_type *mfd)
 	struct fb_var_screeninfo *var;
 	int *id;
 	int fbram_offset;
+	int remainder, remainder_mode2;
 
 	/*
 	 * fb info initialization
@@ -1157,6 +1158,29 @@ static int msm_fb_register(struct msm_fb_data_type *mfd)
 
 	fix->line_length = msm_fb_line_length(mfd->index, panel_info->xres,
 					      bpp);
+
+	/* Make sure all buffers can be addressed on a page boundary by an x
+	 * and y offset */
+
+	remainder = (fix->line_length * panel_info->yres) % PAGE_SIZE;
+	if (!remainder)
+		remainder = PAGE_SIZE;
+	remainder_mode2 = (fix->line_length *
+				panel_info->mode2_yres) % PAGE_SIZE;
+	if (!remainder_mode2)
+		remainder_mode2 = PAGE_SIZE;
+
+	/* calculate smem_len based on max size of two supplied modes */
+	fix->smem_len = MAX((msm_fb_line_length(mfd->index, panel_info->xres,
+					      bpp) *
+			    panel_info->yres + PAGE_SIZE -
+				remainder) * mfd->fb_page,
+			    (msm_fb_line_length(mfd->index,
+					       panel_info->mode2_xres,
+					       bpp) *
+			    panel_info->mode2_yres + PAGE_SIZE -
+				remainder_mode2) * mfd->fb_page);
+
 
 	/*
 	 * calculate smem_len based on max size of two supplied modes.
