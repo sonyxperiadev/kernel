6a91ba2aa336c302b44fc2c5790b54fa7f83c523
Karthikeyan Ramasubramanian <kramasub@codeaurora.org>
2012-05-15 17:49:57 -0600

    msm: ipc: Add support to load and unload the modem
    When all the user-space clients are killed, modem gets unloaded and goes
    to silent reset. When the user-space clients come up again, modem resets
    its side of the RPCRPY_CNTL SMD channel. This leaves the SMD channel in
    stale state. Hence modem starts reading stale data from the SMD channel
    and crashes. So as long as the IPC Router uses the SMD channel, cast a
    vote to PIL to ensure that the modem is loaded.
    
    Change-Id: I27a49b8ba3f953df316c6a2f171e0d0f0c7a9355
    CRs-Fixed: 359773
    Signed-off-by: Karthikeyan Ramasubramanian <kramasub@codeaurora.org>
    (cherry picked from commit 9a5aca7ecfbe51b1f3d9d462429546d10374f2b3)

--- arch/arm/mach-msm/ipc_router_smd_xprt.c
+++ arch/arm/mach-msm/ipc_router_smd_xprt.c
@@ -19,6 +19,7 @@
 #include <linux/types.h>
 
 #include <mach/msm_smd.h>
+#include <mach/peripheral-loader.h>
 
 #include "ipc_router.h"
 #include "smd_private.h"
@@ -40,7 +41,7 @@ if (msm_ipc_router_smd_xprt_debug_mask) \
 
 struct msm_ipc_router_smd_xprt {
 	struct msm_ipc_router_xprt xprt;
-
+	void *pil;
 	smd_channel_t *channel;
 };
 
@@ -160,8 +161,14 @@ static int msm_ipc_router_smd_remote_write(void *data,
 
 static int msm_ipc_router_smd_remote_close(void)
 {
+	int rc;
 	smsm_change_state(SMSM_APPS_STATE, SMSM_RPCINIT, 0);
-	return smd_close(smd_remote_xprt.channel);
+	rc = smd_close(smd_remote_xprt.channel);
+	if (smd_remote_xprt.pil) {
+		pil_put(smd_remote_xprt.pil);
+		smd_remote_xprt.pil = NULL;
+	}
+	return rc;
 }
 
 static void smd_xprt_read_data(struct work_struct *work)
@@ -349,9 +356,19 @@ static int msm_ipc_router_smd_remote_probe(struct platform_device *pdev)
 
 	init_waitqueue_head(&write_avail_wait_q);
 
+	smd_remote_xprt.pil = pil_get("modem");
+	if (IS_ERR(smd_remote_xprt.pil)) {
+		pr_err("%s: modem load failed\n", __func__);
+		smd_remote_xprt.pil = NULL;
+		destroy_workqueue(smd_xprt_workqueue);
+		return -ENODEV;
+	}
+
 	rc = smd_open("RPCRPY_CNTL", &smd_remote_xprt.channel, NULL,
 		      msm_ipc_router_smd_remote_notify);
 	if (rc < 0) {
+		pil_put(smd_remote_xprt.pil);
+		smd_remote_xprt.pil = NULL;
 		destroy_workqueue(smd_xprt_workqueue);
 		return rc;
 	}
