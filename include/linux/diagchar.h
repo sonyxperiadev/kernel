c44534a4edc58090726399be60b4cd34e173bcb8
Shalabh Jain <shalabhj@codeaurora.org>
2012-06-15 13:39:27 -0700

    diag: Add missing SSID range
    OEMs use SSID in a specific range. Recently DIAG changed the mask
    update algorithm and this range was missed as its not used internally.
    This change adds it back.
    
    CRs-Fixed: 359379
    Signed-off-by: Shalabh Jain <shalabhj@codeaurora.org>
    (cherry picked from commit 44b79b7bb5aeeaa2c956ce0fd1e605d840c55a73)
    
    Conflicts:
    
    	include/linux/diagchar.h
    
    Signed-off-by: Shruthi Krishna <skrish@codeaurora.org>
    (cherry picked from commit 2bfe8e5272130f99fe2f1a8c625baea557bca883)
    
    Change-Id: I62d9c7b0a78d3c76e826ab7724e922ea524acd7b
    Signed-off-by: Shruthi Krishna <skrish@codeaurora.org>

--- include/linux/diagchar.h
+++ include/linux/diagchar.h
@@ -109,8 +109,8 @@ the appropriate macros. */
 
 /* This needs to be modified manually now, when we add
  a new RANGE of SSIDs to the msg_mask_tbl */
-#define MSG_MASK_TBL_CNT		23
-#define EVENT_LAST_ID			0x083F
+#define MSG_MASK_TBL_CNT		24
+#define EVENT_LAST_ID			0x08AD
 
 #define MSG_SSID_0			0
 #define MSG_SSID_0_LAST			90
@@ -158,6 +158,8 @@ the appropriate macros. */
 #define MSG_SSID_21_LAST		10300
 #define MSG_SSID_22			10350
 #define MSG_SSID_22_LAST		10361
+#define MSG_SSID_23			0xC000
+#define MSG_SSID_23_LAST		0xC063
 
 struct diagpkt_delay_params {
 	void *rsp_ptr;
