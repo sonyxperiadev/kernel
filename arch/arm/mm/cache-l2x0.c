40d58bbc0b630fa3c4d72e12b280e31d3e7a0253
Shankar B <bshankar@broadcom.com>
2012-06-27 17:46:10 +0530

    L2 Cache: Harden Sync operations
    [Defect]
    MobC00226345
    
    [Problem]
    Harden the L2 sync operation code to follow the sequence proposed by ASIC team
    as follows
    -	Disable interrupts
    -	Poll for C bit (should fall through immediately)
    -	Start operation
    -	Poll fof C bit (may take time)
    -	Re-Enable interrupts
    
    [Solution]
    Implemented the change
    
    [Reviewers]
    Naga, Srivathsa, Vikram, Sajith
    
    [Testing]
        [Test Cases]
        Performed the standard system test cases to ensure no side effects
        Verified by Jeff/Harhsa and team
    
        [Label/CL# Synced To]
    
        [Platform]
        Rheastone EDN20
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: If252b152c5247898bccee1060353218d11645ebe

--- arch/arm/mm/cache-l2x0.c
+++ arch/arm/mm/cache-l2x0.c
@@ -24,6 +24,7 @@
 #include <asm/hardware/cache-l2x0.h>
 
 #define CACHE_LINE_SIZE		32
+#define POLL_C_BIT_FOR_CACHE_OPERATIONS
 
 static void __iomem *l2x0_base;
 static DEFINE_SPINLOCK(l2x0_lock);
@@ -50,7 +51,20 @@ static inline void cache_wait_way(void __iomem *reg, unsigned long mask)
 #ifdef CONFIG_CACHE_PL310
 static inline void cache_wait(void __iomem *reg, unsigned long mask)
 {
+#ifdef POLL_C_BIT_FOR_CACHE_OPERATIONS
+
+	/*
+	 * Introduce a sync barrier to ensure the order. That is
+	 * previous transactions are finished.
+	 */
+	dsb();
+
+	/* Wait for the corresponding bit to become 0 */
+	while (readl_relaxed(reg) & mask)
+		;
+#else
 	/* cache operations by line are atomic on PL310 */
+#endif
 }
 #else
 #define cache_wait	cache_wait_way
@@ -59,6 +73,10 @@ static inline void cache_wait(void __iomem *reg, unsigned long mask)
 static inline void cache_sync(void)
 {
 	void __iomem *base = l2x0_base;
+
+#ifdef POLL_C_BIT_FOR_CACHE_OPERATIONS
+	cache_wait(base + L2X0_CACHE_SYNC, 1);
+#endif
 
 #ifdef CONFIG_ARM_ERRATA_753970
 	/* write to an unmmapped register */
@@ -74,6 +92,9 @@ static inline void l2x0_clean_line(unsigned long addr)
 	void __iomem *base = l2x0_base;
 	cache_wait(base + L2X0_CLEAN_LINE_PA, 1);
 	writel_relaxed(addr, base + L2X0_CLEAN_LINE_PA);
+#ifdef POLL_C_BIT_FOR_CACHE_OPERATIONS
+	cache_wait(base + L2X0_CLEAN_LINE_PA, 1);
+#endif
 }
 
 static inline void l2x0_inv_line(unsigned long addr)
@@ -81,6 +102,9 @@ static inline void l2x0_inv_line(unsigned long addr)
 	void __iomem *base = l2x0_base;
 	cache_wait(base + L2X0_INV_LINE_PA, 1);
 	writel_relaxed(addr, base + L2X0_INV_LINE_PA);
+#ifdef POLL_C_BIT_FOR_CACHE_OPERATIONS
+	cache_wait(base + L2X0_INV_LINE_PA, 1);
+#endif
 }
 
 #if defined(CONFIG_PL310_ERRATA_588369) || defined(CONFIG_PL310_ERRATA_727915)
@@ -110,6 +134,9 @@ static inline void l2x0_flush_line(unsigned long addr)
 	writel_relaxed(addr, base + L2X0_CLEAN_LINE_PA);
 	cache_wait(base + L2X0_INV_LINE_PA, 1);
 	writel_relaxed(addr, base + L2X0_INV_LINE_PA);
+#ifdef POLL_C_BIT_FOR_CACHE_OPERATIONS
+	cache_wait(base + L2X0_INV_LINE_PA, 1);
+#endif
 }
 #else
 
@@ -118,6 +145,9 @@ static inline void l2x0_flush_line(unsigned long addr)
 	void __iomem *base = l2x0_base;
 	cache_wait(base + L2X0_CLEAN_INV_LINE_PA, 1);
 	writel_relaxed(addr, base + L2X0_CLEAN_INV_LINE_PA);
+#ifdef POLL_C_BIT_FOR_CACHE_OPERATIONS
+	cache_wait(base + L2X0_INV_LINE_PA, 1);
+#endif
 }
 #endif
 
