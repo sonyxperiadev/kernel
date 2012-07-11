3b771ea383d5c2fd2a5da02a67f86874d80b65f3
Varun Wadekar <vwadekar@nvidia.com>
2012-07-11 14:28:06 +0530

    scheduler: Re-compute time-average nr_running on read
    Port commit 1802afb2a (http://git-master/r/111637) from v3.1
    
    Re-compute time-average nr_running when it is read. This would
    prevent reading stalled average value if there were no run-queue
    changes for a long time. New average value is returned to the reader,
    but not stored to avoid concurrent writes. Light-weight sequential
    counter synchronization is used to assure data consistency for
    re-computing average.
    
    Original author: Alex Frid <afrid@nvidia.com>
    
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>
    
    Change-Id: Ic486006d62436fb61cda4ab6897e933f5c102b52
    
    Rebase-Id: Re0ca57c84a644e8e2d474930379cdcc386a2135a

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3864,6 +3864,32 @@ unsigned long this_cpu_load(void)
 	return this->cpu_load[0];
 }
 
+unsigned long avg_nr_running(void)
+{
+	unsigned long i, sum = 0;
+	unsigned int seqcnt, ave_nr_running;
+
+	for_each_online_cpu(i) {
+		struct rq *q = cpu_rq(i);
+
+		/*
+		 * Update average to avoid reading stalled value if there were
+		 * no run-queue changes for a long time. On the other hand if
+		 * the changes are happening right now, just read current value
+		 * directly.
+		 */
+		seqcnt = read_seqcount_begin(&q->ave_seqcnt);
+		ave_nr_running = do_avg_nr_running(q);
+		if (read_seqcount_retry(&q->ave_seqcnt, seqcnt)) {
+			read_seqcount_begin(&q->ave_seqcnt);
+			ave_nr_running = q->ave_nr_running;
+		}
+
+		sum += ave_nr_running;
+	}
+
+	return sum;
+}
 
 /*
  * Global load-average calculations
