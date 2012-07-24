4407a1cc7a8e57e3205ab3e410f1a1b491ab4460
Nishanth Peethambaran <nishanth@broadcom.com>
2012-07-24 16:09:02 +0530

    M4U: Update M4U to call from ION
    a) APIs to add regions, map and unmap is added.
    b) A new header file exporting the global device, APIs
    	and structures is added.
    c) Function filling the page table split to support interleaving.
    d) Cleanup done.

--- drivers/gpu/m4u/m4u.c
+++ drivers/gpu/m4u/m4u.c
@@ -19,15 +19,15 @@
 #include <linux/file.h>
 #include <linux/fs.h>
 #include <linux/err.h>
-#include <linux/scatterlist.h>
+#include <linux/list.h>
 #include <linux/platform_device.h>
 #include <linux/miscdevice.h>
 #include <linux/interrupt.h>
 #include <linux/slab.h>
-#include <linux/uaccess.h>
 #include <linux/dma-mapping.h>
 #include <linux/log2.h>
 #include <linux/debugfs.h>
+#include <linux/broadcom/m4u.h>
 
 #include <mach/irqs.h>
 #include <mach/rdb/brcm_rdb_sysmap.h>
@@ -40,23 +40,15 @@
 
 #define BCM_INT_ID_M4U_OPEN		(BCM_INT_ID_RESERVED181)
 #define	M4U_PAGE_SHIFT			(12)
-#define	M4U_TLB_LINE_SIZE		(8)
+#define	M4U_TLB_LINE_SHIFT		(3)
+#define	M4U_TLB_LINE_SIZE		(1<<M4U_TLB_LINE_SHIFT)
 
-struct m4u_region {
-	u32 pa;
-	u32 mma;
-	u32 size;
-	u32 page_size;
+struct m4u_region_node {
+	struct m4u_region region;
+	struct list_head list;
 };
+
 /* To be moved to board file - Start */
-struct m4u_platform_data {
-	u32 mma_start;
-	u32 mma_end;
-	u32 mma_sg_start;
-	int nr;
-	struct m4u_region *regions;
-};
-
 static struct m4u_region m4u_regions[] = {
 	[0] = {
 		.mma			= 0x80000000,
@@ -101,28 +93,27 @@ static struct platform_device m4u_platform_device = {
 struct m4u_device {
 	struct miscdevice 		dev;
 	struct mutex 			lock;
-	void __iomem			*base;
-	int						page_size;
+	void __iomem			*ioreg_base;
 	struct page				*garbage_page;
 	dma_addr_t 				pt_handle;
 	u32						*pt_base;
 	struct m4u_platform_data pdata;
+	struct list_head		static_region_list;
 	struct dentry 			*debug_root;
 };
 
+struct m4u_device *g_mdev;
 #ifdef DEBUG_M4U
-struct m4u_device *dbg_mdev;
-u32 dbg_page_size;
 #endif
 
 static inline void m4u_write_reg(struct m4u_device *mdev, u32 offset, u32 value)
 {
-	iowrite32(value, (mdev->base + offset));
+	iowrite32(value, (mdev->ioreg_base + offset));
 }
 
 static inline u32 m4u_read_reg(struct m4u_device *mdev, u32 offset)
 {
-	return ioread32(mdev->base + offset);
+	return ioread32(mdev->ioreg_base + offset);
 }
 
 static void m4u_print_platform_data(struct m4u_platform_data *pdata)
@@ -160,36 +151,48 @@ static inline u32 m4u_pte(u32 pa, u32 order, u32 valid)
 	return pte;
 }
 
+static void m4u_map_big_page(struct m4u_device *mdev, u32 mma, u32 pa, int page_order)
+{
+	int i;
+	int page_order_base4k;
+	u32 pte, num_pte;
+	u32 *pt;
+	u32 mma_aligned;
+
+	page_order_base4k = page_order - M4U_PAGE_SHIFT;
+	num_pte = (1 << page_order_base4k);
+	mma_aligned = (mma >> (page_order+M4U_TLB_LINE_SHIFT)) << (page_order+M4U_TLB_LINE_SHIFT);
+	pt = mdev->pt_base + ((mma_aligned - mdev->pdata.mma_start) >> M4U_PAGE_SHIFT);
+	pt += (mma >> page_order) & (M4U_TLB_LINE_SIZE-1);
+	pte = m4u_pte(pa, page_order_base4k, 1);
+	for (i=0; i<num_pte; i++) {
+		*pt = pte;
+		pt += M4U_TLB_LINE_SIZE;
+	}
+}
+
 static int m4u_map_pages(struct m4u_device *mdev, u32 mma, u32 pa, int page_order, int n)
 {
-	u32 pa_group = pa;
-	int page_order_base4k;
-	u32 num_pte;
+	int i;
 	u32 *pt;
-	int i, j, k;
 
 #ifdef DEBUG_M4U
 	pr_info("(Debug) mma(0x%x) pa(0x%08x) page_order(%d) n(%d) \n", 
 			mma, pa, page_order, n);
 #endif
-	pt = mdev->pt_base + ((mma - mdev->pdata.mma_start) >> M4U_PAGE_SHIFT);
-	page_order_base4k = page_order - M4U_PAGE_SHIFT;
-	num_pte = (1 << page_order_base4k);
-	for(i=0; i<(n/M4U_TLB_LINE_SIZE); i++) {
-		for(j=0; j<num_pte; j++) {
-			pa = pa_group;
-			for(k=0; k<M4U_TLB_LINE_SIZE; k++) {
-				*pt++ = m4u_pte(pa, page_order_base4k, 1);
-				pa += 1 << page_order;
-			}
+	if (page_order == M4U_PAGE_SHIFT) {
+		pt = mdev->pt_base + ((mma - mdev->pdata.mma_start) >> M4U_PAGE_SHIFT);
+		for (i=0; i<n; i++) {
+			*pt++ = m4u_pte(pa, 0, 1);
+			pa += 1 << M4U_PAGE_SHIFT;
 		}
-		pa_group = pa;
+	} else {
+		for (i=0; i<n; i++) {
+			m4u_map_big_page(mdev, mma, pa, page_order);
+			mma += 1 << page_order;
+			pa += 1 << page_order;
+		}
 	}
-	return 0;
-}
-
-static int m4u_map(struct m4u_device *mdev, u32 mma, u32 pa, u32 size)
-{
 	return 0;
 }
 
@@ -277,6 +280,79 @@ error:
 	return -ENOMEM;
 }
 
+int m4u_map(struct m4u_device *mdev, struct sg_table *sgt, u32 size, u32 align)
+{
+	struct m4u_region_node *region_node;
+	struct m4u_region *r;
+	struct list_head *reg;
+	u32 pa = 0, mma = 0, pa_end;
+
+	/* Only contiguous blocks assumed */
+	if (!IS_ERR_OR_NULL(sgt) && (sgt->nents == 1))
+		pa = sg_phys(sgt->sgl);
+	if (pa) {
+		pa_end = pa + size - 1;
+		list_for_each(reg, &mdev->static_region_list) {
+			region_node = list_entry(reg, struct m4u_region_node, list);
+			r = &region_node->region;
+			pr_debug("map region mma(0x%08x) pa(0x%08x) size(0x%08x) \n",
+				   r->mma, r->pa, r->size);
+			if ((pa > r->pa) && (pa_end < r->pa + r->size)) {
+				mma = r->mma + (pa - r->pa);
+				break;
+			}
+		}
+	}
+	pr_debug("map mma(0x%08x) pa(0x%08x) size(0x%08x) align(%d) \n",
+		mma, pa, size, align);
+	return mma;
+}
+EXPORT_SYMBOL(m4u_map);
+
+void m4u_unmap(struct m4u_device *mdev, u32 mma, u32 size)
+{
+	struct m4u_region_node *region_node;
+	struct m4u_region *r;
+	struct list_head *reg;
+	u32 pa = 0, mma_end = 0;
+
+	list_for_each(reg, &mdev->static_region_list) {
+		region_node = list_entry(reg, struct m4u_region_node, list);
+		r = &region_node->region;
+		pr_debug("map region mma(0x%08x) pa(0x%08x) size(0x%08x) \n",
+				r->mma, r->pa, r->size);
+		if ((mma > r->mma) && (mma_end < r->mma + r->size)) {
+			pa = r->pa + (mma - r->mma);
+			break;
+		}
+	}
+	pr_debug("unmap mma(0x%08x) pa(0x%08x) size(0x%08x) \n",
+		mma, pa, size);
+}
+EXPORT_SYMBOL(m4u_unmap);
+
+int m4u_add_region(struct m4u_device *mdev, struct m4u_region *region)
+{
+	struct m4u_region_node *region_node;
+
+	if (!region) {
+		pr_err("Failed adding invalid region \n");
+		return -EINVAL;
+	}
+	region_node = kmalloc(sizeof(struct m4u_region_node), GFP_KERNEL);
+	if (!region_node) {
+		pr_err("Failed allocating metadata for region \n");
+		return -ENOMEM;
+	}
+	region_node->region = *region;
+	list_add_tail(&region_node->list, &mdev->static_region_list);
+		pr_info("Add region - mma(0x%08x) pa(0x%08x) size(0x%08x) page_size(0x%08x) \n", 
+				region->mma, region->pa, region->size, region->page_size);
+
+	return 0;
+}
+EXPORT_SYMBOL(m4u_add_region);
+
 static irqreturn_t m4u_isr(int irq, void *data)
 {
 	struct m4u_device *mdev = (struct m4u_device *)data;
@@ -351,6 +427,7 @@ static int m4u_probe(struct platform_device *pdev)
 	struct resource *mem_res = NULL;
 	unsigned int irq = 0;
 	int ret;
+	int i;
 
 	pr_debug("M4U device probe \n");
 	if (!pdev || !pdev->dev.platform_data) {
@@ -366,14 +443,19 @@ static int m4u_probe(struct platform_device *pdev)
 		pr_err("Allocation of context failed. \n");
 		return -ENOMEM;
 	}
+	g_mdev = mdev;
 	mutex_init(&mdev->lock);
 	memcpy(&mdev->pdata, pdata, sizeof(*pdata));
-	/* TODO: m4u_regions are not copied */
+	INIT_LIST_HEAD(&mdev->static_region_list);
+	for(i=0; i<pdata->nr; i++) {
+		ret = m4u_add_region(mdev, &pdata->regions[i]);
+		if (ret) {
+			goto err_freemem;
+		}
+	}
+	/* TODO: Free region buffers */
 #ifdef DEBUG_M4U
-	dbg_mdev = mdev;
-	dbg_page_size = mdev->page_size;
-	pr_info("(Debug) mdev(%p) page_size(0x%x) \n", 
-			dbg_mdev, dbg_page_size);
+	pr_info("(Debug) mdev(%p) \n", g_mdev);
 #endif
 
 	/* Map the register space */
@@ -390,8 +472,8 @@ static int m4u_probe(struct platform_device *pdev)
 		ret = -EBUSY;
 		goto err_freemem;
 	}
-	mdev->base = ioremap_nocache(mem_res->start, resource_size(mem_res));
-	if (!mdev->base) {
+	mdev->ioreg_base = ioremap_nocache(mem_res->start, resource_size(mem_res));
+	if (!mdev->ioreg_base) {
 		pr_err("Failed to map the registers. \n");
 		ret = -ENOMEM;
 		goto err_freereg;
@@ -436,8 +518,8 @@ static int m4u_probe(struct platform_device *pdev)
 
 	/* Set the mdev and platform device private data */
 	platform_set_drvdata(pdev, mdev);
-	pr_info("probe successfull - page_size(%d) irq(%d) reg-base(%p). \n",
-			mdev->page_size, irq, mdev->base);
+	pr_info("probe successfull - irq(%d) reg-base(%p). \n",
+			irq, mdev->ioreg_base);
 
 	return 0;
 
@@ -448,7 +530,7 @@ err_deregister:
 	misc_deregister(&mdev->dev);
 
 err_unmap:
-	iounmap(mdev->base);
+	iounmap(mdev->ioreg_base);
 
 err_freereg:
 	if (mem_res)
@@ -472,7 +554,7 @@ static int m4u_remove(struct platform_device *pdev)
 		irq = platform_get_irq(pdev, 0);
 		free_irq(irq, mdev);
 		misc_deregister(&mdev->dev);
-		iounmap(mdev->base);
+		iounmap(mdev->ioreg_base);
 		mem_res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 		if (mem_res)
 			release_mem_region(mem_res->start, resource_size(mem_res));
@@ -494,8 +576,8 @@ static int m4u_resume(struct platform_device *pdev)
 
 #else
 
-#define	v3d_suspend	NULL
-#define	v3d_resume	NULL
+#define	m4u_suspend	NULL
+#define	m4u_resume	NULL
 
 #endif
 
---
+++ include/linux/broadcom/m4u.h
@@ -0,0 +1,47 @@
+/*
+ * include/linux/broadcom/m4u.h
+ *
+ * Copyright (C) 2011 Broadcom, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _LINUX_M4U_H
+#define _LINUX_M4U_H
+
+#include <linux/scatterlist.h>
+#include <linux/uaccess.h>
+
+struct m4u_region {
+	u32 pa;
+	u32 mma;
+	u32 size;
+	u32 page_size;
+};
+
+struct m4u_platform_data {
+	u32 mma_start;
+	u32 mma_end;
+	u32 mma_sg_start;
+	int nr;
+	struct m4u_region *regions;
+};
+
+struct m4u_device;
+extern struct m4u_device *g_mdev;
+
+int m4u_add_region(struct m4u_device *mdev, struct m4u_region *region);
+int m4u_map(struct m4u_device *mdev, struct sg_table *sgt, u32 size, u32 align);
+void m4u_unmap(struct m4u_device *mdev, u32 mma, u32 size);
+
+
+#endif /* _LINUX_M4U_H */
+
