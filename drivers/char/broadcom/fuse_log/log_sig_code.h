fa89e2da341747fcfc01c27754b48c08e39be70d
xxu <xxu@broadcom.com>
2012-07-30 14:18:10 -0700

    Logging: Update Logging ID file for AP side
    [Defect / Enhancement / New Feature]
    MobC00233606
    
    [Problem]
    Logging id file does not match between AP and CP.
    
    [Solution]
    Update logging id file on AP side to match CP side
    
    [Reviewers]
    Jack Yu
    
    [Testing]
        [Test Cases]
    no logging id mismatch
    
        [Label/CL# Synced To]
    Latest
    
        [Platform]
    Rhea
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: Ibf47eb31d1210b49adc4240b42a4b8a4c3bcf58b

--- drivers/char/broadcom/fuse_log/log_sig_code.h
+++ drivers/char/broadcom/fuse_log/log_sig_code.h
@@ -111,6 +111,8 @@
 #define	P_no_receiver		253
 #define	P_many_receiver		254
 
+#include "logapi.h"
+
 typedef enum {
 #if defined(STACK_wedge) && defined(UMTS)
 	/* Start of log_urrcm */
@@ -146,7 +148,7 @@ typedef enum {
 	EGPRS_TX_BLK,
 #endif
 	EDGE_RX_HDR,
-	DSP_DATA,
+	DSP_DATA_OBSOLETE,
 	AGC_TRACK_DATA,
 	RX_TRACK_DATA,
 	SACCH_TRACK_DATA,
@@ -185,7 +187,7 @@ typedef enum {
 	L1DATA_FACCH,
 	L1DATA_BPM_STATUS,  /* //<Payload type {::BPM_STATUS_LOG_t} */
 	L1DATA_TWIF,		/*//<Payload type {::TWIF_LOG_t} */
-	AUDIO_DATA,
+	AUDIO_DATA_OBSOLETE,
 	L1DATA_RF_LOGGING,
 	VMS_TRACE_KEYS,
 	/* End of log_gl1s */
@@ -3111,6 +3113,11 @@ API_CAPI_MSG_ELEM_MSDB_PTR_MS_LOCAL_PHCTRL_ELEM_PLMNSEL_REGISTERED_PLMN
 #endif
 	/* End of umtsfw */
 #endif /* #if defined(STACK_wedge) && defined(UMTS) */
+
+	/* Start of AUDIO */
+	DSP_DATA = ((LOGID_AUDIO << 16) | 1),
+	AUDIO_DATA = ((LOGID_AUDIO << 16) | 2),
+	/* End of AUDIO */
 
 	LAST_LOG_SIGNAL
 } log_signal_id_t;
