838bc67a3ae57f228934c2f5d27beb32a00816cf
Jeevan Shriram <jshriram@codeaurora.org>
2012-08-09 19:07:04 +0530

    msm_fb: display: Enable pre-multiplied alpha for MDP303
    For images which are pre-multiplied with alpha, driver
    support is now added to blend them properly
    
    Change-Id: I1c07496f9d85c50f1df9eb8d70362ff23dbd90f8
    Signed-off-by: Jeevan Shriram <jshriram@codeaurora.org>

--- drivers/video/msm/mdp_ppp.c
+++ drivers/video/msm/mdp_ppp.c
@@ -1399,7 +1399,7 @@ static int mdp_ppp_blit_addr(struct fb_info *info, struct mdp_blit_req *req,
 		iBuf.mdpImg.mdpOp |= MDPOP_DITHER;
 
 	if (req->flags & MDP_BLEND_FG_PREMULT) {
-#ifdef CONFIG_FB_MSM_MDP31
+#if defined(CONFIG_FB_MSM_MDP31) || defined(CONFIG_FB_MSM_MDP303)
 		iBuf.mdpImg.mdpOp |= MDPOP_FG_PM_ALPHA;
 #else
 		put_img(p_src_file);
