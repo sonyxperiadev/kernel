4cf8333bf7cd95bf14113f00dc6645942a45c671
Nishanth Peethambaran <nishanth@broadcom.com>
2012-08-11 00:13:46 +0530

    ION: Update debug print for client to group based on id

--- drivers/gpu/ion/ion.c
+++ drivers/gpu/ion/ion.c
@@ -612,11 +612,11 @@ static int ion_debug_client_show(struct seq_file *s, void *unused)
 	for (n = rb_first(&client->handles); n; n = rb_next(n)) {
 		struct ion_handle *handle = rb_entry(n, struct ion_handle,
 						     node);
-		enum ion_heap_type type = handle->buffer->heap->type;
+		int id = handle->buffer->heap->id;
 
-		if (!names[type])
-			names[type] = handle->buffer->heap->name;
-		sizes[type] += handle->buffer->size;
+		if (!names[id])
+			names[id] = handle->buffer->heap->name;
+		sizes[id] += handle->buffer->size;
 	}
 	mutex_unlock(&client->lock);
 
