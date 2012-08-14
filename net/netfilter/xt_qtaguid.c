b90e203a96f88fb13294eaf0dd24d53dc8ac725d
jinhwang <jinhwang@broadcom.com>
2012-08-14 13:46:58 -0700

    Fix kernle Panic occurred in tag_stat_update function.
    [Defect / Enhancement / New Feature]
    MobC00287262 (Hawaii)
    MobC00235589
    
    [Problem]
    [SS-Lucas][P120809-3387] Kernle Panic occurred in tag_stat_update function.
    
    [Solution]
    Check NULL pointer before access it.
    
    [Reviewers]
    Qingyun Wei/Guo Jin
    
    [Testing]
        [Test Cases]
        boot/data work/samsung closed CSP.
    
        [Label/CL# Synced To]
    
        [Platform]
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: I8391948ce97033fddf150c295d91f1d92e904830
    Reviewed-on: http://mps-gerrit.broadcom.com/14434
    Reviewed-by: Jin Wang <jinhwang@broadcom.com>
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Reviewed-by: Naga Chandra Prahalada Rao <nagachandra@broadcom.com>
    Tested-by: AutoBuild Status <autobuild_status_noreply@broadcom.com>
    Tested-by: AutoTest Status <autotest_status_noreply@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- net/netfilter/xt_qtaguid.c
+++ net/netfilter/xt_qtaguid.c
@@ -1461,6 +1461,7 @@ static void if_tag_stat_update(const char *ifname, uid_t uid,
 		 *  - No {0, uid_tag} stats and no {acc_tag, uid_tag} stats.
 		 */
 		new_tag_stat = create_if_tag_stat(iface_entry, uid_tag);
+		if (new_tag_stat)
 			uid_tag_counters = &new_tag_stat->counters;
 	} else {
 		uid_tag_counters = &tag_stat_entry->counters;
@@ -1469,7 +1470,8 @@ static void if_tag_stat_update(const char *ifname, uid_t uid,
 	if (acct_tag) {
 		/* Create the child {acct_tag, uid_tag} and hook up parent. */
 		new_tag_stat = create_if_tag_stat(iface_entry, tag);
-		new_tag_stat->parent_counters = uid_tag_counters;
+		if (new_tag_stat)
+			new_tag_stat->parent_counters = uid_tag_counters;
 	} else {
 		/*
 		 * For new_tag_stat to be still NULL here would require:
@@ -1481,6 +1483,7 @@ static void if_tag_stat_update(const char *ifname, uid_t uid,
 		 */
 		BUG_ON(!new_tag_stat);
 	}
+	if (new_tag_stat)
 		tag_stat_update(new_tag_stat, direction, proto, bytes);
 	spin_unlock_bh(&iface_entry->tag_stat_list_lock);
 }
