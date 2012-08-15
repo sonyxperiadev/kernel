fbe26f0b2e307061b732a0828476e7ab687c3f52
Al Viro <viro@zeniv.linux.org.uk>
2012-08-15 18:23:36 -0400

    binder: don't allow mmap() by process other than proc->tsk
    we really shouldn't do get_files_struct() on a different process
    and use it to modify the sucker later on.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Change-Id: I4186cf7e48b7cb0dbec557951d2e1243170d216f
    Reviewed-on: http://mps-gerrit.broadcom.com/8303
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Reviewed-by: Vikram Prakash <vikramp@broadcom.com>
    Tested-by: AutoBuild Status <autobuild_status_noreply@broadcom.com>
    Tested-by: AutoTest Status <autotest_status_noreply@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -2842,6 +2842,9 @@ static int binder_mmap(struct file *filp, struct vm_area_struct *vma)
 	const char *failure_string;
 	struct binder_buffer *buffer;
 
+	if (proc->tsk != current)
+		return -EINVAL;
+
 	if ((vma->vm_end - vma->vm_start) > SZ_4M)
 		vma->vm_end = vma->vm_start + SZ_4M;
 
@@ -2906,7 +2909,7 @@ static int binder_mmap(struct file *filp, struct vm_area_struct *vma)
 	binder_insert_free_buffer(proc, buffer);
 	proc->free_async_space = proc->buffer_size / 2;
 	barrier();
-	proc->files = get_files_struct(proc->tsk);
+	proc->files = get_files_struct(current);
 	proc->vma = vma;
 	proc->vma_vm_mm = vma->vm_mm;
 
