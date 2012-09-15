d1de0ef54a4ae0f90ded2137d29bc213a0a37a4a
Nishanth Peethambaran <nishanth@broadcom.com>
2012-09-15 21:18:59 +0530

    hawaii fpga: Make ion carveout size as 48M

--- arch/arm/mach-hawaii/devices.c
+++ arch/arm/mach-hawaii/devices.c
@@ -116,7 +116,7 @@ static struct ion_platform_data ion_data0 = {
 			.type = ION_HEAP_TYPE_CARVEOUT,
 			.name = "ion-carveout-0",
 			.base = 0,
-			.size = (16 * SZ_1M),
+			.size = (48 * SZ_1M),
 		},
 	},
 };
