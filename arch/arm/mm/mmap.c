d314298c7b4a9e91411f22435aa5b9d632b0103d
Shankar B <bshankar@broadcom.com>
2012-09-19 11:17:36 +0530

    mmap: Make it work in legacy mode [Defect] MobC00238168
    [Problem]
    From JB, when we try to launch the trial x third party application libmono was
    not getting loaded successfully. The same is not observed with Capri
    platforms. In Capri the Kernel is 3.0.31.
    In 3.4 kernel (used in Rhea) mmap grows from high to low address. This seems
    to not go well with the behaviour of libmono library.
    
    [Solution]
    Make the mmap work in legacy mode as in older kernels, the mmap grows from
    lower address to higher address.
    
    THIS CHANG IS TEMPORARY
    This is done only to enable the applications to work seamless on 3.4 kernel.s
    At later point of time if this problem is fixed in JB or the appropriate
    library we should undo this commit.
    
    [Reviewers]
    Nick, Vikram, Sundar, Aditya
    
    [Testing]
        [Test Cases]
        Verified that the process's mmap starts from 0x40000000 previously it was
        starting at 0xa00000000
    
        [Label/CL# Synced To]
    
        [Platform]
        Rhea
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: Ib4b5c35e970994526f6994032c2552e369759945

--- arch/arm/mm/mmap.c
+++ arch/arm/mm/mmap.c
@@ -33,6 +33,7 @@ static inline unsigned long COLOUR_ALIGN_DOWN(unsigned long addr,
 
 static int mmap_is_legacy(void)
 {
+#if 0
 	if (current->personality & ADDR_COMPAT_LAYOUT)
 		return 1;
 
@@ -40,6 +41,8 @@ static int mmap_is_legacy(void)
 		return 1;
 
 	return sysctl_legacy_va_layout;
+#endif
+	return 1;
 }
 
 static unsigned long mmap_base(unsigned long rnd)
