ec9e1bab925caecca97c06b0516722e8723423fa
Pontus Fuchs <pontus.fuchs@gmail.com>
2012-11-19 11:44:51 -0800

    netfilter: qtaguid: Don't BUG_ON if create_if_tag_stat fails
    If create_if_tag_stat fails to allocate memory (GFP_ATOMIC) the
    following will happen:
    
    qtaguid: iface_stat: tag stat alloc failed
    ...
    kernel BUG at xt_qtaguid.c:1482!
    
    Signed-off-by: Pontus Fuchs <pontus.fuchs@gmail.com>
    Change-Id: If5f81be9491b559d35a9cd10f00c3b98ee61e134
    Reviewed-on: http://mps-gerrit.broadcom.com/32763
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Reviewed-by: Vikram Prakash <vikramp@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- net/netfilter/xt_qtaguid.c
+++ net/netfilter/xt_qtaguid.c
@@ -1461,8 +1461,9 @@ static void if_tag_stat_update(const char *ifname, uid_t uid,
 		 *  - No {0, uid_tag} stats and no {acc_tag, uid_tag} stats.
 		 */
 		new_tag_stat = create_if_tag_stat(iface_entry, uid_tag);
-		if (new_tag_stat)
-			uid_tag_counters = &new_tag_stat->counters;
+		if (!new_tag_stat)
+			goto unlock;
+		uid_tag_counters = &new_tag_stat->counters;
 	} else {
 		uid_tag_counters = &tag_stat_entry->counters;
 	}
@@ -1470,8 +1471,9 @@ static void if_tag_stat_update(const char *ifname, uid_t uid,
 	if (acct_tag) {
 		/* Create the child {acct_tag, uid_tag} and hook up parent. */
 		new_tag_stat = create_if_tag_stat(iface_entry, tag);
-		if (new_tag_stat)
-			new_tag_stat->parent_counters = uid_tag_counters;
+		if (!new_tag_stat)
+			goto unlock;
+		new_tag_stat->parent_counters = uid_tag_counters;
 	} else {
 		/*
 		 * For new_tag_stat to be still NULL here would require:
@@ -1485,6 +1487,7 @@ static void if_tag_stat_update(const char *ifname, uid_t uid,
 	}
 	if (new_tag_stat)
 		tag_stat_update(new_tag_stat, direction, proto, bytes);
+unlock:
 	spin_unlock_bh(&iface_entry->tag_stat_list_lock);
 }
 
