9bfc20869f6513dd5d9ca0ab832aaa5e73f67b15
imoseyon <imoseyon@gmail.com>
2013-01-04 13:05:20 -0800

    random: prevent add_input from doing anything

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -734,16 +734,7 @@ out:
 void add_input_randomness(unsigned int type, unsigned int code,
 				 unsigned int value)
 {
-	static unsigned char last_value;
-
-	/* ignore autorepeat and the like */
-	if (value == last_value)
-		return;
-
-	DEBUG_ENT("input event\n");
-	last_value = value;
-	add_timer_randomness(&input_timer_state,
-			     (type << 4) ^ code ^ (code >> 4) ^ value);
+	return;
 }
 EXPORT_SYMBOL_GPL(add_input_randomness);
 
