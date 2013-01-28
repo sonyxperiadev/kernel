4047c83a82451ebe0ecc57e67d9dcb9799e464b0
lishen zhang <lishen@broadcom.com>
2013-01-28 10:56:17 +0800

    Hawaii-garnet:Camera power supply assignment
    [Defect / Enhancement / New Feature]
    	MobC00276265
    
    [Problem]
    Camera power supply assignment is changed
    
    [Solution]
    Change regulators in dts
    
    [Reviewers]
    Binghu
    
    [Testing]
        [Test Cases]
    	Camera power up.
    
    [Label/CL# Synced To]
    	head of sdb-common-android-jb-4.2
    [Platform]
    	Hawaii
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: Id29b6079543b1f957bdaebb7bd03440057402c09
    Reviewed-on: http://mps-gerrit.broadcom.com/11458
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: CQ Status <cq_status_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Reviewed-by: Vinay Krishnamurthy <vinayk@broadcom.com>
    Tested-by: AutoBuild Status <autobuild_status_noreply@broadcom.com>
    Tested-by: AutoTest Status <autotest_status_noreply@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- arch/arm/mach-hawaii/board-garnet.c
+++ arch/arm/mach-hawaii/board-garnet.c
@@ -341,6 +341,8 @@ static int hawaii_camera_power(struct device *dev, int on)
 	struct clk *lp_clock;
 	struct clk *axi_clk;
 	static struct pi_mgr_dfs_node unicam_dfs_node;
+	struct soc_camera_device *icd = to_soc_camera_dev(dev);
+	struct soc_camera_link *icl = to_soc_camera_link(icd);
 
 	printk(KERN_INFO "%s:camera power %s\n", __func__, (on ? "on" : "off"));
 	if (!unicam_dfs_node.valid) {
@@ -361,13 +363,14 @@ static int hawaii_camera_power(struct device *dev, int on)
 			return -1;
 		}
 		/*MMC1 VCC */
-		d_1v8_mmc1_vcc = regulator_get(NULL, "mmc1_vcc");
+		d_1v8_mmc1_vcc = regulator_get(NULL, icl->regulators[1].supply);
 		if (IS_ERR_OR_NULL(d_1v8_mmc1_vcc))
 			printk(KERN_ERR "Failed to  get d_1v8_mmc1_vcc\n");
-		d_3v0_mmc1_vcc = regulator_get(NULL, "mmc2_vcc");
+		d_3v0_mmc1_vcc = regulator_get(NULL, icl->regulators[2].supply);
 		if (IS_ERR_OR_NULL(d_3v0_mmc1_vcc))
 			printk(KERN_ERR "Failed to  get d_3v0_mmc1_vcc\n");
-		d_gpsr_cam0_1v8 = regulator_get(NULL, "vsr_uc");
+		d_gpsr_cam0_1v8 = regulator_get(NULL,
+			icl->regulators[0].supply);
 		if (IS_ERR_OR_NULL(d_gpsr_cam0_1v8))
 			printk(KERN_ERR "Failed to  get d_gpsr_cam0_1v8\n");
 	}
@@ -492,6 +495,9 @@ static int hawaii_camera_power_front(struct device *dev, int on)
 	struct clk *lp_clock_0;
 	struct clk *lp_clock_1;
 	static struct pi_mgr_dfs_node unicam_dfs_node;
+	struct soc_camera_device *icd = to_soc_camera_dev(dev);
+	struct soc_camera_link *icl = to_soc_camera_link(icd);
+
 
 	printk(KERN_INFO "%s:camera power %s\n", __func__, (on ? "on" : "off"));
 	if (!unicam_dfs_node.valid) {
@@ -506,15 +512,13 @@ static int hawaii_camera_power_front(struct device *dev, int on)
 			printk(KERN_ERR "Unable to get CAM1PWDN\n");
 			return -1;
 		}
-
-		if (d_gpsr_cam0_1v8 == NULL){
-			d_gpsr_cam0_1v8 = regulator_get(NULL, "vsr_uc");
-			if (IS_ERR_OR_NULL(d_gpsr_cam0_1v8))
-				printk(KERN_ERR "Failed to  get d_gpsr_cam0_1v8\n");
-		}
-
+		d_lvldo2_cam1_1v8 = regulator_get(NULL,
+			icl->regulators[0].supply);
+		if (IS_ERR_OR_NULL(d_lvldo2_cam1_1v8))
+			printk(KERN_ERR "Failed to get d_lvldo2_cam1_1v8\n");
 		if (d_1v8_mmc1_vcc == NULL) {
-			d_1v8_mmc1_vcc = regulator_get(NULL, "mmc1_vcc");
+			d_1v8_mmc1_vcc = regulator_get(NULL,
+				icl->regulators[1].supply);
 			if (IS_ERR_OR_NULL(d_1v8_mmc1_vcc))
 				printk(KERN_ERR "Err d_1v8_mmc1_vcc\n");
 		}
@@ -555,7 +559,7 @@ static int hawaii_camera_power_front(struct device *dev, int on)
 			printk("DVFS for UNICAM failed\n");
 		gpio_set_value(SENSOR_1_GPIO_PWRDN, 1);
 		usleep_range(5000, 5010);
-		regulator_enable(d_gpsr_cam0_1v8);
+		regulator_enable(d_lvldo2_cam1_1v8);
 		usleep_range(1000, 1010);
 		regulator_enable(d_1v8_mmc1_vcc);
 		usleep_range(1000, 1010);
@@ -627,7 +631,7 @@ static int hawaii_camera_power_front(struct device *dev, int on)
 		clk_disable(clock);
 		clk_disable(axi_clk);
 		clk_disable(axi_clk_0);
-		regulator_disable(d_gpsr_cam0_1v8);
+		regulator_disable(d_lvldo2_cam1_1v8);
 		regulator_disable(d_1v8_mmc1_vcc);
 		if (pi_mgr_dfs_request_update
 		    (&unicam_dfs_node, PI_MGR_DFS_MIN_VALUE)) {
