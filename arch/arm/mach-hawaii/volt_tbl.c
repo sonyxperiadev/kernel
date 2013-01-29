60aedf2b1e208c0aefbf03b0a97cd18789b17aaf
sathyanr <sathyanr@broadcom.com>
2013-01-29 19:55:39 +0530

    Hawaii: Voltage table revision
    [Defect / Enhancement / New Feature]
    
    [Problem]
    New voltage tables have been given for the different silicon types.
    
    [Solution]
    Modified the existing voltage tables.
    Added debugfs interface to read the voltage tables.
    
    [Reviewers]
    Shameer
    
    [Testing]
        [Test Cases]
    
        [Label/CL# Synced To]
    
        [Platform]
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: I05444bf3a34b52176f7f45ebe70188dcbb859c6d
    Signed-off-by: sathyanr <sathyanr@broadcom.com>
    Reviewed-on: http://mps-gerrit.broadcom.com/11630
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Reviewed-by: Shameer Puthalan <shameer@broadcom.com>
    Tested-by: AutoBuild Status <autobuild_status_noreply@broadcom.com>
    Tested-by: AutoTest Status <autotest_status_noreply@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- arch/arm/mach-hawaii/volt_tbl.c
+++ arch/arm/mach-hawaii/volt_tbl.c
@@ -19,7 +19,19 @@
 #include <plat/kona_avs.h>
 #include "pm_params.h"
 
+#ifdef CONFIG_DEBUG_FS
+#include <linux/debugfs.h>
+#include <asm/uaccess.h>
+#include <linux/seq_file.h>
+#endif
+
 #define ARRAY_LIST(...) {__VA_ARGS__}
+
+#ifdef CONFIG_DEBUG_FS
+static struct dentry *dent_vlt_root_dir;
+#endif
+
+#define REV_ID	"0.8"
 
 #define MSR_RETN_ID				0x1
 #define MSR_RETN_ID_SS				0x2
@@ -28,55 +40,55 @@
 #define MSR_RETN_ID_TF				0x1
 #define MSR_RETN_ID_FF				0x1
 
-#define CSR_ECO_ID_1G_SS			0xB
+#define CSR_ECO_ID_1G_SS			0x4
 #define CSR_NM_ID_1G_SS				0xB
-#define CSR_TURBO_ID_1G_SS			0x14
-#define CSR_SUPER_TURBO_ID_1G_SS		0x28
+#define CSR_TURBO_ID_1G_SS			0x12
+#define CSR_SUPER_TURBO_ID_1G_SS		0x23
 
-#define CSR_ECO_ID_1G_TS			0xB
-#define CSR_NM_ID_1G_TS				0xB
-#define CSR_TURBO_ID_1G_TS			0x12
-#define CSR_SUPER_TURBO_ID_1G_TS		0x25
+#define CSR_ECO_ID_1G_TS			0x4
+#define CSR_NM_ID_1G_TS				0xA
+#define CSR_TURBO_ID_1G_TS			0x10
+#define CSR_SUPER_TURBO_ID_1G_TS		0x21
 
-#define CSR_ECO_ID_1G_TT			0xB
-#define CSR_NM_ID_1G_TT				0xB
-#define CSR_TURBO_ID_1G_TT			0x11
-#define CSR_SUPER_TURBO_ID_1G_TT		0x23
+#define CSR_ECO_ID_1G_TT			0x3
+#define CSR_NM_ID_1G_TT				0x8
+#define CSR_TURBO_ID_1G_TT			0xE
+#define CSR_SUPER_TURBO_ID_1G_TT		0x1E
 
-#define CSR_ECO_ID_1G_TF			0xB
-#define CSR_NM_ID_1G_TF				0xB
-#define CSR_TURBO_ID_1G_TF			0xE
-#define CSR_SUPER_TURBO_ID_1G_TF		0x1F
+#define CSR_ECO_ID_1G_TF			0x2
+#define CSR_NM_ID_1G_TF				0x7
+#define CSR_TURBO_ID_1G_TF			0xC
+#define CSR_SUPER_TURBO_ID_1G_TF		0x1C
 
-#define CSR_ECO_ID_1G_FF			0xB
-#define CSR_NM_ID_1G_FF				0xB
-#define CSR_TURBO_ID_1G_FF			0xC
-#define CSR_SUPER_TURBO_ID_1G_FF		0x1B
+#define CSR_ECO_ID_1G_FF			0x2
+#define CSR_NM_ID_1G_FF				0x4
+#define CSR_TURBO_ID_1G_FF			0x9
+#define CSR_SUPER_TURBO_ID_1G_FF		0x18
 
-#define CSR_ECO_ID_1200M_SS			0xB
-#define CSR_NM_ID_1200M_SS			0xD
-#define CSR_TURBO_ID_1200M_SS			0x18
+#define CSR_ECO_ID_1200M_SS			0x4
+#define CSR_NM_ID_1200M_SS			0x10
+#define CSR_TURBO_ID_1200M_SS			0x1A
 #define CSR_SUPER_TURBO_ID_1200M_SS		0x30
 
-#define CSR_ECO_ID_1200M_TS			0xB
-#define CSR_NM_ID_1200M_TS			0xC
-#define CSR_TURBO_ID_1200M_TS			0x16
+#define CSR_ECO_ID_1200M_TS			0x4
+#define CSR_NM_ID_1200M_TS			0xE
+#define CSR_TURBO_ID_1200M_TS			0x18
 #define CSR_SUPER_TURBO_ID_1200M_TS		0x2D
 
-#define CSR_ECO_ID_1200M_TT			0xB
+#define CSR_ECO_ID_1200M_TT			0x3
 #define CSR_NM_ID_1200M_TT			0xC
 #define CSR_TURBO_ID_1200M_TT			0x15
-#define CSR_SUPER_TURBO_ID_1200M_TT		0x2B
+#define CSR_SUPER_TURBO_ID_1200M_TT		0x29
 
-#define CSR_ECO_ID_1200M_TF			0xB
-#define CSR_NM_ID_1200M_TF			0xB
-#define CSR_TURBO_ID_1200M_TF			0x12
-#define CSR_SUPER_TURBO_ID_1200M_TF		0x27
+#define CSR_ECO_ID_1200M_TF			0x2
+#define CSR_NM_ID_1200M_TF			0xA
+#define CSR_TURBO_ID_1200M_TF			0x13
+#define CSR_SUPER_TURBO_ID_1200M_TF		0x26
 
-#define CSR_ECO_ID_1200M_FF			0xB
-#define CSR_NM_ID_1200M_FF			0xB
-#define CSR_TURBO_ID_1200M_FF			0x10
-#define CSR_SUPER_TURBO_ID_1200M_FF		0x23
+#define CSR_ECO_ID_1200M_FF			0x2
+#define CSR_NM_ID_1200M_FF			0x7
+#define CSR_TURBO_ID_1200M_FF			0xF
+#define CSR_SUPER_TURBO_ID_1200M_FF		0x22
 
 #define CSR_ECO_ID_1500M_SS			0xB
 #define CSR_NM_ID_1500M_SS			0xB
@@ -103,55 +115,57 @@
 #define CSR_TURBO_ID_1500M_FF			0x1d
 #define CSR_SUPER_TURBO_ID_1500M_FF		0x34
 
-#define MSR_ECO_ID_1G_SS			0xF
-#define MSR_NM_ID_1G_SS				0x1A
-#define MSR_TURBO_ID_1G_SS			0x28
-#define MSR_SUPER_TURBO_ID_1G_SS		0x30
+/* MSR_SUPER_TURBO for 1 GHZ is defined only for testing purposes */
 
-#define MSR_ECO_ID_1G_TS			0xF
-#define MSR_NM_ID_1G_TS				0x18
+#define MSR_ECO_ID_1G_SS			0x8
+#define MSR_NM_ID_1G_SS				0x16
+#define MSR_TURBO_ID_1G_SS			0x27
+#define MSR_SUPER_TURBO_ID_1G_SS		0x34
+
+#define MSR_ECO_ID_1G_TS			0x8
+#define MSR_NM_ID_1G_TS				0x14
 #define MSR_TURBO_ID_1G_TS			0x25
-#define MSR_SUPER_TURBO_ID_1G_TS		0x2D
+#define MSR_SUPER_TURBO_ID_1G_TS		0x32
 
-#define MSR_ECO_ID_1G_TT			0xF
-#define MSR_NM_ID_1G_TT				0x16
-#define MSR_TURBO_ID_1G_TT			0x23
-#define MSR_SUPER_TURBO_ID_1G_TT		0x2B
+#define MSR_ECO_ID_1G_TT			0x6
+#define MSR_NM_ID_1G_TT				0x11
+#define MSR_TURBO_ID_1G_TT			0x21
+#define MSR_SUPER_TURBO_ID_1G_TT		0x30
 
-#define MSR_ECO_ID_1G_TF			0xF
-#define MSR_NM_ID_1G_TF				0x14
+#define MSR_ECO_ID_1G_TF			0x5
+#define MSR_NM_ID_1G_TF				0xF
 #define MSR_TURBO_ID_1G_TF			0x1F
-#define MSR_SUPER_TURBO_ID_1G_TF		0x27
+#define MSR_SUPER_TURBO_ID_1G_TF		0x2E
 
-#define MSR_ECO_ID_1G_FF			0xF
-#define MSR_NM_ID_1G_FF				0x12
-#define MSR_TURBO_ID_1G_FF			0x1B
-#define MSR_SUPER_TURBO_ID_1G_FF		0x23
+#define MSR_ECO_ID_1G_FF			0x4
+#define MSR_NM_ID_1G_FF				0xB
+#define MSR_TURBO_ID_1G_FF			0x1A
+#define MSR_SUPER_TURBO_ID_1G_FF		0x2C
 
-#define MSR_ECO_ID_1200M_SS			0xF
-#define MSR_NM_ID_1200M_SS			0x1A
-#define MSR_TURBO_ID_1200M_SS			0x28
-#define MSR_SUPER_TURBO_ID_1200M_SS		0x30
+#define MSR_ECO_ID_1200M_SS			0x8
+#define MSR_NM_ID_1200M_SS			0x16
+#define MSR_TURBO_ID_1200M_SS			0x27
+#define MSR_SUPER_TURBO_ID_1200M_SS		0x34
 
-#define MSR_ECO_ID_1200M_TS			0xF
-#define MSR_NM_ID_1200M_TS			0x18
+#define MSR_ECO_ID_1200M_TS			0x8
+#define MSR_NM_ID_1200M_TS			0x14
 #define MSR_TURBO_ID_1200M_TS			0x25
-#define MSR_SUPER_TURBO_ID_1200M_TS		0x2D
+#define MSR_SUPER_TURBO_ID_1200M_TS		0x32
 
-#define MSR_ECO_ID_1200M_TT			0xF
-#define MSR_NM_ID_1200M_TT			0x16
-#define MSR_TURBO_ID_1200M_TT			0x23
-#define MSR_SUPER_TURBO_ID_1200M_TT		0x2B
+#define MSR_ECO_ID_1200M_TT			0x6
+#define MSR_NM_ID_1200M_TT			0x11
+#define MSR_TURBO_ID_1200M_TT			0x21
+#define MSR_SUPER_TURBO_ID_1200M_TT		0x30
 
-#define MSR_ECO_ID_1200M_TF			0xF
-#define MSR_NM_ID_1200M_TF			0x14
+#define MSR_ECO_ID_1200M_TF			0x5
+#define MSR_NM_ID_1200M_TF			0xF
 #define MSR_TURBO_ID_1200M_TF			0x1F
-#define MSR_SUPER_TURBO_ID_1200M_TF		0x27
+#define MSR_SUPER_TURBO_ID_1200M_TF		0x2E
 
-#define MSR_ECO_ID_1200M_FF			0xF
-#define MSR_NM_ID_1200M_FF			0x12
-#define MSR_TURBO_ID_1200M_FF			0x1B
-#define MSR_SUPER_TURBO_ID_1200M_FF		0x23
+#define MSR_ECO_ID_1200M_FF			0x4
+#define MSR_NM_ID_1200M_FF			0xB
+#define MSR_TURBO_ID_1200M_FF			0x1A
+#define MSR_SUPER_TURBO_ID_1200M_FF		0x2C
 
 
 #define MSR_ECO_ID_1500M_SS			0xF
@@ -378,3 +392,90 @@ u8 get_retention_vlt_id(u32 vlt_domain, u32 silicon_type)
 #endif
 	return MSR_RETN_ID_SS;
 }
+
+#ifdef CONFIG_DEBUG_FS
+static int volt_tbl_open(struct inode *inode, struct file *file)
+{
+	file->private_data = inode->i_private;
+	return 0;
+}
+
+static ssize_t read_volt_tbl_rev_id(struct file *file, char __user *user_buf,
+		size_t count, loff_t *ppos)
+{
+	u32 len = 0;
+	char debug_fs_buf[50];
+	len += snprintf(debug_fs_buf + len, sizeof(debug_fs_buf) - len,
+			"VOLTAGE TABLE REVISION ID:: %s\n", REV_ID);
+
+	return simple_read_from_buffer(user_buf, count, ppos, debug_fs_buf,
+			len);
+
+}
+
+static const struct file_operations volt_tbl_rev_id_fops = {
+	.open = volt_tbl_open,
+	.read = read_volt_tbl_rev_id,
+};
+
+static ssize_t read_volt_tbl(struct file *file, char __user *buf,
+				  size_t count, loff_t *ppos)
+{
+	u32 len = 0;
+	int freq_id = A9_FREQ_1000_MHZ;
+	int silicon_type = SILICON_TYPE_SLOW;
+	char input_str[10];
+	u8 *volt_table;
+	int i;
+	if (count > sizeof(input_str))
+		len = sizeof(input_str);
+	else
+		len = count;
+
+	if (copy_from_user(input_str, buf, len))
+		return -EFAULT;
+	sscanf(input_str, "%d%d", &silicon_type, &freq_id);
+	if ((silicon_type >= SILICON_TYPE_MAX) || (silicon_type < 0)) {
+		pr_err("%s: Invalid silicon type\n", __func__);
+		return count;
+	}
+	if (freq_id < 0 || freq_id >= A9_FREQ_MAX) {
+		pr_err("%s: Invalid freq id\n", __func__);
+		return count;
+	}
+	volt_table = (u8 *) get_sr_vlt_table(silicon_type, freq_id, NULL);
+	pr_info("Silicon Type: %d, Freq Id: %d", silicon_type, freq_id);
+	for (i = 0; i < 0x10; i++)
+		pr_info("[%x] = %x ", i, volt_table[i]);
+
+	return count;
+}
+
+static const struct file_operations volt_tbl_fops = {
+	.open = volt_tbl_open,
+	.write = read_volt_tbl,
+};
+#endif
+
+static int volt_tbl_init(void)
+{
+	pr_info("Voltage Table Init\n");
+#ifdef CONFIG_DEBUG_FS
+	dent_vlt_root_dir = debugfs_create_dir("voltage_table", 0);
+	if (!dent_vlt_root_dir)
+		return -ENOMEM;
+
+	if (!debugfs_create_file
+	    ("volt_tbl_rev_id", S_IRUGO, dent_vlt_root_dir, NULL,
+	     &volt_tbl_rev_id_fops))
+		return -ENOMEM;
+
+	if (!debugfs_create_file
+	    ("volt_tbl", S_IWUSR, dent_vlt_root_dir, NULL,
+	     &volt_tbl_fops))
+		return -ENOMEM;
+#endif
+	return 0;
+}
+
+late_initcall(volt_tbl_init);
