024ccd42e2731416ec92668b35a34a9bd5339d46
Vinay Krishnamurthy <vinayk@broadcom.com>
2013-02-11 10:17:09 +0530

    Revert "sensors: set proper permission to device files"
    This reverts commit 66407f976a93dd3e6f040dd3f1d8adaf306f09c4.
    
    Change-Id: I422998e4bc719eaad0ef6f10522f9f7fd6e4c6aa
    Reviewed-on: http://mps-gerrit.broadcom.com/12694
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Vikram Prakash <vikramp@broadcom.com>
    Tested-by: AutoBuild Status <autobuild_status_noreply@broadcom.com>
    Tested-by: AutoTest Status <autotest_status_noreply@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- drivers/staging/iio/industrialio-trigger.c
+++ drivers/staging/iio/industrialio-trigger.c
@@ -367,7 +367,7 @@ static ssize_t iio_trigger_write_current(struct device *dev,
 	return len;
 }
 
-static DEVICE_ATTR(current_trigger, S_IRUGO | S_IWUSR,
+static DEVICE_ATTR(current_trigger, S_IRUGO | S_IWUGO,
 		   iio_trigger_read_current,
 		   iio_trigger_write_current);
 
