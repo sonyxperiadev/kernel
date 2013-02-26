1830a93525536e02b28eca430f16f64d315b9c32
anjanavk <anjanak@broadcom.com>
2013-02-26 18:31:59 +0530

    FIX: Warnings in staging/iio.
    [Defect / Enhancement / New Feature]
    
    [Problem]
    use of EXPORT_SYMBOL_GPL was raising warnings.
    module.h was not included.
    
    [Solution]
    included module.h
    
    [Reviewers]
    Vinayak, Vikram, Naga
    
    [Testing]
        [Test Cases]
        Checked sensors using Broadcom Sensor Detect apk.
    
        [Label/CL# Synced To]
    
        [Platform]
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: Ia2090bf7e2a8c9c7f8a6b21ed9cbf4d50a6370c2
    Signed-off-by: anjanavk <anjanak@broadcom.com>
    Reviewed-on: http://mps-gerrit.broadcom.com/14178
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Reviewed-by: Dmitry Shmulevich <dshmulev@broadcom.com>
    Reviewed-by: Vinayak Menon <vinayak.menon@broadcom.com>
    Reviewed-by: Oza (Pawandeep) Oza <oza@broadcom.com>
    Reviewed-by: Naga Chandra Prahalada Rao <nagachandra@broadcom.com>
    Tested-by: AutoBuild Status <autobuild_status_noreply@broadcom.com>
    Tested-by: AutoTest Status <autotest_status_noreply@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- drivers/staging/iio/industrialio-buffer.c
+++ drivers/staging/iio/industrialio-buffer.c
@@ -14,7 +14,7 @@
  * - Alternative access techniques?
  */
 #include <linux/kernel.h>
-//#include <linux/export.h>
+#include <linux/module.h>
 #include <linux/device.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
