4622f6413e4a9b5aa317d6e0df563e2fce169bb6
imoseyon <imoseyon@gmail.com>
2013-03-17 17:53:33 -0700

    random: remove warning

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -660,8 +660,6 @@ void add_device_randomness(const void *buf, unsigned int size)
 }
 EXPORT_SYMBOL(add_device_randomness);
 
-static struct timer_rand_state input_timer_state;
-
 /*
  * This function adds entropy to the entropy "pool" by using timing
  * delays.  It uses the timer_rand_state structure to make an estimate
