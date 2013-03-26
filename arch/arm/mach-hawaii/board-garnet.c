a9978c9eb64a2b1422059451ccd8f283e49fc789
Raghu M J <raghumj@broadcom.com>
2013-03-26 18:13:09 +0530

    Camera: Prevent backdrive from 2.8V supply
    [Defect / Enhancement / New Feature]
    MobC00296792
    
    [Problem]
    On Garnet EDN010 when using the view finder with
    the CAM0 sensor (back camera), I am measuring
    -14mA backdrive current through CAM1_DOVDD even
    though this supply is turned off. We need to have
    both CAM0_DOVDD and CAM1_DOVDD turned on when
    either of the cameras is turned on. This is
    because we are sharing the 2.8V analog supply and
    when the 2.8V supply is on, it powers both
    sensors. We can't power a camera sensor with the
    2.8V supply and not power the 1.8V supply.
    
    [Solution]
    Turn on both CAM0_DOVDD and CAM1_DOVDD when either
    CAM0 or CAM1 is used to prevent backdriving from
    2.8V supply
    
    [Reviewers]
    Vinay K
    
    [Testing]
        [Test Cases]
        Camera sanity
    
        [Label/CL# Synced To]
        sdb-common-android-jb-4.2.2
    
        [Platform]
        Hawaii
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: I496a2996fce0d77447f2196d09233e8980e58259
    Signed-off-by: Raghu M J <raghumj@broadcom.com>
    Reviewed-on: http://mps-gerrit.broadcom.com/16880
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: Manoj Arvind <manoja@broadcom.com>
    Tested-by: AutoBuild Status <autobuild_status_noreply@broadcom.com>
    Tested-by: AutoTest Status <autotest_status_noreply@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- arch/arm/mach-hawaii/board-garnet.c
+++ arch/arm/mach-hawaii/board-garnet.c
@@ -365,6 +365,12 @@ static int hawaii_camera_power(struct device *dev, int on)
 			icl->regulators[0].supply);
 		if (IS_ERR_OR_NULL(d_gpsr_cam0_1v8))
 			printk(KERN_ERR "Failed to  get d_gpsr_cam0_1v8\n");
+		if (d_lvldo2_cam1_1v8 == NULL) {
+			d_lvldo2_cam1_1v8 = regulator_get(NULL,
+			icl->regulators[3].supply);
+			if (IS_ERR_OR_NULL(d_lvldo2_cam1_1v8))
+				printk(KERN_ERR "Fd_lvldo2_cam1_1v8 cam\n");
+		}
 	}
 
 	ret = -1;
@@ -393,6 +399,8 @@ static int hawaii_camera_power(struct device *dev, int on)
 		regulator_enable(d_3v0_mmc1_vcc);
 		usleep_range(1000, 1010);
 		regulator_enable(d_1v8_mmc1_vcc);
+		usleep_range(1000, 1010);
+		regulator_enable(d_lvldo2_cam1_1v8);
 		usleep_range(1000, 1010);
 
 		if (mm_ccu_set_pll_select(CSI0_BYTE1_PLL, 8)) {
@@ -448,6 +456,7 @@ static int hawaii_camera_power(struct device *dev, int on)
 		clk_disable(clock);
 		clk_disable(lp_clock);
 		clk_disable(axi_clk);
+		regulator_disable(d_lvldo2_cam1_1v8);
 		regulator_disable(d_3v0_mmc1_vcc);
 		regulator_disable(d_1v8_mmc1_vcc);
 		regulator_disable(d_gpsr_cam0_1v8);
@@ -514,6 +523,19 @@ static int hawaii_camera_power_front(struct device *dev, int on)
 			if (IS_ERR_OR_NULL(d_1v8_mmc1_vcc))
 				printk(KERN_ERR "Err d_1v8_mmc1_vcc\n");
 		}
+		if (d_3v0_mmc1_vcc == NULL) {
+			d_3v0_mmc1_vcc = regulator_get(NULL,
+			icl->regulators[2].supply);
+			if (IS_ERR_OR_NULL(d_3v0_mmc1_vcc))
+				printk(KERN_ERR "d_3v0_mmc1_vcc");
+		}
+		if (d_gpsr_cam0_1v8 == NULL) {
+			d_gpsr_cam0_1v8 = regulator_get(NULL,
+			icl->regulators[3].supply);
+			if (IS_ERR_OR_NULL(d_gpsr_cam0_1v8))
+				printk(KERN_ERR "Fl d_gpsr_cam0_1v8 get	fail");
+		}
+
 	}
 
 	ret = -1;
@@ -554,6 +576,11 @@ static int hawaii_camera_power_front(struct device *dev, int on)
 		regulator_enable(d_lvldo2_cam1_1v8);
 		usleep_range(1000, 1010);
 		regulator_enable(d_1v8_mmc1_vcc);
+		usleep_range(1000, 1010);
+		/* Secondary cam addition */
+		regulator_enable(d_gpsr_cam0_1v8);
+		usleep_range(1000, 1010);
+		regulator_enable(d_3v0_mmc1_vcc);
 		usleep_range(1000, 1010);
 
 		if (mm_ccu_set_pll_select(CSI1_BYTE1_PLL, 8)) {
@@ -625,6 +652,8 @@ static int hawaii_camera_power_front(struct device *dev, int on)
 		clk_disable(axi_clk_0);
 		regulator_disable(d_lvldo2_cam1_1v8);
 		regulator_disable(d_1v8_mmc1_vcc);
+		regulator_disable(d_gpsr_cam0_1v8);
+		regulator_disable(d_3v0_mmc1_vcc);
 		if (pi_mgr_dfs_request_update
 		    (&unicam_dfs_node, PI_MGR_DFS_MIN_VALUE)) {
 			printk("Failed to set DVFS for unicam\n");
