66fd71db4b79f4d55ca347796153c771947cfc83
u_sinha <u_sinha@broadcom.com>
2013-03-29 15:27:31 -0700

    BT: Include board_bcmbt_lpm_device in device init
    [Defect / Enhancement / New Feature]
    MobC00299001 Removing board_bcmbt_lpm_device
                 causes build failure for variant:
                 android_hawaii_edn01x_nocon.
    
    [Problem]
    Removing board_bcmbt_lpm_device causes build
    failure for variant: android_hawaii_edn01x_nocon.
    Also need to re-verify two times platform device
    registration as previously reported.
    
    [Solution]
    Revert the change made by previous commit in
    board-hawaii.c and board-garnet.c back to:
    
           &board_bcmbt_lpm_device,
    
    [Reviewers]
    Dheeresh, Min
    
    [Testing]
        [Test Cases]
        Build verified.
    
    [Label/CL# Synced To]
        HEAD sdb-common-android-jb-4.2.2
    
    [Platform]
        HawaiiGarnet edn010
    
    Change-Id: I72acfc9711ca0e974f2f62ee1b9b9014f878e1f1
    Reviewed-on: http://mps-gerrit.broadcom.com/17413
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: CQ Status <cq_status_noreply@broadcom.com>
    Reviewed-by: Min Kim <minkim@broadcom.com>
    Tested-by: AutoBuild Status <autobuild_status_noreply@broadcom.com>
    Tested-by: AutoTest Status <autotest_status_noreply@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- arch/arm/mach-hawaii/board-garnet.c
+++ arch/arm/mach-hawaii/board-garnet.c
@@ -1114,7 +1114,7 @@ static struct platform_device *hawaii_devices[] __initdata = {
 	&haptic_pwm_device,
 #endif
 
-#ifndef CONFIG_BCM_BT_LPM
+#ifdef CONFIG_BCM_BT_LPM
 	&board_bcmbt_lpm_device,
 #endif
 
