01f421b6658c2d0be7287a0aa01542a4105a9763
Dongkai Huang <dongkaih@broadcom.com>
2013-04-01 16:18:32 +0800

    BCM4330: Fix FW hung during suspend and resume issue
    [Defect / Enhancement / New Feature]
    MobC00296272
    
    [Problem]
    WiFi FW heng up, only re-open wifi can repair this issue
    
    [Solution]
    update correct register set for 4330:
    
    bcmsdh_reg_write (bus->sdh, 0x18000620, 2, 10);
    bcmsdh_reg_write(bus->sdh, 0x18000628, 2, 0x0000ff01);
    
    [Reviewers]
    
    [Testing]
        [Test Cases]
    
        [Label/CL# Synced To]
    
        [Platform]
    	Hawaii
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: Ibc0ffe494ff5ca3d8b1563954f8d785a360b196f
    Reviewed-on: http://mps-gerrit.broadcom.com/17521
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Heng Luo <hengl@broadcom.com>
    Reviewed-by: Partha Mukherjee <pmuk@broadcom.com>
    Reviewed-by: Vikram Prakash <vikramp@broadcom.com>
    Tested-by: AutoBuild Status <autobuild_status_noreply@broadcom.com>
    Tested-by: AutoTest Status <autotest_status_noreply@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- drivers/net/wireless/bcmdhd_4330/dhd_sdio.c
+++ drivers/net/wireless/bcmdhd_4330/dhd_sdio.c
@@ -6868,8 +6868,8 @@ dhdsdio_probe(uint16 venid, uint16 devid, uint16 bus_no, uint16 slot,
 	}
 
 #if defined(CUSTOMER_HW4) && defined(BCMHOST_XTAL_PU_TIME_MOD)
-	bcmsdh_reg_write(bus->sdh, 0x18000620, 2, 11);
-	bcmsdh_reg_write(bus->sdh, 0x18000628, 4, 0x00F80001);
+	bcmsdh_reg_write(bus->sdh, 0x18000620, 2, 10);
+	bcmsdh_reg_write(bus->sdh, 0x18000628, 2, 0x0000FF01);
 #endif
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 25))
