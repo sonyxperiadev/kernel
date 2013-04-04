973aea425cabf22b0f0ab178b5085c875c85f250
satyavani <mutyalas@broadcom.com>
2013-04-04 15:47:09 +0530

    Hawaii - ISP Cleanup
        [Defect / Enhancement / New Feature]
        MobC00301195
    
        [Problem]
        ISP cleanup
    
        [Solution]
        Move all the ISP configuration from kenel space to User space
    
        [Reviewers]
        KaliCharan
        Vinay K
    
        [Testing]
            [Test Cases]
            Boot the device
            Verify AudioVideo Playback
            Verify Camera Capture/Camcoder recording
    
            [Label/CL# Synced To]
            sdb-common-android-jb-4.2.2
    
            [Platform]
            HawaiiGarnet
    
        [BROADCOM INTERNAL NOTES]
    
    Change-Id: Ibcff057e6f9b5f2dd9eac345cc84622d0428e398
    Reviewed-on: http://mps-gerrit.broadcom.com/17939
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: CQ Status <cq_status_noreply@broadcom.com>
    Reviewed-by: Vinay Krishnamurthy <vinayk@broadcom.com>
    Tested-by: AutoBuild Status <autobuild_status_noreply@broadcom.com>
    Tested-by: AutoTest Status <autotest_status_noreply@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- drivers/char/broadcom/mm/isp/isp.c
+++ drivers/char/broadcom/mm/isp/isp.c
@@ -35,7 +35,6 @@ the GPL, without Broadcom's express prior written consent.
 #include <mach/rdb/brcm_rdb_sysmap.h>
 #include <mach/rdb/brcm_rdb_pwrmgr.h>
 #include <mach/rdb/brcm_rdb_mm_rst_mgr_reg.h>
-#include <mach/rdb/brcm_rdb_isp.h>
 #include <linux/broadcom/mm_fw_hw_ifc.h>
 #include <linux/broadcom/mm_fw_usr_ifc.h>
 
@@ -57,53 +56,52 @@ void printispregs(struct isp_device_t *isp)
 {
 	pr_err("ISP_STATUS = 0x%lx\n",
 		(unsigned long)(isp_read(ISP_STATUS_OFFSET)));
-	pr_err("ISP_TILE_STATUS = 0x%lx\n",
-		(unsigned long) (isp_read(ISP_TILE_STATUS_OFFSET)));
-	pr_err("ispid = 0x%lx\n",
-		(unsigned long)(isp_read(ISP_ID_OFFSET)));
-	pr_err("ISP_TILE_CTRL = 0x%lx\n",
-		(unsigned long)(isp_read(ISP_TILE_CTRL_OFFSET)));
-	pr_err("ISP_TILE_ADDR = 0x%lx\n",
-		(unsigned long)(isp_read(ISP_TILE_ADDR_OFFSET)));
-	pr_err("ISPFR_SIZE = 0x%lx\n",
-		(unsigned long)(isp_read(ISPFR_SIZE_OFFSET)));
-	pr_err("ISPFR_CTRL = 0x%lx\n",
-		(unsigned long)(isp_read(ISPFR_CTRL_OFFSET)));
-	pr_err("ISPFR_BAYER_EN = 0x%lx\n",
-		(unsigned long)(isp_read(ISPFR_BAYER_EN_OFFSET)));
-	pr_err("ISPFR_YCBCR_EN = 0x%lx\n",
-		(unsigned long)(isp_read(ISPFR_YCBCR_EN_OFFSET)));
-	pr_err("ISPLO_CTRL = 0x%lx\n",
-		(unsigned long)(isp_read(ISPLO_CTRL_OFFSET)));
-	pr_err("ISPLO_ADDRESS1 = 0x%lx\n",
-		(unsigned long)(isp_read(ISPLO_ADDRESS1_OFFSET)));
-	pr_err("ISPLO_ADDRESS2 = 0x%lx\n",
-		(unsigned long)(isp_read(ISPLO_ADDRESS2_OFFSET)));
-	pr_err("ISPLO_ADDRESS3 = 0x%lx\n",
-		(unsigned long)(isp_read(ISPLO_ADDRESS3_OFFSET)));
-	pr_err("ISPLO_STRIDE1 = 0x%lx\n",
-		(unsigned long)(isp_read(ISPLO_STRIDE1_OFFSET)));
-	pr_err("ISPLO_STRIDE2 = 0x%lx\n",
-		(unsigned long)(isp_read(ISPLO_STRIDE2_OFFSET)));
-	pr_err("ISPYI_CTRL = 0x%lx\n",
-		(unsigned long)(isp_read(ISPYI_CTRL_OFFSET)));
-	pr_err("ISPYI_RY_ADDR = 0x%lx\n",
-		(unsigned long)(isp_read(ISPYI_RY_ADDR_OFFSET)));
-	pr_err("ISPYI_GU_ADDR = 0x%lx\n",
-		(unsigned long)(isp_read(ISPYI_GU_ADDR_OFFSET)));
-	pr_err("ISPYI_BV_ADDR = 0x%lx\n",
-		(unsigned long)(isp_read(ISPYI_BV_ADDR_OFFSET)));
-	pr_err("ISPYI_STRIDE1 = 0x%lx\n",
-		(unsigned long)(isp_read(ISPYI_STRIDE1_OFFSET)));
-	pr_err("ISPYI_STRIDE2 = 0x%lx\n",
-		(unsigned long)(isp_read(ISPYI_STRIDE2_OFFSET)));
-	pr_err("ISPYI_RY_EADDR = 0x%lx\n",
-		(unsigned long)(isp_read(ISPYI_RY_EADDR_OFFSET)));
-	pr_err("ISPYI_GU_EADDR = 0x%lx\n",
-		(unsigned long)(isp_read(ISPYI_GU_EADDR_OFFSET)));
-	pr_err("ISPYI_BV_EADDR = 0x%lx\n",
-		(unsigned long)(isp_read(ISPYI_BV_EADDR_OFFSET)));
-
+	pr_err("offset = 0x00000018 value = 0x%lx\n",
+		(unsigned long) (isp_read(0x00000018)));
+	pr_err("offset = 0x00000008 value = 0x%lx\n",
+		(unsigned long)(isp_read(0x00000008)));
+	pr_err("offset = 0x00000010 value = 0x%lx\n",
+		(unsigned long)(isp_read(0x00000010)));
+	pr_err("offset = 0x0000001C value = 0x%lx\n",
+		(unsigned long)(isp_read(0x0000001C)));
+	pr_err("offset = 0x00000030 value = 0x%lx\n",
+		(unsigned long)(isp_read(0x00000030)));
+	pr_err("offset = 0x00000034 value = 0x%lx\n",
+		(unsigned long)(isp_read(0x00000034)));
+	pr_err("offset = 0x00000038 value = 0x%lx\n",
+		(unsigned long)(isp_read(0x00000038)));
+	pr_err("offset = 0x0000003C value = 0x%lx\n",
+		(unsigned long)(isp_read(0x0000003C)));
+	pr_err("offset = 0x00000DC0 value = 0x%lx\n",
+		(unsigned long)(isp_read(0x00000DC0)));
+	pr_err("offset = 0x00000DCC value = 0x%lx\n",
+		(unsigned long)(isp_read(0x00000DCC)));
+	pr_err("offset = 0x00000DD0 value = 0x%lx\n",
+		(unsigned long)(isp_read(0x00000DD0)));
+	pr_err("offset = 0x00000DD4 value = 0x%lx\n",
+		(unsigned long)(isp_read(0x00000DD4)));
+	pr_err("offset = 0x00000DD8 value = 0x%lx\n",
+		(unsigned long)(isp_read(0x00000DD8)));
+	pr_err("offset = 0x00000DDC value = 0x%lx\n",
+		(unsigned long)(isp_read(0x00000DDC)));
+	pr_err("offset = 0x00000700 value = 0x%lx\n",
+		(unsigned long)(isp_read(0x00000700)));
+	pr_err("offset = 0X00000704 value = 0x%lx\n",
+		(unsigned long)(isp_read(0X00000704)));
+	pr_err("offset = 0X00000708 value = 0x%lx\n",
+		(unsigned long)(isp_read(0X00000708)));
+	pr_err("offset = 0X0000070C value = 0x%lx\n",
+		(unsigned long)(isp_read(0X0000070C)));
+	pr_err("offset = 0X00000710 value = 0x%lx\n",
+		(unsigned long)(isp_read(0X00000710)));
+	pr_err("offset = 0X00000714 value = 0x%lx\n",
+		(unsigned long)(isp_read(0X00000714)));
+	pr_err("offset = 0X00000720 value = 0x%lx\n",
+		(unsigned long)(isp_read(0X00000720)));
+	pr_err("offset = 0X00000724 value = 0x%lx\n",
+		(unsigned long)(isp_read(0X00000724)));
+	pr_err("offset = 0X00000728 value = 0x%lx\n",
+		(unsigned long)(isp_read(0X00000728)));
 	return;
 }
 
@@ -136,246 +134,6 @@ static bool get_isp_status(void *id)
 	if (ispStatus & 0x1)
 		return true;
 	return false;
-}
-
-int isp_write_frame(struct isp_device_t *isp,
-		struct ISP_FRAME_PARAMS_T *pFrameParam)
-{
-
-	/* uint32_t mosaic_patterns[] =
-		{0xDD22DD22, 0x22DD22DD, 0x88778877, 0x77887788}; */
-
-	uint32_t val = (((uint32_t)pFrameParam->height) << 16) +
-			(uint32_t)pFrameParam->width;
-	isp_write(ISPFR_SIZE_OFFSET, val);
-
-	/* clear ISPFR_CTRL register */
-	isp_write(ISPFR_CTRL_OFFSET, 0);
-	isp_clr_bit32(ISPFR_CTRL_OFFSET, ISPFR_CTRL_BAYER_ORDER_MASK);
-	isp_clr_bit32(ISPFR_CTRL_OFFSET, ISPFR_CTRL_TRANSPOSED_MASK);
-	isp_clr_bit32(ISPFR_CTRL_OFFSET, ISPFR_CTRL_SW_OUT_POS_MASK);
-	isp_clr_bit32(ISPFR_CTRL_OFFSET, ISPFR_CTRL_SW_IN_POS_MASK);
-
-	isp_write(ISPFR_BAYER_EN_OFFSET, pFrameParam->bayer_en);
-	isp_write(ISPFR_YCBCR_EN_OFFSET, pFrameParam->ycbcr_en);
-
-	/* isp_write( ISPFR_MOSAIC_OFFSET,
-			mosaic_patterns[pFrameParam->bayer_order]); */
-
-	return 0;
-}
-
-int isp_write_tile_offsets(struct isp_device_t *isp,
-			struct ISP_TILE_OFFSET_PARAMS_T *tile_offsets)
-{
-	isp_clr_bit32(ISPFR_OFF0_OFFSET, ISPFR_OFF0_DM_MASK);
-	isp_clr_bit32(ISPFR_OFF0_OFFSET, ISPFR_OFF0_XC_MASK);
-	isp_clr_bit32(ISPFR_OFF0_OFFSET, ISPFR_OFF0_RS_MASK);
-	isp_clr_bit32(ISPFR_OFF0_OFFSET, ISPFR_OFF0_DP_MASK);
-	isp_clr_bit32(ISPFR_OFF1_OFFSET, ISPFR_OFF1_ST_MASK);
-	isp_clr_bit32(ISPFR_OFF1_OFFSET, ISPFR_OFF1_DN_MASK);
-	isp_clr_bit32(ISPFR_OFF1_OFFSET, ISPFR_OFF1_BL_MASK);
-	isp_clr_bit32(ISPFR_OFF1_OFFSET, ISPFR_OFF1_LS_MASK);
-	isp_clr_bit32(ISPFR_OFF2_OFFSET, ISPFR_OFF2_HRO_MASK);
-	isp_clr_bit32(ISPFR_OFF2_OFFSET, ISPFR_OFF2_FCSH_MASK);
-	isp_clr_bit32(ISPFR_OFF2_OFFSET, ISPFR_OFF2_GD_MASK);
-	isp_clr_bit32(ISPFR_SWOFF_OFFSET, ISPFR_SWOFF_SW_IN_MASK);
-	isp_clr_bit32(ISPFR_SWOFF_OFFSET, ISPFR_SWOFF_SW_OUT_MASK);
-
-	return 0;
-}
-
-int isp_write_y_g_coeffs(struct isp_device_t *isp,
-			struct ISP_Y_G_COEFFS_PARAMS_T *pYGCoeffsParam)
-{
-	int ret = 0;
-	/* Clear registers */
-	isp_write(ISPYG_Y_OFFSET, 0);
-	isp_write(ISPYG_OFFSET1_OFFSET, 0);
-	isp_write(ISPYG_OFFSET2_OFFSET, 0);
-	isp_write(ISPYG_SCALE_OFFSET, 0);
-
-	isp_write_bit32(ISPYG_Y_OFFSET, (SINT4P12(0.114)));
-	isp_write_bit32(ISPYG_Y_OFFSET,
-			((SINT4P12(0.3))<<ISPYG_Y_COEFF1_SHIFT));
-	isp_write_bit32(ISPYG_OFFSET1_OFFSET, 1024);
-	isp_write_bit32(ISPYG_OFFSET2_OFFSET, 1024);
-	isp_write_bit32(ISPYG_SCALE_OFFSET,
-			(UINT4P12(0.59)<<ISPYG_SCALE_U_SHIFT));
-	isp_write_bit32(ISPYG_SCALE_OFFSET,
-			(UINT4P12(0.7)<<ISPYG_SCALE_V_SHIFT));
-	return ret;
-}
-
-int isp_write_yuv_input(struct isp_device_t *isp,
-			struct ISP_YUV_INPUT_PARAMS_T *pYUVInputParam)
-{
-	/*Clear ISPYI_CTRL register */
-	isp_write(ISPYI_CTRL_OFFSET, 0);
-
-	/* NO need as col_width_isp will be written */
-	isp_write_bit32(ISPYI_CTRL_OFFSET,
-		(pYUVInputParam->col_width<<ISPYI_CTRL_COL_WIDTH_SHIFT));
-	isp_write_bit32(ISPYI_CTRL_OFFSET,
-		(pYUVInputParam->col_mode<<ISPYI_CTRL_COL_MODE_SHIFT));
-	isp_write_bit32(ISPYI_CTRL_OFFSET,
-		(pYUVInputParam->cc_order<<ISPYI_CTRL_CC_ORDER_SHIFT));
-	isp_write_bit32(ISPYI_CTRL_OFFSET,
-		(pYUVInputParam->data_format<<ISPYI_CTRL_DATA_FORMAT_SHIFT));
-	isp_clr_bit32(ISPYI_CTRL_OFFSET, ISPYI_CTRL_BYTES_SAMPLE_MASK);
-	isp_write_bit32(ISPYI_CTRL_OFFSET, pYUVInputParam->cs_format);
-	isp_write(ISPYI_RY_ADDR_OFFSET, (uint32_t)(pYUVInputParam->ry_address));
-	isp_write(ISPYI_GU_ADDR_OFFSET, (uint32_t)(pYUVInputParam->gu_address));
-	isp_write(ISPYI_BV_ADDR_OFFSET, (uint32_t)(pYUVInputParam->bv_address));
-	isp_write(ISPYI_STRIDE1_OFFSET, pYUVInputParam->y_stride);
-	isp_write(ISPYI_STRIDE2_OFFSET, pYUVInputParam->uv_stride);
-	isp_write(ISPYI_COL_STRIDE1_OFFSET, pYUVInputParam->col_stride1);
-	isp_write(ISPYI_COL_STRIDE2_OFFSET, pYUVInputParam->col_stride2);
-	isp_write(ISPYI_RY_EADDR_OFFSET,
-			(uint32_t)(pYUVInputParam->ry_end_address));
-	isp_write(ISPYI_GU_EADDR_OFFSET,
-			(uint32_t)(pYUVInputParam->gu_end_address));
-	isp_write(ISPYI_BV_EADDR_OFFSET,
-			(uint32_t)(pYUVInputParam->bv_end_address));
-
-	return 0;
-}
-
-int isp_write_lresize(struct isp_device_t *isp,
-		struct ISP_LRESIZE_PARAMS_T *pLresizeParam)
-{
-	uint32_t norm;
-
-	/* Clear registers */
-	isp_write(ISPLR_TSCALEX_OFFSET, 0);
-	isp_write(ISPLR_TSCALEY_OFFSET, 0);
-
-	isp_write_bit32(ISPLR_TSCALEX_OFFSET,
-		(pLresizeParam->tscale_x<<ISPLR_TSCALEX_SCALE_FRAC_SHIFT));
-	isp_write_bit32(ISPLR_TSCALEY_OFFSET,
-		(pLresizeParam->tscale_y<<ISPLR_TSCALEY_SCALE_FRAC_SHIFT));
-	isp_write_bit32(ISPLR_TSCALEX_OFFSET,
-		((pLresizeParam->tscale_x>>12)<<ISPLR_TSCALEX_SCALE_INT_SHIFT));
-	isp_write_bit32(ISPLR_TSCALEY_OFFSET,
-		((pLresizeParam->tscale_y>>12)<<ISPLR_TSCALEY_SCALE_INT_SHIFT));
-
-	norm = (pLresizeParam->tnorm_0) | (pLresizeParam->tnorm_1 << 16);
-	isp_write(ISPLR_NORM_0_1_OFFSET, norm);
-	norm = (pLresizeParam->tnorm_2) | (pLresizeParam->tnorm_3 << 16);
-	isp_write(ISPLR_NORM_2_3_OFFSET, norm);
-
-	if (pLresizeParam->rb_swap) {
-		isp_write(ISPLR_SHIFT_OFFSET,
-			((uint32_t)pLresizeParam->shift | (1<<8)));
-	} else
-		isp_write(ISPLR_SHIFT_OFFSET, pLresizeParam->shift);
-
-	return 0;
-}
-
-int isp_write_lr_shift(struct isp_device_t *isp,
-		struct ISP_LRESIZE_PARAMS_T *pLresizeParam)
-{
-	if (pLresizeParam->rb_swap) {
-		isp_write(ISPLR_SHIFT_OFFSET,
-			((uint32_t)pLresizeParam->shift | (1<<8)));
-	} else
-		isp_write(ISPLR_SHIFT_OFFSET, pLresizeParam->shift);
-	return 0;
-}
-
-int isp_write_lo_res_output(struct isp_device_t *isp,
-			struct ISP_OUTPUT_PARAMS_T *pOutputParam)
-{
-	isp_write(ISPLO_CTRL_OFFSET, 0);
-
-	isp_clr_bit32(ISPLO_CTRL_OFFSET, ISPLO_CTRL_OUTPUT_WIDTH_MASK);
-	isp_write_bit32(ISPLO_CTRL_OFFSET,
-			((pOutputParam->data_format&0x7) <<
-				ISPLO_CTRL_DATA_FORMAT_SHIFT));
-	isp_write_bit32(ISPLO_CTRL_OFFSET,
-			(((pOutputParam->data_format&0x8)>>3) <<
-				ISPLO_CTRL_DATA_FORMAT_BIT_4_SHIFT));
-	isp_write_bit32(ISPLO_CTRL_OFFSET,
-		(pOutputParam->col_mode<<ISPLO_CTRL_COL_MODE_SHIFT));
-	isp_clr_bit32(ISPLO_CTRL_OFFSET, ISPLO_CTRL_VFLIP_MASK);
-	isp_write_bit32(ISPLO_CTRL_OFFSET,
-		(pOutputParam->shift<<ISPLO_CTRL_SHIFT_SHIFT));
-	isp_write_bit32(ISPLO_CTRL_OFFSET,
-		(pOutputParam->col_width<<ISPLO_CTRL_COL_WIDTH_SHIFT));
-	isp_write(ISPLO_COL_STRIDE1_OFFSET, pOutputParam->col_stride1);
-	isp_write(ISPLO_COL_STRIDE2_OFFSET, pOutputParam->col_stride2);
-	isp_write(ISPLO_ADDRESS1_OFFSET, (uint32_t)pOutputParam->address1);
-	isp_write(ISPLO_ADDRESS2_OFFSET, (uint32_t)pOutputParam->address2);
-	isp_write(ISPLO_ADDRESS3_OFFSET, (uint32_t)pOutputParam->address3);
-	isp_write(ISPLO_STRIDE1_OFFSET, pOutputParam->stride1);
-	isp_write(ISPLO_STRIDE2_OFFSET, pOutputParam->stride2);
-
-	return 0;
-}
-
-int isp_write_conversion(struct isp_device_t *isp, uint32_t informat,
-				uint32_t outformat)
-{
-	int ret = 0;
-	int i, j;
-
-	struct ISP_CONVERSION_PARAMS_T yuv_conversion;
-	int16_t matrix_t[3][3] = {{0x128F, 0, 0x1970},
-			{0x128F, 0xF9C3, 0xF30B}, {0x128F, 0x2028, 0} };
-	int32_t offsets_t[3] = {-222*8, 150*8, -276*8};
-
-	if ((informat < 4) || (outformat > 3))
-		return 0;
-
-	/* Coeff Need to tuned */
-	/* yuv_conversion.matrix = {{TRUNC12(1.16), TRUNC12(0),	TRUNC12(1.59)},
-			  {TRUNC12(1.16), TRUNC12(-0.39), TRUNC12(-0.81)},
-			  {TRUNC12(1.16),  TRUNC12(2.01),	TRUNC12(0)}};*/
-
-	for (i = 0; i < 3; i++)
-		for (j = 0; j < 3; j++)
-			yuv_conversion.matrix[i][j] = matrix_t[i][j];
-
-	for (i = 0; i < 3; i++)
-		yuv_conversion.offsets[i] = offsets_t[i];
-
-	for (i = 0; i < 5; i++)	{
-		pr_debug("%s: yuv_conversion.matrix[%d] = 0x%x\n", __func__,
-				i, (((uint32_t *)yuv_conversion.matrix)[i]));
-		isp_write((ISPYC_MATRIX_OFFSET+(sizeof(uint32_t)*i)),
-				(((uint32_t *)yuv_conversion.matrix)[i]));
-	}
-
-	for (i = 0; i < 3; i++)	{
-		pr_debug("%s: yuv_conversion.offset[%d] = 0x%x", __func__,
-				i, ((uint32_t *)yuv_conversion.offsets)[i]);
-		isp_write((ISPYC_OFFSET_OFFSET+(sizeof(u32)*i)),
-				(((uint32_t *)yuv_conversion.offsets)[i]));
-	}
-
-	return ret;
-
-}
-
-int isp_write_hresize(struct isp_device_t *isp,
-		struct ISP_HRESIZE_PARAMS_T *pHresizeParam)
-{
-	isp_write(ISPHR_CTRL_OFFSET, pHresizeParam->interp_filter_mode);
-	isp_write_bit32(ISPHR_SCALE_X_OFFSET,
-		pHresizeParam->wscalef_x << ISPHR_SCALE_X_SCALE_INT_SHIFT);
-	isp_write_bit32(ISPHR_SCALE_X_OFFSET,
-		pHresizeParam->fscalef_x << ISPHR_SCALE_X_SCALE_FRAC_SHIFT);
-	isp_write_bit32(ISPHR_SCALE_Y_OFFSET,
-		pHresizeParam->wscalef_y << ISPHR_SCALE_Y_SCALE_INT_SHIFT);
-	isp_write_bit32(ISPHR_SCALE_Y_OFFSET,
-		pHresizeParam->fscalef_y << ISPHR_SCALE_Y_SCALE_FRAC_SHIFT);
-	isp_write_bit32(ISPHR_NORM_OFFSET,
-		pHresizeParam->normf_x << ISPHR_NORM_NORM_X_SHIFT);
-	isp_write_bit32(ISPHR_NORM_OFFSET,
-		pHresizeParam->normf_y << ISPHR_NORM_NORM_Y_SHIFT);
-
-	return 0;
 }
 
 static int isp_reset(void *id)
@@ -430,96 +188,18 @@ static int isp_abort(void *id, mm_job_post_t *job)
 	return ret;
 }
 
-int isp_clearstat(struct isp_device_t *isp)
-{
-	int ret = 0;
-	isp_write(ISP_CTRL_OFFSET, (1<<ISP_CTRL_CLR_STATS_SHIFT));/* JITENDRA */
-	isp_write(ISPBL_SUM_LO_OFFSET, 0);
-	isp_write(ISPBL_SUM_HI_OFFSET, 0);
-	isp_write(ISPBL_COUNT_OFFSET, 0);
-	return ret;
-}
-
-int isp_programtiles(struct isp_device_t *isp, struct isp_job_post_t *job_post)
-{
-	int ret = 0;
-	isp_write(ISP_TILE_ADDR_OFFSET, job_post->tile_addr);
-	isp_write(ISP_TILE_CTRL_OFFSET,
-		(job_post->tile_count<<ISP_TILE_CTRL_TILE_COUNT_SHIFT));
-	return ret;
-}
-
-int isp_enableint(struct isp_device_t *isp)
-{
-	int ret = 0;
-	isp_clr_bit32(ISP_CTRL_OFFSET, ISP_CTRL_EOT_IMASK_MASK);
-	isp_clr_bit32(ISP_CTRL_OFFSET, ISP_CTRL_ERROR_IMASK_MASK);
-	return ret;
-}
-
 int isp_program(struct isp_device_t *isp, struct isp_job_post_t *job_post)
 {
 	int ret = 0;
+	int i;
 	ret = isp_reset(isp);
 	if (ret != 0) {
 		pr_err("isp_reset failed with %d", ret);
 		return ret;
 	}
-	ret = isp_write_frame(isp, &job_post->frame);
-	if (ret != 0) {
-		pr_err("isp_programfrparam failed with %d", ret);
-		return ret;
-	}
-	ret = isp_write_tile_offsets(isp, &job_post->tile_offsets);
-	if (ret != 0) {
-		pr_err("isp_programtileoff failed with %d", ret);
-		return ret;
-	}
-	ret = isp_write_yuv_input(isp, &job_post->yuv_input);
-	if (ret != 0) {
-		pr_err("isp_programinput failed with %d", ret);
-		return ret;
-	}
-	ret = isp_write_y_g_coeffs(isp, &job_post->y_g_coeffs);
-	if (ret != 0) {
-		pr_err("isp_programygcoeff failed with %d", ret);
-		return ret;
-	}
-	ret = isp_write_conversion(isp, job_post->informat,
-					job_post->outformat);
-	if (ret != 0) {
-		pr_err("isp_programccmatrix failed with %d", ret);
-		return ret;
-	}
-	ret = isp_write_lresize(isp, &job_post->lresize);
-	if (ret != 0) {
-		pr_err("isp_programlresize failed with %d", ret);
-		return ret;
-	}
-	ret = isp_write_lo_res_output(isp, &job_post->lo_res_output);
-	if (ret != 0) {
-		pr_err("isp_programloresoutput failed with %d", ret);
-		return ret;
-	}
-	ret = isp_write_hresize(isp, &job_post->hresize);
-	if (ret != 0) {
-		pr_err("isp_programhresize failed with %d", ret);
-		return ret;
-	}
-	ret = isp_clearstat(isp);
-	if (ret != 0) {
-		pr_err("isp_clearstat failed with %d", ret);
-		return ret;
-	}
-	ret = isp_programtiles(isp, job_post);
-	if (ret != 0) {
-		pr_err("isp_programtiles failed with %d", ret);
-		return ret;
-	}
-	ret = isp_enableint(isp);
-	if (ret != 0) {
-		pr_err("isp_enableint failed with %d", ret);
-		return ret;
+	for (i = 0; i < job_post->num_regs; i++)
+	{
+		isp_write(job_post->isp_regs[i].offset, job_post->isp_regs[i].value);
 	}
 
 	return ret;
--- drivers/char/broadcom/mm/isp/mm_isp.h
+++ drivers/char/broadcom/mm/isp/mm_isp.h
@@ -29,244 +29,16 @@ the GPL, without Broadcom's express prior written consent.
 
 #define TRUNC12(x) (((int)(x*4096))&0xFFFF)
 
-/******************************************************************************
-Structures. These define the parameters that can be sent to the ISP or ISP
-control software, and correspond directly, for the most part, to the registers
-of the functional blocks of the ISP.
-******************************************************************************/
+#define MAX_NUM_ISP_REGS 70
 
-/* Number of input lines available.*/
-
-struct ISP_LINES_AVAIL_PARAMS_T {
-	uint16_t lines_avail;
-	uint8_t pad[2];
-};
-
-
-/*  ISP version parameters. */
-
-struct ISP_VERSION_PARAMS_T {
-	uint8_t revision;
-	uint8_t major_version;
-	uint8_t pad[2];
-};
-
-
-/*  Frame parameters. */
-
-struct ISP_FRAME_PARAMS_T {
-	uint16_t width;
-	uint16_t height;
-	int32_t /*ISP_BAYER_ORDER_T*/ bayer_order;
-	int32_t /*ISP_SW_POSITION_T*/ sw_out_pos;
-	int32_t /*ISP_SW_POSITION_T*/ sw_in_pos;
-	uint32_t bayer_en;
-	uint32_t ycbcr_en;
-	uint8_t transposed;
-	uint8_t pad[3];
-#if HERA_ISP
-	/* For Hera, we'll munge bayer_en and ycbcr_en to enable only those
-	* blocks that exist. The requested enables we'll store here,
-	* so that they can be fed back unmunged.
-	* Note that the hresize bit will be used for the window/crop function */
-	uint32_t bayer_en_public;
-	uint32_t ycbcr_en_public;
-#endif
-};
-
-/* Tile offset parameters. */
-
-struct ISP_TILE_OFFSET_PARAMS_T {
-	uint8_t dp; /* defective pixel correction */
-	uint8_t rs; /* resampling */
-	uint8_t fr; /* fringing correction */
-	uint8_t xc; /* crosstalk correction */
-	uint8_t dm; /* demosaicing */
-	uint8_t ls; /* lens shading correction */
-	uint8_t bl; /* black level correction */
-	uint8_t dn; /* denoising */
-	uint8_t st; /* statistics */
-	uint8_t gd; /* geometrical distortion correction */
-	uint8_t fcsh; /* false colour/sharpening */
-	uint8_t hro; /* hires output */
-	uint8_t dummy; /* make bs_out aligned */
-	uint8_t sw_out; /* software stage out */
-	uint8_t sw_in; /* software stage in */
-	uint8_t pad[1];
-};
-
-/* Bayer input formatter parameters. */
-
-struct ISP_RAW_INPUT_PARAMS_T {
-	void *address;
-	uint32_t stride;
-	void *end_address;
-	int32_t /*ISP_RAW_INPUT_FORMAT_T*/ format;
-	uint8_t dpcm_adv;
-	uint8_t pad[1];
-	uint16_t dpcm_blocklen;
-};
-
-
-/* Coefficients used for converting RGB into YCbCr and vice versa. */
-
-struct ISP_Y_G_COEFFS_PARAMS_T {
-	signed short y_coeffs[2]; /* B-G and R-G coefficients to make Y
-				from R, G and B */
-#if HERA_ISP
-	unsigned short cb_scale;
-	unsigned short cr_scale;
-	uint16_t cb_offset;
-	uint16_t cr_offset;
-#endif
-};
-
-/* YUV input formatter. */
-
-struct ISP_YUV_INPUT_PARAMS_T {
-	int32_t /*ISP_CS_FORMAT_T*/ cs_format;
-	uint8_t bytes_sample;
-	uint8_t pad1[3];
-	/* \todo - consider just using ISP_DATA_FORMAT_T for symmetry with the
-	* output formatters */
-	int32_t /*ISP_INTERLEAVE_T*/ data_format;
-	int32_t /*ISP_CC_ORDER_T*/ cc_order;
-	void *ry_address;
-	void *gu_address;
-	void *bv_address;
-	uint32_t y_stride;
-	uint32_t uv_stride;
-	uint8_t col_mode;
-	uint8_t col_width;   /* 00-32 pix, 01-64, 10-128, 11-256 */
-	uint8_t pad2[2];
-	uint32_t col_stride1;
-	uint32_t col_stride2;
-	void *ry_end_address;
-	void *gu_end_address;
-	void *bv_end_address;
-};
-
-/* Colour conversion parameters. */
-
-struct ISP_CONVERSION_PARAMS_T {
-	int16_t matrix[3][3];  /* actually agnostic about the fixed/fractional
-				* parts here */
-	uint8_t pad[2];
-	int32_t offsets[3];
-};
-
-/* High resolution resize parameters. */
-
-struct ISP_HRESIZE_PARAMS_T {
-	/* Fields starting with an underscore do not actually have registers in
-	* the hardware, so should not be used in the software simulator. */
-	uint16_t _input_offset_x;	/* LH edge of crop window */
-	uint16_t _input_offset_y;	/* top edge of crop window */
-	uint16_t _input_width;		/* width of input window */
-	uint16_t _input_height;		/* height of input window */
-	int32_t /*ISP_INTERP_FILTER_T*/ interp_filter_mode; /* type of resize,
-							* e.g. bilinear etc. */
-	uint16_t wscalef_x;		/* Whole part of the scaling factor
-						in the horizontal direction */
-	uint16_t fscalef_x;		/* Fractional part of the scaling factor
-						 in the horizontal direction */
-	uint16_t normf_x;		/* Normalization factor for
-					the horizontal direction */
-	uint16_t wscalef_y;		/* Whole part of the scaling factor
-						in the vertical direction */
-	uint16_t fscalef_y;		/* Fractional part of the scaling factor
-						in the vertical direction */
-	uint16_t normf_y;		/* Normalization factor
-						for the vertical direction */
-	uint16_t output_width;		/* width of output window */
-	uint16_t output_height;		/* height of output window */
-};
-
-/* Low resolution resize parameters. */
-
-struct ISP_LRESIZE_PARAMS_T {
-	/* Fields starting with an underscore do not actually have registers in
-	the hardware, so should not be used in the software simulator. */
-	uint16_t scale_x;	/* Scaling factor in the horizontal direction */
-	uint16_t scale_y;	/* Scaling factor in the vertical direction */
-#if ISP_VERSION >= 300
-	uint32_t tscale_x;	/* scale for trap filter x */
-	uint32_t tscale_y;	/* scale for trap filter y */
-	uint16_t tnorm_0;	/* norm param (x[i]   * y[i]) */
-	uint16_t tnorm_1;	/* norm param (x[i]+1 * y[i]) */
-	uint16_t tnorm_2;	/* norm param (x[i]   * y[i]+1) */
-	uint16_t tnorm_3;	/* norm param (x[i]+1 * y[i]+1) */
-	uint16_t _output_width; /* Output Image Width for new
-					trapezoidal resizer */
-	uint16_t _output_height;/* Output Image Height for new
-					trapezoidal resizer */
-#else
-	uint16_t norm_x;	/* Normalization factor for the
-					horizontal direction */
-	uint16_t norm_y;	/* Normalization factor for the
-					vertical direction */
-	uint16_t lnorm_x;	/* Normalization factor for the horizontal
-					direction for the last column */
-	uint16_t lnorm_y;	/* Normalization factor for the vertical
-					direction for the last row */
-#endif
-	uint8_t shift;		/* Left shift applied to data. */
-#if ISP_VERSION >= 410
-	uint8_t rb_swap;	/* Swap primary and tertiary components */
-	uint8_t pad[2];
-#else
-	uint8_t pad[3];
-#endif
-};
-
-/* Output formatter parameters. */
-
-struct ISP_OUTPUT_PARAMS_T {
-	uint8_t data_format;
-	uint8_t output_width;
-	uint8_t col_mode;
-	uint8_t hflip;
-	uint8_t vflip;
-	uint8_t shift;
-	uint8_t col_width;	/* 00-32 pix, 01-64, 10-128, 11-256 */
-	uint8_t pad1[1];
-	uint32_t col_stride1;
-	uint32_t col_stride2;
-	void *address1;
-	void *address2;
-	void *address3;
-#if ISP_VERSION < 200
-	uint32_t stride1;
-	uint32_t stride2;
-#else
-	int32_t stride1;	/* stored -ve when vflipped */
-	int32_t stride2;	/* stored -ve when vflipped */
-#endif
-	uint16_t _buffer_height; /* used only by ISP control s/w,
-			not used by the h/w itself. This gets passed to the ISP
-			as the vflip flag - it's the version of the flag that's
-			been adjusted for the image height parity. */
-	uint8_t v_subsample;
-	uint8_t pad2[1];
+struct regs_t {
+	unsigned long offset;
+	unsigned long value;
 };
 
 struct isp_job_post_t {
-	uint16_t informat;
-	uint32_t outformat;
-	uint32_t tile_addr;
-	uint32_t tile_count;
-	struct ISP_FRAME_PARAMS_T frame;
-	struct ISP_TILE_OFFSET_PARAMS_T tile_offsets;
-	struct ISP_LINES_AVAIL_PARAMS_T lines_avail;
-	struct ISP_Y_G_COEFFS_PARAMS_T y_g_coeffs;
-	struct ISP_RAW_INPUT_PARAMS_T raw_input;
-	struct ISP_YUV_INPUT_PARAMS_T yuv_input;
-	struct ISP_CONVERSION_PARAMS_T yuv_conversion;
-	struct ISP_HRESIZE_PARAMS_T hresize;
-	struct ISP_CONVERSION_PARAMS_T conversion;
-	struct ISP_LRESIZE_PARAMS_T lresize;
-	struct ISP_OUTPUT_PARAMS_T lo_res_output;
-	struct ISP_OUTPUT_PARAMS_T hi_res_output;
+	struct regs_t isp_regs[MAX_NUM_ISP_REGS];
+	unsigned int num_regs;
 };
 
 enum {
@@ -280,6 +52,48 @@ enum {
 #define ISP_IOCTL_CLK_RESET _IOR(BCM_ISP_MAGIC, ISP_CMD_CLK_RESET, unsigned int)
 #define ISP_IOCTL_RELEASE_IRQ _IOR(BCM_ISP_MAGIC, ISP_CMD_RELEASE_IRQ, \
 							unsigned int)
+
+#define ISP_CTRL_OFFSET                                                   0x00000000
+#define ISP_CTRL_TYPE                                                     UInt32
+#define ISP_CTRL_RESERVED_MASK                                            0xFFEEFE80
+#define    ISP_CTRL_FORCE_CLKEN_SHIFT                                     20
+#define    ISP_CTRL_FORCE_CLKEN_MASK                                      0x00100000
+#define    ISP_CTRL_TRANSFER_SHIFT                                        16
+#define    ISP_CTRL_TRANSFER_MASK                                         0x00010000
+#define    ISP_CTRL_STATS_IMASK_SHIFT                                     8
+#define    ISP_CTRL_STATS_IMASK_MASK                                      0x00000100
+#define    ISP_CTRL_SW_IMASK_SHIFT                                        6
+#define    ISP_CTRL_SW_IMASK_MASK                                         0x00000040
+#define    ISP_CTRL_EOD_IMASK_SHIFT                                       5
+#define    ISP_CTRL_EOD_IMASK_MASK                                        0x00000020
+#define    ISP_CTRL_EOT_IMASK_SHIFT                                       4
+#define    ISP_CTRL_EOT_IMASK_MASK                                        0x00000010
+#define    ISP_CTRL_ERROR_IMASK_SHIFT                                     3
+#define    ISP_CTRL_ERROR_IMASK_MASK                                      0x00000008
+#define    ISP_CTRL_CLR_STATS_SHIFT                                       2
+#define    ISP_CTRL_CLR_STATS_MASK                                        0x00000004
+#define    ISP_CTRL_FLUSH_SHIFT                                           1
+#define    ISP_CTRL_FLUSH_MASK                                            0x00000002
+#define    ISP_CTRL_ENABLE_SHIFT                                          0
+#define    ISP_CTRL_ENABLE_MASK                                           0x00000001
+
+#define ISP_STATUS_OFFSET                                                 0x00000004
+#define ISP_STATUS_TYPE                                                   UInt32
+#define ISP_STATUS_RESERVED_MASK                                          0xFFFFFEC4
+#define    ISP_STATUS_STATS_INT_SHIFT                                     8
+#define    ISP_STATUS_STATS_INT_MASK                                      0x00000100
+#define    ISP_STATUS_EOD_INT_SHIFT                                       5
+#define    ISP_STATUS_EOD_INT_MASK                                        0x00000020
+#define    ISP_STATUS_EOT_INT_SHIFT                                       4
+#define    ISP_STATUS_EOT_INT_MASK                                        0x00000010
+#define    ISP_STATUS_ERROR_INT_SHIFT                                     3
+#define    ISP_STATUS_ERROR_INT_MASK                                      0x00000008
+#define    ISP_STATUS_STATE_SHIFT                                         0
+#define    ISP_STATUS_STATE_MASK                                          0x00000003
+#define       ISP_STATUS_STATE_INACTIVE                                   0
+#define       ISP_STATUS_STATE_ENABLED                                    1
+#define       ISP_STATUS_STATE_SUSPENDED                                  2
+#define       ISP_STATUS_STATE_SUSPENDING                                 3
 
 
 #endif
