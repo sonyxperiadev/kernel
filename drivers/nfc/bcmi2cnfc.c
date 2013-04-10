1eba1d094c9cc20f8fcd428cfbb18e0873a1cbad
kendai <kendai@broadcom.com>
2013-04-10 09:50:25 -0700

    NFC: Fix payment issue in CE4
    [Defect / Enhancement / New Feature]
    MobC00299031
    
    [Problem]
    VIVO pay transaction does not execute completely for
    CE4mode(Display OFF) after 2-3 successful transactions
    
    [Solution]
    The issue is BB go to sleep before the transaction is
    finished. So the solution is Hold the wake_lock until the
    transaction is finished.
    
    [Reviewers]
    
    [Testing]
        [Test Cases]
         Make sure payment is fully working in CE4
    
        [Label/CL# Synced To]
    
        [Platform]
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: I48bc6431f03b13a6fe3b58a3c15532519d351ac2
    Reviewed-on: http://mps-gerrit.broadcom.com/18625
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: CQ Status <cq_status_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Reviewed-by: Babu Arunachalam <babua@broadcom.com>
    Tested-by: AutoBuild Status <autobuild_status_noreply@broadcom.com>
    Tested-by: AutoTest Status <autotest_status_noreply@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- drivers/nfc/bcmi2cnfc.c
+++ drivers/nfc/bcmi2cnfc.c
@@ -186,17 +186,20 @@ static irqreturn_t bcmi2cnfc_dev_irq_handler(int irq, void *dev_id)
 {
 	struct bcmi2cnfc_dev *bcmi2cnfc_dev = dev_id;
 	unsigned long flags;
+	if (0 == gpio_get_value(bcmi2cnfc_dev->en_gpio))
+		return IRQ_HANDLED;
 
 	DBG2(dev_info(&bcmi2cnfc_dev->client->dev,
 		      "irq go high %llu\n", get_jiffies_64()));
 
 	spin_lock_irqsave(&bcmi2cnfc_dev->irq_enabled_lock, flags);
-
 	bcmi2cnfc_dev->count_irq++;
-
+	spin_unlock_irqrestore(&bcmi2cnfc_dev->irq_enabled_lock, flags);
 	wake_up(&bcmi2cnfc_dev->read_wq);
 
-	spin_unlock_irqrestore(&bcmi2cnfc_dev->irq_enabled_lock, flags);
+#ifdef CONFIG_HAS_WAKELOCK
+	wake_lock(&nfc_wake_lock);
+#endif
 
 	return IRQ_HANDLED;
 }
@@ -211,7 +214,6 @@ static unsigned int bcmi2cnfc_dev_poll(struct file *filp, poll_table *wait)
 
 	spin_lock_irqsave(&bcmi2cnfc_dev->irq_enabled_lock, flags);
 	if (bcmi2cnfc_dev->count_irq > 0) {
-		bcmi2cnfc_dev->count_irq--;
 		mask |= POLLIN | POLLRDNORM;
 	}
 	spin_unlock_irqrestore(&bcmi2cnfc_dev->irq_enabled_lock, flags);
@@ -230,12 +232,12 @@ static ssize_t bcmi2cnfc_dev_read(struct file *filp, char __user *buf,
 
 	DBG2(dev_info(&bcmi2cnfc_dev->client->dev,
 		      "bcmi2cnfc_dev_read %llu\n", get_jiffies_64()));
-#ifdef CONFIG_HAS_WAKELOCK
-	wake_lock(&nfc_wake_lock);
-#endif
 
 	total = 0;
 	len = 0;
+
+	if (bcmi2cnfc_dev->count_irq > 0)
+		bcmi2cnfc_dev->count_irq--;
 
 	bcmi2cnfc_dev->count_read++;
 	if (count > MAX_BUFFER_SIZE)
@@ -290,7 +292,8 @@ static ssize_t bcmi2cnfc_dev_read(struct file *filp, char __user *buf,
 		bcmi2cnfc_dev->error_read++;
 	}
 #ifdef CONFIG_HAS_WAKELOCK
-	wake_unlock(&nfc_wake_lock);
+	if (bcmi2cnfc_dev->count_irq == 0)
+		wake_unlock(&nfc_wake_lock);
 #endif
 
 	DBG2(dev_info(&bcmi2cnfc_dev->client->dev,
@@ -322,9 +325,6 @@ static ssize_t bcmi2cnfc_dev_write(struct file *filp, const char __user *buf,
 			"failed to copy from user space\n");
 		return -EFAULT;
 	}
-#ifdef CONFIG_HAS_WAKELOCK
-	wake_lock(&nfc_wake_lock);
-#endif
 
 	mutex_lock(&bcmi2cnfc_dev->read_mutex);
 	/* Write data */
@@ -346,9 +346,6 @@ static ssize_t bcmi2cnfc_dev_write(struct file *filp, const char __user *buf,
 		}
 	}
 	mutex_unlock(&bcmi2cnfc_dev->read_mutex);
-#ifdef CONFIG_HAS_WAKELOCK
-	wake_unlock(&nfc_wake_lock);
-#endif
 
 	DBG2(dev_info(&bcmi2cnfc_dev->client->dev,
 		      "bcmi2cnfc_dev_write leave %llu\n", get_jiffies_64()));
@@ -404,9 +401,14 @@ static long bcmi2cnfc_dev_unlocked_ioctl(struct file *filp,
 		} else {
 			if (bcmi2cnfc_dev->irq_enabled == true) {
 				bcmi2cnfc_dev->irq_enabled = FALSE;
-				disable_irq_nosync(bcmi2cnfc_dev->client->irq);
+				disable_irq(bcmi2cnfc_dev->client->irq);
+
 			}
 			gpio_set_value(bcmi2cnfc_dev->en_gpio, 0);
+			udelay(10);
+#ifdef CONFIG_HAS_WAKELOCK
+			wake_unlock(&nfc_wake_lock);
+#endif
 			set_client_addr(bcmi2cnfc_dev,
 					bcmi2cnfc_dev->original_address);
 		}
