16bfe85acc37d7cf1f59bc5ce5893cca697ccdd5
Yuvaraj Kumar C D <yuvaraj.cd@gmail.com>
2013-05-21 09:38:43 +0000

    mmc: core: Update the ext-csd.rev check for eMMC5.1
    With the new eMMC5.1 spec, there is a new EXT_CSD register with
    the revision number(EXT_CSD_REV) 7. This patch updates the check
    for ext-csd.rev number as 7.
    
    Change-Id: I884ac449e6caa4d3571868e300552b96ada177ef
    Signed-off-by: Alim Akhtar <alim.akhtar@samsung.com>
    Signed-off-by: Yuvaraj Kumar C D <yuvaraj.cd@samsung.com>
    Reviewed-by: Doug Anderson <dianders@chromium.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>
    Signed-off-by: Prasanna NAVARATNA <prasanna.navaratna@broadcom.com>
    Reviewed-on: http://mps-gerrit.broadcom.com/55161
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Reviewed-by: Vikram Prakash <vikramp@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -298,7 +298,7 @@ static int mmc_read_ext_csd(struct mmc_card *card, u8 *ext_csd)
 
 	card->ext_csd.rev = ext_csd[EXT_CSD_REV];
 #ifdef CONFIG_MMC_BCM_SD
-	if (card->ext_csd.rev > 6) {
+	if (card->ext_csd.rev > 7) {
 #else
 	if (card->ext_csd.rev > 3) {
 #endif
