77323736ecd52dee40143fd3a6beda8869cff4dd
Murali Mohan T.D <murali.td@broadcom.com>
2013-06-03 15:46:39 +0530

    USB: USB detection issue during ADB reboot
    [Defect / Enhancement / New Feature]
    	MobC00308363
    [Problem]
    	USB detection issue upon adb reboot
    [Solution]
    	Issue due to sending of USB connect event to PC early
    	before complete GADGET driverinitialization.
    	Adaptation of DCD driver interfaces using UDC frame
    	old style interfaces.
    [Reviewers]
    	Vikram, Naga
    [Testing]
    	[Test Cases]
    		Tested adb reboot multiple times on Ubuntu PC.
    	[Label/CL# Synced To]
    	[Platform]
    		Hawaii Garnet EDN020
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: Id59f152138fb926339860a72c42b8d87f4c71e8b
    Signed-off-by: Murali Mohan T.D <murali.td@broadcom.com>
    Reviewed-on: http://mps-gerrit.broadcom.com/25503
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: CQ Status <cq_status_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Reviewed-by: Naga Chandra Prahalada Rao <nagachandra@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- drivers/usb/gadget/dwc_otg/driver/dwc_otg_pcd_linux.c
+++ drivers/usb/gadget/dwc_otg/driver/dwc_otg_pcd_linux.c
@@ -790,10 +790,10 @@ static int set_selfpowered(struct usb_gadget *gadget, int is_selfpowered)
 	return 0;
 }
 
-
-static int dwc_udc_start(struct usb_gadget *gadget,
-					struct usb_gadget_driver *driver)
+static int dwc_udc_start(struct usb_gadget_driver *driver,
+				int (*bind)(struct usb_gadget *))
 {
+	unsigned int ret = 0;
 	DWC_DEBUGPL(DBG_PCD, "probing gadget driver '%s'\n",
 		    driver->driver.name);
 
@@ -813,7 +813,11 @@ static int dwc_udc_start(struct usb_gadget *gadget,
 
 	/* hook up the driver */
 	gadget_wrapper->driver = driver;
-	gadget->dev.driver = &driver->driver;
+	gadget_wrapper->gadget.dev.driver = &driver->driver;
+
+	ret = bind(&gadget_wrapper->gadget);
+	if (ret)
+		return ret;
 
 #ifdef CONFIG_USB_OTG_UTILS
 #ifndef CONFIG_USB_OTG
@@ -831,27 +835,10 @@ static int dwc_udc_start(struct usb_gadget *gadget,
 	}
 #endif /* CONFIG_USB_OTG_UTILS */
 
-	dwc_otg_disable_global_interrupts(gadget_wrapper->pcd->core_if);
-	/* Default is to connect to USB host. Gadget driver may override
-	 * this during its bind using the pullup() API. We will set our internal
-	 * gadget_pullup_on to reflect that we are coming up connected by
-	 * default. This way if bind doesn't do anything to connect/disconnect,
-	 * driver will maintain correct gadget pullup status. If bind overrides this
-	 * connect/disconnect via pullup function then gadget_pullup_on will be
-	 * updated by pullup function
-	 */
-	gadget_wrapper->pcd->core_if->gadget_pullup_on = true;
-	dwc_otg_pcd_disconnect(gadget_wrapper->pcd, false);
-
-	DWC_DEBUGPL(DBG_ANY, "probed gadget driver '%s'\n",
-		    driver->driver.name);
-
-	dwc_otg_enable_global_interrupts(gadget_wrapper->pcd->core_if);
-
 #ifdef CONFIG_USB_OTG_UTILS
 	if (gadget_wrapper->pcd->core_if->xceiver->otg->set_peripheral)
 		otg_set_peripheral(gadget_wrapper->pcd->core_if->xceiver->otg,
-				   gadget);
+				&gadget_wrapper->gadget);
 #endif
 
 	return 0;
@@ -863,8 +850,7 @@ static int dwc_udc_start(struct usb_gadget *gadget,
  *
  * @param driver The driver being unregistered
  */
-static int dwc_udc_stop(struct usb_gadget *gadget,
-					struct usb_gadget_driver *driver)
+static int dwc_udc_stop(struct usb_gadget_driver *driver)
 {
 	/*DWC_DEBUGPL(DBG_PCDV,"%s(%p)\n", __func__, _driver); */
 
@@ -878,6 +864,8 @@ static int dwc_udc_stop(struct usb_gadget *gadget,
 			    -EINVAL);
 		return -EINVAL;
 	}
+
+	driver->unbind(&gadget_wrapper->gadget);
 
 	dwc_otg_disable_global_interrupts(gadget_wrapper->pcd->core_if);
 	/* Gadget about to unbound, disable connection to USB host */
@@ -893,7 +881,7 @@ static int dwc_udc_stop(struct usb_gadget *gadget,
 	DWC_WORKQ_SCHEDULE(gadget_wrapper->pcd->core_if->wq_otg,
 			   w_shutdown_core, gadget_wrapper->pcd->core_if,
 			   "Shutdown core");
-	gadget->dev.driver = NULL;
+	gadget_wrapper->gadget.dev.driver = NULL;
 	gadget_wrapper->driver = 0;
 
 #ifdef CONFIG_USB_OTG_UTILS
@@ -915,8 +903,9 @@ static const struct usb_gadget_ops dwc_otg_pcd_ops = {
 	.lpm_support = test_lpm_enabled,
 #endif
 	.set_selfpowered = set_selfpowered,
-	.udc_start = dwc_udc_start,
-	.udc_stop = dwc_udc_stop,
+
+	.start = dwc_udc_start,
+	.stop = dwc_udc_stop,
 };
 
 static int _setup(dwc_otg_pcd_t *pcd, uint8_t *bytes)
