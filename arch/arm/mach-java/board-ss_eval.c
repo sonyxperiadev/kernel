c288b3e6850e877b1991925de8c68146470dccd8
demian <demian@broadcom.com>
2013-06-07 15:48:46 +0900

    Backport crb-java-demo-android-jb-4.2.2
    commit : a3766f3ba51e3f1e42989861355be4e814c3ec66
    
    Java EVAL bringup changes.
    
    Update Dormant mode enable
    
    Change-Id: I47735017d55a54fbe1f81f87ffd9259fde0dbc61
    Reviewed-on: http://mps-gerrit.broadcom.com/26532
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: Vikram Prakash <vikramp@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- arch/arm/mach-java/board-ss_eval.c
+++ arch/arm/mach-java/board-ss_eval.c
@@ -781,6 +781,10 @@ struct platform_device *hawaii_common_plat_devices[] __initdata = {
 	&hawaii_spum_aes_device,
 #endif
 
+#ifdef CONFIG_BRCM_CDC
+        &brcm_cdc_device,
+#endif
+
 #ifdef CONFIG_UNICAM
 	&hawaii_unicam_device,
 #endif
