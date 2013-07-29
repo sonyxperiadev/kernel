bf92f1b48879413da004670d41b65e59f856f309
Yongqiang Wang <yongqian@broadcom.com>
2013-07-29 15:30:12 +0800

    add proximity cali. API
    [Defect / Enhancement / New Feature]
    MobC00340365
    
    [Problem]
    There is no proximity calibration API in driver and HAL.
    It is necessary do implement them, because this feature
    can cover the mechanical design issues.
    
    [Solution]
    add them
    
    [Reviewers]
    BSP
    
    [Testing]
    [Test Cases]
    check it with z-device "and sensorcalibrate -s x"
    
    [Label/CL# Synced To]
    sdb-common-android-jb-4.2.2
    
    [Platform]
    java garnet M56
    
    [BROADCOM INTERNAL NOTES]
    Integrate commit
    
    Change-Id: I15b7a2e7b1270d0ea765eb826e5cdbf937dc876b
    Reviewed-on: http://mps-gerrit.broadcom.com/32257
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: CQ Status <cq_status_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Reviewed-by: Oza (Pawandeep) Oza <oza@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>
    Reviewed-on: http://mps-gerrit.broadcom.com/33953
    Reviewed-by: Vikram Prakash <vikramp@broadcom.com>
    Reviewed-by: Pierre Couillaud <pierre@broadcom.com>

--- include/linux/ap3212c_pls.h
+++ include/linux/ap3212c_pls.h
@@ -14,7 +14,8 @@
 #define LTR_IOCTL_GET_LFLAG _IOR(LTR_IOCTL_MAGIC, 2, int)
 #define LTR_IOCTL_SET_PFLAG _IOW(LTR_IOCTL_MAGIC, 3, int)
 #define LTR_IOCTL_SET_LFLAG _IOW(LTR_IOCTL_MAGIC, 4, int)
-#define LTR_IOCTL_GET_DATA _IOW(LTR_IOCTL_MAGIC, 5, unsigned char)
+#define LTR_IOCTL_PROX_CALI _IOR(LTR_IOCTL_MAGIC, 5, int)
+#define LTR_IOCTL_GET_DATA _IOW(LTR_IOCTL_MAGIC, 6, unsigned char)
 
 #define AP3212C_RAN_COMMAND 0x10
 #define AP3212C_RAN_MASK 0x30
@@ -33,6 +34,8 @@
 #define AP3212C_MODE_COMMAND 0x00
 #define AP3212C_MODE_SHIFT (0)
 #define AP3212C_MODE_MASK 0x07
+#define AP3212C_MODE_ALS_MASK	0x01
+#define AP3212C_MODE_PRX_MASK	0x02
 
 #define	AP3212C_ADC_LSB	 0x0c
 #define	AP3212C_ADC_MSB 0x0d
@@ -87,6 +90,7 @@
 #define AP3212C_PX_CONFIGURE	0x20
 #define AP3212C_PX_LED		0x21
 
+
 #define LSC_DBG
 #ifdef LSC_DBG
 #define LDBG(s, args...) { printk(KERN_INFO"LDBG: func[%s],line [%d], ",\
@@ -103,6 +107,38 @@
 #define AP3212B_NUM_CACHABLE_REGS	23
 #define AP3216C_NUM_CACHABLE_REGS	26
 
+/*system regitser table*/
+#define AP321XC_SYS_CFG_REG	0x00
+#define AP321XC_INT_STS_REG	0x01
+#define AP321XC_INT_CLR_REG	0x02
+#define AP321XC_IR_DL_REG	0x0A
+#define AP321XC_IR_DH_REG	0x0B
+#define AP321XC_ALS_DL_REG	0x0C
+#define AP321XC_ALS_DH_REG	0x0D
+#define AP321XC_PS_DL_REG	0x0E
+#define AP321XC_PS_DH_REG	0x0F
+
+/*ALS register table*/
+#define AP321XC_ALS_CFG_REG	0x10
+#define AP321XC_ALS_CAL_REG	0x19
+#define AP321XC_ALS_LTL_REG	0x1A
+#define AP321XC_ALS_LTH_REG	0x1B
+#define AP321XC_ALS_HTL_REG	0x1C
+#define AP321XC_ALS_HTH_REG	0x1D
+
+/*PS register table*/
+#define AP321XC_PS_CFG_REG	0x20
+#define AP321XC_PS_LED_REG	0x21
+#define AP321XC_PS_INT_REG	0x22
+#define AP321XC_PS_MT_REG	0x23
+#define AP321XC_PS_LEDWT_REG	0x24
+#define AP321XC_PS_CALL_REG	0x28
+#define AP321XC_PS_CALH_REG	0x29
+#define AP321XC_PS_LTL_REG	0x2A
+#define AP321XC_PS_LTH_REG	0x2B
+#define AP321XC_PS_HTL_REG	0x2C
+#define AP321XC_PS_HTH_REG	0x2D
+
 enum SENSOR_TYPE {
 	AP3212C_PLS_ALPS = 0,
 	AP3212C_PLS_PXY,
@@ -115,10 +151,15 @@ struct ap3212c_pls_t {
 	struct work_struct work;
 	struct workqueue_struct *ltr_work_queue;
 	struct early_suspend ltr_early_suspend;
+	struct mutex proximity_calibrating;
 	u16 prox_threshold_hi;
 	u16 prox_threshold_lo;
+	u16 prox_threshold_hi_def;
+	u16 prox_threshold_lo_def;
 	u8 prox_pulse_cnt;
 	u8 prox_gain;
+	u8 prox_offset;
+	u8 prox_boot_cali;
 };
 
 
