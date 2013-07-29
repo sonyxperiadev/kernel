8e8d1096078072cf87c7304e6925d65a5f816c24
Prasanna NAVARATNA <prasanna.navaratna@broadcom.com>
2013-07-29 15:36:46 +0530

    RPMB: Update License to GPLv2 for emmc rpmb driver
    [Enhancement]
    Since code is linking with GPL hence update emmc rpmb driver with
    GPLv2 license.
    
    [Problem]
    Broadcom license are there in emmc rpmb driver with GPL linking code.
    
    [Solution]
    Update the license in emmc rpmb driver to GPLv2.
    
    [Reviewers]
    Vikram, Sundar, Babu
    
    [Testing]
        [Test Cases]
        N/A
    
        [Label/CL# Synced To]
        sdb-common-android-jb-4.2.2
    
        [Platform]
        Hawaii, Java
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: I0dea0b0ae0ab514c73cd167e5ab0c98c329a98d0
    Signed-off-by: Prasanna NAVARATNA <prasanna.navaratna@broadcom.com>
    Reviewed-on: http://mps-gerrit.broadcom.com/32281
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Reviewed-by: Babu Arunachalam <babua@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- drivers/misc/emmc_rpmb/test_emmc_rpmb_rw.c
+++ drivers/misc/emmc_rpmb/test_emmc_rpmb_rw.c
@@ -1,41 +1,25 @@
-/*******************************************************************************
-Copyright 2010 Broadcom Corporation.  All rights reserved.
-This program is the proprietary software of Broadcom Corporation and/or its
-licensors, and may only be used, duplicated, modified or distributed pursuant
-to the terms and conditions of a separate, written license agreement executed
-between you and Broadcom (an "Authorized License").
-
-Except as set forth in an Authorized License, Broadcom grants no
-license(express or implied), right to use, or waiver of any kind with respect
-to the Software, and Broadcom expressly reserves all rights in and to the
-Software and all intellectual property rights therein.  IF YOU HAVE NO
-AUTHORIZED LICENSE, THEN YOU HAVE NO RIGHT TO USE THIS SOFTWARE IN ANY WAY,
-AND SHOULD IMMEDIATELY NOTIFY BROADCOM AND DISCONTINUE ALL USE OF THE SOFTWARE.
-
-Except as expressly set forth in the Authorized License,
-1. This program, including its structure, sequence and organization, constitutes
-the valuable trade secrets of Broadcom, and you shall use all reasonable efforts
-to protect the confidentiality thereof, and to use this information only
-in connection with your use of Broadcom integrated circuit products.
-
-2. TO THE MAXIMUM EXTENT PERMITTED BY LAW, THE SOFTWARE IS PROVIDED "AS IS" AND
-WITH ALL FAULTS AND BROADCOM MAKES NO PROMISES, REPRESENTATIONS OR WARRANTIES,
-EITHER EXPRESS, IMPLIED, STATUTORY, OR OTHERWISE, WITH RESPECT TO THE SOFTWARE.
-BROADCOM SPECIFICALLY DISCLAIMS ANY AND ALL IMPLIED WARRANTIES OF TITLE,
-MERCHANTABILITY, NONINFRINGEMENT, FITNESS FOR A PARTICULAR PURPOSE, LACK OF
-VIRUSES, ACCURACY OR COMPLETENESS, QUIET ENJOYMENT, QUIET POSSESSION OR
-CORRESPONDENCE TO DESCRIPTION. YOU ASSUME THE ENTIRE RISK ARISING OUT OF USE OR
-PERFORMANCE OF THE SOFTWARE.
-
-3. TO THE MAXIMUM EXTENT PERMITTED BY LAW, IN NO EVENT SHALL BROADCOM OR ITS
-LICENSORS BE LIABLE FOR (i) CONSEQUENTIAL, INCIDENTAL, SPECIAL, INDIRECT, OR
-EXEMPLARY DAMAGES WHATSOEVER ARISING OUT OF OR IN ANY WAY RELATING TO YOUR USE
-OF OR INABILITY TO USE THE SOFTWARE EVEN IF BROADCOM HAS BEEN ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGES; OR (ii) ANY AMOUNT IN EXCESS OF THE AMOUNT ACTUALLY
-PAID FOR THE SOFTWARE ITSELF OR U.S. $1, WHICHEVER IS GREATER. THESE LIMITATIONS
-SHALL APPLY NOTWITHSTANDING ANY FAILURE OF ESSENTIAL PURPOSE OF ANY LIMITED
-REMEDY.
-*******************************************************************************/
+/*********************************************************************
+ * *
+ * *  Copyright 2010 Broadcom Corporation
+ * *
+ * *  Unless you and Broadcom execute a separate written software license
+ * *  agreement governing use of this software, this software is licensed
+ * *  to you under the terms of the GNU
+ * *  General Public License version 2 (the GPL), available at
+ * *  http://www.broadcom.com/licenses/GPLv2.php with the following added
+ * *  to such license:
+ * *  As a special exception, the copyright holders of this software give
+ * *  you permission to link this software with independent modules, and
+ * *  to copy and distribute the resulting executable under terms of your
+ * *  choice, provided that you also meet, for each linked independent module,
+ * *  the terms and conditions of the license of that module. An independent
+ * *  module is a module which is not derived from this software.  The special
+ * *  exception does not apply to any modifications of the software.
+ * *  Notwithstanding the above, under no circumstances may you combine this
+ * *  software in any way with any other Broadcom software provided under a
+ * *  license other than the GPL, without Broadcom's express prior written
+ * *  consent.
+ * ***********************************************************************/
 
 #include <linux/slab.h>
 #include <linux/delay.h>
