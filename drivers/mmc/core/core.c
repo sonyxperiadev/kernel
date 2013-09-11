a9ba6e87542fe4e4d060aebea11c982fd5f1b9f7
Prasanna NAVARATNA <prasanna.navaratna@broadcom.com>
2013-09-11 17:49:54 +0530

    mmc: core: detect_wake_lock is never released
    [Defect]
    
    [Problem]
    detect_wake_lock is never released after going for first
    suspend. Hence susbsequent suspend calls fails due to always
    consumed detect_wake_lock.
    
    [Solution]
    After calling mmc_rescan for first time, rescan_entered will be 1.
    Subsequent mmc_rescan calls for MMC_CAP_NONREMOVABLE device will
    return back without unlocking the detect_wake_lock.
    
    For non-removable devices, instead of returning when rescan_entered=1,
    goto out which will properly unlock the detect_wake_lock.
    
    [Reviewers]
    Naga, Vikram, Vikas
    
    [Testing]
        [Test Cases]
        Suspend is entered everytime PON key is pressed.
    
        [Label/CL# Synced To]
        sdb-linux-3.10.0-hawaii-java
    
        [Platform]
        android_hawaii_garnet_edn020
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: I052f9de8b3ea8d8e0cfc88e77b67cddfb35c8d4b
    Signed-off-by: Prasanna NAVARATNA <prasanna.navaratna@broadcom.com>
    Reviewed-on: http://mps-gerrit.broadcom.com/37687
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Reviewed-by: Vikram Prakash <vikramp@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -2418,7 +2418,7 @@ void mmc_rescan(struct work_struct *work)
 
 	/* If there is a non-removable card registered, only scan once */
 	if ((host->caps & MMC_CAP_NONREMOVABLE) && host->rescan_entered)
-		return;
+		goto out;
 	host->rescan_entered = 1;
 
 	mmc_bus_get(host);
