508283414ddb7172ad1d6de2edef43b7803657c9
Chittampally Vasanth Raja <vasantch@broadcom.com>
2013-09-16 19:36:10 +0530

    Enable secure watchdog only in normal mode
    [Defect / Enhancement / New Feature]
    Enhancement
    
    [Problem]
    Currently secure watchdog is enabled in all modes
    
    [Solution]
    1. Enable secure watchdog only in normal mode
    2. Add DT support for secure watchdog
    
    [Reviewers]
    Oza, Vikram, Srivathsa
    
    [Testing]
        [Test Cases]
        Tested with soft reboot, power off charging,
        power on reset.
    
        [Label/CL# Synced To]
        sdb-common-android-jb-4.2.2
    
        [Platform]
        Java
    
    [BROADCOM INTERNAL NOTES]
    
    Signed-off-by: Chittampally Vasanth Raja <vasantch@broadcom.com>
    Change-Id: I0f2bf89fb792f3d19685afaac4fec1bd26293ff1
    Reviewed-on: http://mps-gerrit.broadcom.com/38682
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Reviewed-by: Oza (Pawandeep) Oza <oza@broadcom.com>
    Reviewed-by: Vikram Prakash <vikramp@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>
    (cherry picked from commit 9c9bfd5b570e1c0816bef41935c896facf16788c)
    Reviewed-on: http://mps-gerrit.broadcom.com/46445
    Reviewed-by: Babu Arunachalam <babua@broadcom.com>

--- drivers/watchdog/kona-sec-wd.c
+++ drivers/watchdog/kona-sec-wd.c
@@ -22,16 +22,25 @@
 #include <linux/module.h>
 #include <linux/workqueue.h>
 #include <mach/sec_api.h>
+#include <linux/platform_device.h>
+#include <mach/io_map.h>
+#include <plat/kona_reset_reason.h>
+#include <mach/rdb/brcm_rdb_root_rst_mgr_reg.h>
+#include <linux/types.h>
+#include <linux/io.h>
+#include <mach/sram_config.h>
 
 #define SEC_EXIT_NORMAL			1
-#define SEC_WD_TAP_DELAY		120000
+#define SEC_WD_TAP_DELAY		12000
 #define SSAPI_ACTIVATE_SEC_WATCHDOG     0x01000006
 #define SSAPI_ENABLE_SEC_WATCHDOG       0x01000007
 #define SSAPI_DISABLE_SEC_WATCHDOG      0x01000008
-#define SSAPI_PAT_SEC_WATCHDOG          0x01000009
-
+#define SSAPI_PAT_SEC_WATCHDOG		0x01000009
 
 static struct delayed_work sec_wd_work;
+struct notifier_block sec_wd_panic_block;
+struct notifier_block sec_wd_reboot_block;
+unsigned int sec_wd_enabled_mode;
 
 static void sec_wd_pat(struct work_struct *work)
 {
@@ -48,31 +57,152 @@ static void init_patter(void)
 	msecs_to_jiffies(SEC_WD_TAP_DELAY));
 }
 
-void sec_wd_enable()
+void sec_wd_enable(void)
 {
 	secure_api_call(SSAPI_ENABLE_SEC_WATCHDOG, 0, 0, 0, 0);
 }
 EXPORT_SYMBOL(sec_wd_enable);
 
-void sec_wd_disable()
+void sec_wd_disable(void)
 {
 	secure_api_call(SSAPI_DISABLE_SEC_WATCHDOG, 0, 0, 0, 0);
 }
 EXPORT_SYMBOL(sec_wd_disable);
 
-static int __init sec_wd_init(void)
+static int sec_wd_panic_event(struct notifier_block *this,
+		unsigned long event, void *ptr)
 {
-	printk(KERN_ALERT "initializing secure watchdog");
-	secure_api_call(SSAPI_ACTIVATE_SEC_WATCHDOG, 0, 0, 0, 0);
-	init_patter();
+	secure_api_call(SSAPI_DISABLE_SEC_WATCHDOG, 0, 0, 0, 0);
+	return NOTIFY_DONE;
+}
+
+static int sec_wd_reboot_event(struct notifier_block *this,
+		unsigned long event, void *ptr)
+{
+	secure_api_call(SSAPI_DISABLE_SEC_WATCHDOG, 0, 0, 0, 0);
+	return NOTIFY_DONE;
+}
+
+int is_soft_reset_boot(void)
+{
+	u32 reg;
+	int soft_rst;
+
+	reg = readl(KONA_ROOT_RST_VA + ROOT_RST_MGR_REG_RSTSTS_OFFSET);
+	soft_rst = (reg & ROOT_RST_MGR_REG_RSTSTS_CHIPSFTRST_DET_MASK)
+		>> ROOT_RST_MGR_REG_RSTSTS_CHIPSFTRST_DET_SHIFT;
+
+	return soft_rst;
+}
+
+int is_power_on_reset(void)
+{
+	unsigned int *reset_reason = (unsigned int *)
+				ioremap(SRAM_RST_REASON_BASE, 0x4);
+	unsigned int rst;
+
+	pr_debug("%s: reset_reason 0x%x\n", __func__, *reset_reason);
+
+	rst = (*reset_reason) & RST_REASON_MASK;
+
+	iounmap(reset_reason);
+	if (rst == POWERON_RESET)
+		return 1;
+	else
+		return 0;
+
+}
+
+static int __devinit sec_wd_probe(struct platform_device *pdev)
+{
+	sec_wd_enabled_mode = 0;
+
+	if (is_soft_reset_boot() || is_power_on_reset()) {
+		sec_wd_enabled_mode = 1;
+		sec_wd_panic_block.notifier_call = sec_wd_panic_event;
+		sec_wd_panic_block.priority = INT_MAX;
+
+		sec_wd_reboot_block.notifier_call = sec_wd_reboot_event;
+		sec_wd_reboot_block.priority = INT_MAX;
+
+
+		atomic_notifier_chain_register(&panic_notifier_list,
+				&sec_wd_panic_block);
+		blocking_notifier_chain_register(&reboot_notifier_list,
+				&sec_wd_reboot_block);
+
+		printk(KERN_ALERT "________initializing secure watchdog_____\n");
+		secure_api_call(SSAPI_ACTIVATE_SEC_WATCHDOG, 0, 0, 0, 0);
+		init_patter();
+	} else
+		printk(KERN_ALERT "_______disabling Sec watchdog_______\n");
+
+	printk(KERN_ALERT "_______sec_wd_probe succesfull_______\n");
 	return 0;
 }
-module_init(sec_wd_init);
+
+unsigned int is_sec_wd_enabled(void)
+{
+	return sec_wd_enabled_mode;
+}
+EXPORT_SYMBOL(is_sec_wd_enabled);
+
+static int sec_wd_suspend(struct platform_device *pdev)
+{
+	if  (is_sec_wd_enabled()) {
+		printk(KERN_ALERT "_______suspend:disabling secure watchdog_______\n");
+		sec_wd_disable();
+	}
+	return 0;
+}
+
+static int sec_wd_resume(struct platform_device *pdev)
+{
+	if  (is_sec_wd_enabled()) {
+		printk(KERN_ALERT "_______resume:enabling secure watchdog_______\n");
+		sec_wd_enable();
+	}
+	return 0;
+}
+
+static int __devexit sec_wd_remove(struct platform_device *pdev);
+
+
+static const struct of_device_id sec_wd_match[] = {
+	{ .compatible = "bcm,secure_watchdog" },
+	{ /* Sentinel */ }
+};
+
+static struct platform_driver sec_wd_pltfm_driver = {
+	.driver = {
+		   .name = "secure_watchdog",
+		   .owner = THIS_MODULE,
+	   .of_match_table = sec_wd_match,
+		   },
+	.probe = sec_wd_probe,
+	.remove = __devexit_p(sec_wd_remove),
+	.suspend = sec_wd_suspend,
+	.resume = sec_wd_resume,
+};
+
+static int __devexit sec_wd_remove(struct platform_device *pdev)
+{
+	if (is_sec_wd_enabled())
+		sec_wd_disable();
+	return 0;
+}
+
+static int __init sec_wd_init(void)
+{
+	return platform_driver_register(&sec_wd_pltfm_driver);
+}
 
 static void __exit sec_wd_exit(void)
 {
-	printk(KERN_ALERT "inside sec_wd_exit");
+	platform_driver_unregister(&sec_wd_pltfm_driver);
 }
+
+module_init(sec_wd_init);
 module_exit(sec_wd_exit);
 
 MODULE_AUTHOR("Oza");
