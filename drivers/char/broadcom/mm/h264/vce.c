bf46581c38fe7f0e161c78c289bebc45da96c0c4
anirudhp <anirudhp@broadcom.com>
2013-10-07 16:08:28 +0530

    VCE Codec :Performance optimisation
    [Defect / Enhancement / New Feature]
    Enhancement
    
    [Problem]
    vce_write uses writel for register writes
    Unnecessary calls to vce_reset
    
    [Solution]
    Directly writing the registers
    Removed the unnecessary calls
    
    [Reviewers]
    Kalicharan Baswa
    
    [Testing]
        [Test Cases]
        Test recording and playback
    
        [Label/CL# Synced To]
        sdb-common-android-jb-4.2.2
    
        [Platform]
        Hawaii
    
    Change-Id: I69a2b929adf96f7d8a05903a556339ff1b3ae40e
        [BROADCOM INTERNAL NOTES]
    Signed-off-by: anirudhp <anirudhp@broadcom.com>
    Reviewed-on: http://mps-gerrit.broadcom.com/40174
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Reviewed-by: Vinay Krishnamurthy <vinayk@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- drivers/char/broadcom/mm/h264/vce.c
+++ drivers/char/broadcom/mm/h264/vce.c
@@ -61,8 +61,8 @@ u32 h264_read(void *id, u32 reg)
 }
 static void vce_write(struct vce_device_t *vce, u32 reg, u32 value)
 {
-	mm_write_reg(vce->vaddr,
-		reg + (VCE_BASE - VIDEOCODEC_BASE_ADDRESS), value);
+	*((u32 *)(vce->vaddr + reg + VCE_BASE - VIDEOCODEC_BASE_ADDRESS)) =
+									value;
 }
 
 static u32 vce_read(struct vce_device_t *vce, u32 reg)
@@ -225,8 +225,10 @@ mm_job_status_e vce_start_job(void *device_id, mm_job_post_t *job,
 
 	switch (job->type) {
 	case H264_VCE_ENC_SLICE_JOB:
-		enc_info = (struct enc_info_t *)(jp + \
-					(sizeof(struct vce_launch_info_t)));
+		if (job->size != sizeof(struct vce_launch_info_t))
+			enc_info = (struct enc_info_t *)(jp + \
+					sizeof(struct vce_launch_info_t));
+
 		vce_info = (struct vce_launch_info_t *) \
 				(jp);
 		break;
@@ -256,7 +258,6 @@ mm_job_status_e vce_start_job(void *device_id, mm_job_post_t *job,
 		if (dec_info != NULL)
 			decodeSlice(id, dec_info);
 		/*Reset VCE*/
-		vce_reset(id);
 		/*Bound checks*/
 		/*vce_info->codesize is in words*/
 			if (vce_info->codesize > VCE_PROGRAM_MEM_SIZE) {
@@ -358,7 +359,6 @@ mm_job_status_e vce_start_job(void *device_id, mm_job_post_t *job,
 		}
 
 		/*Write the registers passed*/
-		vce_reset(id);
 		for (i = 0; i < VCE_REGISTERS_COUNT; i++) {
 			if (((vce_info->vce_regpst.changed_mask) >> i) & 0x1) {
 				vce_write(id, \
@@ -418,7 +418,6 @@ mm_job_status_e vce_start_job(void *device_id, mm_job_post_t *job,
 				vce_read(id, (VCE_REGISTERS_OFFSET+(i*4)));
 		}
 
-		vce_reset(id);
 
 		/*Write the registers passed*/
 		vce_write(id, (VCE_REGISTERS_OFFSET+(1*4)), \
