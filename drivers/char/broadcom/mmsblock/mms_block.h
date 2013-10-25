849cba05b727d7a99e0cbd389483bc24e4e71efe
jimji <jimji@broadcom.com>
2013-10-25 15:11:48 +0800

    Add API level control for MMS blocking
    [Defect / Enhancement / New Feature]
    MobC00375850
    
    [Problem]
    Details:
    In CTA security testing, the test APK will directly call http API
    to test MMS blocking control.Please add API level control for MMS
    blocking to meet CTA requirements.
    
    [Solution]
    
    [Reviewers]
    
    [Testing]
    [Test Cases]
    
    [Label/CL# Synced To]
    
    [Platform]
    Java Garnet
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: Icf0a58f49f1ff75988fab5f32ce1013bcfc19786
    Signed-off-by: jimji <jimji@broadcom.com>
    Reviewed-on: http://mps-gerrit.broadcom.com/41940
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>
    (cherry picked from commit 4c5fc67850bb8d6df8519716920346b5b2e125fb)
    Reviewed-on: http://mps-gerrit.broadcom.com/41998
    Reviewed-by: Vikram Prakash <vikramp@broadcom.com>
    (cherry picked from commit 429d16d24eef25ee2c4ea518af2f550b4860d573)
    Reviewed-on: http://mps-gerrit.broadcom.com/46609
    Reviewed-by: Babu Arunachalam <babua@broadcom.com>

---
+++ drivers/char/broadcom/mmsblock/mms_block.h
@@ -0,0 +1,34 @@
+/****************************************************************************
+*
+*     Copyright (c) 2009 Broadcom Corporation
+*
+*   Unless you and Broadcom execute a separate written software license
+*   agreement governing use of this software, this software is licensed to you
+*   under the terms of the GNU General Public License version 2, available
+*    at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html (the "GPL").
+*
+* Notwithstanding the above, under no circumstances may you combine this
+* software in any way with any other Broadcom software provided under a license
+* other than the GPL, without Broadcom's express prior written consent.
+*
+****************************************************************************/
+
+#ifndef _MMS_BLOCK_H
+#define _MMS_BLOCK_H
+
+struct white_list_node {
+	int uid;
+	struct white_list_node *next;
+};
+
+struct mms_event {
+	int uid;
+	char dest[24];
+};
+
+struct white_list_node *g_whitelistnode;
+int g_pid;
+
+extern void send_event_to_security_center(struct mms_event *pevent);
+
+#endif /* _MMS_BLOCK_H */
