cefa67ee55b29dee5a72ed556847c5f72e1e6658
Jean Lee <leej@broadcom.com>
2013-11-11 11:26:13 -0800

    WIFI: Dummy filer for preventing suspeding disconnection
    [Defects]
    	MobC00378128
    
    Change-Id: Iaddcf5abb081c785009508a940a1bdb57467e3e1
    Signed-off-by: Jean Lee <leej@broadcom.com>
    Reviewed-on: http://mps-gerrit.broadcom.com/45438
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Reviewed-by: Pierre Couillaud <pierre@broadcom.com>
    Reviewed-by: Vikram Prakash <vikramp@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- net/wireless/sysfs.c
+++ net/wireless/sysfs.c
@@ -100,7 +100,7 @@ static int wiphy_suspend(struct device *dev, pm_message_t state)
 
 	rtnl_lock();
 	if (rdev->wiphy.registered) {
-		if (!rdev->wowlan)
+		if (!rdev->wowlan && !rdev->wiphy.wowlan.flags)
 			cfg80211_leave_all(rdev);
 		if (rdev->ops->suspend)
 			ret = rdev_suspend(rdev, rdev->wowlan);
