645203442884c3d6f3779b01e65e567468412659
Juro Bystricky <juro@broadcom.com>
2013-12-21 15:28:03 -0800

    netpoll: zap_completion_queue: check IRQ state
    Return immediately from zap_completion_queue if called in_irq() or
    with irqs_disabled(). This is done to prevent calling __kfree_skb under
    any of these conditions, as this would result in a warning message from
    local_bh_enable_ip, most likely followed by a kernel panic.
    In addition, it also prevents calling of dev_kfree_skb_any(), but this
    is harmless as under these interrupt conditions the skb is simply
    re-queued in the completion queue to be zapped later.
    
    This patch addresses the kernel warning followed by an AP reset as
    documented in MobC00387309.
    
    Change-Id: If2ad00649810bbbb860d585a13b2c2cbe5b2aa35
    Signed-off-by: Juro Bystricky <juro@broadcom.com>
    Reviewed-on: http://mps-gerrit.broadcom.com/53433
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Reviewed-by: Chris Brand <chris.brand@broadcom.com>
    Reviewed-by: Pierre Couillaud <pierre@broadcom.com>
    Reviewed-by: Edwin Chan <echan@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- net/core/netpoll.c
+++ net/core/netpoll.c
@@ -377,8 +377,12 @@ static void reserve_skbs_list(void)
 static void zap_completion_queue(void)
 {
 	unsigned long flags;
-	struct softnet_data *sd = &get_cpu_var(softnet_data);
+	struct softnet_data *sd;
 
+	if (in_irq() || irqs_disabled())
+		return;
+
+	sd = &get_cpu_var(softnet_data);
 	if (sd->completion_queue) {
 		struct sk_buff *clist;
 
