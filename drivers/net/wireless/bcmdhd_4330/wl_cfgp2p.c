061614cddfbc21f549af5c161e624c84cdf67bfd
qiuxu <qiuxu.zhuo@broadcom>
2014-01-07 10:56:07 +0800

    WifiP2p: Fix nil p2p capability.
    [Defect / Enhancement / New Feature]
    MobC00390380
    
    [Problem]
    1. Download the cts verifier 4.4 R1 from
       "http://source.android.com/compatibility/downloads.html"
    2. Install the CTS verifier 4.4 R1.
    3. Open the CTS verifier.
    4. Tap "Wi-Fi Direct Test" and follow the instruction to test.
    5. Join p2p group test (PIN) and Join p2p group test (push
       button) failed.
    
    [Solution]
    Fix nil p2p capability.
    
    [Reviewers]
    
    [Testing]
        [Test Cases]
        Pass joining p2p group test (PIN) and Join p2p group test.
    
        [Label/CL# Synced To]
    	HEAD
    
        [Platform]
    	sdb-linux-3.10.0-hawaii-java
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: I04eb6b78ed8c8b7a48de3cd97af382706178fdb3
    Signed-off-by: qiuxu <qiuxu.zhuo@broadcom>
    Reviewed-on: http://mps-gerrit.broadcom.com/54880
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: Henry (Heng) Lien <lienh@broadcom.com>
    Reviewed-by: Shawn (Shaoqing) Chen <shaoqing@broadcom.com>
    Reviewed-by: Jean (JinEui) Lee <leej@broadcom.com>
    Reviewed-by: Dheeresh Mathur <dheeresh@broadcom.com>
    Reviewed-by: Vikram Prakash <vikramp@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- drivers/net/wireless/bcmdhd_4330/wl_cfgp2p.c
+++ drivers/net/wireless/bcmdhd_4330/wl_cfgp2p.c
@@ -2250,7 +2250,6 @@ wl_cfgp2p_find_attrib_in_all_p2p_Ies(u8 *parse, u32 len, u32 attrib)
 u8 *
 wl_cfgp2p_retreive_p2p_dev_addr(wl_bss_info_t *bi, u32 bi_length)
 {
-	wifi_p2p_ie_t * p2p_ie = NULL;
 	u8 *capability = NULL;
 	bool p2p_go	= 0;
 	u8 *ptr = NULL;
@@ -2268,11 +2267,13 @@ wl_cfgp2p_retreive_p2p_dev_addr(wl_bss_info_t *bi, u32 bi_length)
 	}
 
 	/* In probe responses, DEVICE INFO attribute will be present */
-	if (!(ptr = wl_cfgp2p_retreive_p2pattrib(p2p_ie, P2P_SEID_DEV_INFO))) {
+	if (!(ptr = wl_cfgp2p_retreive_p2pattrib(((u8 *) bi) + bi->ie_offset,
+			P2P_SEID_DEV_INFO))) {
 		/* If DEVICE_INFO is not found, this might be a beacon frame.
 		 * check for DEVICE_ID in the beacon frame.
 		 */
-		ptr = wl_cfgp2p_retreive_p2pattrib(p2p_ie, P2P_SEID_DEV_ID);
+		ptr = wl_cfgp2p_retreive_p2pattrib(((u8 *) bi) + bi->ie_offset,
+			P2P_SEID_DEV_ID);
 	}
 
 	if (!ptr)
