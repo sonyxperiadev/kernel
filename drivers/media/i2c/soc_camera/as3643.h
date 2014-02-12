89e17840ddcb88cf57ab9529c3fadb17ae13bfb2
Jun HE <junhe@broadcom.com>
2014-02-12 15:19:14 +0800

    Torch not working.
    [Defect / Enhancement / New Feature]
    MobC00400209
    
    [Problem]
    Flash sync commit broke torch feature
    
    [Solution]
    Merged flash lamp interface with flash sync commit.
    
    [Reviewers]
    Vinay, Alex
    
    [Testing]
        [Test Cases]
    
        [Label/CL# Synced To]
    
        [Platform]
        Java_KK
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: I894c3af842c18e54f3e758771d720bd9838be335
    Signed-off-by: Jun HE <junhe@broadcom.com>
    Reviewed-on: http://mps-gerrit.broadcom.com/63567
    Reviewed-by: Vinay Krishnamurthy <vinayk@broadcom.com>
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- drivers/media/i2c/soc_camera/as3643.h
+++ drivers/media/i2c/soc_camera/as3643.h
@@ -13,6 +13,7 @@
  * GNU General Public License for more details.
  */
 
+#if 0
 extern int as3643_set_flash(const u8 intensity, const u32 duration_us);
 extern int as3643_set_ind_led(int iled, const u32 duration_us);
 extern int as3643_set_torch_flash(int hpled);
@@ -21,3 +22,4 @@ extern int as3643_set_timer(int timer_val);
 extern int as3643_gpio_toggle(bool en);
 extern int as3643_clear_all(void);
 extern int as3643_gpio_strobe(int on);
+#endif
