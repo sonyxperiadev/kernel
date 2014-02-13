cc4264d1c6ee164672b999b96a8c49db6de0f4d6
Ashray Kulkarni <ashrayk@codeaurora.org>
2014-02-12 20:08:06 -0800

    msm: vidc: Add support for setting initial qp
    Adds support to set initial qp, thereby allowing the client to set
    initial qp for I,P, and B frames.
    
    Change-Id: Ie956651bde85e800d97a0007769af9aec8ca16a4
    Signed-off-by: Ashray Kulkarni <ashrayk@codeaurora.org>

--- drivers/media/platform/msm/vidc/vidc_hfi_api.h
+++ drivers/media/platform/msm/vidc/vidc_hfi_api.h
@@ -187,6 +187,7 @@ enum hal_property {
 	HAL_CONFIG_VENC_USELTRFRAME,
 	HAL_CONFIG_VENC_LTRPERIOD,
 	HAL_PARAM_VENC_HIER_P_NUM_FRAMES,
+	HAL_PARAM_VENC_ENABLE_INITIAL_QP,
 };
 
 enum hal_domain {
@@ -630,6 +631,13 @@ struct hal_quantization {
 	u32 qpp;
 	u32 qpb;
 	u32 layer_id;
+};
+
+struct hal_initial_quantization {
+	u32 qpi;
+	u32 qpp;
+	u32 qpb;
+	u32 initqp_enable;
 };
 
 struct hal_quantization_range {
