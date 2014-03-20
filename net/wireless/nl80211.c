d71f7d01da7bd75b17ada677f0832ab05d26f429
Amar Singhal <asinghal@codeaurora.org>
2014-03-19 18:36:58 -0700

    cfg80211: Use new wiphy flag WIPHY_FLAG_DFS_OFFLOAD
    When flag WIPHY_FLAG_DFS_OFFLOAD is defined, the driver would handle
    all the DFS related operations. Therefore the kernel needs to ignore
    the DFS state that it uses to block the userspace calls to the driver
    through cfg80211 APIs. Also it should treat the userspace calls to
    start radar detection as a no-op.
    
    Change-Id: I9dd2076945581ca67e54dfc96dd3dbc526c6f0a2
    CRs-Fixed: 630797
    Signed-off-by: Amar Singhal <asinghal@codeaurora.org>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -5717,6 +5717,9 @@ static int nl80211_start_radar_detection(struct sk_buff *skb,
 	if (err)
 		return err;
 
+	if (rdev->wiphy.flags & WIPHY_FLAG_DFS_OFFLOAD)
+		return -EOPNOTSUPP;
+
 	if (wdev->cac_started)
 		return -EBUSY;
 
