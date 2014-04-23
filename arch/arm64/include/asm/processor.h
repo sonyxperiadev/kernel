80793bec130c0f23ff57677fccd517834f50c2e4
Sumit Singh <sumsingh@nvidia.com>
2014-04-23 11:56:55 +0530

    arm64: processor.h: remove redundant macros
    Removing cpu_relaxed_read and cpu_relaxed_read_long macros from
    processor.h, as these macros are defined in asm/relaxed.h.
    
    bug 1440421
    
    Change-Id: Ic766ac6e34eefe93f90349c088626a0fb277670c
    Signed-off-by: Sumit Singh <sumsingh@nvidia.com>
    Reviewed-on: http://git-master/r/400127
    (cherry picked from commit 108bf0b30d72c52e33dd4fec71dd1ed5baf13ed2)
    Reviewed-on: http://git-master/r/422214
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- arch/arm64/include/asm/processor.h
+++ arch/arm64/include/asm/processor.h
@@ -34,7 +34,7 @@
 #include <asm/hw_breakpoint.h>
 #include <asm/ptrace.h>
 #include <asm/types.h>
-#include <asm/atomic.h>
+#include <asm/relaxed.h>
 
 #ifdef __KERNEL__
 #define STACK_TOP_MAX		TASK_SIZE_64
@@ -135,8 +135,6 @@ unsigned long get_wchan(struct task_struct *p);
 
 #define cpu_relax()			barrier()
 
-#define cpu_relaxed_read(p)		ldax32(p)
-#define cpu_relaxed_read_long(p)	ldax64((u64 *)p)
 #define cpu_read_relax()		wfe()
 
 /* Thread switching */
