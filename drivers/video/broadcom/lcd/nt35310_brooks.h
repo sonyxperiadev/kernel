f1ee94b836026593d525f7ea4c3ce6967c8d1480
Johan Olson <johan.olson@sonymobile.com>
2014-05-28 10:32:06 +0200

    video: bcm: nt35310 pwm polarity change
    Change CABC PWM polarity to active low instead of high. This will
    save some microamps but also in case we loose the PWM singal we
    will still get backlight.
    
    Change-Id: I43094597c7f588dded3e77dd56bcb44932a0b00b

--- drivers/video/broadcom/lcd/nt35310_brooks.h
+++ drivers/video/broadcom/lcd/nt35310_brooks.h
@@ -147,6 +147,11 @@ __initdata DISPCTRL_REC_T nt35310_brooks_cabc[] = {
 	{DISPCTRL_WR_DATA, 0x10},
 	{DISPCTRL_WR_CMND, 0xBF}, /* CMD 2 Page 1 */
 	{DISPCTRL_WR_DATA, 0xAA},
+	{DISPCTRL_WR_CMND, 0xBA}, /* LEDPWM polarity active LOW */
+	{DISPCTRL_WR_DATA, 0x24}, /* default */
+	{DISPCTRL_WR_DATA, 0x00}, /* default */
+	{DISPCTRL_WR_DATA, 0x20}, /* LEDPWM polarity bit */
+	{DISPCTRL_WR_DATA, 0x00}, /* default */
 	{DISPCTRL_WR_CMND, 0xC1}, /* PWM frequency 25.3K */
 	{DISPCTRL_WR_DATA, 0x20},
 	{DISPCTRL_WR_DATA, 0x00},
