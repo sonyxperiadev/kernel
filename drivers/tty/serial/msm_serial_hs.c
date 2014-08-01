a24979c703e9544febe8d57afd4ae62c8d4ba5c5
Dilip Kota <c_dkota@codeaurora.org>
2014-08-01 09:04:40 +0530

    msm_serial_hs: Handle core interrupt irrespective of wakeup interrupt
    Core interrupt should be disabled or enabled during
    clock on/off sequence irrespective of wakeup interrupt.
    
    Change-Id: Ia4fd824f0cc0ff9f054e98d6a52ef3988f9e0254
    Signed-off-by: Dilip Kota <c_dkota@codeaurora.org>

--- drivers/tty/serial/msm_serial_hs.c
+++ drivers/tty/serial/msm_serial_hs.c
@@ -2073,13 +2073,12 @@ static int msm_hs_check_clock_off(struct uart_port *uport)
 	if (use_low_power_wakeup(msm_uport)) {
 		msm_uport->wakeup.ignore = 1;
 		enable_irq(msm_uport->wakeup.irq);
-		/*
-		 * keeping uport-irq enabled all the time
-		 * gates XO shutdown in idle power collapse. Disable
-		 * this only when wakeup irq is set.
-		 */
-		disable_irq(uport->irq);
 	}
+	/*
+	 * keeping uport-irq enabled all the time
+	 * gates XO shutdown in idle power collapse.
+	 */
+	disable_irq(uport->irq);
 	wake_unlock(&msm_uport->dma_wake_lock);
 
 	spin_unlock_irqrestore(&uport->lock, flags);
@@ -2280,11 +2279,9 @@ void msm_hs_request_clock_on(struct uart_port *uport)
 	switch (cur_clk_state) {
 	case MSM_HS_CLK_OFF:
 		wake_lock(&msm_uport->dma_wake_lock);
-		if (use_low_power_wakeup(msm_uport)) {
+		if (use_low_power_wakeup(msm_uport))
 			disable_irq_nosync(msm_uport->wakeup.irq);
-			/* uport-irq was disabled when clocked off */
-			enable_irq(uport->irq);
-		}
+
 		spin_unlock_irqrestore(&uport->lock, flags);
 		mutex_unlock(&msm_uport->clk_mutex);
 		ret = msm_hs_clock_vote(msm_uport);
@@ -2295,6 +2292,8 @@ void msm_hs_request_clock_on(struct uart_port *uport)
 			"For UART CLK Stalling HSUART\n");
 			break;
 		}
+		/* uport-irq was disabled when clocked off */
+		enable_irq(uport->irq);
 
 		/* else fall-through */
 	case MSM_HS_CLK_REQUEST_OFF:
