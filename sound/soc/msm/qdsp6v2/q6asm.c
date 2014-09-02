ce0766e7338074a277e4c25ec0c18bffa83f5e04
Ajay Dudani <adudani@codeaurora.org>
2014-09-02 11:56:26 -0700

    ASoC: msm: Add Buffer overflow check
    The overflow check is required to ensure that user space data
    in kernel may not go beyond buffer boundary.
    
    Change-Id: Ia0c785b8c895c317e9f904bc738df5217b837191
    CRs-Fixed: 563086
    Signed-off-by: Asish Bhattacharya <asishb@codeaurora.org>
    Signed-off-by: Mohammad Johny Shaik <mjshai@codeaurora.org>
    Signed-off-by: Ajay Dudani <adudani@codeaurora.org>
    
    Conflicts:
    	sound/soc/msm/qdsp6v2/q6asm.c

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -52,6 +52,8 @@ enum {
 	ASM_MAX_CAL_TYPES
 };
 
+#define FRAME_NUM   (8)
+
 /* TODO, combine them together */
 static DEFINE_MUTEX(session_lock);
 struct asm_mmap {
@@ -1096,6 +1098,8 @@ int q6asm_audio_client_buf_alloc(unsigned int dir,
 			pr_debug("%s: buffer already allocated\n", __func__);
 			return 0;
 		}
+		if (bufcnt != FRAME_NUM)
+			goto fail;
 		mutex_lock(&ac->cmd_lock);
 		if (bufcnt > (LONG_MAX/sizeof(struct audio_buffer))) {
 			pr_err("%s: Buffer size overflows", __func__);
