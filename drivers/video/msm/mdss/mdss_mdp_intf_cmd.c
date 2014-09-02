67665c44bd1c4b39899050ffb98e32bf5c6cda15
Naseer Ahmed <naseer@codeaurora.org>
2014-09-02 18:44:38 -0400

    mdss: Change kickoff timeout to 300ms
    Bug: 17265465
    Signed-off-by: Naseer Ahmed <naseer@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
@@ -23,9 +23,6 @@
 
 #define MAX_SESSIONS 2
 
-/* wait for at most 2 vsync for lowest refresh rate (24hz) */
-#define KOFF_TIMEOUT msecs_to_jiffies(84)
-
 #define STOP_TIMEOUT(hz) \
 	msecs_to_jiffies(2 * (1000 / hz) * (VSYNC_EXPIRE_TICK + 2))
 #define POWER_COLLAPSE_TIME msecs_to_jiffies(100)
