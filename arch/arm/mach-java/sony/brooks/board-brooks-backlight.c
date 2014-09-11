059d43f7ed74bd317f93662ec67323c6e6ad704c
Johan Olson <johan.olson@sonymobile.com>
2014-09-11 15:53:38 +0200

    Revert "tetra: Add backlight config for turning on fast"
    This reverts commit 43db02ffb293be6fa8e7ec07d9b86eeb32924143.
    
    Change-Id: Icd59f0db7a78094472e2ff78fa61f4035a2e8e29
    [bug: 17481248]

--- arch/arm/mach-java/sony/brooks/board-brooks-backlight.c
+++ arch/arm/mach-java/sony/brooks/board-brooks-backlight.c
@@ -5,20 +5,10 @@
 #include <linux/platform_device.h>
 #include <linux/regulator/consumer.h>
 #include <linux/platform_data/lm3630a_bl.h>
-#include <video/kona_fb.h>
 #include "board-brooks.h"
 
 #define BMU_NFC_I2C_BUS_ID 1
 #define BMU_HW_EN 24
-
-static struct lm3630a_first_frame_ctrl first_frame_data = {
-	.register_notifier = &kona_fb_register_notifier,
-	.notifier_event = KONA_FB_EVENT_FIRST_FRAME,
-	.backlight_value = 75,
-	.ramp_on_off = 0x8,
-	.leda_ctrl = true,
-	.ledb_ctrl = false,
-};
 
 static struct lm3630a_platform_data lm3630a_bmu_platform_data = {
 	.leda_init_brt = 128,
@@ -32,7 +22,6 @@ static struct lm3630a_platform_data lm3630a_bmu_platform_data = {
 	.ramp_on_off = 0x0,
 	.ramp_run = 0x0,
 	.enable_gpio = BMU_HW_EN,
-	.first_frame_ctrl = &first_frame_data,
 };
 
 static struct i2c_board_info __initdata lm3630a_i2c_boardinfo[] = {
