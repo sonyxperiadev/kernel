f9d697d61689867a7503fb35f279d9bb69e1e01a
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-09-11 16:33:11 +0530

    sched: Fix reference to stale task_struct in try_to_wake_up()
    try_to_wake_up() currently drops p->pi_lock and later checks for need
    to notify cpufreq governor on task migrations or wakeups. However the
    woken task could exit between the time p->pi_lock is released and the
    time the test for notification is run. As a result, the test for
    notification could refer to an exited task. task_notify_on_migrate(p)
    could thus lead to invalid memory reference.
    
    Fix this by running the test for notification with task's pi_lock
    held.
    
    Change-Id: I1c7a337473d2d8e79342a015a179174ce00702e1
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    [osvaldob@codeaurora.org: resolve merge conflicts on rel. branch]
    Signed-off-by: Osvaldo Banuelos <osvaldob@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1633,6 +1633,8 @@ try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags)
 	unsigned long flags;
 	int cpu, success = 0;
 	unsigned long src_cpu;
+	int notify = 0;
+	struct migration_notify_data mnd;
 
 	/*
 	 * If we are going to wake up a thread waiting for CONDITION we
@@ -1683,12 +1685,8 @@ try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags)
 	ttwu_queue(p, cpu);
 stat:
 	ttwu_stat(p, cpu, wake_flags);
-out:
-	raw_spin_unlock_irqrestore(&p->pi_lock, flags);
 
 	if (task_notify_on_migrate(p)) {
-		struct migration_notify_data mnd;
-
 		mnd.src_cpu = src_cpu;
 		mnd.dest_cpu = cpu;
 		mnd.load = pct_task_load(p);
@@ -1702,9 +1700,16 @@ out:
 		 */
 		if ((src_cpu != cpu) || (mnd.load >
 					sysctl_sched_wakeup_load_threshold))
-			atomic_notifier_call_chain(&migration_notifier_head,
-					   0, (void *)&mnd);
+			notify = 1;
 	}
+
+out:
+	raw_spin_unlock_irqrestore(&p->pi_lock, flags);
+
+	if (notify)
+		atomic_notifier_call_chain(&migration_notifier_head,
+					   0, (void *)&mnd);
+
 	return success;
 }
 
