217492baa56677692ff593e904fb2818f665bbdb
Chirayu Desai <chirayudesai1@gmail.com>
2014-09-18 11:37:27 +0530

    Add a common stub config for shinano
    * Add stubs CONFIG_MACH_SHINANO,
      selected if any of their devices are selected.
    * Much cleaner ifdefs in {clock,board}-8974.c now.
    
    Change-Id: I315a9599c8430da18a70ac2154798e5f30089525
    
    Conflicts:
    	arch/arm/mach-msm/Kconfig

--- drivers/tty/serial/msm_serial_hs.c
+++ drivers/tty/serial/msm_serial_hs.c
@@ -2568,8 +2568,9 @@ struct msm_serial_hs_platform_data
 		return ERR_PTR(-EINVAL);
 	}
 
+#ifdef CONFIG_MACH_SONY_SHINANO
 	pdata->exit_lpm_cb = bcm_bt_lpm_exit_lpm_locked;
-
+#endif
 	MSM_HS_DBG("tx_ep_pipe_index:%d rx_ep_pipe_index:%d\n"
 		"tx_gpio:%d rx_gpio:%d rfr_gpio:%d cts_gpio:%d",
 		pdata->bam_tx_ep_pipe_index, pdata->bam_rx_ep_pipe_index,
