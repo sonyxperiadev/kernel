b8f9aa9dfa14b88b59f2ad7b61024e5d0662ebfc
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-11-04 14:24:19 +0530

    sched: Add API to set task's initial task load
    Add a per-task attribute, init_load_pct, that is used to initialize
    newly created children's initial task load. This helps important
    applications to be launched on cpus with highest capacity.
    
    CRs-Fixed: 761799
    Change-Id: Ie9665fd2aeb15203f95fd7f211c50bebbaa18727
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1164,6 +1164,11 @@ struct task_struct {
 #ifdef CONFIG_SCHED_HMP
 	struct ravg ravg;
 	u64 run_start;
+	/*
+	 * 'init_load_pct' represents the initial task load assigned to children
+	 * of this task
+	 */
+	u32 init_load_pct;
 #endif
 #ifdef CONFIG_CGROUP_SCHED
 	struct task_group *sched_task_group;
@@ -1910,6 +1915,8 @@ sched_set_cpu_cstate(int cpu, int cstate, int wakeup_energy, int wakeup_latency)
 
 #ifdef CONFIG_SCHED_HMP
 extern int sched_set_boost(int enable);
+extern int sched_set_init_task_load(struct task_struct *p, int init_load_pct);
+extern u32 sched_get_init_task_load(struct task_struct *p);
 #else
 static inline int sched_set_boost(int enable)
 {
