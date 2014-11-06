d4db5968419b7b0f95370a6889b3ef679011a4ce
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-11-06 15:14:55 +0100

    sony: msm8974: implement last_kmsg

--- arch/arm/mach-msm/board-8974-console.c
+++ arch/arm/mach-msm/board-8974-console.c
@@ -11,6 +11,7 @@
 #include <linux/kernel.h>
 #include <linux/console.h>
 #include <linux/string.h>
+#include <ram_console.h>
 #include <asm/setup.h>
 
 #define CONSOLE_NAME "ttyHSL"
@@ -33,3 +34,25 @@ static int __init setup_serial_console(char *console_flag)
 * enable flag will be passed as Kernel boot arg with tag babe09A9.
 */
 __setup("oemandroidboot.babe09a9=", setup_serial_console);
+
+#ifdef CONFIG_ANDROID_RAM_CONSOLE
+static char bootreason[128];
+
+int __init msm_boot_reason(char *s)
+{
+	int n;
+
+	if (*s == '=')
+		s++;
+	n = snprintf(bootreason, sizeof(bootreason),
+		 "Boot info:\n"
+		 "Last boot reason: %s\n", s);
+	bootreason[n] = '\0';
+	return 1;
+}
+__setup("bootreason", msm_boot_reason);
+
+struct ram_console_platform_data ram_console_pdata = {
+	.bootinfo = bootreason,
+};
+#endif
