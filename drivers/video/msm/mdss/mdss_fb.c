71e684447cc73c4801e172b44e5a06c5b350fc97
Adam Farden <adam@farden.cz>
2014-11-23 20:53:20 +0100

    video: Fix build on Flamingo due to bad ifdef
    Signed-off-by: Adam Farden <adam@farden.cz>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -504,7 +504,7 @@ static int mdss_fb_probe(struct platform_device *pdev)
 		break;
 	}
 
-#ifdef CONFIG_DEBUG_FS
+#if defined(CONFIG_DEBUG_FS) && defined(CONFIG_FB_MSM_MDSS_PANEL_SPECIFIC)
 	if ((mfd->panel_info->type == MIPI_VIDEO_PANEL) ||
 		(mfd->panel_info->type == MIPI_CMD_PANEL))
 		mipi_dsi_panel_create_debugfs(mfd);
@@ -566,7 +566,7 @@ static int mdss_fb_remove(struct platform_device *pdev)
 	if (mfd->key != MFD_KEY)
 		return -EINVAL;
 
-#ifdef CONFIG_DEBUG_FS
+#if defined(CONFIG_DEBUG_FS) && defined(CONFIG_FB_MSM_MDSS_PANEL_SPECIFIC)
 	if ((mfd->panel_info->type == MIPI_VIDEO_PANEL) ||
 		(mfd->panel_info->type == MIPI_CMD_PANEL))
 		mipi_dsi_panel_remove_debugfs(mfd);
