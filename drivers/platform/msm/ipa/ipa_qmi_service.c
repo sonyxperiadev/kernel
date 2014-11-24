c8fc6428aec7fe4d4b10b9454911cf2802b1f70a
Skylar Chang <chiaweic@codeaurora.org>
2014-11-24 14:18:49 -0800

    msm ipa: fix the APPS QMI SSR reset sequence
    During modem-encryption enable scenario, we saw
    RILD in the Android framework always initializes
    the modem SSR in the first time reboot and we
    saw modem crashed because apps-side didn't
    clean internal QMI-flag well and continue the last
    time's bootup sequence which causes modem crash.
    The fix is to move the flag reset to the
    qmi_service_init.
    
    Change-Id: I5dba3cb8f44cddf180d5210543ea2ad8399645a3
    Signed-off-by: Skylar Chang <chiaweic@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_qmi_service.c
+++ drivers/platform/msm/ipa/ipa_qmi_service.c
@@ -632,8 +632,6 @@ static void ipa_qmi_service_init_worker(struct work_struct *work)
 
 	/* Initialize QMI-service*/
 	IPAWANDBG("IPA A7 QMI init OK :>>>>\n");
-	qmi_modem_init_fin = false;
-	qmi_indication_fin = false;
 
 	ipa_svc_workqueue = create_singlethread_workqueue("ipa_A7_svc");
 	if (!ipa_svc_workqueue) {
@@ -708,6 +706,8 @@ int ipa_qmi_service_init(bool load_uc, uint32_t wan_platform_type)
 {
 	ipa_wan_platform = wan_platform_type;
 	is_load_uc = load_uc;
+	qmi_modem_init_fin = false;
+	qmi_indication_fin = false;
 	if (!ipa_svc_handle) {
 		INIT_WORK(&ipa_qmi_service_init_work,
 			ipa_qmi_service_init_worker);
@@ -759,4 +759,6 @@ void ipa_qmi_service_exit(void)
 	}
 
 	ipa_svc_handle = 0;
+	qmi_modem_init_fin = false;
+	qmi_indication_fin = false;
 }
