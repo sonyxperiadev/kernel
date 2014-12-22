778b0f1a79a3fcd554d762f09a117cba8e57df7b
Angelo G. Del Regno -- EternityProject Team Manager <kholk11@gmail.com>
2014-12-22 16:04:57 +0000

    arm: msm: 8226: Remove ifdefs and "refactor" Sony clocks
    Well, ifdefs are bad and not really clean.
    Remove the ifdefs and initialize the clocks after detecting
    the board we're running on.

---
+++ arch/arm/mach-msm/sony_board.h
@@ -0,0 +1,20 @@
+/* arch/arm/mach-msm/sony_board.h
+ *
+ * Copyright (C) 2014 Sony Mobile Communications AB.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2, as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
+
+#ifndef __SONY_BOARD_H
+#define __SONY_BOARD_H
+
+#include <linux/kernel.h>
+
+extern struct clock_init_data msm8226_eagle_clock_init_data __initdata;
+extern struct clock_init_data msm8226_flamingo_clock_init_data __initdata;
+extern struct clock_init_data msm8226_seagull_clock_init_data __initdata;
+
+#endif
