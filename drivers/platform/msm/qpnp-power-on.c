ec04d85a77be97e7d51057d35932a65a9b7230a1
Angelo G. Del Regno -- EternityProject Team Manager <kholk11@gmail.com>
2014-12-24 19:49:41 +0000

    platform: msm: Add QPNP Power Key force-crash

--- drivers/platform/msm/qpnp-power-on.c
+++ drivers/platform/msm/qpnp-power-on.c
@@ -26,6 +26,9 @@
 
 /* Common PNP defines */
 #define QPNP_PON_REVISION2(base)		(base + 0x01)
+#ifdef CONFIG_POWERKEY_FORCECRASH
+#include "forcecrash.h"
+#endif
 
 /* PON common register addresses */
 #define QPNP_PON_RT_STS(base)			(base + 0x10)
@@ -97,6 +100,11 @@
 #define QPNP_PON_REV_B				0x01
 
 #define QPNP_PON_BUFFER_SIZE			9
+
+#ifdef CONFIG_POWERKEY_FORCECRASH
+static int forcecrash_on;
+module_param(forcecrash_on, int, S_IRUGO | S_IWUSR);
+#endif
 
 enum pon_type {
 	PON_KPDPWR,
@@ -462,6 +470,11 @@ qpnp_pon_input_dispatch(struct qpnp_pon *pon, u32 pon_type)
 	switch (cfg->pon_type) {
 	case PON_KPDPWR:
 		pon_rt_bit = QPNP_PON_KPDPWR_N_SET;
+#ifdef CONFIG_POWERKEY_FORCECRASH
+		if (forcecrash_on)
+			qpnp_powerkey_forcecrash_timer_setup(pon_rt_sts &
+					pon_rt_bit);
+#endif
 		break;
 	case PON_RESIN:
 		pon_rt_bit = QPNP_PON_RESIN_N_SET;
@@ -1373,6 +1386,10 @@ static int qpnp_pon_probe(struct spmi_device *spmi)
 
 	INIT_DELAYED_WORK(&pon->bark_work, bark_work_func);
 
+#ifdef CONFIG_POWERKEY_FORCECRASH
+	qpnp_powerkey_forcecrash_init(spmi, pon->base);
+#endif
+
 	/* register the PON configurations */
 	rc = qpnp_pon_config_init(pon);
 	if (rc) {
@@ -1413,6 +1430,10 @@ static int qpnp_pon_remove(struct spmi_device *spmi)
 
 	if (pon->pon_input)
 		input_unregister_device(pon->pon_input);
+
+#ifdef CONFIG_POWERKEY_FORCECRASH
+	qpnp_powerkey_forcecrash_exit(spmi);
+#endif
 
 	return 0;
 }
