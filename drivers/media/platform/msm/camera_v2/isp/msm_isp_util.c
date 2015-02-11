arter97/caf_msm-5.4

msm: camera: Propagate IOMMU page fault error
Propagate IOMMU page fault to user space.

Change-Id: Ic5b00d4f45a73c3f69c4de1b4f11f87762e88b59
Signed-off-by: Nagesh Subba Reddy <nageshsreddy@codeaurora.org>

drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
@@ -1526,6 +1526,22 @@ void msm_isp_update_error_frame_count(struct vfe_device *vfe_dev)
 		error_info->info_dump_frame_count++;
 }
 
+
+void ms_isp_process_iommu_page_fault(struct vfe_device *vfe_dev)
+{
+	struct msm_isp_event_data error_event;
+
+	pr_err("%s:%d] vfe_dev %p id %d\n", __func__,
+		__LINE__, vfe_dev, vfe_dev->pdev->id);
+
+	vfe_dev->buf_mgr->ops->buf_mgr_debug(vfe_dev->buf_mgr);
+
+	error_event.frame_id =
+		vfe_dev->axi_data.src_info[VFE_PIX_0].frame_id;
+	msm_isp_send_event(vfe_dev,
+					ISP_EVENT_IOMMU_P_FAULT, &error_event);
+}
+
 void msm_isp_process_error_info(struct vfe_device *vfe_dev)
 {
 	int i;
@@ -1754,10 +1770,7 @@ void msm_isp_do_tasklet(unsigned long data)
 		queue_cmd->iommu_page_fault = 0;
 		spin_unlock_irqrestore(&vfe_dev->tasklet_lock, flags);
 		if (iommu_page_fault > 0) {
-			pr_err("%s:%d] vfe_dev %p id %d\n", __func__,
-				__LINE__, vfe_dev, vfe_dev->pdev->id);
-			vfe_dev->buf_mgr->ops->
-				buf_mgr_debug(vfe_dev->buf_mgr);
+			ms_isp_process_iommu_page_fault(vfe_dev);
 			continue;
 		}
 		ISP_DBG("%s: status0: 0x%x status1: 0x%x\n",

sonyxperiadev/kernel

drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
@@ -1526,6 +1526,22 @@ void msm_isp_update_error_frame_count(struct vfe_device *vfe_dev)
 		error_info->info_dump_frame_count++;
 }
 
+
+void msm_isp_process_iommu_page_fault(struct vfe_device *vfe_dev)
+{
+	struct msm_isp_event_data error_event;
+
+	pr_err("%s:%d] vfe_dev %p id %d\n", __func__,
+		__LINE__, vfe_dev, vfe_dev->pdev->id);
+
+	vfe_dev->buf_mgr->ops->buf_mgr_debug(vfe_dev->buf_mgr);
+
+	error_event.frame_id =
+		vfe_dev->axi_data.src_info[VFE_PIX_0].frame_id;
+	msm_isp_send_event(vfe_dev,
+					ISP_EVENT_IOMMU_P_FAULT, &error_event);
+}
+
 void msm_isp_process_error_info(struct vfe_device *vfe_dev)
 {
 	int i;
@@ -1754,10 +1770,7 @@ void msm_isp_do_tasklet(unsigned long data)
 		queue_cmd->iommu_page_fault = 0;
 		spin_unlock_irqrestore(&vfe_dev->tasklet_lock, flags);
 		if (iommu_page_fault > 0) {
-			pr_err("%s:%d] vfe_dev %p id %d\n", __func__,
-				__LINE__, vfe_dev, vfe_dev->pdev->id);
-			vfe_dev->buf_mgr->ops->
-				buf_mgr_debug(vfe_dev->buf_mgr);
+			msm_isp_process_iommu_page_fault(vfe_dev);
 			continue;
 		}
 		ISP_DBG("%s: status0: 0x%x status1: 0x%x\n",