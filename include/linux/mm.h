41e77044d613b0093f8d48be871341dbb11f564f
Tommy-Geenexus <roboter972@gmail.com>
2015-03-23 19:04:57 +0100

    mm: lower maximum readahead size * This is the root cause of some sudden performance issues. Lowering this value to the minimum (and normally default) solves these lags.

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1601,7 +1601,7 @@ int write_one_page(struct page *page, int wait);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-#define VM_MAX_READAHEAD	2048	/* kbytes */
+#define VM_MAX_READAHEAD	128	/* kbytes */
 #define VM_MIN_READAHEAD	32	/* kbytes (includes current page) */
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
