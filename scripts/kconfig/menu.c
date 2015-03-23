3e4e2cc3dcc828f2b324e051641ddc3daf64c4e6
Tommy-Geenexus <roboter972@gmail.com>
2015-03-23 19:02:27 +0100

    scripts: fix warnings about uninitialized variables

--- scripts/kconfig/menu.c
+++ scripts/kconfig/menu.c
@@ -525,7 +525,7 @@ static void get_prompt_str(struct gstr *r, struct property *prop,
 {
 	int i, j;
 	struct menu *submenu[8], *menu, *location = NULL;
-	struct jump_key *jump;
+	struct jump_key *jump = NULL;
 
 	str_printf(r, _("Prompt: %s\n"), _(prop->text));
 	menu = prop->menu->parent;
