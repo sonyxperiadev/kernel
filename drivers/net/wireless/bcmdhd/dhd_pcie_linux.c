8b27f258c3f4bb11449d120e0e62b266451bc52f
Insun Song <isong@broadcom.com>
2015-05-18 13:00:47 -0700

    net: wireless: bcmdhd: Add bcm4358 support
    Change-Id: Ic12fcdade86a10e9d4248cd85f59efd83caf734a
    Signed-off-by: Insun Song <isong@broadcom.com>

--- drivers/net/wireless/bcmdhd/dhd_pcie_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_pcie_linux.c
@@ -49,7 +49,11 @@
 #include <dhd_pcie.h>
 #include <dhd_linux.h>
 #if defined (CONFIG_ARCH_MSM)
+#ifdef CONFIG_64BIT
+#include <linux/msm_pcie.h>
+#else
 #include <mach/msm_pcie.h>
+#endif
 #endif
 
 #define PCI_CFG_RETRY 		10
