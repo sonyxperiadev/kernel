a49083e0d393275628eb7f4a65970fd34b42b5f8
Angelo G. Del Regno <kholk11@gmail.com>
2015-07-16 18:04:31 +0200

    media: msm: sensor: Rename ZFRONT to IMX132
    We've finally got a name. Update the driver accordingly.

--- drivers/media/platform/msm/camera_v2/sensor/imx132.c
+++ drivers/media/platform/msm/camera_v2/sensor/imx132.c
@@ -19,13 +19,13 @@
 #include "msm_camera_i2c_mux.h"
 #include "eeprom/msm_eeprom.h"
 
-#define ZFRONT_SENSOR_NAME "zfront"
+#define IMX132_SENSOR_NAME "imx132"
 #define SONY_SENSOR_NAME_LEN 8
-DEFINE_MSM_MUTEX(zfront_mut);
-static struct msm_sensor_ctrl_t zfront_s_ctrl;
+DEFINE_MSM_MUTEX(imx132_mut);
+static struct msm_sensor_ctrl_t imx132_s_ctrl;
 extern struct msm_eeprom_memory_block_t *sony_eeprom_blocks;
 
-static struct msm_sensor_power_setting zfront_power_setting[] = {
+static struct msm_sensor_power_setting imx132_power_setting[] = {
 	{
 		.seq_type = SENSOR_VREG,
 		.seq_val = CAM_VDIG,
@@ -64,7 +64,7 @@ static struct msm_sensor_power_setting zfront_power_setting[] = {
 	},
 };
 
-static int32_t zfront_sensor_match_id(
+static int32_t imx132_sensor_match_id(
 		struct msm_sensor_ctrl_t *s_ctrl)
 {
 #ifdef CONFIG_SONY_CAMERA
@@ -89,7 +89,7 @@ static int32_t zfront_sensor_match_id(
 	return -EINVAL;
 }
 
-static struct v4l2_subdev_info zfront_subdev_info[] = {
+static struct v4l2_subdev_info imx132_subdev_info[] = {
 	{
 		.code = 0, //V4L2_MBUS_FMT_SRGGB10_1X10,
 		.colorspace = V4L2_COLORSPACE_JPEG,
@@ -98,110 +98,110 @@ static struct v4l2_subdev_info zfront_subdev_info[] = {
 	},
 };
 
-static const struct i2c_device_id zfront_i2c_id[] = {
+static const struct i2c_device_id imx132_i2c_id[] = {
 	{
-		ZFRONT_SENSOR_NAME,
-		(kernel_ulong_t)&zfront_s_ctrl
+		IMX132_SENSOR_NAME,
+		(kernel_ulong_t)&imx132_s_ctrl
 	},
 	{ }
 };
 
-static int32_t zfront_i2c_probe(struct i2c_client *client,
+static int32_t imx132_i2c_probe(struct i2c_client *client,
 		const struct i2c_device_id *id)
 {
-	return msm_sensor_i2c_probe(client, id, &zfront_s_ctrl);
+	return msm_sensor_i2c_probe(client, id, &imx132_s_ctrl);
 }
 
-static struct i2c_driver zfront_i2c_driver = {
-	.id_table = zfront_i2c_id,
-	.probe  = zfront_i2c_probe,
+static struct i2c_driver imx132_i2c_driver = {
+	.id_table = imx132_i2c_id,
+	.probe  = imx132_i2c_probe,
 	.driver = {
-		.name = ZFRONT_SENSOR_NAME,
+		.name = IMX132_SENSOR_NAME,
 	},
 };
 
-static struct msm_camera_i2c_client zfront_sensor_i2c_client = {
+static struct msm_camera_i2c_client imx132_sensor_i2c_client = {
 	.addr_type = MSM_CAMERA_I2C_WORD_ADDR,
 };
 
-static const struct of_device_id zfront_dt_match[] = {
+static const struct of_device_id imx132_dt_match[] = {
 	{
-		.compatible = "qcom,zfront",
-		.data = &zfront_s_ctrl},
+		.compatible = "qcom,imx132",
+		.data = &imx132_s_ctrl},
 	{ }
 };
 
-MODULE_DEVICE_TABLE(of, zfront_dt_match);
+MODULE_DEVICE_TABLE(of, imx132_dt_match);
 
-static struct platform_driver zfront_platform_driver = {
+static struct platform_driver imx132_platform_driver = {
 	.driver = {
-		.name = "qcom,zfront",
+		.name = "qcom,imx132",
 		.owner = THIS_MODULE,
-		.of_match_table = zfront_dt_match,
+		.of_match_table = imx132_dt_match,
 	},
 };
 
-static int32_t zfront_platform_probe(
+static int32_t imx132_platform_probe(
 		struct platform_device *pdev)
 {
 	int32_t rc = 0;
 	const struct of_device_id *match;
-	match = of_match_device(zfront_dt_match, &pdev->dev);
+	match = of_match_device(imx132_dt_match, &pdev->dev);
 	rc = msm_sensor_platform_probe(pdev, match->data);
 	return rc;
 }
 
-static int __init zfront_init_module(void)
+static int __init imx132_init_module(void)
 {
 	int32_t rc = 0;
 	pr_info("%s:%d\n", __func__, __LINE__);
-	zfront_s_ctrl.power_setting_array.power_setting =
-				zfront_power_setting;
-	zfront_s_ctrl.power_setting_array.size =
-				ARRAY_SIZE(zfront_power_setting);
+	imx132_s_ctrl.power_setting_array.power_setting =
+				imx132_power_setting;
+	imx132_s_ctrl.power_setting_array.size =
+				ARRAY_SIZE(imx132_power_setting);
 
-	rc = platform_driver_probe(&zfront_platform_driver,
-		zfront_platform_probe);
+	rc = platform_driver_probe(&imx132_platform_driver,
+		imx132_platform_probe);
 	if (!rc)
 		return rc;
 	pr_err("%s:%d rc %d\n", __func__, __LINE__, rc);
-	return i2c_add_driver(&zfront_i2c_driver);
+	return i2c_add_driver(&imx132_i2c_driver);
 }
 
-static void __exit zfront_exit_module(void)
+static void __exit imx132_exit_module(void)
 {
 	pr_info("%s:%d\n", __func__, __LINE__);
-	if (zfront_s_ctrl.pdev) {
-		msm_sensor_free_sensor_data(&zfront_s_ctrl);
-		platform_driver_unregister(&zfront_platform_driver);
+	if (imx132_s_ctrl.pdev) {
+		msm_sensor_free_sensor_data(&imx132_s_ctrl);
+		platform_driver_unregister(&imx132_platform_driver);
 	} else
-		i2c_del_driver(&zfront_i2c_driver);
+		i2c_del_driver(&imx132_i2c_driver);
 	return;
 }
 
 #ifdef CONFIG_SONY_CAMERA
-static struct msm_sensor_fn_t zfront_sensor_func_tbl = {
+static struct msm_sensor_fn_t imx132_sensor_func_tbl = {
 	.sensor_config = msm_sensor_config,
 #ifdef CONFIG_COMPAT
 	.sensor_config32 = msm_sensor_config32,
 #endif
 	.sensor_power_up = msm_sensor_power_up,
 	.sensor_power_down = msm_sensor_power_down,
-	.sensor_match_id = zfront_sensor_match_id,
+	.sensor_match_id = imx132_sensor_match_id,
 };
 #endif
 
-static struct msm_sensor_ctrl_t zfront_s_ctrl = {
-	.sensor_i2c_client = &zfront_sensor_i2c_client,
-	.msm_sensor_mutex = &zfront_mut,
-	.sensor_v4l2_subdev_info = zfront_subdev_info,
-	.sensor_v4l2_subdev_info_size = ARRAY_SIZE(zfront_subdev_info),
+static struct msm_sensor_ctrl_t imx132_s_ctrl = {
+	.sensor_i2c_client = &imx132_sensor_i2c_client,
+	.msm_sensor_mutex = &imx132_mut,
+	.sensor_v4l2_subdev_info = imx132_subdev_info,
+	.sensor_v4l2_subdev_info_size = ARRAY_SIZE(imx132_subdev_info),
 #ifdef CONFIG_SONY_CAMERA
-	.func_tbl = &zfront_sensor_func_tbl,
+	.func_tbl = &imx132_sensor_func_tbl,
 #endif
 };
 
-module_init(zfront_init_module);
-module_exit(zfront_exit_module);
-MODULE_DESCRIPTION("zfront");
+module_init(imx132_init_module);
+module_exit(imx132_exit_module);
+MODULE_DESCRIPTION("imx132");
 MODULE_LICENSE("GPL v2");
