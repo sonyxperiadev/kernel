aa5580325605d43655965b06985b2473914eefbd
Angelo G. Del Regno <kholk11@gmail.com>
2015-07-29 15:50:29 +0200

    drivers: leds: qpnp-wled: Add SoMC properties for current calculation
    This way we will control the backlight brightness current, needed
    on devices which may draw too much current from the battery at
    boot up time.

--- drivers/leds/leds-qpnp-wled.c
+++ drivers/leds/leds-qpnp-wled.c
@@ -170,6 +170,8 @@
 #define QPNP_WLED_MIN_MSLEEP		20
 #define QPNP_WLED_SC_DLY_MS		20
 
+#define QPNP_WLED_CURR_SCALE_MAX	100
+
 /* output feedback mode */
 enum qpnp_wled_fdbk_op {
 	QPNP_WLED_FDBK_AUTO,
@@ -283,6 +285,9 @@ struct qpnp_wled {
 	bool ibb_bias_active;
 	bool lab_fast_precharge;
 	bool en_ext_pfet_sc_pro;
+
+	bool calc_curr;
+	int curr_scale;
 };
 
 static struct qpnp_wled *gwled;
@@ -335,6 +340,10 @@ static int qpnp_wled_set_level(struct qpnp_wled *wled, int level)
 {
 	int i, rc;
 	u8 reg;
+
+	if (wled->calc_curr &&
+		wled->curr_scale != QPNP_WLED_CURR_SCALE_MAX)
+		level = (level * wled->curr_scale) / QPNP_WLED_CURR_SCALE_MAX;
 
 	/* set brightness registers */
 	for (i = 0; i < wled->num_strings; i++) {
@@ -1131,7 +1140,13 @@ static int qpnp_wled_config(struct qpnp_wled *wled)
 		if (rc < 0)
 			return rc;
 		reg &= QPNP_WLED_FS_CURR_MASK;
-		temp = wled->fs_curr_ua / QPNP_WLED_FS_CURR_STEP_UA;
+		if (wled->calc_curr)
+			temp = (wled->fs_curr_ua +
+				(QPNP_WLED_FS_CURR_STEP_UA - 1)) /
+					QPNP_WLED_FS_CURR_STEP_UA;
+		else
+			temp = wled->fs_curr_ua /
+					QPNP_WLED_FS_CURR_STEP_UA;
 		reg |= temp;
 		rc = qpnp_wled_write_reg(wled, &reg,
 				QPNP_WLED_FS_CURR_REG(wled->sink_base,
@@ -1275,6 +1290,27 @@ static int qpnp_wled_config(struct qpnp_wled *wled)
 		}
 	}
 
+	return 0;
+}
+
+static int qpnp_wled_set_scale(struct qpnp_wled *wled)
+{
+	u8 reg = 0;
+	int rc, curr;
+
+	wled->curr_scale = QPNP_WLED_CURR_SCALE_MAX;
+	rc = qpnp_wled_read_reg(wled, &reg,
+			QPNP_WLED_FS_CURR_REG(wled->sink_base,
+				wled->strings[0]));
+	if (rc)
+		return rc;
+	reg &= ~QPNP_WLED_FS_CURR_MASK;
+	curr = (reg * QPNP_WLED_FS_CURR_STEP_UA);
+	if (curr > wled->fs_curr_ua)
+		wled->curr_scale = (wled->fs_curr_ua *
+			QPNP_WLED_CURR_SCALE_MAX) / curr;
+	pr_debug("%s: curr=%d fs_curr_ua=%d curr_scale=%d\n", __func__,
+				curr, wled->fs_curr_ua, wled->curr_scale);
 	return 0;
 }
 
@@ -1432,6 +1468,8 @@ static int qpnp_wled_parse_dt(struct qpnp_wled *wled)
 		return rc;
 	}
 
+	wled->calc_curr = of_property_read_bool(spmi->dev.of_node,
+			"somc,calc-curr");
 	wled->en_9b_dim_res = of_property_read_bool(spmi->dev.of_node,
 			"qcom,en-9b-dim-res");
 	wled->en_phase_stag = of_property_read_bool(spmi->dev.of_node,
@@ -1477,7 +1515,6 @@ static int qpnp_wled_parse_dt(struct qpnp_wled *wled)
 
 	wled->en_ext_pfet_sc_pro = of_property_read_bool(spmi->dev.of_node,
 					"qcom,en-ext-pfet-sc-pro");
-
 	return 0;
 }
 
@@ -1541,6 +1578,15 @@ static int qpnp_wled_probe(struct spmi_device *spmi)
 	if (rc) {
 		dev_err(&spmi->dev, "wled config failed\n");
 		return rc;
+	}
+
+	/* SoMC calc_curr */
+	if (wled->calc_curr) {
+		rc = qpnp_wled_set_scale(wled);
+		if (rc) {
+			dev_err(&spmi->dev, "wled config failed\n");
+			return rc;
+		}
 	}
 
 	mutex_init(&wled->lock);
