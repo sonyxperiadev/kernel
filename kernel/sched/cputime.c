71259a81d16a9ea9e5a6bfd47435a6b280e05b4b
Naveen Ramaraj <nramaraj@codeaurora.org>
2015-08-18 17:04:11 -0700

    sched: Do not track cpu power for tasks if arch does not support it
    This fixes the undefined references to acct_update_power() when
    building for ARCH=um introduced by commit: c0a74c5d
    
    Bug: 21631099
    Change-Id: I8bd9ac69652b84585b06b7589a5030d8e5104387
    Signed-off-by: Naveen Ramaraj <nramaraj@codeaurora.org>

--- kernel/sched/cputime.c
+++ kernel/sched/cputime.c
@@ -160,8 +160,10 @@ void account_user_time(struct task_struct *p, cputime_t cputime,
 	/* Account for user time used */
 	acct_account_cputime(p);
 
+#ifdef CONFIG_CPU_FREQ_STAT
 	/* Account power usage for user time */
 	acct_update_power(p, cputime);
+#endif
 }
 
 /*
@@ -213,8 +215,10 @@ void __account_system_time(struct task_struct *p, cputime_t cputime,
 	/* Account for system time used */
 	acct_account_cputime(p);
 
+#ifdef CONFIG_CPU_FREQ_STAT
 	/* Account power usage for system time */
 	acct_update_power(p, cputime);
+#endif
 }
 
 /*
