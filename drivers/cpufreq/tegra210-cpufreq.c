7d7588269971df04ae13c3e1b8fa67425055ed18
Francisco Rojo <frojo@google.com>
2015-08-24 10:59:17 -0700

    CHROMIUM: cpuquiet: Make cpuquiet platform agnostic and tegra support
    If platforms wish to use cpuquiet, they must:
    
    1) Provide a struct cpuquiet_platform_info with relevant platform
    information
    2) Call cpuquiet_init() and pass in struct cpuquiet_platform_info
    
    This was done for tegra210 in this patch
    
    Additionally, fixes a few bugs which would cause crashes during boot due
    to trying to initialize cpuquiet governors before the actual cpuquiet
    module was initialized.
    
    BUG=chrome-os-partner:44148
    TEST=Boot smaug and check that /sys/devices/system/cpu/cpuquiet is
    	present, and that /sys/devices/system/cpu/online is changing
    
    Change-Id: I18401be6eed036c70042dcbc3c3a15d51cfbc487
    Signed-off-by: Francisco Rojo <frojo@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/295394
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>

--- drivers/cpufreq/tegra210-cpufreq.c
+++ drivers/cpufreq/tegra210-cpufreq.c
@@ -19,6 +19,7 @@
 #include <linux/clk.h>
 #include <linux/clk-provider.h>
 #include <linux/cpufreq.h>
+#include <linux/cpuquiet.h>
 #include <linux/err.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
@@ -395,6 +396,11 @@ static struct platform_driver tegra210_cpufreq_platdrv = {
 	.remove		= tegra210_cpufreq_remove,
 };
 
+static struct cpuquiet_platform_info tegra_plat_info = {
+	.plat_name = "tegra",
+	.avg_hotplug_latency_ms = 2,
+};
+
 static int __init tegra_cpufreq_init(void)
 {
 	int ret;
@@ -412,6 +418,8 @@ static int __init tegra_cpufreq_init(void)
 		platform_driver_unregister(&tegra210_cpufreq_platdrv);
 		return PTR_ERR(pdev);
 	}
+
+	cpuquiet_init(&tegra_plat_info);
 
 	return 0;
 }
