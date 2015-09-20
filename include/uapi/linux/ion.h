1341be55c95a457bc901713644f1b687f6ef1f71
David Viteri <davidteri91@gmail.com>
2015-09-20 15:12:08 +0200

    ion: correct header include path
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- include/uapi/linux/ion.h
+++ include/uapi/linux/ion.h
@@ -1,6 +1,6 @@
 #ifndef __UAPI_LINUX_ION_H__
 #define __UAPI_LINUX_ION_H__
 
-#include <linux/staging/android/uapi/ion.h>
+#include <linux/../../../drivers/staging/android/uapi/ion.h>
 
 #endif /* __UAPI_LINUX_ION_H__ */
