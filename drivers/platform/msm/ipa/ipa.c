36e6cd418db91fdc562b2a27397607c04a69c6c4
Niranjan Pendharkar <npendhar@codeaurora.org>
2015-09-30 15:55:44 -0700

    msm: ipa: Fix missing wakelock issue
    In some use-case, when ipa-driver's
    sps_event_cb is called however the
    sps_process_irq_work is not scheduled
    immediately. The fix is to hold the
    wakelock until our workqueue is
    scheduled.
    
    b/24338185
    
    Change-Id: I8e1d9cd1c736cad2b1bb43fb8cb2c0014d28ee1c
    Signed-off-by: Skylar Chang <chiaweic@codeaurora.org>
    Signed-off-by: Niranjan Pendharkar <npendhar@codeaurora.org>

--- drivers/platform/msm/ipa/ipa.c
+++ drivers/platform/msm/ipa/ipa.c
@@ -2849,6 +2849,7 @@ static void ipa_sps_process_irq(struct work_struct *work)
 
 	/* release IPA clocks */
 	ipa_sps_process_irq_schedule_rel();
+	ipa_dec_release_wakelock();
 	spin_unlock_irqrestore(&ipa_ctx->sps_pm.lock, flags);
 }
 
@@ -2941,6 +2942,7 @@ static void sps_event_cb(enum sps_callback_case event, void *param)
 				ipa_ctx->sps_pm.res_granted = true;
 				*ready = true;
 			} else {
+				ipa_inc_acquire_wakelock();
 				queue_work(ipa_ctx->sps_power_mgmt_wq,
 					   &ipa_sps_process_irq_work);
 				*ready = false;
