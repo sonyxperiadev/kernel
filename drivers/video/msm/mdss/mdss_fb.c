9a9a0ccf15227b435d592cdb1b7e883067181a0a
Dhaval Patel <pdhaval@codeaurora.org>
2015-10-21 10:55:51 -0700

    msm: mdss: fix backlight update between unblank and kickoff
    The backlight update between unblank and kickoff
    call may result in garbage/snow screen on panel
    based on panel ram initialized status.
    
    For ex:
      -> Unblank the panel - black frame transfer start,
                             panel is on but ram is still
                             uninitialized.
      -> backlight update - turn on the backlight. It will show
                            data from uninitialized ram because
                            frame transfer is not complete yet.
    
    This can be avoided by blocking the backlight update
    request between unblank and first frame kickoff. This
    gives guarantee that panel has valid data to display
    when backlight was turned on.
    
    Change-Id: I09c707cd10acd53bd6aa0935885269ffc5aec649
    Signed-off-by: Dhaval Patel <pdhaval@codeaurora.org>
    Signed-off-by: Raghavendra Ambadas <rambad@codeaurora.org>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -796,7 +796,7 @@ static int mdss_fb_blanking_mode_switch(struct msm_fb_data_type *mfd, int mode)
 	unlock_fb_info(mfd->fbi);
 
 	mutex_lock(&mfd->bl_lock);
-	mfd->bl_updated = true;
+	mfd->allow_bl_update = true;
 	mdss_fb_set_backlight(mfd, bl_lvl);
 	mutex_unlock(&mfd->bl_lock);
 
@@ -1629,7 +1629,7 @@ void mdss_fb_set_backlight(struct msm_fb_data_type *mfd, u32 bkl_lvl)
 #endif /* CONFIG_FB_MSM_MDSS_SPECIFIC_PANEL */
 
 	if ((((mdss_fb_is_power_off(mfd) && mfd->dcm_state != DCM_ENTER)
-		|| !mfd->bl_updated) && !IS_CALIB_MODE_BL(mfd)) ||
+		|| !mfd->allow_bl_update) && !IS_CALIB_MODE_BL(mfd)) ||
 		mfd->panel_info->cont_splash_enabled) {
 		mfd->unset_bl_level = bkl_lvl;
 		return;
@@ -1687,7 +1687,7 @@ void mdss_fb_update_backlight(struct msm_fb_data_type *mfd)
 	if (!mfd->unset_bl_level)
 		return;
 	mutex_lock(&mfd->bl_lock);
-	if (!mfd->bl_updated) {
+	if (!mfd->allow_bl_update) {
 		pdata = dev_get_platdata(&mfd->pdev->dev);
 		if ((pdata) && (pdata->set_backlight)) {
 			mfd->bl_level = mfd->unset_bl_level;
@@ -1700,7 +1700,7 @@ void mdss_fb_update_backlight(struct msm_fb_data_type *mfd)
 								"pp_bl_event");
 			pdata->set_backlight(pdata, temp);
 			mfd->bl_level_scaled = mfd->unset_bl_level;
-			mfd->bl_updated = 1;
+			mfd->allow_bl_update = true;
 		}
 	}
 	mutex_unlock(&mfd->bl_lock);
@@ -1803,7 +1803,7 @@ static int mdss_fb_blank_blank(struct msm_fb_data_type *mfd,
 			mdss_fb_stop_disp_thread(mfd);
 		mutex_lock(&mfd->bl_lock);
 		mdss_fb_set_backlight(mfd, 0);
-		mfd->bl_updated = 0;
+		mfd->allow_bl_update = false;
 		mfd->unset_bl_level = current_bl;
 #ifdef CONFIG_SOMC_PANEL_LEGACY
 /* TODO: CHECKME!! This piece of code may be deprecated by recent
@@ -1903,8 +1903,8 @@ static int mdss_fb_blank_unblank(struct msm_fb_data_type *mfd)
 	/* Reset the backlight only if the panel was off */
 	if (mdss_panel_is_power_off(cur_power_state)) {
 		mutex_lock(&mfd->bl_lock);
-		if (!mfd->bl_updated) {
-			mfd->bl_updated = 1;
+		if (!mfd->allow_bl_update) {
+			mfd->allow_bl_update = true;
 			/*
 			 * 1.) If in AD calibration mode then frameworks would
 			 * not be allowed to update backlight hence post unblank
@@ -1923,6 +1923,13 @@ static int mdss_fb_blank_unblank(struct msm_fb_data_type *mfd)
 			else if (!mfd->panel_info->mipi.post_init_delay ||
 				cur_panel_dead)
 				mdss_fb_set_backlight(mfd, mfd->unset_bl_level);
+
+			/*
+			 * it blocks the backlight update between unblank and
+			 * first kickoff to avoid backlight turn on before black
+			 * frame is transferred to panel through unblank call.
+			 */
+			mfd->allow_bl_update = false;
 		}
 		mutex_unlock(&mfd->bl_lock);
 	}
