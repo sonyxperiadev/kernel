d87178ffede06bba431d3c819c10cda388e0500d
Angelo G. Del Regno <kholk11@gmail.com>
2015-11-19 15:09:29 +0100

    usb: dwc3: Make pwr_event_irq optional
    Some devices do not need it. For example Sony MSM8974 devices.

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -3096,7 +3096,8 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 	mdwc->pwr_event_irq = platform_get_irq_byname(pdev, "pwr_event_irq");
 	if (mdwc->pwr_event_irq < 0) {
 		dev_err(&pdev->dev, "pget_irq for pwr_event_irq failed\n");
-		goto disable_ref_clk;
+		//goto disable_ref_clk;
+		mdwc->pwr_event_irq = 0;
 	} else {
 		/*
 		 * enable pwr event irq early during PM resume to meet bus
