b51202fff834db3592568dcd077b5dcc0815ba2d
Humberto Borba <humberos@gmail.com>
2015-11-19 17:10:38 -0200

    drivers: msm_serial: Use pinctrl when uart port is closed
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I8a09e63282541937bf249776e78e822ccac54f4d

--- drivers/tty/serial/msm_serial_hs.c
+++ drivers/tty/serial/msm_serial_hs.c
@@ -2392,7 +2392,10 @@ static void msm_hs_unconfig_uart_gpios(struct uart_port *uport)
 					pdev->dev.platform_data;
 	struct msm_hs_port *msm_uport = UARTDM_TO_MSM(uport);
 
-	if (pdata) {
+	if (!IS_ERR_OR_NULL(msm_uport->pinctrl)) {
+		pinctrl_select_state(msm_uport->pinctrl,
+				msm_uport->gpio_state_suspend);
+	} else if (pdata) {
 		if (gpio_is_valid(pdata->uart_tx_gpio))
 			gpio_free(pdata->uart_tx_gpio);
 		if (gpio_is_valid(pdata->uart_rx_gpio))
