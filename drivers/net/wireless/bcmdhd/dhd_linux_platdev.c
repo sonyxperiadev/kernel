c2df2c4c6e0ea949a20072bf77c997fc91e44647
Angelo G. Del Regno <kholk11@gmail.com>
2015-11-19 12:54:09 +0100

    bcmdhd: Fix call to get_country_code
    Pass flags to get_country_code function

--- drivers/net/wireless/bcmdhd/dhd_linux_platdev.c
+++ drivers/net/wireless/bcmdhd/dhd_linux_platdev.c
@@ -254,7 +254,7 @@ void *wifi_platform_get_country_code(wifi_adapter_info_t *adapter, char *ccode,
 
 	DHD_TRACE(("%s\n", __FUNCTION__));
 	if (plat_data->get_country_code) {
-		return plat_data->get_country_code(ccode);
+		return plat_data->get_country_code(ccode, flags);
 	}
 #endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39)) */
 
