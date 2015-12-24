42a625856b862af0d221fea6ab7c7c27a13cfdfe
Humberto Borba <humberos@gmail.com>
2015-12-24 01:06:29 -0200

    mdss: mdp: Update partial_update_enable during mode switch
    This is part of https://github.com/sonyxperiadev/kernel/commit/ade25ccc836de911f6efe290da28fd3845ec96ea
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I684b51dcd300f0ab1fe6242ff13ae85f23860fe3

--- drivers/video/msm/mdss/mdss_dsi_panel_driver.c
+++ drivers/video/msm/mdss/mdss_dsi_panel_driver.c
@@ -3062,9 +3062,11 @@ static int mdss_dsi_parse_panel_features(struct device_node *np,
 //	pinfo->cont_splash_enabled = of_property_read_bool(np,
 //		"qcom,cont-splash-enabled");
 
+	pinfo->partial_update_supported = of_property_read_bool(np,
+		"qcom,partial-update-enabled");
+
 	if (pinfo->mipi.mode == DSI_CMD_MODE) {
-		pinfo->partial_update_enabled = of_property_read_bool(np,
-				"qcom,partial-update-enabled");
+		pinfo->partial_update_enabled = pinfo->partial_update_supported;
 		pr_info("%s: partial_update_enabled=%d\n", __func__,
 					pinfo->partial_update_enabled);
 		ctrl->set_col_page_addr = mdss_dsi_set_col_page_addr;
