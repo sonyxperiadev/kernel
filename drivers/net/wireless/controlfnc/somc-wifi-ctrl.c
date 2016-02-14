b1bd1aefc244d2a3bf0b67500142ef94de42bb7f
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-02-14 12:00:58 +0100

    wireless:controlfnc: split wlan_mem_prealloc
    wlan_mem_prealloc is Broadcom memory alocation and
    should be split from main somc-control-func
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- drivers/net/wireless/controlfnc/somc-wifi-ctrl.c
+++ drivers/net/wireless/controlfnc/somc-wifi-ctrl.c
@@ -26,6 +26,8 @@
 #include <linux/of_gpio.h>
 #include <linux/module.h>
 #include <linux/fs.h>
+#include "dhd_custom_memprealloc.h"
+
 #if defined(CONFIG_BCMDHD_SDIO)
 #include <asm/mach/mmc.h>
 #include <../drivers/mmc/host/msm_sdcc.h>
@@ -45,61 +47,6 @@ static char *intf_macaddr = NULL;
 #define WIFI_POWER_PMIC_GPIO 18
 #define WIFI_IRQ_GPIO 67
 
-/* These definitions need to be aligned with bcmdhd */
-#define WLAN_STATIC_SCAN_BUF 5
-#define WLAN_STATIC_DHD_INFO_BUF 7
-#define WLAN_STATIC_DHD_IF_FLOW_LKUP 9
-#define WLAN_STATIC_DHD_PKTID_MAP 12
-
-#define ESCAN_BUF_SIZE (64 * 1024) /* for WIPHY_ESCAN0 */
-#define PREALLOC_WLAN_SEC_NUM 4
-#define PREALLOC_WLAN_BUF_NUM 160
-#define PREALLOC_WLAN_SECTION_HEADER 24
-
-#define WLAN_DHD_INFO_BUF_SIZE (24 * 1024)
-#define WLAN_DHD_IF_FLOW_LKUP_SIZE (36 * 1024)
-
-#define DHD_PKTIDMAP_FIFO_MAX 4
-#define WLAN_MAX_PKTID_ITEMS (8192)
-#define WLAN_DHD_PKTID_MAP_HDR_SIZE (20 + 4 * (WLAN_MAX_PKTID_ITEMS + 1))
-#define WLAN_DHD_PKTID_MAP_ITEM_SIZE (48)
-#define WLAN_DHD_PKTID_MAP_SIZE (WLAN_DHD_PKTID_MAP_HDR_SIZE + \
-	(DHD_PKTIDMAP_FIFO_MAX * (WLAN_MAX_PKTID_ITEMS + 1) * \
-	WLAN_DHD_PKTID_MAP_ITEM_SIZE))
-
-#define WLAN_SECTION_SIZE_0 (PREALLOC_WLAN_BUF_NUM * 128)  /* for PROT */
-#define WLAN_SECTION_SIZE_1 0                              /* for RXBUF */
-#define WLAN_SECTION_SIZE_2 0                              /* for DATABUF */
-#define WLAN_SECTION_SIZE_3 (PREALLOC_WLAN_BUF_NUM * 1024) /* for OSL_BUF */
-
-/* These definitions are copied from bcmdhd */
-#define DHD_SKB_1PAGE_BUFSIZE (PAGE_SIZE * 1)
-#define DHD_SKB_2PAGE_BUFSIZE (PAGE_SIZE * 2)
-#define DHD_SKB_4PAGE_BUFSIZE (PAGE_SIZE * 4)
-
-#define DHD_SKB_1PAGE_BUF_NUM 0
-#define DHD_SKB_2PAGE_BUF_NUM 64
-#define DHD_SKB_4PAGE_BUF_NUM 0
-
-#define WLAN_SKB_1_2PAGE_BUF_NUM ((DHD_SKB_1PAGE_BUF_NUM) + \
-	(DHD_SKB_2PAGE_BUF_NUM))
-#define WLAN_SKB_BUF_NUM ((WLAN_SKB_1_2PAGE_BUF_NUM) + \
-	(DHD_SKB_4PAGE_BUF_NUM))
-
-static struct sk_buff *wlan_static_skb[WLAN_SKB_BUF_NUM];
-
-struct wifi_mem_prealloc {
-	void *mem_ptr;
-	unsigned long size;
-};
-
-static struct wifi_mem_prealloc wifi_mem_array[PREALLOC_WLAN_SEC_NUM] = {
-	{ NULL, (WLAN_SECTION_SIZE_0 + PREALLOC_WLAN_SECTION_HEADER) },
-	{ NULL, (WLAN_SECTION_SIZE_1 + PREALLOC_WLAN_SECTION_HEADER) },
-	{ NULL, (WLAN_SECTION_SIZE_2 + PREALLOC_WLAN_SECTION_HEADER) },
-	{ NULL, (WLAN_SECTION_SIZE_3 + PREALLOC_WLAN_SECTION_HEADER) }
-};
-
 struct bcmdhd_platform_data {
 	struct platform_device *pdev;
 	struct pinctrl *pinctrl;
@@ -110,94 +57,6 @@ struct bcmdhd_platform_data {
 };
 
 static struct bcmdhd_platform_data *bcmdhd_data;
-
-static void *wlan_static_scan_buf;
-static void *wlan_static_dhd_info_buf;
-static void *wlan_static_if_flow_lkup;
-static void *wlan_static_dhd_pktid_map;
-
-
-static int somc_wifi_init_mem(void)
-{
-	int i;
-	for (i = 0; i < WLAN_SKB_BUF_NUM; i++)
-		wlan_static_skb[i] = NULL;
-
-	for (i = 0; i < DHD_SKB_1PAGE_BUF_NUM; i++) {
-		wlan_static_skb[i] = dev_alloc_skb(DHD_SKB_1PAGE_BUFSIZE);
-		if (!wlan_static_skb[i])
-			goto err_skb_alloc;
-	}
-
-	for (i = DHD_SKB_1PAGE_BUF_NUM; i < WLAN_SKB_1_2PAGE_BUF_NUM; i++) {
-		wlan_static_skb[i] = dev_alloc_skb(DHD_SKB_2PAGE_BUFSIZE);
-		if (!wlan_static_skb[i])
-			goto err_skb_alloc;
-	}
-
-	for (i = 0; i < PREALLOC_WLAN_SEC_NUM; i++) {
-		if (wifi_mem_array[i].size > 0) {
-			wifi_mem_array[i].mem_ptr =
-				kzalloc(wifi_mem_array[i].size, GFP_KERNEL);
-
-			if (!wifi_mem_array[i].mem_ptr)
-				goto err_mem_alloc;
-		}
-	}
-
-	wlan_static_scan_buf = kzalloc(ESCAN_BUF_SIZE, GFP_KERNEL);
-	if (!wlan_static_scan_buf) {
-		printk("%s: failed to allocate wlan_static_scan_buf\n",
-			__func__);
-		goto err_mem_alloc;
-	}
-
-	wlan_static_dhd_info_buf = kzalloc(WLAN_DHD_INFO_BUF_SIZE, GFP_KERNEL);
-	if (!wlan_static_dhd_info_buf) {
-		printk("%s: failed to allocate wlan_static_dhd_info_buf\n",
-			__func__);
-		goto err_mem_alloc;
-	}
-
-	wlan_static_if_flow_lkup = kzalloc(WLAN_DHD_IF_FLOW_LKUP_SIZE,
-		GFP_KERNEL);
-	if (!wlan_static_if_flow_lkup) {
-		printk("%s: failed to allocate wlan_static_if_flow_lkup\n",
-			__func__);
-		goto err_mem_alloc;
-	}
-	wlan_static_dhd_pktid_map = kzalloc(WLAN_DHD_PKTID_MAP_SIZE,
-		GFP_KERNEL);
-	if (!wlan_static_dhd_pktid_map) {
-		printk("%s: failed to allocate wlan_static_dhd_pktid_map\n",
-			__func__);
-		goto err_mem_alloc;
-	}
-
-	printk("%s: Wi-Fi static memory allocated\n", __func__);
-	return 0;
-
-err_mem_alloc:
-	kzfree(wlan_static_dhd_pktid_map);
-	kzfree(wlan_static_if_flow_lkup);
-	kzfree(wlan_static_dhd_info_buf);
-	kzfree(wlan_static_scan_buf);
-	printk("%s: failed to allocate mem_alloc\n", __func__);
-	for (i--; i >= 0; i--) {
-		kzfree(wifi_mem_array[i].mem_ptr);
-		wifi_mem_array[i].mem_ptr = NULL;
-	}
-
-	i = WLAN_SKB_BUF_NUM;
-err_skb_alloc:
-	printk(KERN_ERR "%s: failed to allocate skb_alloc\n", __func__);
-	for (i--; i >= 0; i--) {
-		dev_kfree_skb(wlan_static_skb[i]);
-		wlan_static_skb[i] = NULL;
-	}
-
-	return -ENOMEM;
-}
 
 #if defined(CONFIG_BCMDHD_SDIO)
 int wcf_status_register(void (*cb)(int card_present, void *dev), void *dev)
@@ -220,50 +79,6 @@ unsigned int wcf_status(struct device *dev)
 	return g_wifi_detect;
 }
 #endif
-
-static void *somc_wifi_mem_prealloc(int section, unsigned long size)
-{
-	if (section == PREALLOC_WLAN_SEC_NUM)
-		return wlan_static_skb;
-	if (section == WLAN_STATIC_SCAN_BUF)
-		return wlan_static_scan_buf;
-
-	if (section == WLAN_STATIC_DHD_INFO_BUF) {
-		if (size > WLAN_DHD_INFO_BUF_SIZE) {
-			printk("%s: request DHD_INFO size(%lu) is bigger than"
-				" static size(%d).\n", __func__, size,
-				WLAN_DHD_INFO_BUF_SIZE);
-			return NULL;
-		}
-		return wlan_static_dhd_info_buf;
-	}
-
-	if (section == WLAN_STATIC_DHD_IF_FLOW_LKUP)  {
-		if (size > WLAN_DHD_IF_FLOW_LKUP_SIZE) {
-			printk("%s: request DHD_IF_FLOW size(%lu) is bigger"
-				" than static size(%d).\n", __func__, size,
-				WLAN_DHD_IF_FLOW_LKUP_SIZE);
-			return NULL;
-		}
-		return wlan_static_if_flow_lkup;
-	}
-
-	if (section == WLAN_STATIC_DHD_PKTID_MAP)  {
-		if (size > WLAN_DHD_PKTID_MAP_SIZE) {
-			printk("%s: request DHD_PKTID size(%lu) is"
-				" bigger than static size(%d).\n", __func__,
-				size, WLAN_DHD_PKTID_MAP_SIZE);
-			return NULL;
-		}
-		return wlan_static_dhd_pktid_map;
-	}
-
-	if ((section < 0) || (section > PREALLOC_WLAN_SEC_NUM))
-		return NULL;
-	if (size > wifi_mem_array[section].size)
-		return NULL;
-	return wifi_mem_array[section].mem_ptr;
-}
 
 int somc_wifi_set_power(int on)
 {
@@ -614,7 +429,7 @@ struct wifi_platform_data somc_wifi_control = {
 	.set_reset	= somc_wifi_set_reset,
 #endif
 	.set_carddetect	= somc_wifi_set_carddetect,
-	.mem_prealloc	= somc_wifi_mem_prealloc,
+	.mem_prealloc	= dhd_wlan_mem_prealloc,
 	.get_mac_addr	= somc_wifi_get_mac_addr,
 };
 
@@ -644,8 +459,6 @@ static struct platform_device somc_wifi = {
 
 static int __init somc_wifi_init_on_boot(void)
 {
-	if (somc_wifi_init_mem())
-		return -ENOMEM;
 #if defined(CONFIG_BCMDHD_SDIO)
 	somc_wifi.resource->start = gpio_to_irq(WIFI_IRQ_GPIO);
 	somc_wifi.resource->end = gpio_to_irq(WIFI_IRQ_GPIO);
