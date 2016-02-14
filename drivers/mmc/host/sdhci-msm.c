b2be358f0c7cc5dd5f7ec3a22dbaa6ede9ffcad9
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-02-14 13:04:54 +0100

    mmc:host:sdhci-msm add WIFI_CONTROL_FUNC
    The patch is needed for Broadcom SDHCI WiFi implementation on Sony
    Devices
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -1960,6 +1960,11 @@ struct sdhci_msm_pltfm_data *sdhci_msm_populate_pdata(struct device *dev,
 	if (of_get_property(np, "qcom,core_3_0v_support", NULL))
 		pdata->core_3_0v_support = true;
 
+#ifdef CONFIG_WIFI_CONTROL_FUNC
+	if (of_get_property(np, "somc,use-for-wifi", NULL))
+		pdata->use_for_wifi = true;
+#endif
+
 	return pdata;
 out:
 	return NULL;
@@ -4190,6 +4195,8 @@ static bool sdhci_msm_is_bootdevice(struct device *dev)
 	return true;
 }
 
+extern void somc_wifi_mmc_host_register(struct mmc_host *host);
+
 static int sdhci_msm_probe(struct platform_device *pdev)
 {
 	const struct sdhci_msm_offset *msm_host_offset;
@@ -4650,6 +4657,13 @@ static int sdhci_msm_probe(struct platform_device *pdev)
 	pm_runtime_enable(&pdev->dev);
 	pm_runtime_set_autosuspend_delay(&pdev->dev, MSM_AUTOSUSPEND_DELAY_MS);
 	pm_runtime_use_autosuspend(&pdev->dev);
+
+#ifdef CONFIG_WIFI_CONTROL_FUNC
+	if (msm_host->pdata->use_for_wifi) {
+		msm_host->mmc->caps &= ~MMC_CAP_NEEDS_POLL;
+		somc_wifi_mmc_host_register(msm_host->mmc);
+	}
+#endif
 
 	msm_host->msm_bus_vote.max_bus_bw.show = show_sdhci_max_bus_bw;
 	msm_host->msm_bus_vote.max_bus_bw.store = store_sdhci_max_bus_bw;
--- drivers/mmc/host/sdhci-msm.h
+++ drivers/mmc/host/sdhci-msm.h
@@ -153,6 +153,10 @@ struct sdhci_msm_pltfm_data {
 	u32 ice_clk_min;
 	struct sdhci_msm_pm_qos_data pm_qos_data;
 	bool core_3_0v_support;
+
+#ifdef CONFIG_WIFI_CONTROL_FUNC
+	bool use_for_wifi;
+#endif
 };
 
 struct sdhci_msm_bus_vote {
