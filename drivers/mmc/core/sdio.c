6621d4f12aa0118ccde412e0acb373fab3f7f5d6
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-02-15 07:15:56 +0100

    force 1.8v if host has the capability
    change is needed for Broadcom Wifi
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- drivers/mmc/core/sdio.c
+++ drivers/mmc/core/sdio.c
@@ -1091,6 +1091,25 @@ static int mmc_sdio_resume(struct mmc_host *host)
 	return err;
 }
 
+/*
+ * Select low voltage 1.8v if mmc host has the capability
+ * and other volatages are not available.
+ */
+static int mmc_select_low_voltage(struct mmc_host *host, u32 ocr)
+{
+	int ret = 0;
+
+	if ((host->ocr_avail == MMC_VDD_165_195) && mmc_host_uhs(host) &&
+		((ocr & host->ocr_avail) == 0)) {
+		/* lowest voltage can be selected in mmc_power_cycle */
+		mmc_power_cycle(host);
+		host->ocr = 0;
+		ret = 1;
+	}
+
+	return ret;
+}
+
 static int mmc_sdio_power_restore(struct mmc_host *host)
 {
 	int ret;
@@ -1131,17 +1150,19 @@ static int mmc_sdio_power_restore(struct mmc_host *host)
 	if (host->ocr_avail_sdio)
 		host->ocr_avail = host->ocr_avail_sdio;
 
-	host->ocr = mmc_select_voltage(host, ocr & ~0x7F);
-	if (!host->ocr) {
-		ret = -EINVAL;
-		goto out;
+	if (!mmc_select_low_voltage(host, ocr)) {
+		host->ocr = mmc_select_voltage(host, ocr & ~0x7F);
+		if (!host->ocr) {
+			ret = -EINVAL;
+			goto out;
+		}
 	}
 
 	if (mmc_host_uhs(host))
 		/* to query card if 1.8V signalling is supported */
 		host->ocr |= R4_18V_PRESENT;
 
-	ret = mmc_sdio_init_card(host, host->ocr, host->card, 0);
+	ret = mmc_sdio_init_card(host, host->ocr, host->card, mmc_card_keep_power(host));
 	if (!ret && host->sdio_irqs)
 		mmc_signal_sdio_irq(host);
 
@@ -1192,14 +1213,16 @@ int mmc_attach_sdio(struct mmc_host *host)
 		ocr &= ~0x7F;
 	}
 
-	host->ocr = mmc_select_voltage(host, ocr);
+	if (!mmc_select_low_voltage(host, ocr)) {
+		host->ocr = mmc_select_voltage(host, ocr);
 
-	/*
-	 * Can we support the voltage(s) of the card(s)?
-	 */
-	if (!host->ocr) {
-		err = -EINVAL;
-		goto err;
+		/*
+		 * Can we support the voltage(s) of the card(s)?
+		 */
+		if (!host->ocr) {
+			err = -EINVAL;
+			goto err;
+		}
 	}
 
 	/*
