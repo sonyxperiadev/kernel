8cd606c504559eadaa176ca1f691980fd4e53f78
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2016-02-15 19:21:15 +0100

    Revert "sched: fix bug in small task CPU selection"
    This reverts commit b79fbf67f3f229130f8d528d6ab2be5a3f483aa6.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1822,6 +1822,8 @@ static int best_small_task_cpu(struct task_struct *p, int sync)
 	do {
 		rq = cpu_rq(i);
 
+		cpumask_clear_cpu(i, &search_cpu);
+
 		trace_sched_cpu_load(rq, idle_cpu(i),
 				     mostly_idle_cpu_sync(i,
 						  cpu_load_sync(i, sync), sync),
@@ -1838,8 +1840,6 @@ static int best_small_task_cpu(struct task_struct *p, int sync)
 				       &rq->freq_domain_cpumask);
 			continue;
 		}
-
-		cpumask_clear_cpu(i, &search_cpu);
 
 		if (sched_cpu_high_irqload(i))
 			continue;
