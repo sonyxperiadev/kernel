fe0d3c9f6244297b5efc50b45f0b39f8c6395ba8
Veera Sundaram Sankaran <veeras@codeaurora.org>
2016-02-18 11:12:53 -0800

    msm: mdss: allocate new cursor buf based on FB_CUR_SETIMAGE
    Cursor buffer is allocated and the img data is copied only
    when there is a cursor size change. This might cause issues
    when the cursor img changes retaining the same size. Update
    the cursor buffer based on size change and also based on
    the flag FB_CUR_SETIMAGE, which is set only when there is a
    change in img data.
    
    Change-Id: Iaf7aabd147b5fd1c2d72d26709c982601b3623da
    Signed-off-by: Veera Sundaram Sankaran <veeras@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -3287,10 +3287,13 @@ static int mdss_mdp_hw_cursor_pipe_update(struct msm_fb_data_type *mfd,
 
 	size = img->width * img->height * 4;
 #ifdef CONFIG_FB_MSM_MDSS_SPECIFIC_PANEL /* REMOVEME???? */
-	if ((size != mfd->cursor_buf_size) || (pre_img_data != img->data)) {
+	if ((size != mfd->cursor_buf_size) ||
+			(cursor->set & FB_CUR_SETIMAGE) ||
+			(pre_img_data != img->data)) {
 		pre_img_data = (char *)(img->data);
 #else
-	if (size != mfd->cursor_buf_size) {
+	if ((size != mfd->cursor_buf_size) ||
+			(cursor->set & FB_CUR_SETIMAGE)) {
 #endif /* CONFIG_FB_MSM_MDSS_SPECIFIC_PANEL */
 		pr_debug("allocating cursor mem size:%zd\n", size);
 
