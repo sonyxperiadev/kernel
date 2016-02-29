832aa27810de203a15e173f07557e088691dbeb8
Humberto Borba <humberos@gmail.com>
2016-02-29 17:32:44 -0300

    drivers: mmc: Fix mmc host voltage for Wi-Fi
    Only suzuran requires this for now.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I1a1c9192e1edae3112f83f610ef8e466589246a6

--- drivers/mmc/core/sdio.c
+++ drivers/mmc/core/sdio.c
@@ -1048,6 +1048,7 @@ static int mmc_select_low_voltage(struct mmc_host *host, u32 ocr)
 {
 	int ret = 0;
 
+#ifdef CONFIG_MACH_SONY_SUZURAN
 	if ((host->ocr_avail == MMC_VDD_165_195) && mmc_host_uhs(host) &&
 		((ocr & host->ocr_avail) == 0)) {
 		/* lowest voltage can be selected in mmc_power_cycle */
@@ -1055,6 +1056,7 @@ static int mmc_select_low_voltage(struct mmc_host *host, u32 ocr)
 		host->ocr = 0;
 		ret = 1;
 	}
+#endif
 
 	return ret;
 }
