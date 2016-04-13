58fd7a4172c60ea576ae91bade04aa403b598bcb
Angelo G. Del Regno <kholk11@gmail.com>
2016-04-13 10:54:52 +0200

    Revert "msm: mdss: remove mode select GPIO configuration for MDSS"
    This reverts commit 3d5b6cb1ef3db676e61d56fdf73af49afbb0351b.

--- drivers/video/msm/mdss/mdss_panel.h
+++ drivers/video/msm/mdss/mdss_panel.h
@@ -110,10 +110,13 @@ enum {
 
 enum {
 	MODE_GPIO_NOT_VALID = 0,
-	MODE_SEL_SPLIT,
-	MODE_SEL_DSC_SINGLE,
 	MODE_GPIO_HIGH,
 	MODE_GPIO_LOW,
+};
+
+enum {
+	MODE_SEL_SPLIT = 0,
+	MODE_SEL_DSC_SINGLE,
 };
 
 struct mdss_rect {
@@ -566,6 +569,7 @@ struct mdss_panel_info {
 	int new_fps;
 	int panel_max_fps;
 	int panel_max_vtotal;
+	u32 mode_gpio_state;
 	u32 mode_sel_state;
 	u32 xstart_pix_align;
 	u32 width_pix_align;
