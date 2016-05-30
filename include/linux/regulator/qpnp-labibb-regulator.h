457349e156b3f58b5e54321e7055d910124884eb
Angelo G. Del Regno <kholk11@gmail.com>
2016-05-30 12:46:26 +0200

    regulator: qpnp-labibb-regulator: Implement new SoMC extensions
    Implement new SoMC extensions for labibb regulator.
    Those extensions will allow to set precharge, soft start,
    pull down configurations for both lab and ibb from an
    external driver.

--- include/linux/regulator/qpnp-labibb-regulator.h
+++ include/linux/regulator/qpnp-labibb-regulator.h
@@ -65,17 +65,92 @@ int qpnp_ibb_set_pd_strength(struct regulator *regulator, u32 strength);
  */
 int qpnp_ibb_pd_enable_ctl(struct regulator *regulator, bool enable);
 #ifdef SOMC_LABIBB_REGULATOR_ORG_IMPL
+/** This API is used to set precharge of LAB regulator
+ * regulator: the reglator device
+ * time: precharge time
+ * en: precharge control enable or not
+ */
+int qpnp_lab_set_precharge(struct regulator *regulator, u32 time, bool en);
+
+/** This API is used to set soft-start of LAB regulator
+ * regulator: the reglator device
+ * time: soft start time
+ */
+int qpnp_lab_set_soft_start(struct regulator *regulator, u32 time);
+
+/** This API is used to set pull-down of LAB regulator
+ * regulator: the reglator device
+ * en: pull-down enable or not
+ * strength: strength pull-down
+ */
+int qpnp_lab_set_pull_down(struct regulator *regulator, u8 strength);
+
 /** This API is used to set current max of LAB regulator
  * regulator: the reglator device
  * limit: current max of LAB regulator
  */
 int qpnp_lab_set_current_max(struct regulator *regulator, u32 limit);
 
+/** This API is used to set soft-start of IBB regulator
+ * regulator: the reglator device
+ * time: soft start time
+ */
+int qpnp_ibb_set_soft_start(struct regulator *regulator, u32 time);
+
+/** This API is used to set pull-down of IBB regulator
+ * regulator: the reglator device
+ * en: pull-down enable or not
+ * strength: strength pull-down
+ */
+int qpnp_ibb_set_pull_down(struct regulator *regulator, u8 strength);
+
 /** This API is used to set current max of IBB regulator
  * regulator: the reglator device
  * limit: current max of IBB regulator
  */
 int qpnp_ibb_set_current_max(struct regulator *regulator, u32 limit);
+#else
+static inline int qpnp_lab_set_precharge(struct regulator *regulator,
+						u32 time, bool en)
+{
+	return -ENODEV;
+}
+
+static inline int qpnp_lab_set_soft_start(struct regulator *regulator,
+						u32 time)
+{
+	return -ENODEV;
+}
+
+static inline int qpnp_lab_set_pull_down(struct regulator *regulator,
+						u8 strength)
+{
+	return -ENODEV;
+}
+
+static inline int qpnp_lab_set_current_max(struct regulator *regulator,
+						u32 limit)
+{
+	return -ENODEV;
+}
+
+static inline int qpnp_ibb_set_soft_start(struct regulator *regulator,
+						u32 time)
+{
+	return -ENODEV;
+}
+
+static inline int qpnp_ibb_set_pull_down(struct regulator *regulator,
+						u8 strength)
+{
+	return -ENODEV;
+}
+
+static inline int qpnp_ibb_set_current_max(struct regulator *regulator,
+						u32 limit)
+{
+	return -ENODEV;
+}
 #endif /* SOMC_LABIBB_REGULATOR_ORG_IMPL */
 #else
 
