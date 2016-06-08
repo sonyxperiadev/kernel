1dc658285171d93355d5bca7a230142183bdc591
Angelo G. Del Regno <kholk11@gmail.com>
2016-06-08 09:54:00 +0200

    pinctrl: qcom: Add pinctrl for MSM8916 family
    Add pinctrl for MSM8916 family. Snapshot from mainline kernel
    4.6.2.

--- drivers/pinctrl/qcom/pinctrl-msm.c
+++ drivers/pinctrl/qcom/pinctrl-msm.c
@@ -1077,3 +1077,60 @@ int msm_pinctrl_remove(struct platform_device *pdev)
 	return 0;
 }
 EXPORT_SYMBOL(msm_pinctrl_remove);
+
+#ifdef CONFIG_USE_PINCTRL_IRQ
+#include "pinctrl-msm-tlmm.h"
+extern int msm_tlmm_of_gp_irq_init(struct device_node *controller,
+			    struct irq_chip *chip_extn);
+
+/**
+ * struct msm_irq_of_info: represents of init data for tlmm interrupt
+ * controllers
+ * @compat: compat string for tlmm interrup controller instance.
+ * @irq_init: irq chip initialization callback.
+ */
+struct msm_irq_of_info {
+	const char *compat;
+	int (*irq_init)(struct device_node *np, struct irq_chip *ic);
+};
+
+struct irq_chip mpm_tlmm_irq_extn = {
+	.irq_eoi	= NULL,
+	.irq_mask	= NULL,
+	.irq_unmask	= NULL,
+	.irq_retrigger	= NULL,
+	.irq_set_type	= NULL,
+	.irq_set_wake	= NULL,
+	.irq_disable	= NULL,
+};
+
+struct msm_irq_of_info msm_tlmm_irq[] = {
+#ifdef CONFIG_PINCTRL_MSM_TLMM
+	{
+		.compat = "qcom,msm-tlmm-gp",
+		.irq_init = msm_tlmm_of_gp_irq_init,
+	},
+#endif
+};
+
+int __init msm_tlmm_of_irq_init(struct device_node *controller,
+						struct device_node *parent)
+{
+	int rc, i;
+	const char *compat;
+
+	rc = of_property_read_string(controller, "compatible", &compat);
+	if (rc)
+		return rc;
+
+	for (i = 0; i < ARRAY_SIZE(msm_tlmm_irq); i++) {
+		struct msm_irq_of_info *tlmm_info = &msm_tlmm_irq[i];
+
+		if (!of_compat_cmp(tlmm_info->compat, compat, strlen(compat)))
+				return tlmm_info->irq_init(controller,
+							&mpm_tlmm_irq_extn);
+
+	}
+	return -EIO;
+}
+#endif
