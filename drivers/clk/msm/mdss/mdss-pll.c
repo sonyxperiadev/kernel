2614ce434f2d0a9e9c5a2e423532dad4b12f4ef7
Angelo G. Del Regno <kholk11@gmail.com>
2016-06-16 17:13:17 +0200

    clk: msm: mdss: Add support for 20nm and 28hpm PLLs (8974,8994)
    This patch adds support for MSM8974 and MSM8994's MDSS PLLs.

--- drivers/clk/msm/mdss/mdss-pll.c
+++ drivers/clk/msm/mdss/mdss-pll.c
@@ -156,6 +156,26 @@ static int mdss_pll_resource_parse(struct platform_device *pdev,
 	} else if (!strcmp(compatible_stream,
 				"qcom,mdss_hdmi_pll_8996_v3_1p8")) {
 		pll_res->pll_interface_type = MDSS_HDMI_PLL_8996_V3_1_8;
+	} else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_8974")) {
+		pll_res->pll_interface_type = MDSS_DSI_PLL_HPM;
+		pll_res->target_id = MDSS_PLL_TARGET_8974;
+	} else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_8916")) {
+		pll_res->pll_interface_type = MDSS_DSI_PLL_LPM;
+		pll_res->target_id = MDSS_PLL_TARGET_8916;
+	} else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_8939")) {
+		pll_res->pll_interface_type = MDSS_DSI_PLL_LPM;
+		pll_res->target_id = MDSS_PLL_TARGET_8939;
+	} else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_8994")) {
+		pll_res->pll_interface_type = MDSS_DSI_PLL_20NM;
+		pll_res->target_id = MDSS_PLL_TARGET_8994;
+	} else if (!strcmp(compatible_stream, "qcom,mdss_edp_pll")) {
+		pll_res->pll_interface_type = MDSS_EDP_PLL;
+	} else if (!strcmp(compatible_stream, "qcom,mdss_hdmi_pll")) {
+		pll_res->pll_interface_type = MDSS_HDMI_PLL;
+	} else if (!strcmp(compatible_stream, "qcom,mdss_hdmi_pll_8994")) {
+		pll_res->pll_interface_type = MDSS_HDMI_PLL_20NM;
+	} else if (!strcmp(compatible_stream, "qcom,mdss_hdmi_pll_8992")) {
+		pll_res->pll_interface_type = MDSS_HDMI_PLL_20NM;
 	} else {
 		goto err;
 	}
