b0c6575907f05ab42ff9d168490519a1bc69ccf8
Patrick Tjin <pattjin@google.com>
2016-07-20 15:19:21 -0700

    android: binder: remove useless preemption disable on target_wait
    This patch brings binder up match the change:
    
      commit dca4d0cd31e5aea458f75f35a22ea1cdfd9f135d
      Author: Riley Andrews <riandrews@google.com>
      Date:   Tue Sep 15 10:49:46 2015 -0700
    
      android: binder: Disable preemption while holding the global binder lock.
    
      Change-Id: I90fe02cdedb8a5677b900a68528fb443b9204322
      Signed-off-by: Riley Andrews <riandrews@google.com>
    
    Change-Id: Ibe9c6edcc55abe330ce962388278590171b588d2
    Signed-off-by: Patrick Tjin <pattjin@google.com>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -1776,9 +1776,7 @@ static void binder_transaction(struct binder_proc *proc,
 	list_add_tail(&tcomplete->entry, &thread->todo);
 	if (target_wait) {
 		if (reply || !(t->flags & TF_ONE_WAY)) {
-			preempt_disable();
 			wake_up_interruptible_sync(target_wait);
-			preempt_enable_no_resched();
 		}
 		else {
 			wake_up_interruptible(target_wait);
