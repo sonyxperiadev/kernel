ba5049a5d9bf9269ec2d23b0eb6a2e9f027cd8cc
David <Davidteri91@gmail.com>
2016-07-21 10:19:38 +0200

    Revert "qseecom: Use correct flag for 64bits targets"
    This reverts commit 2dc55d529d4f1e1a1d0174ef1c9d7c76b4a4a894.

--- include/uapi/linux/qseecom.h
+++ include/uapi/linux/qseecom.h
@@ -5,7 +5,7 @@
 #include <linux/ioctl.h>
 
 #define MAX_ION_FD  4
-#if defined(CONFIG_ARM64) || defined(CONFIG_64BIT)
+#ifdef CONFIG_ARCH_MSM8994
 #define MAX_APP_NAME_SIZE  64
 #else
 #define MAX_APP_NAME_SIZE  32
