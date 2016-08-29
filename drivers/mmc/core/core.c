ca38234b962714514413ff77fdc524926e30a2af
scafroglia93 <scafroglia57@gmail.com>
2016-08-29 02:40:02 +0200

    Disable Software CRC / Toggleable Software CRC

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -69,8 +69,8 @@ static const unsigned freqs[] = { 400000, 300000, 200000, 100000 };
  * performance cost, and for other reasons may not always be desired.
  * So we allow it it to be disabled.
  */
-bool use_spi_crc = 1;
-module_param(use_spi_crc, bool, 0);
+bool use_spi_crc = 0;
+module_param(use_spi_crc, bool, 0644);
 
 /*
  * We normally treat cards as removed during suspend if they are not
