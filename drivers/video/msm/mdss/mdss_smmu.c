65ef302a287b6c3619588052d59e9a1f4c0e52ce
Angelo G. Del Regno <kholk11@gmail.com>
2016-10-07 14:03:03 +0200

    msm: mdss: Fix race condition between IOMMUv1 attach and sending DCS cmds
    There might be cases during bootup when backlight cmds or ESD thread
    sends DCS cmds to the panel when iommu attach is yet to happen. DSI
    uses physical or virtual address based on the iommu_attached status
    check. If iommu attach happens when the DCS cmds are using the
    physical address, it would lead to SMMU page faults. Protect iommu
    attach/detach and DCS cmd sending with an iommu_lock to avoid such
    race conditions.
    
    This is ported to IOMMUv1 from QC's commit dd7ad27eb3de6b088b6525b066c40f5a29ac4c60

--- drivers/video/msm/mdss/mdss_smmu.c
+++ drivers/video/msm/mdss/mdss_smmu.c
@@ -157,6 +157,7 @@ static int mdss_smmu_attach_v1(struct mdss_data_type *mdata)
 	struct mdss_iommu_map_type *iomap;
 	int i, rc = 0;
 
+	mutex_lock(&mdp_iommu_lock);
 	for (i = 0; i < MDSS_IOMMU_MAX_DOMAIN; i++) {
 
 		if (!mdss_smmu_is_valid_domain_type(mdata, i))
@@ -184,6 +185,7 @@ static int mdss_smmu_attach_v1(struct mdss_data_type *mdata)
 		}
 	}
 end:
+	mutex_unlock(&mdp_iommu_lock);
 	return rc;
 }
 
@@ -307,6 +309,7 @@ static int mdss_smmu_detach_v1(struct mdss_data_type *mdata)
 	struct mdss_iommu_map_type *iomap;
 	int i;
 
+	mutex_lock(&mdp_iommu_lock);
 	for (i = 0; i < MDSS_IOMMU_MAX_DOMAIN; i++) {
 		if (!mdss_smmu_is_valid_domain_type(mdata, i))
 			continue;
@@ -321,6 +324,7 @@ static int mdss_smmu_detach_v1(struct mdss_data_type *mdata)
 		}
 		iommu_detach_device(domain, iomap->ctx);
 	}
+	mutex_unlock(&mdp_iommu_lock);
 	return 0;
 }
 
