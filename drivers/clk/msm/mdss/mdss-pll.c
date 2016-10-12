52eceda22c42705f6f96f3c8d44dd69b4e9c7b4c
Angelo G. Del Regno <kholk11@gmail.com>
2016-10-12 18:59:42 +0200

    clk: msm: mdss-pll: Add MSM8976 SoC support
    Add 8976/8956 SoC support: it will use HPM PLL, like MSM8974.

--- drivers/clk/msm/mdss/mdss-pll.c
+++ drivers/clk/msm/mdss/mdss-pll.c
@@ -153,6 +153,9 @@ static int mdss_pll_resource_parse(struct platform_device *pdev,
 	} else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_8974")) {
 		pll_res->pll_interface_type = MDSS_DSI_PLL_HPM;
 		pll_res->target_id = MDSS_PLL_TARGET_8974;
+	} else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_8976")) {
+		pll_res->pll_interface_type = MDSS_DSI_PLL_HPM;
+		pll_res->target_id = MDSS_PLL_TARGET_8976;
 	} else if (!strcmp(compatible_stream, "qcom,mdss_edp_pll")) {
 		pll_res->pll_interface_type = MDSS_EDP_PLL;
 	} else if (!strcmp(compatible_stream, "qcom,mdss_hdmi_pll")) {
@@ -413,6 +416,7 @@ static const struct of_device_id mdss_pll_dt_match[] = {
 	{.compatible = "qcom,mdss_hdmi_pll_8996_v3_1p8"},
 	{.compatible = "qcom,mdss_dp_pll_8998"},
 	{.compatible = "qcom,mdss_hdmi_pll_8998"},
+	{.compatible = "qcom,mdss_dsi_pll_8976"},
 	{}
 };
 
