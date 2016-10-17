1e3c0005d00f84e2827a4f720c43d811e0f79682
Angelo G. Del Regno <kholk11@gmail.com>
2016-10-17 12:36:01 +0200

    soc: qcom: socinfo: Fix and add APQ/MSM 8956/76 SoC informations
    Add socinfo informations to correctly identify the following SoC:
    - MSM8956
    - MSM8976
    - APQ8956
    - APQ8976

--- include/soc/qcom/socinfo.h
+++ include/soc/qcom/socinfo.h
@@ -88,6 +88,8 @@
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,fsm9010")
 #define early_machine_is_msm8976()	\
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,msm8976")
+#define early_machine_is_msm8956()	\
+	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,msm8956")
 #define early_machine_is_msm8952()	\
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,msm8952")
 #define early_machine_is_msm8953()	\
@@ -139,6 +141,7 @@
 #define early_machine_is_msm8937()	0
 #define early_machine_is_msm8996()	0
 #define early_machine_is_msm8976() 0
+#define early_machine_is_msm8956()	0
 #define early_machine_is_msm8929()	0
 #define early_machine_is_mdm9607()	0
 #define early_machine_is_mdmcalifornium()	0
@@ -203,6 +206,7 @@ enum msm_cpu {
 	MSM_CPU_8937,
 	MSM_CPU_8996,
 	MSM_CPU_8976,
+	MSM_CPU_8956,
 	MSM_CPU_8929,
 	MSM_CPU_9607,
 	MSM_CPU_CALIFORNIUM,
