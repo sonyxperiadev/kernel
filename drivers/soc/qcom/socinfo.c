878890b65158c2eba27cc756c7054a05f10c91bc
Angelo G. Del Regno <kholk11@gmail.com>
2016-10-17 12:36:01 +0200

    soc: qcom: socinfo: Fix and add APQ/MSM 8956/76 SoC informations
    Add socinfo informations to correctly identify the following SoC:
    - MSM8956
    - MSM8976
    - APQ8956
    - APQ8976

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -520,7 +520,11 @@ static struct msm_soc_info cpu_of_id[] = {
 	[312] = {MSM_CPU_8996, "APQ8096pro"},
 
 	/* 8976 ID */
-	[266] = {MSM_CPU_8976, "MSM8976"},
+	[278] = {MSM_CPU_8976, "MSM8976"},
+	[277] = {MSM_CPU_8976, "APQ8076"},
+	/* 8956 ID */
+	[266] = {MSM_CPU_8956, "MSM8956"},
+	[274] = {MSM_CPU_8956, "APQ8056"},
 
 	/* 8929 IDs */
 	[268] = {MSM_CPU_8929, "MSM8929"},
@@ -1184,8 +1188,12 @@ static void * __init setup_dummy_socinfo(void)
 		strlcpy(dummy_socinfo.build_id, "msm8992 - ",
 			sizeof(dummy_socinfo.build_id));
 	} else if (early_machine_is_msm8976()) {
-		dummy_socinfo.id = 266;
+		dummy_socinfo.id = 278;
 		strlcpy(dummy_socinfo.build_id, "msm8976 - ",
+			sizeof(dummy_socinfo.build_id));
+	} else if (early_machine_is_msm8956()) {
+		dummy_socinfo.id = 266;
+		strlcpy(dummy_socinfo.build_id, "msm8956 - ",
 			sizeof(dummy_socinfo.build_id));
 	} else if (early_machine_is_msmtellurium()) {
 		dummy_socinfo.id = 264;
