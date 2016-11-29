62cf5ae5a71ebc2ac6f06625bacf3cf75cbf2371
Jeff Johnson <jjohnson@codeaurora.org>
2016-11-29 08:54:18 -0800

    qcacld-3.0: Avoid overflow of "significant change" params
    This is a qcacld-2.0 to qcacld-3.0 propagation.
    
    The wlan driver supports the following vendor command:
    	QCA_NL80211_VENDOR_SUBCMD_EXTSCAN_SET_SIGNIFICANT_CHANGE
    
    This command supplies a "number of APs" attribute as well as a list of
    per-AP attributes.  However there is no validation that the number of
    APs provided won't overflow the destination buffer.  In addition there
    is no validation that the number of APs actually provided matches the
    number of APs expected.
    
    To address these issues:
    * Verify that the expected number of APs doesn't exceed the maximum
      allowed number of APs
    * Verify that the actual number of APs supplied doesn't exceed the
      expected number of APs
    * Only process the actual number of supplied APs if it is less than
      the expected number of APs.
    
    Change-Id: I0513ffbc4a38f1d7ddbc0815d3618fc9a2ea4f77
    CRs-Fixed: 1095009

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg80211.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -3633,6 +3633,12 @@ static int __wlan_hdd_cfg80211_extscan_set_significant_change(struct wiphy *wiph
     }
     pReqMsg->numAp = nla_get_u32(
             tb[QCA_WLAN_VENDOR_ATTR_EXTSCAN_SIGNIFICANT_CHANGE_PARAMS_NUM_AP]);
+    if (pReqMsg->numAp > WLAN_EXTSCAN_MAX_SIGNIFICANT_CHANGE_APS) {
+        hdd_err("Number of AP %u exceeds max %u",
+                pReqMsg->numAp,
+                WLAN_EXTSCAN_MAX_SIGNIFICANT_CHANGE_APS);
+        goto fail;
+    }
     hddLog(VOS_TRACE_LEVEL_INFO, FL("Number of AP (%d)"), pReqMsg->numAp);
 
     pReqMsg->sessionId = pAdapter->sessionId;
@@ -3640,6 +3646,12 @@ static int __wlan_hdd_cfg80211_extscan_set_significant_change(struct wiphy *wiph
 
     nla_for_each_nested(apTh,
                 tb[QCA_WLAN_VENDOR_ATTR_EXTSCAN_AP_THRESHOLD_PARAM], rem) {
+
+        if (i == pReqMsg->numAp) {
+            hdd_warn("Ignoring excess AP");
+            break;
+        }
+
         if(nla_parse(tb2,
                 QCA_WLAN_VENDOR_ATTR_EXTSCAN_SUBCMD_CONFIG_PARAM_MAX,
                 nla_data(apTh), nla_len(apTh),
@@ -3686,6 +3698,11 @@ static int __wlan_hdd_cfg80211_extscan_set_significant_change(struct wiphy *wiph
         hddLog(VOS_TRACE_LEVEL_INFO,
                            FL("Channel (%u)"), pReqMsg->ap[i].channel);
         i++;
+    }
+    if (i < pReqMsg->numAp) {
+        hdd_warn("Number of AP %u less than expected %u",
+                 i, pReqMsg->numAp);
+        pReqMsg->numAp = i;
     }
 
     status = sme_SetSignificantChange(pHddCtx->hHal, pReqMsg);
