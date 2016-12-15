e5e2d5cedfdd5c497b87a0a96d3c37b8d0504f64
Banajit Goswami <bgoswami@codeaurora.org>
2016-12-14 22:20:15 -0800

    msm: qdsp6v2: Add timestamp support for compress capture
    Add timestamp support for compress driver.
    
    CRs-fixed: 1072067
    Change-Id: I1c46befc223285495b3c0650e6c3eaae81f58771
    Signed-off-by: Satish Babu Patakokila <sbpata@codeaurora.org>
    Signed-off-by: Banajit Goswami <bgoswami@codeaurora.org>
    Signed-off-by: Meng Wang <mwang@codeaurora.org>
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- include/uapi/sound/compress_params.h
+++ include/uapi/sound/compress_params.h
@@ -60,6 +60,11 @@
 #define MAX_NUM_BITRATES 32
 #define MAX_NUM_SAMPLE_RATES 32
 
+/* Timestamp flag */
+/* Bit-0 - 1 : Enable Timestamp mode */
+/* Bit-0 - 0 : Disable Timestamp mode */
+#define COMPRESSED_TIMESTAMP_FLAG 0x0001
+
 /* Codecs are listed linearly to allow for extensibility */
 #define SND_AUDIOCODEC_PCM                   ((__u32) 0x00000001)
 #define SND_AUDIOCODEC_MP3                   ((__u32) 0x00000002)
@@ -445,7 +450,23 @@ struct snd_codec {
 	__u32 format;
 	__u32 align;
 	union snd_codec_options options;
-	__u32 reserved[3];
+	__u32 flags;
+	__u32 reserved[2];
 } __attribute__((packed, aligned(4)));
+
+/** struct snd_codec_metadata
+ * @length: Length of the encoded buffer.
+ * @offset: Offset from the buffer address to the first byte of the first
+ *		encoded frame. All encoded frames are consecutive starting
+ *		from this offset.
+ * @timestamp: Session time in microseconds of the first sample in the buffer.
+ * @reserved: Reserved for future use.
+ */
+struct snd_codec_metadata {
+	__u32 length;
+	__u32 offset;
+	__u64 timestamp;
+	__u32 reserved[4];
+};
 
 #endif
