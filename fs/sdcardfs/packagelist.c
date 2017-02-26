bf7433fe954cb0c426c40b1847eaec14366fed7c
Angelo G. Del Regno <kholk11@gmail.com>
2017-02-26 18:28:10 +0100

    fs: sdcardfs: Fix UID and GID types usage for global namespaces support
    When we want UID namespace, we need to initialize and use KUID
    and KGID correctly.

--- fs/sdcardfs/packagelist.c
+++ fs/sdcardfs/packagelist.c
@@ -99,7 +99,7 @@ int check_caller_access_to_name(struct inode *parent_node, const char* name) {
 
 	/* Root always has access; access for any other UIDs should always
 	 * be controlled through packages.list. */
-	if (current_fsuid() == 0) {
+	if (from_kuid(&init_user_ns, current_fsuid()) == 0) {
 		return 1;
 	}
 
