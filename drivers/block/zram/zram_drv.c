67e1d8b7d4b0e486c986d09a8c3789bb85900a31
Johannes Thumshirn <jthumshirn@suse.de>
2017-03-06 11:23:35 +0100

    zram: set physical queue limits to avoid array out of bounds accesses
    [ Upstream commit 0bc315381fe9ed9fb91db8b0e82171b645ac008f ]
    
    zram can handle at most SECTORS_PER_PAGE sectors in a bio's bvec. When using
    the NVMe over Fabrics loopback target which potentially sends a huge bulk of
    pages attached to the bio's bvec this results in a kernel panic because of
    array out of bounds accesses in zram_decompress_page().
    
    Signed-off-by: Johannes Thumshirn <jthumshirn@suse.de>
    Reviewed-by: Hannes Reinecke <hare@suse.com>
    Reviewed-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>
    Signed-off-by: Sasha Levin <alexander.levin@verizon.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1570,6 +1570,8 @@ static int zram_add(void)
 	blk_queue_io_min(zram->disk->queue, PAGE_SIZE);
 	blk_queue_io_opt(zram->disk->queue, PAGE_SIZE);
 	zram->disk->queue->limits.discard_granularity = PAGE_SIZE;
+	zram->disk->queue->limits.max_sectors = SECTORS_PER_PAGE;
+	zram->disk->queue->limits.chunk_sectors = 0;
 	blk_queue_max_discard_sectors(zram->disk->queue, UINT_MAX);
 	queue_flag_set_unlocked(QUEUE_FLAG_DISCARD, zram->disk->queue);
 
