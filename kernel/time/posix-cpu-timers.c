0ed4f641f78ffb5ffb764a52c153bdfcb96afd85
Chris Fries <cfries@google.com>
2017-03-07 15:37:47 -0600

    posix_cpu_timer: check return on cpu_timer_sample_group
    If error, don't trust "now" time that it should be setting
    
    kernel/time/posix-cpu-timers.c:1269:13: warning: 'now' may be used uninitialized in this function [-Wmaybe-uninitialized]
    
    Signed-off-by: Chris Fries <cfries@google.com>

--- kernel/time/posix-cpu-timers.c
+++ kernel/time/posix-cpu-timers.c
@@ -1253,7 +1253,8 @@ void set_process_cpu_timer(struct task_struct *tsk, unsigned int clock_idx,
 	unsigned long long now = 0;
 
 	WARN_ON_ONCE(clock_idx == CPUCLOCK_SCHED);
-	cpu_timer_sample_group(clock_idx, tsk, &now);
+	if (cpu_timer_sample_group(clock_idx, tsk, &now))
+		return;
 
 	if (oldval) {
 		/*
