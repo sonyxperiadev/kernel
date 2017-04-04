0dbdf3fa7303346dd9997a85b6dbcc73e91ab10e
Humberto Borba <humberos@gmail.com>
2017-04-04 13:11:20 -0300

    driver: staging: Android Low Memory Killer: consider swap free size
    Use freeswap as otherfile when watermark is grater than
    low_watermark. This helps to make use of zram swap.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -776,6 +776,12 @@ static inline bool pgdat_is_empty(pg_data_t *pgdat)
 #include <linux/memory_hotplug.h>
 
 void build_all_zonelists(pg_data_t *pgdat);
+#ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_CONSIDER_SWAP
+void _wake_all_kswapds(unsigned int order,
+			     struct zonelist *zonelist,
+			     enum zone_type high_zoneidx,
+			     struct zone *preferred_zone);
+#endif
 void wakeup_kswapd(struct zone *zone, gfp_t gfp_mask, int order,
 		   enum zone_type classzone_idx);
 bool __zone_watermark_ok(struct zone *z, unsigned int order, unsigned long mark,
