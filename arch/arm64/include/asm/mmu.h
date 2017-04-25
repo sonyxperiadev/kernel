9bb9eb898751d0458d82f85e75aa97b819d10c54
Pablo Mendez Hernandez <pablomh@gmail.com>
2017-04-25 17:44:00 +0000

    Revert "arm64: mmu: add ability to write kernel word"
    This reverts commit f15f3173d568313fa6916eebd967d1436b4c13e1.

--- arch/arm64/include/asm/mmu.h
+++ arch/arm64/include/asm/mmu.h
@@ -31,7 +31,6 @@ extern void paging_init(void);
 extern void setup_mm_for_reboot(void);
 extern void __iomem *early_io_map(phys_addr_t phys, unsigned long virt);
 extern void init_mem_pgprot(void);
-extern void mem_text_write_kernel_word(u32 *addr, u32 word);
 extern void create_pgd_mapping(struct mm_struct *mm, phys_addr_t phys,
 			       unsigned long virt, phys_addr_t size,
 			       pgprot_t prot);
