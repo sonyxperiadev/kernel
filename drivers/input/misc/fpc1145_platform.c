10c898952c741793902787cb98a1df31f9838c4f
Angelo G. Del Regno <kholk11@gmail.com>
2017-05-15 15:05:33 +0200

    input: FPC1145: Use regulator_set_load function
    The function regulator_set_optimum_mode	got renamed as of kernel
    4.4.

--- drivers/input/misc/fpc1145_platform.c
+++ drivers/input/misc/fpc1145_platform.c
@@ -126,7 +126,7 @@ found:
 					"Unable to set voltage on %s, %d\n",
 					name, rc);
 		}
-		rc = regulator_set_optimum_mode(vreg, vreg_conf[i].ua_load);
+		rc = regulator_set_load(vreg, vreg_conf[i].ua_load);
 		if (rc < 0)
 			dev_err(dev, "Unable to set current on %s, %d\n",
 					name, rc);
