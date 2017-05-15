a4ff22905d4419a397905448d4d305b6bd88b17e
Angelo G. Del Regno <kholk11@gmail.com>
2017-05-15 15:06:40 +0200

    fbdev: msm: mdss_dsi: Use regulator_set_load function
    The function regulator_set_optimum_mode	got renamed as of kernel
    4.4.

--- drivers/video/fbdev/msm/mdss_dsi.c
+++ drivers/video/fbdev/msm/mdss_dsi.c
@@ -3997,7 +3997,7 @@ int mdss_dsi_panel_power_detect(struct platform_device *pdev, int enable)
 	}
 
 	if (enable) {
-		ret = regulator_set_optimum_mode(vdd_vreg, 100000);
+		ret = regulator_set_load(vdd_vreg, 100000);
 		if (ret < 0) {
 			pr_err("%s: vdd_vreg set regulator mode failed.\n",
 						       __func__);
@@ -4019,7 +4019,7 @@ int mdss_dsi_panel_power_detect(struct platform_device *pdev, int enable)
 			return ret;
 		}
 
-		ret = regulator_set_optimum_mode(vdd_vreg, 100);
+		ret = regulator_set_load(vdd_vreg, 100);
 		if (ret < 0) {
 			pr_err("%s: vdd_vreg set regulator mode failed.\n",
 						       __func__);
@@ -4045,7 +4045,7 @@ int mdss_dsi_panel_power_detect(struct platform_device *pdev, int enable)
 	}
 
 	if (enable) {
-		ret = regulator_set_optimum_mode(vddio_vreg, 100000);
+		ret = regulator_set_load(vddio_vreg, 100000);
 		if (ret < 0) {
 			pr_err("%s: vdd_vreg set regulator mode failed.\n",
 						       __func__);
@@ -4067,7 +4067,7 @@ int mdss_dsi_panel_power_detect(struct platform_device *pdev, int enable)
 			return ret;
 		}
 
-		ret = regulator_set_optimum_mode(vddio_vreg, 100);
+		ret = regulator_set_load(vddio_vreg, 100);
 		if (ret < 0) {
 			pr_err("%s: vdd_vreg set regulator mode failed.\n",
 						       __func__);
