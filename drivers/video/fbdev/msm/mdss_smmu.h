0b69ff830b5b124d41e3956d677dacfce816fa96
Angelo G. Del Regno <kholk11@gmail.com>
2017-05-23 19:40:44 +0200

    fbdev: msm: mdss_smmu: Add variable init check to avoid panic
    The kernel can panic in there. And it's very easy.
    The mdss_mdp driver calls clock initialization from the probe
    function by calling the footswitch control for continuous splash:
    this *sometimes* requires starting clocks, either due to a bad
    configuration coming from bootloader, or due to a bad clocks
    handling at init time from the MDSS driver.
    
    Either way, the mdss_mdp_probe function initializes mdp mdata,
    which is REQUIRED for the mdss_smmu to initialize all the structs
    and all the variables (mdss_smmu dynamically sets those to support
    various IOMMUs), hence when we make a call to get clocks on we
    actually also try to attach the IOMMU.
    
    While this is a strict requirement in basically all conditions,
    when booting with continuous splash on, the IOMMU is already
    configured by the bootloader, hence we can skip this configuration
    safely.
    
    When we are not in continuous splash mode, the clocks will not be
    onlined in footswitch control, so will be fine as earlier.

--- drivers/video/fbdev/msm/mdss_smmu.h
+++ drivers/video/fbdev/msm/mdss_smmu.h
@@ -125,8 +125,13 @@ static inline int mdss_smmu_get_domain_type(u32 flags, bool rotator)
 static inline int mdss_smmu_attach(struct mdss_data_type *mdata)
 {
 	int rc;
+	bool skip_lock = false;
 
-	mdata->mdss_util->iommu_lock();
+	if (unlikely(!mdata->mdss_util->iommu_lock))
+		skip_lock = true;
+
+	if (likely(!skip_lock))
+		mdata->mdss_util->iommu_lock();
 	MDSS_XLOG(mdata->iommu_attached);
 
 	if (mdata->iommu_attached) {
@@ -136,6 +141,7 @@ static inline int mdss_smmu_attach(struct mdss_data_type *mdata)
 	}
 
 	if (!mdata->smmu_ops.smmu_attach) {
+		pr_err("No smmu_attach function!!!!\n");
 		rc = -ENODEV;
 		goto end;
 	}
@@ -145,7 +151,8 @@ static inline int mdss_smmu_attach(struct mdss_data_type *mdata)
 		mdata->iommu_attached = true;
 
 end:
-	mdata->mdss_util->iommu_unlock();
+	if (likely(!skip_lock))
+		mdata->mdss_util->iommu_unlock();
 	return rc;
 }
 
