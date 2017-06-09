5a455cc61fb30de6524f56570e23aba15a2de45e
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-06-09 14:10:11 +0200

    msm8998: fix header inclusion
    fix inclusion on kernel compilation outside framework
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- sound/soc/msm/msm8998.c
+++ sound/soc/msm/msm8998.c
@@ -33,7 +33,7 @@
 #include <sound/q6core.h>
 #include <sound/pcm_params.h>
 #include <sound/info.h>
-#include <device_event.h>
+#include "device_event.h"
 #include <linux/qdsp6v2/audio_notifier.h>
 #include "qdsp6v2/msm-pcm-routing-v2.h"
 #include "../codecs/wcd9335.h"
