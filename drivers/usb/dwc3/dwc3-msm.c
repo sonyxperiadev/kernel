af625bb053c3fcb5a11b3b01e5873e91d6917c38
Angelo G. Del Regno <kholk11@gmail.com>
2017-06-15 14:35:28 +0200

    usb: dwc3: dwc3-msm: Fix ifdef for otg_present variable
    This variable is used by both extcon SoMC extension and
    dwc3 id polling functionality.

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -248,6 +248,10 @@ struct dwc3_msm {
 	int pm_qos_latency;
 #ifdef CONFIG_EXTCON_SOMC_EXTENSION
 	bool			send_vbus_drop_ue;
+#endif
+
+#if defined(CONFIG_EXTCON_SOMC_EXTENSION) || \
+    defined(CONFIG_USB_DWC3_MSM_ID_POLL)
 	bool			otg_present;
 #endif
 #ifdef CONFIG_USB_DWC3_MSM_ID_POLL
