ff274d94ab4300494dc24b3a53d84102be63b149
Angelo G. Del Regno <kholk11@gmail.com>
2017-06-15 14:55:46 +0200

    hwmon: qpnp-adc-common: Update decidegc functions for SoMC
    These functions got updated for MSM8998 devices and kernel 4.4.
    The update is necessary for SoMC Yoshino.
    
    Taken from copyleft release 45.0.A.5.1

--- include/linux/qpnp/qpnp-adc.h
+++ include/linux/qpnp/qpnp-adc.h
@@ -1745,19 +1745,25 @@ int32_t qpnp_adc_tm_scale_voltage_therm_pu2(struct qpnp_vadc_chip *dev,
  *		and convert given temperature to voltage on supported
  *		thermistor channels using 100k pull-up.
  * @dev:	Structure device for qpnp vadc
+ * @adc_prop:	adc properties of the qpnp adc such as bit resolution,
+ *		reference voltage.
  * @param:	The input temperature values.
  */
 int32_t qpnp_adc_tm_scale_therm_voltage_pu2_decidegc(struct qpnp_vadc_chip *dev,
+		const struct qpnp_adc_properties *adc_properties,
 				struct qpnp_adc_tm_config *param);
 /**
  * qpnp_adc_tm_scale_voltage_therm_pu2_decidegc() - Performs reverse calibration
  *		and converts the given ADC code to temperature for
  *		thermistor channels using 100k pull-up.
  * @dev:	Structure device for qpnp vadc
+ * @adc_prop:	adc properties of the qpnp adc such as bit resolution,
+ *		reference voltage.
  * @reg:	The input ADC code.
  * @result:	The physical measurement temperature on the thermistor.
  */
 int32_t qpnp_adc_tm_scale_voltage_therm_pu2_decidegc(struct qpnp_vadc_chip *dev,
+			const struct qpnp_adc_properties *adc_prop,
 				uint32_t reg, int64_t *result);
 /**
  * qpnp_adc_usb_scaler() - Performs reverse calibration on the low/high
@@ -2095,11 +2101,13 @@ static inline int32_t qpnp_adc_tm_scale_voltage_therm_pu2(
 { return -ENXIO; }
 static inline int32_t qpnp_adc_tm_scale_therm_voltage_pu2_decidegc(
 				struct qpnp_vadc_chip *dev,
+			const struct qpnp_adc_properties *adc_properties,
 				struct qpnp_adc_tm_config *param)
 { return -ENXIO; }
 static inline int32_t qpnp_adc_tm_scale_voltage_therm_pu2_decidegc(
 				struct qpnp_vadc_chip *dev,
-				uint32_t reg, int64_t *result)
+			const struct qpnp_adc_properties *adc_prop,
+			uint32_t reg, int64_t *result)
 { return -ENXIO; }
 static inline int32_t qpnp_adc_smb_btm_rscaler(struct qpnp_vadc_chip *dev,
 		struct qpnp_adc_tm_btm_param *param,
