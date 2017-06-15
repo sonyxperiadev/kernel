b25d85447ae274bc2590553480d56daa587f44be
Angelo G. Del Regno <kholk11@gmail.com>
2017-06-15 23:27:03 +0200

    Android.mk: Allow to compile kernel for SoMC Yoshino
    Add it to the allowed platforms filter.

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 # Android makefile to build kernel as a part of Android Build
 
 ifeq ($(BUILD_KERNEL),true)
-ifeq ($(filter-out loire tone,$(PRODUCT_PLATFORM)),)
+ifeq ($(filter-out loire tone yoshino,$(PRODUCT_PLATFORM)),)
 
 KERNEL_SRC := $(call my-dir)
 
