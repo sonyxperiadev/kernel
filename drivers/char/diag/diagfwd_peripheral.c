9563c2d42f81afab1d028bbd58a7b626e54ebe3f
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-06-30 23:21:53 +0200

    Revert "diag: Add mutex protection while closing diag channels"
    This reverts commit 0d53eb75e4d3ebb6776cae949b7db2435e182c4e.

--- drivers/char/diag/diagfwd_peripheral.c
+++ drivers/char/diag/diagfwd_peripheral.c
@@ -1242,7 +1242,7 @@ int diagfwd_channel_open(struct diagfwd_info *fwd_info)
 			 __func__, fwd_info->peripheral, fwd_info->type);
 		return 0;
 	}
-	mutex_lock(&driver->diagfwd_channel_mutex[fwd_info->peripheral]);
+
 	fwd_info->ch_open = 1;
 	diagfwd_buffers_init(fwd_info);
 
@@ -1267,7 +1267,7 @@ int diagfwd_channel_open(struct diagfwd_info *fwd_info)
 		if (fwd_info->p_ops && fwd_info->p_ops->open)
 			fwd_info->p_ops->open(fwd_info->ctxt);
 	}
-	mutex_unlock(&driver->diagfwd_channel_mutex[fwd_info->peripheral]);
+
 	return 0;
 }
 
@@ -1280,7 +1280,6 @@ int diagfwd_channel_close(struct diagfwd_info *fwd_info)
 	if (fwd_info->type == TYPE_CNTL)
 		flush_workqueue(driver->cntl_wq);
 
-	mutex_lock(&driver->diagfwd_channel_mutex[fwd_info->peripheral]);
 	fwd_info->ch_open = 0;
 	if (fwd_info && fwd_info->c_ops && fwd_info->c_ops->close)
 		fwd_info->c_ops->close(fwd_info);
@@ -1296,7 +1295,7 @@ int diagfwd_channel_close(struct diagfwd_info *fwd_info)
 	}
 	DIAG_LOG(DIAG_DEBUG_PERIPHERALS, "p: %d t: %d considered closed\n",
 		 fwd_info->peripheral, fwd_info->type);
-	mutex_unlock(&driver->diagfwd_channel_mutex[fwd_info->peripheral]);
+
 	return 0;
 }
 
