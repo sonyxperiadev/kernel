1186584953ae927106884c1c52aa74c35c426a8e
Pavel Dubrova <pashadubrova@gmail.com>
2017-07-13 23:59:07 +0300

    regulator: onsemi-ncp6335d: Fix compilation issues
    drivers/regulator/onsemi-ncp6335d.c:522:14: error: too few arguments to function 'of_get_regulator_init_data'
      init_data = of_get_regulator_init_data(&client->dev,
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/regulator/onsemi-ncp6335d.c
+++ drivers/regulator/onsemi-ncp6335d.c
@@ -520,7 +520,7 @@ static struct ncp6335d_platform_data *
 	int rc;
 
 	init_data = of_get_regulator_init_data(&client->dev,
-				client->dev.of_node);
+				client->dev.of_node, &rdesc);
 	if (!init_data) {
 		dev_err(&client->dev, "regulator init data is missing\n");
 		return pdata;
