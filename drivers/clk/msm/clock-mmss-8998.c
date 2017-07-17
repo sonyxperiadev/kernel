2e123c357c00fca07773dea61d1fd1dde9cd70a8
Angelo G. Del Regno <kholk11@gmail.com>
2017-07-17 19:01:01 +0200

    clk: msm: mmss-8998: Allow 550MHz on MDP clock source
    This clock is stable on MSM8998 and permits us to eventually
    be able to drive a 4k@60Hz panel.

--- drivers/clk/msm/clock-mmss-8998.c
+++ drivers/clk/msm/clock-mmss-8998.c
@@ -351,6 +351,7 @@ static struct clk_freq_tbl ftbl_mdp_clk_src[] = {
 	F_MM( 300000000,   mmsscc_gpll0,    2,    0,     0),
 	F_MM( 330000000, mmpll5_pll_out,  2.5,    0,     0),
 	F_MM( 412500000, mmpll5_pll_out,    2,    0,     0),
+	F_MM( 550000000, mmpll5_pll_out,  1.5,    0,     0),
 	F_END
 };
 
@@ -366,7 +367,7 @@ static struct rcg_clk mdp_clk_src = {
 		.ops = &clk_ops_rcg,
 		.flags = CLKFLAG_NO_RATE_CACHE,
 		VDD_DIG_FMAX_MAP4(LOWER, 171430000, LOW, 275000000,
-					NOMINAL, 330000000, HIGH, 412500000),
+					NOMINAL, 330000000, HIGH, 550000000),
 		CLK_INIT(mdp_clk_src.c),
 	},
 };
