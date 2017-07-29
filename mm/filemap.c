768e4abb12d7e444b3006eb751c5f9700c49b36a
bananafunction <terathree2@web.de>
2017-07-29 19:35:47 +0200

    patch upstream kernel v3.10.93 from kernel.org

--- mm/filemap.c
+++ mm/filemap.c
@@ -2351,6 +2351,11 @@ again:
 			break;
 		}
 
+		if (fatal_signal_pending(current)) {
+			status = -EINTR;
+			break;
+		}
+
 		status = a_ops->write_begin(file, mapping, pos, bytes, flags,
 						&page, &fsdata);
 		if (unlikely(status))
@@ -2391,10 +2396,6 @@ again:
 		written += copied;
 
 		balance_dirty_pages_ratelimited(mapping);
-		if (fatal_signal_pending(current)) {
-			status = -EINTR;
-			break;
-		}
 	} while (iov_iter_count(i));
 
 	return written ? written : status;
