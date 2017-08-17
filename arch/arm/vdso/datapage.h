990bb67b7f196f10ef759453b32239dad8a1133b
Mark Salyzyn <salyzyn@google.com>
2017-08-17 09:16:27 -0700

    FROMLIST: [PATCH v5 02/12] arm: vdso: add include file defining __get_datapage()
    (cherry picked from url https://patchwork.kernel.org/patch/10044481/)
    
    Take an effort to recode the arm64 vdso code from assembler to C
    previously submitted by Andrew Pinski <apinski@cavium.com>, rework
    it for use in both arm and arm64, overlapping any optimizations
    for each architecture. But instead of landing it in arm64, land the
    result into lib/vdso and unify both implementations to simplify
    future maintenance.
    
    Define the prototype for __get_datapage() in local datapage.h header.
    Rename all vdata variable that point to the datapage shortened to vd
    to relect a consistent and concise style. Make sure that all
    references to the datapage in vdso operations are readonly (const).
    Make sure datapage is first parameter to all subroutines to also
    be consistent.
    
    Signed-off-by: Mark Salyzyn <salyzyn@android.com>
    Cc: James Morse <james.morse@arm.com>
    Cc: Russell King <linux@armlinux.org.uk>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Dmitry Safonov <dsafonov@virtuozzo.com>
    Cc: John Stultz <john.stultz@linaro.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Laura Abbott <labbott@redhat.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Andy Gross <andy.gross@linaro.org>
    Cc: Kevin Brodsky <kevin.brodsky@arm.com>
    Cc: Andrew Pinski <apinski@cavium.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-arm-kernel@lists.infradead.org
    Bug: 63737556
    Bug: 20045882
    Change-Id: I9512b49d36d53ca1b71d3ff82219a7c64e0fc613

---
+++ arch/arm/vdso/datapage.h
@@ -0,0 +1,25 @@
+/*
+ * Userspace implementations of __get_datapage
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __VDSO_DATAPAGE_H
+#define __VDSO_DATAPAGE_H
+
+#include <linux/types.h>
+#include <asm/vdso_datapage.h>
+
+extern const struct vdso_data *__get_datapage(void);
+
+#endif /* __VDSO_DATAPAGE_H */
