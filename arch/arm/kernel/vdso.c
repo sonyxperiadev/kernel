f4b688ff2cb17b95154247ac84357600ea4ef9df
Mark Salyzyn <salyzyn@google.com>
2017-08-17 09:16:27 -0700

    FROMLIST: [PATCH v5 06/12] arm: vdso: add support for clock_getres
    (cherry picked from url https://patchwork.kernel.org/patch/10044545/)
    
    Take an effort to recode the arm64 vdso code from assembler to C
    previously submitted by Andrew Pinski <apinski@cavium.com>, rework
    it for use in both arm and arm64, overlapping any optimizations
    for each architecture. But instead of landing it in arm64, land the
    result into lib/vdso and unify both implementations to simplify
    future maintenance.
    
    Add clock_getres vdso support to match up with existing support in
    the arm64's vdso.
    
    Signed-off-by: Mark Salyzyn <salyzyn@android.com>
    Cc: James Morse <james.morse@arm.com>
    Cc: Russell King <linux@armlinux.org.uk>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Dmitry Safonov <dsafonov@virtuozzo.com>
    Cc: John Stultz <john.stultz@linaro.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Laura Abbott <labbott@redhat.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Andy Gross <andy.gross@linaro.org>
    Cc: Kevin Brodsky <kevin.brodsky@arm.com>
    Cc: Andrew Pinski <apinski@cavium.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-arm-kernel@lists.infradead.org
    Bug: 63737556
    Bug: 20045882
    Change-Id: Ie37bf76d2992027f06a2cdd001d8654a860d2aac

--- arch/arm/kernel/vdso.c
+++ arch/arm/kernel/vdso.c
@@ -173,6 +173,7 @@ static void __init patch_vdso(void *ehdr)
 	if (!cntvct_ok) {
 		vdso_nullpatch_one(&einfo, "__vdso_gettimeofday");
 		vdso_nullpatch_one(&einfo, "__vdso_clock_gettime");
+		vdso_nullpatch_one(&einfo, "__vdso_clock_getres");
 	}
 }
 
