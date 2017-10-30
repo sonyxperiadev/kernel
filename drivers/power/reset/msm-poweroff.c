668c82322fc2dfb33adf4bdacdef400cb2980337
Alin Jerpelea <alin.jerpelea@sony.com>
2017-10-30 10:31:03 +0100

    power: msm-poweroff: force warmreset for S1Loader
    S1 loader needs warm reset for all reboot reasons
    
    this will fix the device being stuck in charger mode on reboot
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -303,7 +303,7 @@ static void msm_restart_prepare(const char *cmd)
 				(cmd != NULL && cmd[0] != '\0'));
 	}
 
-#if defined(TARGET_SOMC_S1BOOT) || defined(TARGET_SOMC_XBOOT)
+#if defined(TARGET_SOMC_XBOOT)
 	/* Force warm reset and allow device to
 	 * preserve memory on restart for kernel
 	 * panic or for bootloader and recovery
@@ -315,6 +315,8 @@ static void msm_restart_prepare(const char *cmd)
 		else
 			need_warm_reset = false;
 	}
+#elif defined(TARGET_SOMC_S1BOOT)
+	need_warm_reset = true;
 #endif
 
 	if (need_warm_reset)
