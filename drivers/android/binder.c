d0351e61d91d23d1c731167e8729cc0eb4b094e2
David <Davidteri91@gmail.com>
2017-10-30 20:49:55 +0100

    Revert "ANDROID: binder: call poll_wait() unconditionally."
    This reverts commit 727bd7a6f9cc2af0823992ce983906448684320f.

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -3792,6 +3792,12 @@ static void binder_stat_br(struct binder_proc *proc,
 	}
 }
 
+static int binder_has_thread_work(struct binder_thread *thread)
+{
+	return !binder_worklist_empty(thread->proc, &thread->todo) ||
+		thread->looper_need_return;
+}
+
 static int binder_put_node_cmd(struct binder_proc *proc,
 			       struct binder_thread *thread,
 			       void __user **ptrp,
@@ -4422,9 +4428,12 @@ static unsigned int binder_poll(struct file *filp,
 
 	binder_inner_proc_unlock(thread->proc);
 
+	if (binder_has_work(thread, wait_for_proc_work))
+		return POLLIN;
+
 	poll_wait(filp, &thread->wait, wait);
 
-	if (binder_has_work(thread, wait_for_proc_work))
+	if (binder_has_thread_work(thread))
 		return POLLIN;
 
 	return 0;
