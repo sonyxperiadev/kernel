da68d8247db5ea7c7a4bfec587f6623ce06bbc12
Praneeth Paladugu <ppaladug@codeaurora.org>
2018-01-29 16:15:18 -0800

    msm: vidc: Add support for buffer tags and sub-frame info
       1) Clients send input_tag and output_tag through buffer
          reserved fields. Driver needs to pass them to FW and
          when FW returns them back, send them to client.
       2) Convey sub-frame information to client from FW.
    
    Change-Id: Ia92bca933d18a7795ba17eea9178a4c5de49157b
    Signed-off-by: Praneeth Paladugu <ppaladug@codeaurora.org>
    Signed-off-by: Umesh Pandey <umeshp@codeaurora.org>
    Signed-off-by: Praveen Chavan <pchavan@codeaurora.org>

--- drivers/media/platform/msm/vidc/vidc_hfi_api.h
+++ drivers/media/platform/msm/vidc/vidc_hfi_api.h
@@ -979,6 +979,8 @@ struct vidc_frame_data {
 	u32 filled_len;
 	u32 mark_target;
 	u32 mark_data;
+	u32 input_tag;
+	u32 output_tag;
 	u32 clnt_data;
 	u32 extradata_size;
 };
@@ -1175,6 +1177,8 @@ struct vidc_hal_ebd {
 	u32 mark_data;
 	u32 stats;
 	u32 offset;
+	u32 input_tag;
+	u32 output_tag;
 	u32 alloc_len;
 	u32 filled_len;
 	enum hal_picture picture_type;
@@ -1201,6 +1205,7 @@ struct vidc_hal_fbd {
 	u32 start_y_coord;
 	u32 input_tag;
 	u32 input_tag1;
+	u32 output_tag;
 	enum hal_picture picture_type;
 	u32 packet_buffer1;
 	u32 extra_data_buffer;
@@ -1320,12 +1325,16 @@ struct msm_vidc_cb_event {
 	u32 hal_event_type;
 	u32 packet_buffer;
 	u32 extra_data_buffer;
+	u32 output_tag;
 	u32 pic_struct;
 	u32 colour_space;
 	u32 profile;
 	u32 level;
 	u32 entropy_mode;
 	u32 capture_buf_count;
+	u32 max_dpb_count;
+	u32 max_ref_count;
+	u32 max_dec_buffering;
 	struct hal_index_extradata_input_crop_payload crop_data;
 };
 
