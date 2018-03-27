bad6651406d0ca46f3ca1ac5dd2b4b5a19aa019b
Aditya Bavanari <abavanar@codeaurora.org>
2018-03-27 12:46:44 +0530

    asoc: codecs: sdm660_cdc: Fix LPASS register access during PDR
    When AFE returns failure in setting the digital codec
    core clock during PDR, it leads to LPASS register access
    which results in NOC error and AHB timeout. Put the regmap
    in cache only mode during clock set failure to fix this.
    
    Change-Id: I221bf02f32b1dae19a02435220dec8a85f5dd998
    Signed-off-by: Aditya Bavanari <abavanar@codeaurora.org>

--- techpack/audio/asoc/codecs/sdm660_cdc/msm-digital-cdc.c
+++ techpack/audio/asoc/codecs/sdm660_cdc/msm-digital-cdc.c
@@ -100,11 +100,12 @@ static int msm_digcdc_clock_control(bool flag)
 				       __func__);
 				/*
 				 * Avoid access to lpass register
-				 * as clock enable failed during SSR.
+				 * as clock enable failed during SSR/PDR.
 				 */
-				if (ret == -ENODEV)
-					msm_dig_cdc->regmap->cache_only = true;
+				msm_dig_cdc->regmap->cache_only = true;
 				return ret;
+			} else {
+				msm_dig_cdc->regmap->cache_only = false;
 			}
 			pr_debug("enabled digital codec core clk\n");
 			atomic_set(&pdata->int_mclk0_enabled, true);
