bc15fc3da597bb7f1cdbcbb56cd1ab0c52809d4a
Angelo G. Del Regno <kholk11@gmail.com>
2018-04-08 15:06:58 +0200

    clk: qcom: Introduce the MSM8976 CPU/L2 Clock Controller driver
    This commit adds full support for the CPU (APCS) Clusters and L2
    (CCI) Clock Controllers found on the MSM8976/8956 SoC.
    
    This driver is also responsible of driving the DVFS relative to
    its managed PLLs and clocks and it is compatible with SPM.
    
    Signed-off-by: Angelo G. Del Regno <kholk11@gmail.com>

---
+++ include/dt-bindings/clock/qcom,cpu-8976.h
@@ -0,0 +1,30 @@
+/*
+ *  Copyright (c) 2018, AngeloGioacchino Del Regno <kholk11@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _DT_BINDINGS_CLK_MSM_CPU_8976_H
+#define _DT_BINDINGS_CLK_MSM_CPU_8976_H
+
+/* CPU clock IDs */
+#define P_APCSAUX_2		0
+#define P_APCSAUX_3		1
+#define P_A72_HF_PLL_MAIN	2
+#define P_A72_HF_PLL		3
+#define P_A53_SR_PLL_MAIN	4
+#define P_A53_SR_PLL		5
+#define P_CCI_SR_PLL		6
+#define P_CCI_SR_PLL_MAIN	7
+#define P_A72_CLK		8
+#define P_A53_CLK		9
+#define P_CCI_CLK		10
+
+#endif
