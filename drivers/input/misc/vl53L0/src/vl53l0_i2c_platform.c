4bcf499283436b49421300f125894b30dae6808c
Angelo G. Del Regno <kholk11@gmail.com>
2018-04-13 10:56:34 +0200

    input: misc: vl53L0: Fix header inclusions paths
    As per title. Some files had wrong path.

--- drivers/input/misc/vl53L0/src/vl53l0_i2c_platform.c
+++ drivers/input/misc/vl53L0/src/vl53l0_i2c_platform.c
@@ -22,8 +22,8 @@
 #include <linux/i2c.h>
 #include <linux/module.h>
 #include <linux/delay.h>
-#include "stmvl53l0-i2c.h"
-#include "stmvl53l0-cci.h"
+#include "../stmvl53l0-i2c.h"
+#include "../stmvl53l0-cci.h"
 
 #include "vl53l0_platform.h"
 #include "vl53l0_i2c_platform.h"
--- drivers/input/misc/vl53L0/src/vl53l0_port_i2c.c
+++ drivers/input/misc/vl53L0/src/vl53l0_port_i2c.c
@@ -7,11 +7,11 @@
 
 #include <linux/i2c.h>
 #include <linux/module.h>
-#include "stmvl53l0-i2c.h"
-#include "stmvl53l0-cci.h"
+#include "../stmvl53l0-i2c.h"
+#include "../stmvl53l0-cci.h"
 #include "vl53l0_platform.h"
 #include "vl53l0_i2c_platform.h"
-#include "stmvl53l0.h"
+#include "../stmvl53l0.h"
 
 #define I2C_M_WR			0x00
 #define STATUS_OK			0x00
