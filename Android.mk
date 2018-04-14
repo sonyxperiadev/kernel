787f5a9fe56e7f3762cbb4eca60c1c0da06b1fb3
Michele Bono <bono.michele94@gmail.com>
2018-04-14 15:00:37 +0200

    Android.mk: Automatically remove Clang URLs from kernel version
    Before: Android (4751641 based on r328903) clang version 7.0.2 (https://android.googlesource.com/toolchain/clang 003100370607242ddd5815e4a043907ea9004281) (https://android.googlesource.com/toolchain/llvm 1d739ffb0366421d383e04ff80ec2ee591315116) (based on LLVM 7.0.2svn)
    
    After: Android (4751641 based on r328903) clang version 7.0.2 (based on LLVM 7.0.2svn)
    
    Change-Id: If924924c648baaf80f942a2adc4bd698b0e131c6
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- Android.mk
+++ Android.mk
@@ -118,6 +118,8 @@ ifeq ($(TARGET_KERNEL_CLANG_COMPILE),true)
         KERNEL_CLANG_VERSION := $(shell ls -d $(ANDROID_BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/clang-* | xargs -n 1 basename | tail -1)
     endif
     TARGET_KERNEL_CLANG_PATH ?= $(ANDROID_BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/$(KERNEL_CLANG_VERSION)/bin
+    KBUILD_COMPILER_STRING := $(shell $(TARGET_KERNEL_CLANG_PATH)/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')
+    export KBUILD_COMPILER_STRING
     ifeq ($(KERNEL_ARCH),arm64)
         KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=aarch64-linux-gnu-
     else ifeq ($(KERNEL_ARCH),arm)
