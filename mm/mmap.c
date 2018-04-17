9a796a1cecbb340b7c330b1b9bad878605793687
Laurent Dufour <ldufour@linux.vnet.ibm.com>
2018-04-17 16:33:13 +0200

    mm: introduce INIT_VMA()
    Some VMA struct fields need to be initialized once the VMA structure is
    allocated.
    Currently this only concerns anon_vma_chain field but some other will be
    added to support the speculative page fault.
    
    Instead of spreading the initialization calls all over the code, let's
    introduce a dedicated inline function.
    
    Change-Id: I9f6b29dc74055354318b548e2b6b22c37d4c61bb
    Signed-off-by: Laurent Dufour <ldufour@linux.vnet.ibm.com>
    Patch-mainline: linux-mm @ Tue, 17 Apr 2018 16:33:13
    [vinmenon@codeaurora.org: 4.9 porting changes]
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- mm/mmap.c
+++ mm/mmap.c
@@ -1681,7 +1681,7 @@ unsigned long mmap_region(struct file *file, unsigned long addr,
 	vma->vm_flags = vm_flags;
 	vma->vm_page_prot = vm_get_page_prot(vm_flags);
 	vma->vm_pgoff = pgoff;
-	INIT_LIST_HEAD(&vma->anon_vma_chain);
+	INIT_VMA(vma);
 
 	if (file) {
 		if (vm_flags & VM_DENYWRITE) {
@@ -2572,7 +2572,7 @@ static int __split_vma(struct mm_struct *mm, struct vm_area_struct *vma,
 	/* most fields are the same, copy all, and then fixup */
 	*new = *vma;
 
-	INIT_LIST_HEAD(&new->anon_vma_chain);
+	INIT_VMA(new);
 
 	if (new_below)
 		new->vm_end = addr;
@@ -2929,7 +2929,7 @@ static int do_brk(unsigned long addr, unsigned long request)
 		return -ENOMEM;
 	}
 
-	INIT_LIST_HEAD(&vma->anon_vma_chain);
+	INIT_VMA(vma);
 	vma->vm_mm = mm;
 	vma->vm_start = addr;
 	vma->vm_end = addr + len;
@@ -3127,7 +3127,7 @@ struct vm_area_struct *copy_vma(struct vm_area_struct **vmap,
 		new_vma->vm_pgoff = pgoff;
 		if (vma_dup_policy(vma, new_vma))
 			goto out_free_vma;
-		INIT_LIST_HEAD(&new_vma->anon_vma_chain);
+		INIT_VMA(new_vma);
 		if (anon_vma_clone(new_vma, vma))
 			goto out_free_mempol;
 		if (new_vma->vm_file)
@@ -3265,7 +3265,7 @@ static struct vm_area_struct *__install_special_mapping(
 	if (unlikely(vma == NULL))
 		return ERR_PTR(-ENOMEM);
 
-	INIT_LIST_HEAD(&vma->anon_vma_chain);
+	INIT_VMA(vma);
 	vma->vm_mm = mm;
 	vma->vm_start = addr;
 	vma->vm_end = addr + len;
