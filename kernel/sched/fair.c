9ad418f25b744470f15161e603172ad05f1e7546
Nathan Chancellor <natechancellor@gmail.com>
2018-04-25 17:42:43 -0700

    sched/fair: Fix misleading indentation warning
    See the commit in kernel/common for reference: https://github.com/aosp-mirror/kernel_common/commit/4f8767d1ca307ab8c03889534a3c5a525ce7485d
    
    Fixes: 1b3391fa7f2d
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6543,9 +6543,10 @@ static int find_new_capacity(struct energy_env *eenv,
 	eenv->cap_idx = max_idx;
 
 	for (idx = 0; idx < sge->nr_cap_states; idx++) {
-		if (sge->cap_states[idx].cap >= util)
+		if (sge->cap_states[idx].cap >= util) {
 			eenv->cap_idx = idx;
 			break;
+		}
 	}
 
 	return eenv->cap_idx;
