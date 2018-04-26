2acac1d1b020077972fff812cffe59cd67577f01
Nathan Chancellor <natechancellor@gmail.com>
2018-04-25 22:48:54 -0700

    net: wireless: bcmdhd: Fix constant conversion warnings
    Fixes the following Clang warning: implicit conversion from 'unsigned
    int' to 'uint16' (aka 'unsigned short') changes value from 34078752 to
    32 [-Wconstant-conversion]
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/net/wireless/bcmdhd/wl_android.c
+++ drivers/net/wireless/bcmdhd/wl_android.c
@@ -1713,7 +1713,7 @@ int wl_android_set_ibss_beacon_ouidata(struct net_device *dev, char *command, in
 	vndr_ie_setbuf_t *vndr_ie = NULL;
 	s32 iecount;
 	uint32 pktflag;
-	u16 kflags = in_atomic() ? GFP_ATOMIC : GFP_KERNEL;
+	u32 kflags = in_atomic() ? GFP_ATOMIC : GFP_KERNEL;
 	s32 err = BCME_OK;
 
 	/* Check the VSIE (Vendor Specific IE) which was added.
--- drivers/net/wireless/bcmdhd/wl_cfg80211.c
+++ drivers/net/wireless/bcmdhd/wl_cfg80211.c
@@ -3132,7 +3132,7 @@ channel_to_chanspec(struct wiphy *wiphy, struct net_device *dev, u32 channel, u3
 	int bw = 0, tmp_bw = 0;
 	int i;
 	u32 tmp_c;
-	u16 kflags = in_atomic() ? GFP_ATOMIC : GFP_KERNEL;
+	u32 kflags = in_atomic() ? GFP_ATOMIC : GFP_KERNEL;
 #define LOCAL_BUF_SIZE	1024
 	buf = (u8 *) kzalloc(LOCAL_BUF_SIZE, kflags);
 	if (!buf) {
