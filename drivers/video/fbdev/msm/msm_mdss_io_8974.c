caa48a3645ef713c4777fb709fc3fbe517342433
Nathan Chancellor <natechancellor@gmail.com>
2018-04-25 18:39:45 -0700

    msm: mdss: Fix misleading indentation warning
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/video/fbdev/msm/msm_mdss_io_8974.c
+++ drivers/video/fbdev/msm/msm_mdss_io_8974.c
@@ -1330,17 +1330,17 @@ static void mdss_dsi_phy_regulator_ctrl(struct mdss_dsi_ctrl_pdata *ctrl,
 				mdss_dsi_20nm_phy_regulator_enable(ctrl);
 				break;
 			default:
-			/*
-			 * For dual dsi case, do not reconfigure dsi phy
-			 * regulator if the other dsi controller is still
-			 * active.
-			 */
-			if (!mdss_dsi_is_hw_config_dual(sdata) ||
-				(mdss_dsi_is_ctrl_clk_master(ctrl) &&
-					pinfo->ulps_suspend_enabled) ||
-				(other_ctrl && (!other_ctrl->is_phyreg_enabled
-						|| other_ctrl->mmss_clamp)))
-				mdss_dsi_28nm_phy_regulator_enable(ctrl);
+				/*
+				 * For dual dsi case, do not reconfigure dsi phy
+				 * regulator if the other dsi controller is still
+				 * active.
+				 */
+				if (!mdss_dsi_is_hw_config_dual(sdata) ||
+					(mdss_dsi_is_ctrl_clk_master(ctrl) &&
+						pinfo->ulps_suspend_enabled) ||
+					(other_ctrl && (!other_ctrl->is_phyreg_enabled
+							|| other_ctrl->mmss_clamp)))
+					mdss_dsi_28nm_phy_regulator_enable(ctrl);
 				break;
 			}
 		}
