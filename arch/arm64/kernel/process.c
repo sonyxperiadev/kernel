6d28fff26278348028d5d0f50222a8230f354f68
Sami Tolvanen <samitolvanen@google.com>
2018-05-02 11:05:22 -0700

    ANDROID: arm64: implement Shadow Call Stack
    Bug: 112277034
    Change-Id: Ie18cb3738861828f7befef73b9c957340b0370d1
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -57,6 +57,7 @@
 #include <asm/fpsimd.h>
 #include <asm/mmu_context.h>
 #include <asm/processor.h>
+#include <asm/scs.h>
 #include <asm/stacktrace.h>
 
 #ifdef CONFIG_CC_STACKPROTECTOR
@@ -436,6 +437,8 @@ __notrace_funcgraph struct task_struct *__switch_to(struct task_struct *prev,
 	contextidr_thread_switch(next);
 	entry_task_switch(next);
 	uao_thread_switch(next);
+
+	scs_thread_switch(prev, next);
 
 	/*
 	 * Complete any pending TLB or cache maintenance on this CPU in case
