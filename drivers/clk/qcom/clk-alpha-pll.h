2df83fd9cf68fe9b1b3807048ffe77066335040e
Angelo G. Del Regno <kholk11@gmail.com>
2018-05-22 22:29:44 +0200

    clk: qcom: alpha-pll: Support Fabia and Trion PLLs
    These PLLs are found in SDM845 and SDXPOORWILLS.

--- drivers/clk/qcom/clk-alpha-pll.h
+++ drivers/clk/qcom/clk-alpha-pll.h
@@ -22,6 +22,8 @@ enum {
 	CLK_ALPHA_PLL_TYPE_DEFAULT,
 	CLK_ALPHA_PLL_TYPE_HUAYRA,
 	CLK_ALPHA_PLL_TYPE_BRAMMO,
+	CLK_ALPHA_PLL_TYPE_FABIA,
+	CLK_ALPHA_PLL_TYPE_TRION,
 	CLK_ALPHA_PLL_TYPE_MAX,
 };
 
@@ -31,11 +33,16 @@ enum {
 	PLL_OFF_ALPHA_VAL_U,
 	PLL_OFF_USER_CTL,
 	PLL_OFF_USER_CTL_U,
+	PLL_OFF_USER_CTL_U1,
 	PLL_OFF_CONFIG_CTL,
 	PLL_OFF_CONFIG_CTL_U,
+	PLL_OFF_CONFIG_CTL_U1,
 	PLL_OFF_TEST_CTL,
 	PLL_OFF_TEST_CTL_U,
 	PLL_OFF_STATUS,
+	PLL_OFF_OPMODE_VAL,
+	PLL_OFF_FRAC_VAL,
+	PLL_OFF_CAL_L_VAL,
 	PLL_OFF_MAX_REGS
 };
 
@@ -45,29 +52,6 @@ struct pll_vco {
 	unsigned long min_freq;
 	unsigned long max_freq;
 	u32 val;
-};
-
-/**
- * struct clk_alpha_pll - phase locked loop (PLL)
- * @offset: base address of registers
- * @vco_table: array of VCO settings
- * @regs: alpha pll register map (see @clk_alpha_pll_regs)
- * @clkr: regmap clock handle
- */
-struct clk_alpha_pll {
-	u32 offset;
-	const u8 *regs;
-
-	const struct pll_vco *vco_table;
-	size_t num_vco;
-#define SUPPORTS_OFFLINE_REQ	BIT(0)
-#define SUPPORTS_FSM_MODE	BIT(2)
-#define SUPPORTS_DYNAMIC_UPDATE	BIT(3)
-	u8 flags;
-
-	unsigned long min_supported_freq;
-
-	struct clk_regmap clkr;
 };
 
 /**
@@ -91,12 +75,14 @@ struct alpha_pll_config {
 	u32 alpha_hi;
 	u32 config_ctl_val;
 	u32 config_ctl_hi_val;
+	u32 config_ctl_hi1_val;
 	u32 main_output_mask;
 	u32 aux_output_mask;
 	u32 aux2_output_mask;
 	u32 early_output_mask;
 	u32 alpha_en_mask;
 	u32 alpha_mode_mask;
+	u32 frac;
 	u32 pre_div_val;
 	u32 pre_div_mask;
 	u32 post_div_val;
@@ -104,6 +90,33 @@ struct alpha_pll_config {
 	u32 vco_val;
 	u32 vco_mask;
 };
+
+/**
+ * struct clk_alpha_pll - phase locked loop (PLL)
+ * @offset: base address of registers
+ * @vco_table: array of VCO settings
+ * @regs: alpha pll register map (see @clk_alpha_pll_regs)
+ * @clkr: regmap clock handle
+ */
+struct clk_alpha_pll {
+	u32 offset;
+	const u8 *regs;
+	bool inited;
+
+	struct alpha_pll_config *config;
+
+	const struct pll_vco *vco_table;
+	size_t num_vco;
+#define SUPPORTS_OFFLINE_REQ	BIT(0)
+#define SUPPORTS_FSM_MODE	BIT(2)
+#define SUPPORTS_DYNAMIC_UPDATE	BIT(3)
+	u8 flags;
+
+	unsigned long min_supported_freq;
+
+	struct clk_regmap clkr;
+};
+
 
 extern const struct clk_ops clk_alpha_pll_ops;
 extern const struct clk_ops clk_alpha_pll_hwfsm_ops;
