faadeea364051db3e5392835fb020d66ad4d9ba5
Veerabhadrarao Badiganti <vbadigan@codeaurora.org>
2018-06-06 19:54:17 +0530

    mmc: core: Update SD card removal logic based on cd gpio state
    Update SD card removal event processing logic. Instead of pinging
    the card to know the card presence rely on card-detect gpio state.
    
    On multi-card tray designs, the same card-tray would be used for SD
    card and SIM cards. If SD card is placed at the rightmost location
    in the tray, then SIM card may come in contact with SD card power-
    supply while removing the tray. It may result in SIM damage.
    
    For protecting SIM from this issue, in multi-card tray designs,
    a h/w fix done such that pmic gets a notification of SD card
    removal event (through hardwiring) and it turns off the SD card
    voltage regulators immediately. All this will be done much before SD
    card driver starts processing card removal event.
    
    To support this design, SD card driver shouldn't turn-on the
    regulator while processing card removal event. But the present mmc
    driver turns-on regulator (multiple times if the card was in suspend
    state). To avoid turning on SD card regulator in card removal path,
    updating the card removal processing logic is based on card detect
    gpio state.
    
    Change-Id: I13708a60c9378519713ebec8071ae3b130012a93
    Signed-off-by: Veerabhadrarao Badiganti <vbadigan@codeaurora.org>
    Signed-off-by: Sarthak Garg <sartgarg@codeaurora.org>

--- drivers/mmc/core/sd.c
+++ drivers/mmc/core/sd.c
@@ -1156,6 +1156,9 @@ static void mmc_sd_remove(struct mmc_host *host)
  */
 static int mmc_sd_alive(struct mmc_host *host)
 {
+	if (host->ops->get_cd && !host->ops->get_cd(host))
+		return -ENOMEDIUM;
+
 	return mmc_send_status(host->card, NULL);
 }
 
@@ -1181,6 +1184,16 @@ static void mmc_sd_detect(struct mmc_host *host)
 		return;
 	}
 
+	if (mmc_bus_needs_resume(host))
+		mmc_resume_bus(host);
+
+	if (host->ops->get_cd && !host->ops->get_cd(host)) {
+		err = -ENOMEDIUM;
+		mmc_card_set_removed(host->card);
+		mmc_card_clr_suspended(host->card);
+		goto out;
+	}
+
 	/*
 	 * Just check if our card has been removed.
 	 */
@@ -1203,6 +1216,7 @@ static void mmc_sd_detect(struct mmc_host *host)
 	err = _mmc_detect_card_removed(host);
 #endif
 
+out:
 	mmc_put_card(host->card);
 
 	if (err) {
@@ -1280,6 +1294,11 @@ static int _mmc_sd_resume(struct mmc_host *host)
 
 	if (!mmc_card_suspended(host->card))
 		goto out;
+
+	if (host->ops->get_cd && !host->ops->get_cd(host)) {
+		mmc_card_clr_suspended(host->card);
+		goto out;
+	}
 
 	mmc_power_up(host, host->card->ocr);
 #ifdef CONFIG_MMC_PARANOID_SD_INIT
@@ -1376,6 +1395,9 @@ static int mmc_sd_runtime_resume(struct mmc_host *host)
 
 static int mmc_sd_reset(struct mmc_host *host)
 {
+	if (host->ops->get_cd && !host->ops->get_cd(host))
+		return -ENOMEDIUM;
+
 	mmc_power_cycle(host, host->card->ocr);
 	return mmc_sd_init_card(host, host->card->ocr, host->card);
 }
