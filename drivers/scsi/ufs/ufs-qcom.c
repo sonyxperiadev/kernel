417f60db83dec58b8431ff7b776fc3863a5c7ba4
Angelo G. Del Regno <kholk11@gmail.com>
2018-06-17 21:01:40 +0200

    scsi: ufs: ufs-qcom: Force disabling auto hibernate on Yoshino
    Yoshino platform does not support the auto hibernate feature.

--- drivers/scsi/ufs/ufs-qcom.c
+++ drivers/scsi/ufs/ufs-qcom.c
@@ -1501,7 +1501,9 @@ static void ufs_qcom_advertise_quirks(struct ufs_hba *hba)
 				| UFSHCD_QUIRK_BROKEN_PA_RXHSUNTERMCAP);
 	}
 
+#ifndef CONFIG_ARCH_SONY_YOSHINO
 	if (host->disable_lpm)
+#endif
 		hba->quirks |= UFSHCD_QUIRK_BROKEN_AUTO_HIBERN8;
 }
 
