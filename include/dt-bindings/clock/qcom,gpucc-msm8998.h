8a076949e32a27a96c4d38b516e38fc622bd1021
Angelo G. Del Regno <kholk11@gmail.com>
2018-06-23 13:06:10 +0200

    dt-bindings: gpucc-msm8998: Fix clock bindings for early gpucc
    It makes no sense for the early gpucc to have the RBCPR signaled
    as number 7, 8.

--- include/dt-bindings/clock/qcom,gpucc-msm8998.h
+++ include/dt-bindings/clock/qcom,gpucc-msm8998.h
@@ -13,16 +13,19 @@
  * GNU General Public License for more details.
  */
 
+#define GPU_PLL0_PLL		0
+#define GPU_PLL0_PLL_OUT_EVEN	1
+#define GPU_PLL0_PLL_OUT_ODD	2
+#define GFX3D_CLK_SRC		3
+#define RBBMTIMER_CLK_SRC	4
+#define GFX3D_ISENSE_CLK_SRC	5
+#define GPUCC_RBBMTIMER_CLK	6
+#define GPUCC_GFX3D_ISENSE_CLK	7
+#define GPUCC_GFX3D_CLK		8
+
+
+/* EARLY */
 #define GPUCC_XO		0
-#define GPU_PLL0_PLL		1
-#define GPU_PLL0_PLL_OUT_EVEN	2
-#define GPU_PLL0_PLL_OUT_ODD	3
-#define GFX3D_CLK_SRC		4
-#define RBBMTIMER_CLK_SRC	5
-#define GFX3D_ISENSE_CLK_SRC	6
-#define RBCPR_CLK_SRC		7
-#define GPUCC_RBBMTIMER_CLK	8
-#define GPUCC_GFX3D_ISENSE_CLK	9
-#define GPUCC_RBCPR_CLK		10
-#define GPUCC_GFX3D_CLK		11
+#define RBCPR_CLK_SRC		1
+#define GPUCC_RBCPR_CLK		2
 	
