ec058d46ebde4afdf67a5d6bd7b6443d0357c68c
Angelo G. Del Regno <kholk11@gmail.com>
2018-07-01 14:08:27 +0200

    soc: qcom: icnss: Force assigning IOMMU group if NULL
    On some implementations the IOMMU context doesn't get a group
    at (the missing) of_xlate time.
    In this case, if no group has been assigned to the device, force
    getting one from the IOMMU driver to be able to use it to then
    attach the context.

--- drivers/soc/qcom/icnss.c
+++ drivers/soc/qcom/icnss.c
@@ -2148,6 +2148,8 @@ EXPORT_SYMBOL(icnss_trigger_recovery);
 static int icnss_smmu_init(struct icnss_priv *priv)
 {
 	struct dma_iommu_mapping *mapping;
+	struct device *dev = &priv->pdev->dev;
+	struct iommu_group *grp = NULL;
 	int atomic_ctx = 1;
 	int s1_bypass = 1;
 	int fast = 1;
@@ -2163,6 +2165,12 @@ static int icnss_smmu_init(struct icnss_priv *priv)
 		icnss_pr_err("Create mapping failed, err = %d\n", ret);
 		ret = PTR_ERR(mapping);
 		goto map_fail;
+	}
+
+	if (!dev->iommu_group) {
+		grp = iommu_group_get_for_dev(dev);
+		if (IS_ERR_OR_NULL(grp))
+			return PTR_ERR(grp);
 	}
 
 	if (priv->bypass_s1_smmu) {
