e8cda6939b139f645a1fe154e37fd7e3703d504f
Angelo G. Del Regno <kholk11@gmail.com>
2018-07-13 17:14:41 +0200

    techpack: audio: msm_audio_ion: Add property to force SMMU SID
    soc: qcom: msm_audio_ion: Add property to force SMMU SID
    
    On legacy QC IOMMU v1 we have strange forced mappings, hence we
    need to force the SID to be 0x1 for the correct SID bit to be
    calculated and the memory allocation to succeed

--- techpack/audio/dsp/msm_audio_ion.c
+++ techpack/audio/dsp/msm_audio_ion.c
@@ -706,17 +706,17 @@ static int msm_audio_smmu_init(struct device *dev)
 	struct iommu_group *grp = NULL;
 	int ret;
 
-	mapping = arm_iommu_create_mapping(&platform_bus_type,
-					   MSM_AUDIO_ION_VA_START,
-					   MSM_AUDIO_ION_VA_LEN);
-	if (IS_ERR(mapping))
-		return PTR_ERR(mapping);
-
 	if (!dev->iommu_group) {
 		grp = iommu_group_get_for_dev(dev);
 		if (IS_ERR_OR_NULL(grp))
 			return PTR_ERR(grp);
 	}
+
+	mapping = arm_iommu_create_mapping(&platform_bus_type,
+					   MSM_AUDIO_ION_VA_START,
+					   MSM_AUDIO_ION_VA_LEN);
+	if (IS_ERR(mapping))
+		return PTR_ERR(mapping);
 
 	ret = arm_iommu_attach_device(dev, mapping);
 	if (ret) {
@@ -819,6 +819,7 @@ static int msm_audio_ion_probe(struct platform_device *pdev)
 	const char *msm_audio_ion_smmu_sid_mask = "qcom,smmu-sid-mask";
 	bool smmu_enabled;
 	bool scm_mp_enabled;
+	bool smmu_force_sid;
 	enum apr_subsys_state q6_state;
 	struct device *dev = &pdev->dev;
 
@@ -880,6 +881,7 @@ static int msm_audio_ion_probe(struct platform_device *pdev)
 				__func__);
 			smmu_sid_mask = 0xFFFFFFFFFFFFFFFF;
 		}
+
 		rc = of_parse_phandle_with_args(dev->of_node, "iommus",
 						"#iommu-cells", 0, &iommuspec);
 		if (rc)
@@ -887,6 +889,21 @@ static int msm_audio_ion_probe(struct platform_device *pdev)
 				__func__, rc);
 		else
 			smmu_sid = (iommuspec.args[0] & smmu_sid_mask);
+
+		smmu_force_sid = !!of_find_property(dev->of_node,
+					"qcom,smmu-force-sid", NULL);
+		if (smmu_force_sid) {
+			rc = of_property_read_u64(dev->of_node,
+					"qcom,smmu-force-sid", &smmu_sid);
+			if (rc) {
+				dev_err(dev,
+					"%s: Invalid smmu-force-sid property value\n",
+					__func__);
+				return rc;
+			}
+		} 
+
+		pr_err("msm_audio_ion sid bit is 0x%llx\n", smmu_sid);
 
 		msm_audio_ion_data.smmu_sid_bits =
 			smmu_sid << MSM_AUDIO_SMMU_SID_OFFSET;
