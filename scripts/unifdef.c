9789d0bc270d176d6928978de5ae338a97794e58
Humberto Borba <humberos@omnirom.org>
2018-07-14 15:05:47 -0300

    kconfig: Avoid bad output format warning from GCC
    Make GCC happy.
    
    kernel/sony/msm/scripts/unifdef.c: In function ‘Mpass’:
    kernel/sony/msm/scripts/unifdef.c:453:28: warning: ‘strncpy’ output truncated
    before terminating nul copying 4 bytes from a string of the same length
    [-Wstringop-truncation]
    
     static void Mpass (void) { strncpy(keyword, "if  ", 4); Pelif(); }
                                ^~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Humberto Borba <humberos@omnirom.org>
    Change-Id: I1ac566a33ac4c5a0494da83c81fd9d553423eafc

--- scripts/unifdef.c
+++ scripts/unifdef.c
@@ -450,7 +450,7 @@ static void Idrop (void) { Fdrop();  ignoreon(); }
 static void Itrue (void) { Ftrue();  ignoreon(); }
 static void Ifalse(void) { Ffalse(); ignoreon(); }
 /* modify this line */
-static void Mpass (void) { strncpy(keyword, "if  ", 4); Pelif(); }
+static void Mpass (void) { memcpy(keyword, "if  ", 4); Pelif(); }
 static void Mtrue (void) { keywordedit("else");  state(IS_TRUE_MIDDLE); }
 static void Melif (void) { keywordedit("endif"); state(IS_FALSE_TRAILER); }
 static void Melse (void) { keywordedit("endif"); state(IS_FALSE_ELSE); }
