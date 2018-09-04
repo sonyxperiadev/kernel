a97b131d4eb77c6393422168398bfc42064808a7
Ethan Chen <intervigil@gmail.com>
2018-09-03 19:11:39 -0700

    uapi: Define __BITS_PER_LONG based on compiler target
    * We may compile 32-bit ARM code against these kernel headers in many
      situations, so provide a compiler-defined method of obtaining the width
      of long.
    
    Change-Id: Iac5e48200d70f1258ab3caca1a8f1eb6e8f7f2d3

--- arch/arm64/include/uapi/asm/bitsperlong.h
+++ arch/arm64/include/uapi/asm/bitsperlong.h
@@ -16,7 +16,11 @@
 #ifndef __ASM_BITSPERLONG_H
 #define __ASM_BITSPERLONG_H
 
+#ifdef __aarch64__
 #define __BITS_PER_LONG 64
+#else
+#define __BITS_PER_LONG 32
+#endif
 
 #include <asm-generic/bitsperlong.h>
 
