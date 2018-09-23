b4484539c5b9a60419a8b0f0e02d2166fcab45e2
oshmoun <oshmoun100@googlemail.com>
2018-09-23 18:54:19 +0200

    brcmfmac: add quirks for 43455
    43455 needs the following quirks to work properly:
    - SKIP_ACTION_FRAMES: 43455 seems to have issues handling
      action frames properly, and suffers from firmware crashes.
      Avoid handling action frames to work around the issue.
      This is also how the bcmdhd driver seems to handle this.
    - FAKE_WOWL: WoWLAN support needs to be faked because the
      firmware does not report support for it.
      faking WoWLAN entails advertising support, but never actually
      doing any firmware calls.
      When this quirk is active, all calls to disassociate from AP
      in suspend and on linkdown events are avoided.
      43455 uses oob wakeup to inform the system of any events while
      it is in a suspended state.
    
    Change-Id: I1ead3eaf1bec6caa9d0ec5ba8ead2ed994025474
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.c
@@ -3685,6 +3685,9 @@ static s32 brcmf_cfg80211_resume(struct wiphy *wiphy)
 	struct brcmf_if *ifp = netdev_priv(ndev);
 
 	brcmf_dbg(TRACE, "Enter\n");
+	
+	if (brcmf_feat_is_quirk_enabled(ifp, BRCMF_FEAT_QUIRK_FAKE_WOWL))
+		return 0;
 
 	if (cfg->wowl.active) {
 		brcmf_report_wowl_wakeind(wiphy, ifp);
@@ -3785,6 +3788,9 @@ static s32 brcmf_cfg80211_suspend(struct wiphy *wiphy,
 	/* end any scanning */
 	if (test_bit(BRCMF_SCAN_STATUS_BUSY, &cfg->scan_status))
 		brcmf_abort_scanning(cfg);
+
+	if (brcmf_feat_is_quirk_enabled(ifp, BRCMF_FEAT_QUIRK_FAKE_WOWL))
+		goto exit;
 
 	if (wowl == NULL) {
 		brcmf_bus_wowl_config(cfg->pub->bus_if, false);
@@ -4891,6 +4897,8 @@ brcmf_cfg80211_mgmt_tx(struct wiphy *wiphy, struct wireless_dev *wdev,
 		       struct cfg80211_mgmt_tx_params *params, u64 *cookie)
 {
 	struct brcmf_cfg80211_info *cfg = wiphy_to_cfg(wiphy);
+	struct net_device *ndev = cfg_to_ndev(cfg);
+	struct brcmf_if *ifp = netdev_priv(ndev);
 	struct ieee80211_channel *chan = params->chan;
 	const u8 *buf = params->buf;
 	size_t len = params->len;
@@ -4942,6 +4950,11 @@ brcmf_cfg80211_mgmt_tx(struct wiphy *wiphy, struct wireless_dev *wdev,
 		cfg80211_mgmt_tx_status(wdev, *cookie, buf, len, true,
 					GFP_KERNEL);
 	} else if (ieee80211_is_action(mgmt->frame_control)) {
+		if (brcmf_feat_is_quirk_enabled(ifp,
+					BRCMF_FEAT_QUIRK_SKIP_ACTION_FRAMES)) {
+			brcmf_err("firmware does not handle action frames\n");
+			goto exit;
+		}		
 		if (len > BRCMF_FIL_ACTION_FRAME_SIZE + DOT11_MGMT_HDR_LEN) {
 			brcmf_err("invalid action frame length\n");
 			err = -EINVAL;
@@ -5676,7 +5689,8 @@ brcmf_notify_connect_status(struct brcmf_if *ifp,
 		brcmf_net_setcarrier(ifp, true);
 	} else if (brcmf_is_linkdown(e)) {
 		brcmf_dbg(CONN, "Linkdown\n");
-		if (!brcmf_is_ibssmode(ifp->vif)) {
+		if (!brcmf_is_ibssmode(ifp->vif) &&
+		    !brcmf_feat_is_quirk_enabled(ifp, BRCMF_FEAT_QUIRK_FAKE_WOWL)) {
 			brcmf_bss_connect_done(cfg, ndev, e, false);
 			brcmf_link_down(ifp->vif,
 					brcmf_map_fw_linkdown_reason(e));
@@ -6574,6 +6588,9 @@ static void brcmf_wiphy_wowl_params(struct wiphy *wiphy, struct brcmf_if *ifp)
 		wowl->flags |= WIPHY_WOWLAN_GTK_REKEY_FAILURE;
 	}
 
+	if (brcmf_feat_is_quirk_enabled(ifp, BRCMF_FEAT_QUIRK_FAKE_WOWL))
+		wowl->flags = WIPHY_WOWLAN_ANY;
+
 	wiphy->wowlan = wowl;
 #endif
 }
@@ -6648,7 +6665,8 @@ static int brcmf_setup_wiphy(struct wiphy *wiphy, struct brcmf_if *ifp)
 	wiphy->vendor_commands = brcmf_vendor_cmds;
 	wiphy->n_vendor_commands = BRCMF_VNDR_CMDS_LAST - 1;
 
-	if (brcmf_feat_is_enabled(ifp, BRCMF_FEAT_WOWL))
+	if (brcmf_feat_is_enabled(ifp, BRCMF_FEAT_WOWL) ||
+	    brcmf_feat_is_quirk_enabled(ifp, BRCMF_FEAT_QUIRK_FAKE_WOWL))
 		brcmf_wiphy_wowl_params(wiphy, ifp);
 	err = brcmf_fil_cmd_data_get(ifp, BRCMF_C_GET_BANDLIST, &bandlist,
 				     sizeof(bandlist));
