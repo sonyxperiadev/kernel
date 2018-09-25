2601d4b807195cad6f95eaf008384e9857fccf9e
Angelo G. Del Regno <kholk11@gmail.com>
2018-09-25 15:37:25 +0200

    regulator: qpnp-labibb: Do not globally disable interrupts for OCP
    It is stupid to disable interrupts globally once one of the
    two regulators gets shut down.
    Disable interrupts for LAB and IBB separately in their own
    respective disable functions when SoMC LCD OCP is enabled.

--- drivers/regulator/qpnp-labibb-regulator.c
+++ drivers/regulator/qpnp-labibb-regulator.c
@@ -2853,10 +2853,6 @@ static int qpnp_labibb_regulator_disable(struct qpnp_labibb *labibb)
 	int retries;
 	bool disabled = false;
 
-#ifdef CONFIG_SOMC_LCD_OCP_ENABLED
-	qpnp_labibb_interrupt_disable_ctl(labibb);
-#endif /* CONFIG_SOMC_LCD_OCP_ENABLED */
-
 	/*
 	 * When TTW mode is enabled and LABIBB regulators are disabled, it is
 	 * recommended not to disable IBB through IBB_ENABLE_CTL when switching
@@ -2979,6 +2975,10 @@ static int qpnp_lab_regulator_disable(struct regulator_dev *rdev)
 	int rc;
 	u8 val;
 	struct qpnp_labibb *labibb  = rdev_get_drvdata(rdev);
+
+#ifdef CONFIG_SOMC_LCD_OCP_ENABLED
+	qpnp_lab_interrupt_disable_ctl(labibb);
+#endif /* CONFIG_SOMC_LCD_OCP_ENABLED */
 
 	if (labibb->lab_vreg.vreg_enabled && !labibb->swire_control) {
 
@@ -4468,7 +4468,7 @@ static int qpnp_ibb_regulator_disable(struct regulator_dev *rdev)
 	struct qpnp_labibb *labibb  = rdev_get_drvdata(rdev);
 
 #ifdef CONFIG_SOMC_LCD_OCP_ENABLED
-		qpnp_lab_interrupt_disable_ctl(labibb);
+	qpnp_ibb_interrupt_disable_ctl(labibb);
 #endif /* CONFIG_SOMC_LCD_OCP_ENABLED */
 
 	if (labibb->ibb_vreg.vreg_enabled && !labibb->swire_control) {
