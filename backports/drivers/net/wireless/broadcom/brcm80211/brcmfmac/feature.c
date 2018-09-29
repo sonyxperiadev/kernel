1127466f3fae8bd86232a2d17be81a4737d19270
oshmoun <oshmoun100@googlemail.com>
2018-09-29 11:36:45 +0200

    backports: brcmfmac: add quirks for 43455
    43455 needs the following quirks to work properly:
    - SKIP_ACTION_FRAMES: 43455 seems to have issues handling action frames properly, and suffers from firmware crashes.
      Avoid handling action frames to work around the issue. This is also how the bcmdhd driver seems to handle this.
    - FAKE_WOWL: WoWLAN support needs to be faked because the firmware does not report support for it.
      faking WoWLAN entails advertising support, but never actually doing any firmware calls.
      When this quirk is active, all calls to disassociate from AP in suspend and on linkdown events are avoided.
      43455 uses oob wakeup to inform the system of any events while it is in a suspended state.
    
    Change-Id: I17d1d58637d5026885e3a6de6ee0d1f0c120c0af

--- backports/drivers/net/wireless/broadcom/brcm80211/brcmfmac/feature.c
+++ backports/drivers/net/wireless/broadcom/brcm80211/brcmfmac/feature.c
@@ -299,6 +299,10 @@ void brcmf_feat_attach(struct brcmf_pub *drvr)
 	case BRCM_CC_4329_CHIP_ID:
 		drvr->chip_quirks |= BIT(BRCMF_FEAT_QUIRK_NEED_MPC);
 		break;
+	case BRCM_CC_4345_CHIP_ID:
+		drvr->chip_quirks |= BIT(BRCMF_FEAT_QUIRK_SKIP_ACTION_FRAMES);
+		drvr->chip_quirks |= BIT(BRCMF_FEAT_QUIRK_FAKE_WOWL);
+		break;
 	default:
 		/* no quirks */
 		break;
