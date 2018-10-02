8b91237e56c5e973424cce855cf6df583da78a3f
Andrey Konovalov <andreyknvl@google.com>
2018-10-02 15:12:39 +0200

    FROMLIST: mm, arm64: untag user addresses in mm/gup.c
    (from https://lore.kernel.org/patchwork/patch/994347)
    
    mm/gup.c provides a kernel interface that accepts user addresses and
    manipulates user pages directly (for example get_user_pages, that is used
    by the futex syscall). Since a user can provided tagged addresses, we need
    to handle such case.
    
    Add untagging to gup.c functions that use user addresses for vma lookup.
    
    Bug: 112461694
    Change-Id: I01656a5221d9291dff7f6002e4d4a7bc244299c2
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>

--- mm/gup.c
+++ mm/gup.c
@@ -536,6 +536,8 @@ static long __get_user_pages(struct task_struct *tsk, struct mm_struct *mm,
 	if (!nr_pages)
 		return 0;
 
+	start = untagged_addr(start);
+
 	VM_BUG_ON(!!pages != !!(gup_flags & FOLL_GET));
 
 	/*
@@ -691,6 +693,8 @@ int fixup_user_fault(struct task_struct *tsk, struct mm_struct *mm,
 
 	if (unlocked)
 		fault_flags |= FAULT_FLAG_ALLOW_RETRY;
+
+	address = untagged_addr(address);
 
 retry:
 	vma = find_extend_vma(mm, address);
