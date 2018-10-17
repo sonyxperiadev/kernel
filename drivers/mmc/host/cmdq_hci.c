01eed4824d4c345decc2902e09b0c1715267d029
Ritesh Harjani <riteshh@codeaurora.org>
2018-10-17 10:28:43 +0530

    mmc: cmdq_hci: Do not handle other requests in case of error.
    There can be case where controller may raise success for
    rew queued requests while error for 1 of the requests.
    In case of error we were handling the error but proceeding
    and handling the requests which got completed by the controller.
    
    As of now in cmdq-err handler mechanism, if any error occurs,
    we re-queue all the requests back and request_queue.
    So we need not call done for other requests which care completed
    as this call lot of other contexts to run in parallel while error
    handler is waiting to run.
    
    Change-Id: I75214a870a13b39001d3d7a31e588c4c1113bed8
    Signed-off-by: Ritesh Harjani <riteshh@codeaurora.org>
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- drivers/mmc/host/cmdq_hci.c
+++ drivers/mmc/host/cmdq_hci.c
@@ -923,6 +923,8 @@ irqreturn_t cmdq_irq(struct mmc_host *mmc, int err)
 		err_info = cmdq_readl(cq_host, CQTERRI);
 		pr_err("%s: err: %d status: 0x%08x task-err-info (0x%08lx)\n",
 		       mmc_hostname(mmc), err, status, err_info);
+		/* Dump the registers before clearing Interrupt */
+		cmdq_dumpregs(cq_host);
 
 		/*
 		 * Need to halt CQE in case of error in interrupt context itself
@@ -946,7 +948,6 @@ irqreturn_t cmdq_irq(struct mmc_host *mmc, int err)
 		 */
 		cmdq_writel(cq_host, status, CQIS);
 
-		cmdq_dumpregs(cq_host);
 
 		if (!err_info) {
 			/*
@@ -1108,6 +1109,7 @@ skip_cqterri:
 			}
 		}
 		cmdq_finish_data(mmc, tag);
+		goto hac;
 	} else {
 		cmdq_writel(cq_host, status, CQIS);
 	}
