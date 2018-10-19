4be728b2ee0a3c6d8555fac57c0abf2ef9891184
Abhijit Kulkarni <kabhijit@codeaurora.org>
2018-10-19 10:38:02 -0700

    drm/msm/sde: check resource state before accessing hw
    Add checking in the driver if the clocks and power resource is
    enabled before accessing registers in commit path. This change is
    required to make sure there is no unclocked access if power
    resource enable fails.
    
    Change-Id: I5e8aa12925e2e365e70c91bcde32e66414537f75
    Signed-off-by: Abhijit Kulkarni <kabhijit@codeaurora.org>

--- drivers/gpu/drm/msm/sde/sde_kms.c
+++ drivers/gpu/drm/msm/sde/sde_kms.c
@@ -1111,6 +1111,11 @@ static void sde_kms_wait_for_commit_done(struct msm_kms *kms,
 		return;
 	}
 
+	if (!sde_kms_power_resource_is_enabled(crtc->dev)) {
+		SDE_ERROR("power resource is not enabled\n");
+		return;
+	}
+
 	list_for_each_entry(encoder, &dev->mode_config.encoder_list, head) {
 		if (encoder->crtc != crtc)
 			continue;
