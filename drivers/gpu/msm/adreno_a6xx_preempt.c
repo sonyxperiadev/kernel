d438628d1d2c522263ef99686c06f7b6586a4c17
Deepak Kumar <dkumar@codeaurora.org>
2018-11-01 12:39:16 +0530

    msm: kgsl: Use correct checks for GPMU related functionality
    Use "gmu_core_gpmu_isenabled" check for all GPMU related
    functionality instead of "gmu_core_isenabled" as later will
    return true even when ADRENO_GPMU feature flag is not defined.
    
    Change-Id: Ic577052d8c1cc52965e07c557b178fec444c3613
    Signed-off-by: Deepak Kumar <dkumar@codeaurora.org>

--- drivers/gpu/msm/adreno_a6xx_preempt.c
+++ drivers/gpu/msm/adreno_a6xx_preempt.c
@@ -318,7 +318,7 @@ void a6xx_preemption_trigger(struct adreno_device *adreno_dev)
 	 * free when the GPU is already powered on, whereas an OOB requires an
 	 * unconditional handshake with the GMU.
 	 */
-	if (gmu_core_isenabled(device))
+	if (gmu_core_gpmu_isenabled(device))
 		gmu_core_regrmw(device, A6XX_GMU_AO_SPARE_CNTL, 0x0, 0x2);
 
 	/*
@@ -435,7 +435,7 @@ void a6xx_preemption_callback(struct adreno_device *adreno_dev, int bit)
 	 * We can now safely clear the preemption keepalive bit, allowing
 	 * power collapse to resume its regular activity.
 	 */
-	if (gmu_core_isenabled(KGSL_DEVICE(adreno_dev)))
+	if (gmu_core_gpmu_isenabled(KGSL_DEVICE(adreno_dev)))
 		gmu_core_regrmw(KGSL_DEVICE(adreno_dev),
 				A6XX_GMU_AO_SPARE_CNTL, 0x2, 0x0);
 
