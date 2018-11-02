b92094e23833fd31449d5a42407414fbc7e24606
Ram Chandrasekar <rkumbako@codeaurora.org>
2018-11-02 12:23:40 -0600

    drivers: thermal: Evaluate based on trip temperature
    With virtual sensors, temperature reads by different zones at different
    time can be different and hence can result in different set of
    thresholds. This can lead to a case where we can get interrupt for the
    same temperature back to back if the temperature is fluctuating.
    
    To avoid this, allow sensor drivers to read the temperature and provide
    that information along when notifying for trip notification. Thermal
    framework will now make sure all the thermal zones will use the same
    temperature in all thermal zones to evaluate.
    
    Change-Id: I10861be54f0948081a83a1e839d8f9401929c61b
    Signed-off-by: Ram Chandrasekar <rkumbako@codeaurora.org>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -458,7 +458,7 @@ static void handle_critical_trips(struct thermal_zone_device *tz,
 	}
 }
 
-void handle_thermal_trip(struct thermal_zone_device *tz, int trip)
+static void handle_thermal_trip(struct thermal_zone_device *tz, int trip)
 {
 	enum thermal_trip_type type;
 
@@ -576,19 +576,8 @@ exit:
 }
 EXPORT_SYMBOL_GPL(thermal_zone_set_trips);
 
-static void update_temperature(struct thermal_zone_device *tz)
+static void store_temperature(struct thermal_zone_device *tz, int temp)
 {
-	int temp, ret;
-
-	ret = thermal_zone_get_temp(tz, &temp);
-	if (ret) {
-		if (ret != -EAGAIN)
-			dev_warn(&tz->device,
-				 "failed to read out %s thermal zone (%d)\n",
-				 tz->type, ret);
-		return;
-	}
-
 	mutex_lock(&tz->lock);
 	tz->last_temperature = tz->temperature;
 	tz->temperature = temp;
@@ -612,11 +601,46 @@ static void thermal_zone_device_init(struct thermal_zone_device *tz)
 		pos->initialized = false;
 }
 
+static void update_temperature(struct thermal_zone_device *tz)
+{
+	int temp, ret;
+
+	ret = thermal_zone_get_temp(tz, &temp);
+	if (ret) {
+		if (ret != -EAGAIN)
+			dev_warn(&tz->device,
+				 "failed to read out thermal zone (%d)\n",
+				 ret);
+		return;
+	}
+	store_temperature(tz, temp);
+}
+
 static void thermal_zone_device_reset(struct thermal_zone_device *tz)
 {
 	tz->passive = 0;
 	thermal_zone_device_init(tz);
 }
+
+void thermal_zone_device_update_temp(struct thermal_zone_device *tz,
+				enum thermal_notify_event event, int temp)
+{
+	int count;
+
+	if (atomic_read(&in_suspend))
+		return;
+
+	trace_thermal_device_update(tz, event);
+	store_temperature(tz, temp);
+
+	thermal_zone_set_trips(tz);
+
+	tz->notify_event = event;
+
+	for (count = 0; count < tz->trips; count++)
+		handle_thermal_trip(tz, count);
+}
+EXPORT_SYMBOL(thermal_zone_device_update_temp);
 
 void thermal_zone_device_update(struct thermal_zone_device *tz,
 				enum thermal_notify_event event)
