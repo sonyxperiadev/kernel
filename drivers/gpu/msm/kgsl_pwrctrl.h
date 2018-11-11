04d23665978bda10036b6643f23dd98fd9685f2c
Deepak Kumar <dkumar@codeaurora.org>
2018-11-11 16:56:55 +0530

    msm: kgsl: Add property KGSL_PROP_GPU_FORCE_ON
    Add property KGSL_PROP_GPU_FORCE_ON to allow user
    to force GPU state to ON.
    
    Change-Id: I5e36a01ce8d6b3141761b9ec0b1cf27f18afea59
    Signed-off-by: Deepak Kumar <dkumar@codeaurora.org>

--- drivers/gpu/msm/kgsl_pwrctrl.h
+++ drivers/gpu/msm/kgsl_pwrctrl.h
@@ -135,6 +135,8 @@ struct kgsl_regulator {
  * @min_pwrlevel - minimum allowable powerlevel per the user
  * @num_pwrlevels - number of available power levels
  * @interval_timeout - timeout in jiffies to be idle before a power event
+ * @default_interval_timeout - default timeout in jiffies to be idle before
+ * a power event
  * @clock_times - Each GPU frequency's accumulated active time in us
  * @regulators - array of pointers to kgsl_regulator structs
  * @pcl - bus scale identifier
@@ -192,6 +194,7 @@ struct kgsl_pwrctrl {
 	unsigned int min_pwrlevel;
 	unsigned int num_pwrlevels;
 	unsigned long interval_timeout;
+	unsigned long default_interval_timeout;
 	u64 clock_times[KGSL_MAX_PWRLEVELS];
 	struct kgsl_regulator regulators[KGSL_MAX_REGULATORS];
 	uint32_t pcl;
