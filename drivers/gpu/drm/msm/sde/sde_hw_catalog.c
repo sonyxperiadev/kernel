d1f8a1cb5d973dfdc789890052fa9e0563a0a93a
Angelo G. Del Regno <kholk11@gmail.com>
2018-12-01 14:06:23 +0100

    drm: msm: sde: Signal concurrent writeback support for 8998/630
    MSM8998 and SDM630 MDSS PHYs support CWB: enable it for
    enhanced performance.

--- drivers/gpu/drm/msm/sde/sde_hw_catalog.c
+++ drivers/gpu/drm/msm/sde/sde_hw_catalog.c
@@ -3591,6 +3591,7 @@ static int _sde_hardware_pre_caps(struct sde_mdss_cfg *sde_cfg, uint32_t hw_rev)
 		sde_cfg->perf.min_prefill_lines = 21;
 	} else if (IS_MSM8998_TARGET(hw_rev) || IS_SDM630_TARGET(hw_rev)) {
 		sde_cfg->has_wb_ubwc = true;
+		sde_cfg->has_cwb_support = true;
 		sde_cfg->perf.min_prefill_lines = 25;
 		sde_cfg->vbif_qos_nlvl = 4;
 		sde_cfg->ts_prefill_rev = 1;
