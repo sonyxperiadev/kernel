63454d635ea02c359621c17839fe29bbc4985049
MarijnS95 <marijns95@gmail.com>
2018-12-17 17:23:24 +0100

    input: misc: et51x: Retrieve HW type on Nile and skip probe if FPC.
    Nile devices can be fitted with either sensor. This driver will disable
    itself when the other (FPC) sensor type is detected, allowing the FPC
    driver to take over. The RHWTYPE ioctl has been added for the HAL to
    figure out which of the drivers is actually in use.
    
    Change-Id: Ia7a2129e332815587dab9f960755175d8ce0e994
    Signed-off-by: MarijnS95 <marijns95@gmail.com>

---
+++ drivers/input/misc/cei_fp_detect.h
@@ -0,0 +1,44 @@
+#ifndef _CEI_FP_DETECT_H
+#define _CEI_FP_DETECT_H
+
+#ifdef CONFIG_ARCH_SONY_NILE
+
+#include <linux/delay.h>
+// #include <linux/gpio.h>
+#include <linux/io.h>
+#include <linux/kernel.h>
+
+#define FP_HW_TYPE_EGISTEC 0
+#define FP_HW_TYPE_FPC 1
+
+#define MSM_TLMM_GPIO14_BASE 0x0390E000
+#define MSM_TLMM_SIZE 0x72000
+
+static inline int cei_fp_module_detect(void)
+{
+	int value;
+	void __iomem *cfg_reg = NULL;
+	uint32_t cei_current_dir;
+
+	pr_info("Detecting fingerprint module...\n");
+
+	cfg_reg = ioremap(MSM_TLMM_GPIO14_BASE, MSM_TLMM_SIZE);
+	writel_relaxed(0, cfg_reg);
+	msleep(100);
+	cei_current_dir = readl_relaxed(cfg_reg + 0x4);
+
+	iounmap(cfg_reg);
+
+	value = cei_current_dir & 0x1;
+
+	if (value) {
+		pr_info("fp module is fpc");
+		return FP_HW_TYPE_FPC;
+	}
+
+	pr_info("fp module is egistec or null");
+	return FP_HW_TYPE_EGISTEC;
+}
+
+#endif /* CONFIG_ARCH_SONY_NILE */
+#endif /* _CEI_FP_DETECT_H */
