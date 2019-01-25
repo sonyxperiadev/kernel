c04652ce0962375ca37e8f8d72679fd9a421ae46
Nick Desaulniers <ndesaulniers@google.com>
2019-01-25 12:56:42 -0800

    Revert "selinux: Relocate ss_initialized and selinux_enforcing to separate 4k"
    This reverts commit d4807bd60dc166a6445c3b13684f1d1afb722bc5.
    
    That out of tree patch causes the resulting kernel image to be too
    large, causing ld.lld to error; likely due to the additional section and
    alignment requirements.  Android and floral don't host virtual targets
    and thus do not run at EL2 on arm64.
    
    Bug: 63740206
    Change-Id: I357bf4d5c7e29230746eacd51fef413acec06067
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -102,7 +102,7 @@
 static atomic_t selinux_secmark_refcount = ATOMIC_INIT(0);
 
 #ifdef CONFIG_SECURITY_SELINUX_DEVELOP
-int selinux_enforcing __rticdata;
+int selinux_enforcing;
 
 static int __init enforcing_setup(char *str)
 {
--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -91,7 +91,7 @@ static DEFINE_RWLOCK(policy_rwlock);
 
 static struct sidtab sidtab;
 struct policydb policydb;
-int ss_initialized __rticdata;
+int ss_initialized;
 
 /*
  * The largest sequence number that has been used when
