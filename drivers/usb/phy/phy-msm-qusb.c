5c28d7424e1a6a17a9dd68303ca32904712c1386
Angelo G. Del Regno <kholk11@gmail.com>
2019-02-05 17:06:10 +0100

    usb: phy: phy-msm-qusb: Fix bad indentation issues.
    Fix bad indentation on line 1066.

--- drivers/usb/phy/phy-msm-qusb.c
+++ drivers/usb/phy/phy-msm-qusb.c
@@ -1054,8 +1054,9 @@ static int qusb_phy_probe(struct platform_device *pdev)
 		if (IS_ERR(qphy->iface_clk)) {
 			ret = PTR_ERR(qphy->iface_clk);
 			qphy->iface_clk = NULL;
-		if (ret == -EPROBE_DEFER)
-			return ret;
+			if (ret == -EPROBE_DEFER)
+				return ret;
+
 			dev_err(dev, "couldn't get iface_clk(%d)\n", ret);
 		}
 	}
@@ -1068,6 +1069,7 @@ static int qusb_phy_probe(struct platform_device *pdev)
 			qphy->core_clk = NULL;
 			if (ret == -EPROBE_DEFER)
 				return ret;
+
 			dev_err(dev, "couldn't get core_clk(%d)\n", ret);
 		}
 	}
