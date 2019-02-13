8733a0a1831acecdf51d285c636139b00684955c
Angelo G. Del Regno <kholk11@gmail.com>
2019-02-13 17:39:07 +0100

    rqbalance: Provide compatibility with CPU isolation
    This driver was meant to be used with CPU hotplugging and
    does not support CPU isolation.
    
    Provide compatibility with both isolation and hotplugging by
    switching the mask of "available CPUs" to either a cpumask
    of online CPUs or non-isolated CPUs at governor start time.
    
    Of course, the algorithm was executing for_each_online_cpu,
    but it was really meaning "for each currently usable core"
    and the definition of "usable core" is either one that is
    either plugged in or not isolated.

--- drivers/cpuquiet/governors/rqbalance.c
+++ drivers/cpuquiet/governors/rqbalance.c
@@ -101,6 +101,8 @@ struct cpu_limits {
 	bool cpuhp_scheduled;
 };
 
+struct cpuquiet_governor rqbalance_governor;
+
 static DEFINE_PER_CPU(struct idle_info, idleinfo);
 static DEFINE_PER_CPU(unsigned int, cpu_load);
 
@@ -126,6 +128,9 @@ static struct delayed_work rqbalance_work;
 static RQBALANCE_STATE rqbalance_state;
 static struct notifier_block pm_notifier_block;
 
+/* Either online or unisolated CPUs */
+const struct cpumask *avail_cpus_mask;
+
 static void calculate_load_timer(unsigned long data)
 {
 	int i;
@@ -134,7 +139,7 @@ static void calculate_load_timer(unsigned long data)
 	if (!load_timer_active)
 		return;
 
-	for_each_online_cpu(i) {
+	for_each_cpu(i, avail_cpus_mask) {
 		struct idle_info *iinfo = &per_cpu(idleinfo, i);
 		unsigned int *load = &per_cpu(cpu_load, i);
 
@@ -161,7 +166,7 @@ static void start_load_timer(void)
 
 	load_timer_active = true;
 
-	for_each_online_cpu(i) {
+	for_each_cpu(i, avail_cpus_mask) {
 		struct idle_info *iinfo = &per_cpu(idleinfo, i);
 
 		iinfo->idle_current =
@@ -185,7 +190,7 @@ static unsigned int get_slowest_cpu_n(void)
 	unsigned long minload = ULONG_MAX;
 	int i;
 
-	for_each_online_cpu(i) {
+	for_each_cpu(i, avail_cpus_mask) {
 		unsigned int *load = &per_cpu(cpu_load, i);
 
 		if ((i > 0) && (minload > *load)) {
@@ -202,7 +207,7 @@ static unsigned int cpu_highest_speed(void)
 	unsigned int maxload = 0;
 	int i;
 
-	for_each_online_cpu(i) {
+	for_each_cpu(i, avail_cpus_mask) {
 		unsigned int *load = &per_cpu(cpu_load, i);
 
 		maxload = max(maxload, *load);
@@ -216,7 +221,7 @@ static unsigned int count_slow_cpus(unsigned int limit)
 	unsigned int cnt = 0;
 	int i;
 
-	for_each_online_cpu(i) {
+	for_each_cpu(i, avail_cpus_mask) {
 		unsigned int *load = &per_cpu(cpu_load, i);
 
 		if (*load <= limit)
@@ -231,7 +236,7 @@ static unsigned int num_online_cluster_cpus(bool cluster)
 	unsigned int cnt = 0;
 	int i;
 
-	for_each_online_cpu(i) {
+	for_each_cpu(i, avail_cpus_mask) {
 		if (topology_physical_package_id(i) == cluster)
 			cnt++;
 	}
@@ -1078,6 +1083,9 @@ static int rqbalance_start(void)
 	err = rqbalance_sysfs_init();
 	if (err)
 		return err;
+
+	avail_cpus_mask = rqbalance_governor.use_isolation ?
+				cpu_unisolated_mask : cpu_online_mask;
 
 	rqbalance_wq = alloc_workqueue("cpuquiet-rqbalance",
 			WQ_UNBOUND | WQ_FREEZABLE, 1);
