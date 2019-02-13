756ba8cdf98e6cc59f17b4cd2a6916a0a09e95c9
Angelo G. Del Regno <kholk11@gmail.com>
2019-02-13 17:15:09 +0100

    cpumask: Simplify num_online_uniso_cpus macro
    After the introduction of a specific cpu_unisolated_mask, we
    can avoid inverting the cpu_isolated_mask: just weight the
    cpu_unisolated_mask.

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -106,13 +106,7 @@ extern struct cpumask __cpu_unisolated_mask;
 #define num_present_cpus()	cpumask_weight(cpu_present_mask)
 #define num_active_cpus()	cpumask_weight(cpu_active_mask)
 #define num_isolated_cpus()	cpumask_weight(cpu_isolated_mask)
-#define num_online_uniso_cpus()						\
-({									\
-	cpumask_t mask;							\
-									\
-	cpumask_andnot(&mask, cpu_online_mask, cpu_isolated_mask);	\
-	cpumask_weight(&mask);						\
-})
+#define num_online_uniso_cpus()	cpumask_weight(cpu_unisolated_mask)
 #define cpu_online(cpu)		cpumask_test_cpu((cpu), cpu_online_mask)
 #define cpu_possible(cpu)	cpumask_test_cpu((cpu), cpu_possible_mask)
 #define cpu_present(cpu)	cpumask_test_cpu((cpu), cpu_present_mask)
