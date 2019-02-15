53cbe6501363f86200082a67feb78d18244d1d92
Angelo G. Del Regno <kholk11@gmail.com>
2019-02-15 12:57:36 +0100

    sched/core: Initialize CPU isolated mask at core init time
    The mask of isolated CPUs has no architectural means:
    it is a software scheduling avoidance implementation
    and it makes total sense to initialize it into the
    scheduler core initialization function.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6470,6 +6470,7 @@ void __init sched_init(void)
 		rq->avg_idle = 2*sysctl_sched_migration_cost;
 		rq->max_idle_balance_cost = sysctl_sched_migration_cost;
 		rq->push_task = NULL;
+		set_cpu_isolated(i, false);
 		walt_sched_init_rq(rq);
 
 		INIT_LIST_HEAD(&rq->cfs_tasks);
