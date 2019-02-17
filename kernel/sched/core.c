68b836708f6b98b5206a47fcfebde5963c755ff7
Suren Baghdasaryan <surenb@google.com>
2019-02-17 15:07:38 -0800

    FROMLIST: refactor header includes to allow kthread.h inclusion in psi_types.h
    kthread.h can't be included in psi_types.h because it creates a circular
    inclusion with kthread.h eventually including psi_types.h and complaining
    on kthread structures not being defined because they are defined further
    in the kthread.h. Resolve this by removing psi_types.h inclusion from the
    headers included from kthread.h.
    
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    
    (not upstream yet, latest version published at: https://lore.kernel.org/patchwork/patch/1052417/)
    
    Conflicts:
            include/linux/kthread.h
            kernel/kthread.c
    
    (1. <linux/cgroup.h> include is already missing in kthread.h
    2. <linux/cgroup.h> is already included in kthread.c
    3. include <linux/cgroup-defs.h> in psi.h
    4. move <linux/cgroup-defs.h> include from sched.h into core.c
    5. move threadgroup_change_begin and threadgroup_change_end implementations
    from sched.h into core.c)
    
    Bug: 127712811
    Bug: 129157727
    Test: lmkd in PSI mode
    Change-Id: I07c1f4fddf0c43b3095f505e062d9d179d041544
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -78,6 +78,7 @@
 #include <linux/irq.h>
 #include <linux/cpufreq_times.h>
 #include <linux/sched/loadavg.h>
+#include <linux/cgroup-defs.h>
 
 #include <asm/switch_to.h>
 #include <asm/tlb.h>
@@ -9098,6 +9099,17 @@ int sched_updown_migrate_handler(struct ctl_table *table, int write,
 inline struct task_group *css_tg(struct cgroup_subsys_state *css)
 {
 	return css ? container_of(css, struct task_group, css) : NULL;
+}
+
+void threadgroup_change_begin(struct task_struct *tsk)
+{
+	might_sleep();
+	cgroup_threadgroup_change_begin(tsk);
+}
+
+void threadgroup_change_end(struct task_struct *tsk)
+{
+	cgroup_threadgroup_change_end(tsk);
 }
 
 static struct cgroup_subsys_state *
