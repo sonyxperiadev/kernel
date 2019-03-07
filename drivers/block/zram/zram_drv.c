8a7529f556dd545241ba76f0a18d459731bde5b8
Angelo G. Del Regno <kholk11@gmail.com>
2019-03-07 10:54:23 +0100

    Revert "UPSTREAM: zram: rework copy of compressor name in comp_algorithm_store()"
    This reverts commit 55393f76fe43e15c1546f24f7555b5b8e44f1413.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -357,7 +357,7 @@ static ssize_t comp_algorithm_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t len)
 {
 	struct zram *zram = dev_to_zram(dev);
-	char compressor[ARRAY_SIZE(zram->compressor)];
+	char compressor[CRYPTO_MAX_ALG_NAME];
 	size_t sz;
 
 	strlcpy(compressor, buf, sizeof(compressor));
@@ -376,7 +376,7 @@ static ssize_t comp_algorithm_store(struct device *dev,
 		return -EBUSY;
 	}
 
-	strcpy(zram->compressor, compressor);
+	strlcpy(zram->compressor, compressor, sizeof(compressor));
 	up_write(&zram->init_lock);
 	return len;
 }
