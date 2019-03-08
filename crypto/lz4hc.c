05e2c7e82dbb32c53f7724e5d8bc2a4ecc6d4bf6
Angelo G. Del Regno <kholk11@gmail.com>
2019-03-08 11:38:53 +0100

    Revert "crypto: change LZ4 modules to work with new LZ4 module version"
    This reverts commit 673599d06251a56f91fd75ee316a8406b21a4e52.

--- crypto/lz4hc.c
+++ crypto/lz4hc.c
@@ -49,26 +49,31 @@ static int lz4hc_compress_crypto(struct crypto_tfm *tfm, const u8 *src,
 			    unsigned int slen, u8 *dst, unsigned int *dlen)
 {
 	struct lz4hc_ctx *ctx = crypto_tfm_ctx(tfm);
-	int out_len = LZ4_compress_HC(src, dst, slen,
-		*dlen, LZ4HC_DEFAULT_CLEVEL, ctx);
+	size_t tmp_len = *dlen;
+	int err;
 
-	if (!out_len)
+	err = lz4hc_compress(src, slen, dst, &tmp_len, ctx->lz4hc_comp_mem);
+
+	if (err < 0)
 		return -EINVAL;
 
-	*dlen = out_len;
+	*dlen = tmp_len;
 	return 0;
 }
 
 static int lz4hc_decompress_crypto(struct crypto_tfm *tfm, const u8 *src,
 			      unsigned int slen, u8 *dst, unsigned int *dlen)
 {
-	int out_len = LZ4_decompress_safe(src, dst, slen, *dlen);
+	int err;
+	size_t tmp_len = *dlen;
+	size_t __slen = slen;
 
-	if (out_len < 0)
-		return out_len;
+	err = lz4_decompress_unknownoutputsize(src, __slen, dst, &tmp_len);
+	if (err < 0)
+		return -EINVAL;
 
-	*dlen = out_len;
-	return 0;
+	*dlen = tmp_len;
+	return err;
 }
 
 static struct crypto_alg alg_lz4hc = {
