be0e132b2f4165b123ed8a5e82d2edbbecdc5398
Angelo G. Del Regno <kholk11@gmail.com>
2019-03-29 19:56:53 +0100

    iommu: arm-smmu: Do not bail out if CTX fault IRQ not requested
    If the context fault interrupt cannot be requested, we still
    have big chances of the relative IOMMU context to just work.
    
    While this is totally unsafe, because we wouldn't be able to
    recover from a context fault on these IOMMUs, it can be
    helpful on some Qualcomm platforms.
    
    It is currently unclear whether this is a firmware bug or a
    GIC driver bug.
    This commit may be temporary.

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -2124,7 +2124,7 @@ static int arm_smmu_init_domain_context(struct iommu_domain *domain,
 			dev_err(smmu->dev, "failed to request context IRQ %d (%u)\n",
 				cfg->irptndx, irq);
 			cfg->irptndx = INVALID_IRPTNDX;
-			goto out_clear_smmu;
+			// goto out_clear_smmu; /* FIXME */
 		}
 	} else {
 		cfg->irptndx = INVALID_IRPTNDX;
