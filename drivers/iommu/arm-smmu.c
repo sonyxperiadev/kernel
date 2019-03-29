06d9712c90b365b3d842a151711bd26be6c13866
Angelo G. Del Regno <kholk11@gmail.com>
2019-03-29 19:52:42 +0100

    iommu: arm-smmu: Implement NO_SMR_CHECK option
    Sometimes, when we have an IOMMU configured with predefined
    contexts from the bootloader, a SMR search may not find all
    the usable IDs because the corresponding MASK bits may be
    set, but this doesn't mean that these stream ids cannot be
    used.
    
    This is a workaround for a firmware bug, found in Qualcomm
    Family-A SoCs, from MSM8994 to MSM8998, plus SDM630, SDM636
    and SDM660.

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -579,6 +579,7 @@ static struct arm_smmu_option_prop arm_smmu_options[] = {
 	{ ARM_SMMU_OPT_DISABLE_ATOS, "qcom,disable-atos" },
 	{ ARM_SMMU_OPT_MMU500_ERRATA1, "qcom,mmu500-errata-1" },
 	{ ARM_SMMU_OPT_STATIC_CB, "qcom,enable-static-cb"},
+	{ ARM_SMMU_OPT_NO_SMR_CHECK, "qcom,no-smr-check" },
 	{ ARM_SMMU_OPT_HALT, "qcom,enable-smmu-halt"},
 	{ 0, NULL},
 };
@@ -4451,7 +4452,8 @@ static int arm_smmu_device_cfg_probe(struct arm_smmu_device *smmu)
 		 * bits are set, so check each one separately. We can reject
 		 * masters later if they try to claim IDs outside these masks.
 		 */
-		if (!arm_smmu_is_static_cb(smmu)) {
+		if (!arm_smmu_is_static_cb(smmu) &&
+		    !(smmu->options & ARM_SMMU_OPT_NO_SMR_CHECK)) {
 			for (i = 0; i < size; i++) {
 				smr = readl_relaxed(
 					gr0_base + ARM_SMMU_GR0_SMR(i));
