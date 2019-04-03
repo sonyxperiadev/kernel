cf1911aeea6456bbd88dae9580207b28584e6386
Gustavo A. R. Silva <gustavo@embeddedor.com>
2019-04-03 11:46:11 -0500

    brcmfmac: Use struct_size() in kzalloc()
    One of the more common cases of allocation size calculations is finding
    the size of a structure that has a zero-sized array at the end, along
    with memory for some number of elements for that array. For example:
    
    struct foo {
        int stuff;
        struct boo entry[];
    };
    
    size = sizeof(struct foo) + count * sizeof(struct boo);
    instance = kzalloc(size, GFP_KERNEL)
    
    Instead of leaving these open-coded and prone to type mistakes, we can
    now use the new struct_size() helper:
    
    instance = kzalloc(struct_size(instance, entry, count), GFP_KERNEL)
    
    Notice that, in this case, variable reqsz is not necessary,
    hence it is removed.
    
    This code was detected with the help of Coccinelle.
    
    Signed-off-by: Gustavo A. R. Silva <gustavo@embeddedor.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- backports/drivers/net/wireless/broadcom/brcm80211/brcmfmac/firmware.c
+++ backports/drivers/net/wireless/broadcom/brcm80211/brcmfmac/firmware.c
@@ -635,7 +635,6 @@ brcmf_fw_alloc_request(u32 chip, u32 chiprev,
 	const char *mp_path;
 	u32 i, j;
 	char end;
-	size_t reqsz;
 
 	for (i = 0; i < table_size; i++) {
 		if (mapping_table[i].chipid == chip &&
@@ -650,8 +649,7 @@ brcmf_fw_alloc_request(u32 chip, u32 chiprev,
 		return NULL;
 	}
 
-	reqsz = sizeof(*fwreq) + n_fwnames * sizeof(struct brcmf_fw_item);
-	fwreq = kzalloc(reqsz, GFP_KERNEL);
+	fwreq = kzalloc(struct_size(fwreq, items, n_fwnames), GFP_KERNEL);
 	if (!fwreq)
 		return NULL;
 
