4a2975a28d6d88fad5a790f547a7775632a3997f
Nathan Chancellor <natechancellor@gmail.com>
2019-04-23 16:36:25 -0700

    hwmon: qpnp-adc: Fix logical-not-parentheses warning
    ../drivers/hwmon/qpnp-adc-common.c:1985:7: warning: logical not is only
    applied to the left hand side of this comparison
    [-Wlogical-not-parentheses]
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/hwmon/qpnp-adc-common.c
+++ drivers/hwmon/qpnp-adc-common.c
@@ -1982,7 +1982,7 @@ int32_t qpnp_adc_scale_default(struct qpnp_vadc_chip *vadc,
 	} else {
 		qpnp_adc_scale_with_calib_param(adc_code, adc_properties,
 					chan_properties, &scale_voltage);
-		if (!chan_properties->calib_type == CALIB_ABSOLUTE)
+		if (chan_properties->calib_type)
 			scale_voltage *= 1000;
 	}
 
