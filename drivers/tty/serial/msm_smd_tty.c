2f6b2a8f0ef66ca3a5a99c2773273b3443768f08
Nathan Chancellor <natechancellor@gmail.com>
2019-04-23 17:32:20 -0700

    tty: msm_smd_tty: Remove pointless boolean checks
    Fixes the following clang warnings:
    
    ../drivers/tty/serial/msm_smd_tty.c:373:19: warning: address of array
    'smd_tty[n].dev_name' will always evaluate to 'true'
    [-Wpointer-bool-conversion]
    ../drivers/tty/serial/msm_smd_tty.c:507:39: warning: address of array
    'smd_tty[n].ch_name' will always evaluate to 'true'
    [-Wpointer-bool-conversion]
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/tty/serial/msm_smd_tty.c
+++ drivers/tty/serial/msm_smd_tty.c
@@ -370,9 +370,6 @@ static int smd_tty_dummy_probe(struct platform_device *pdev)
 	int n;
 
 	for (n = 0; n < MAX_SMD_TTYS; ++n) {
-		if (!smd_tty[n].dev_name)
-			continue;
-
 		if (pdev->id == smd_tty[n].edge &&
 			!strcmp(pdev->name, smd_tty[n].dev_name)) {
 			complete_all(&smd_tty[n].ch_allocated);
@@ -504,7 +501,7 @@ static int smd_tty_port_activate(struct tty_port *tport,
 	struct smd_tty_info *info;
 	const char *peripheral = NULL;
 
-	if (n >= MAX_SMD_TTYS || !smd_tty[n].ch_name)
+	if (n >= MAX_SMD_TTYS)
 		return -ENODEV;
 
 	info = smd_tty + n;
