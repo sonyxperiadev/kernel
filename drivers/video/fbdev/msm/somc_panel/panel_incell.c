03bb1b0519527c26ff71b3841f7f55cd7eede7f3
Nathan Chancellor <natechancellor@gmail.com>
2019-04-23 19:42:11 -0700

    fbdev: msm: somc_panel: Use correct enum
    Fixes the following clang warning:
    
    ../drivers/video/fbdev/msm/somc_panel/panel_incell.c:1003:23: warning:
    implicit conversion from enumeration type 'incell_state_change' to
    different enumeration type 'incell_state' [-Wenum-conversion]
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/video/fbdev/msm/somc_panel/panel_incell.c
+++ drivers/video/fbdev/msm/somc_panel/panel_incell.c
@@ -997,7 +997,7 @@ static int incell_dsi_panel_power_on_ex(struct mdss_panel_data *pdata)
 	struct mdss_panel_specific_pdata *spec_pdata = NULL;
 	struct mdss_panel_power_seq *pw_seq = NULL;
 	struct incell_ctrl *incell = incell_get_info();
-	incell_state state = INCELL_STATE_NONE;
+	incell_state state = INCELL_STATE_SLE000_P0;
 	int ret = 0;
 	int wait;
 
