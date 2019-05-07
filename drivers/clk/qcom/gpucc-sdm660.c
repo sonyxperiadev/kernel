6a9a7d51adc6d071b3b637fff17947e9859c6bcf
Angelo G. Del Regno <kholk11@gmail.com>
2019-05-07 16:46:10 +0200

    clk: qcom: gpucc-sdm660: Fix number of parents for gfx3d_clk_src
    The allowed parents for gfx3d_clk_src are 4: xo, gpu-pll0, gpu-pll1 and
    gpll0.

--- drivers/clk/qcom/gpucc-sdm660.c
+++ drivers/clk/qcom/gpucc-sdm660.c
@@ -142,7 +142,7 @@ static struct clk_init_data gpu_clks_init[] = {
 	[0] = {
 		.name = "gfx3d_clk_src",
 		.parent_names = gpucc_parent_names_1,
-		.num_parents = 3,
+		.num_parents = 4,
 		.ops = &clk_gfx3d_src_ops,
 		.flags = CLK_SET_RATE_PARENT,
 	},
