00460a82a3aec95386b5475da542d9e0d8a1ea61
Angelo G. Del Regno <kholk11@gmail.com>
2019-05-24 19:32:58 +0200

    mmc: host: cmdq_hci: Directly return IRQ_NONE on error to fix build
    There is no "out" to go to: in any case, the only thing that we
    can do here is to return IRQ_NONE.

--- drivers/mmc/host/cmdq_hci.c
+++ drivers/mmc/host/cmdq_hci.c
@@ -1059,7 +1059,7 @@ skip_cqterri:
 			/* Temporary fix to avoid Null Pointer access */
 			if (!mrq || !mrq->cmdq_req) {
 				pr_err("%s: mrq is not initialized\n", __func__);
-				goto out;
+				return IRQ_NONE;
 			}
 
 			/*
