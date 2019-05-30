4d09fe384305d69507cb7c23d3e75449ebd55023
Angelo G. Del Regno <kholk11@gmail.com>
2019-05-30 12:38:04 +0200

    rpmsg: qcom_smd: Promote driver to core initcall
    This driver is totally necessary for the clk-smd-rpm to probe
    when it has to, since it requires to communicate with the
    RPM for the initial BIMC vote at probe time.

--- drivers/rpmsg/qcom_smd.c
+++ drivers/rpmsg/qcom_smd.c
@@ -1500,7 +1500,7 @@ static int __init qcom_smd_init(void)
 {
 	return platform_driver_register(&qcom_smd_driver);
 }
-subsys_initcall(qcom_smd_init);
+core_initcall(qcom_smd_init);
 
 static void __exit qcom_smd_exit(void)
 {
