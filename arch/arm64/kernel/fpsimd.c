aca1ac20aa25d40fbd016a15b24d81f8c3f16d7b
Angelo G. Del Regno <kholk11@gmail.com>
2019-06-05 18:30:32 +0200

    arm64: Implement optimized FP and SIMD for Kryo
    Implement enablement of Floating Point and SIMD settings for
    better performance on Kryo during the execution of AArch32
    processes on this AArch64 processor.
    
    This is currently tested on MSM8996, but should be improving
    performance for also other versions of the Kryo cores.

--- arch/arm64/kernel/fpsimd.c
+++ arch/arm64/kernel/fpsimd.c
@@ -21,6 +21,7 @@
 #include <linux/cpu.h>
 #include <linux/cpu_pm.h>
 #include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/init.h>
 #include <linux/percpu.h>
 #include <linux/preempt.h>
@@ -29,6 +30,7 @@
 
 #include <asm/fpsimd.h>
 #include <asm/cputype.h>
+#include <asm/app_api.h>
 #include <asm/simd.h>
 
 #define FPEXC_IOF	(1 << 0)
@@ -37,6 +39,8 @@
 #define FPEXC_UFF	(1 << 3)
 #define FPEXC_IXF	(1 << 4)
 #define FPEXC_IDF	(1 << 7)
+
+#define FP_SIMD_BIT	31
 
 /*
  * In order to reduce the number of times the FPSIMD state is needlessly saved
@@ -98,15 +102,52 @@
  *   whatever is in the FPSIMD registers is not saved to memory, but discarded.
  */
 static DEFINE_PER_CPU(struct fpsimd_state *, fpsimd_last_state);
+static DEFINE_PER_CPU(int, fpsimd_stg_enable);
+
+static int fpsimd_settings = 0x1; /* default = 0x1 */
+module_param(fpsimd_settings, int, 0644);
+
+#ifdef CONFIG_ENABLE_FP_SIMD_SETTINGS
+void fpsimd_settings_enable(void)
+{
+	set_app_setting_bit(FP_SIMD_BIT);
+}
+
+void fpsimd_settings_disable(void)
+{
+	clear_app_setting_bit(FP_SIMD_BIT);
+}
+#endif
 
 /*
  * Trapped FP/ASIMD access.
  */
 void do_fpsimd_acc(unsigned int esr, struct pt_regs *regs)
 {
+#ifdef CONFIG_ENABLE_FP_SIMD_SETTINGS
+	if (!fpsimd_settings)
+		return;
+
+	fpsimd_disable_trap();
+	fpsimd_settings_disable();
+	this_cpu_write(fpsimd_stg_enable, 0);
+#else
 	/* TODO: implement lazy context saving/restoring */
 	WARN_ON(1);
+#endif
 }
+
+#ifdef CONFIG_ENABLE_FP_SIMD_SETTINGS
+void do_fpsimd_acc_compat(unsigned int esr, struct pt_regs *regs)
+{
+	if (!fpsimd_settings)
+		return;
+
+	fpsimd_disable_trap();
+	fpsimd_settings_enable();
+	this_cpu_write(fpsimd_stg_enable, 1);
+}
+#endif
 
 /*
  * Raise a SIGFPE for the current process.
