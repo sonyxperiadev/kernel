fa8a937b7826c9f73253dc01dbc223eada4cd54a
Angelo G. Del Regno <kholk11@gmail.com>
2019-06-06 15:24:54 +0200

    iommu: arm-smmu: Implement no ACTLR read quirk for hypervisor lockup
    On some IOMMU implementations the hypervisor disallows
    reading the implementation defined ACTLR registers and
    trips a security fault which freezes the entire AP.
    
    For this reason, add a DT-activated qcom,no-actlr-read quirk
    to avoid doing that.
    This quirk is currently known to be only needed on Qualcomm
    SoCs like MSM8996, MSM8998, SDM630, SDM660 (and variants).

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -260,6 +260,7 @@ struct arm_smmu_device {
 #define ARM_SMMU_OPT_DISABLE_ATOS	(1 << 7)
 #define ARM_SMMU_OPT_MIN_IOVA_ALIGN	(1 << 8)
 #define ARM_SMMU_OPT_NO_SMR_CHECK	(1 << 9)
+#define ARM_SMMU_OPT_NO_ACTLR_READ	(1 << 10)
 	u32				options;
 	enum arm_smmu_arch_version	version;
 	enum arm_smmu_implementation	model;
@@ -401,6 +402,7 @@ static struct arm_smmu_option_prop arm_smmu_options[] = {
 	{ ARM_SMMU_OPT_DISABLE_ATOS, "qcom,disable-atos" },
 	{ ARM_SMMU_OPT_MIN_IOVA_ALIGN, "qcom,min-iova-align" },
 	{ ARM_SMMU_OPT_NO_SMR_CHECK, "qcom,no-smr-check" },
+	{ ARM_SMMU_OPT_NO_ACTLR_READ, "qcom,no-actlr-read" },
 	{ 0, NULL},
 };
 
@@ -4599,11 +4601,20 @@ static int arm_smmu_device_cfg_probe(struct arm_smmu_device *smmu)
 				 sizeof(*smmu->cbs), GFP_KERNEL);
 	if (!smmu->cbs)
 		return -ENOMEM;
-	for (i = 0; i < smmu->num_context_banks; i++) {
-		void __iomem *cb_base;
 
-		cb_base = ARM_SMMU_CB(smmu, i);
-		smmu->cbs[i].actlr = readl_relaxed(cb_base + ARM_SMMU_CB_ACTLR);
+	/*
+	 * On some IOMMU implementations the hypervisor disallows
+	 * reading the implementation defined ACTLR registers and
+	 * trips a security fault which freezes the entire AP.
+	 */
+	if (!(smmu->options & ARM_SMMU_OPT_NO_ACTLR_READ)) {
+		for (i = 0; i < smmu->num_context_banks; i++) {
+			void __iomem *cb_base;
+
+			cb_base = ARM_SMMU_CB(smmu, i);
+			smmu->cbs[i].actlr = readl_relaxed(
+						cb_base + ARM_SMMU_CB_ACTLR);
+		}
 	}
 
 	/* ID2 */
