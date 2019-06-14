29d4ea134ae61993d98286213e62948c747446f2
Manikanta Kanamarlapudi <kmanikan@codeaurora.org>
2019-06-14 12:52:33 +0530

    msm: vidc: Check image encode capabilities
    For heic/hevc image session, check respective
    capabilities to allow and reject.
    
    Change-Id: I21b2710e16db9f44b7a21d58f8c4711b977df42d
    Signed-off-by: Manikanta Kanamarlapudi <kmanikan@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc_platform.c
+++ drivers/media/platform/msm/vidc/msm_vidc_platform.c
@@ -758,6 +758,14 @@ static struct msm_vidc_ubwc_config trinket_ubwc_data[] = {
 	UBWC_CONFIG(0, 1, 0, 0, 0, 64, 0, 0),
 };
 
+static struct msm_vidc_image_capability default_heic_image_capability = {
+	{512, 8192}, {512, 8192}
+};
+
+static struct msm_vidc_image_capability default_hevc_image_capability = {
+	{512, 512}, {512, 512}
+};
+
 static struct msm_vidc_platform_data default_data = {
 	.codec_data = default_codec_data,
 	.codec_data_length =  ARRAY_SIZE(default_codec_data),
@@ -770,6 +778,8 @@ static struct msm_vidc_platform_data default_data = {
 	.csc_data.vpe_csc_custom_limit_coeff = vpe_csc_custom_limit_coeff,
 	.efuse_data = NULL,
 	.efuse_data_length = 0,
+	.heic_image_capability = &default_heic_image_capability,
+	.hevc_image_capability = &default_hevc_image_capability,
 	.sku_version = 0,
 	.vpu_ver = VPU_VERSION_5,
 };
@@ -786,6 +796,8 @@ static struct msm_vidc_platform_data atoll_data = {
 	.csc_data.vpe_csc_custom_limit_coeff = vpe_csc_custom_limit_coeff,
 	.efuse_data = NULL,
 	.efuse_data_length = 0,
+	.heic_image_capability = NULL,
+	.hevc_image_capability = NULL,
 	.sku_version = 0,
 	.vpu_ver = VPU_VERSION_4,
 };
@@ -802,6 +814,8 @@ static struct msm_vidc_platform_data sm6150_data = {
 	.csc_data.vpe_csc_custom_limit_coeff = vpe_csc_custom_limit_coeff,
 	.efuse_data = NULL,
 	.efuse_data_length = 0,
+	.heic_image_capability = &default_heic_image_capability,
+	.hevc_image_capability = &default_hevc_image_capability,
 	.sku_version = 0,
 	.vpu_ver = VPU_VERSION_4,
 };
@@ -834,6 +848,8 @@ static struct msm_vidc_platform_data sm8150_data = {
 	.csc_data.vpe_csc_custom_limit_coeff = vpe_csc_custom_limit_coeff,
 	.efuse_data = NULL,
 	.efuse_data_length = 0,
+	.heic_image_capability = &default_heic_image_capability,
+	.hevc_image_capability = &default_hevc_image_capability,
 	.sku_version = 0,
 	.vpu_ver = VPU_VERSION_5,
 };
@@ -850,6 +866,8 @@ static struct msm_vidc_platform_data sdmmagpie_data = {
 	.csc_data.vpe_csc_custom_limit_coeff = vpe_csc_custom_limit_coeff,
 	.efuse_data = sdmmagpie_efuse_data,
 	.efuse_data_length = ARRAY_SIZE(sdmmagpie_efuse_data),
+	.heic_image_capability = &default_heic_image_capability,
+	.hevc_image_capability = &default_hevc_image_capability,
 	.sku_version = 0,
 	.vpu_ver = VPU_VERSION_5,
 };
@@ -866,6 +884,8 @@ static struct msm_vidc_platform_data sdm845_data = {
 	.csc_data.vpe_csc_custom_limit_coeff = vpe_csc_custom_limit_coeff,
 	.efuse_data = NULL,
 	.efuse_data_length = 0,
+	.heic_image_capability = NULL,
+	.hevc_image_capability = NULL,
 	.sku_version = 0,
 	.vpu_ver = VPU_VERSION_4,
 };
@@ -882,6 +902,8 @@ static struct msm_vidc_platform_data sdm670_data = {
 	.csc_data.vpe_csc_custom_limit_coeff = vpe_csc_custom_limit_coeff,
 	.efuse_data = sdm670_efuse_data,
 	.efuse_data_length = ARRAY_SIZE(sdm670_efuse_data),
+	.heic_image_capability = NULL,
+	.hevc_image_capability = NULL,
 	.sku_version = 0,
 	.vpu_ver = VPU_VERSION_4,
 };
