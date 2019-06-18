e4f6b5e6fb2905c8ce82a3a89097cc4537caab0b
Angelo G. Del Regno <kholk11@gmail.com>
2019-06-18 10:40:38 +0200

    fs: Convert remaining users of total{ram,high}_pages to atomic
    The patch converting them to atomic was backported and I forgot
    to update fs..... sorry.

--- fs/file_table.c
+++ fs/file_table.c
@@ -323,10 +323,10 @@ void __init files_init(void)
 void __init files_maxfiles_init(void)
 {
 	unsigned long n;
-	unsigned long memreserve = (totalram_pages - nr_free_pages()) * 3/2;
+	unsigned long memreserve = (totalram_pages() - nr_free_pages()) * 3/2;
 
-	memreserve = min(memreserve, totalram_pages - 1);
-	n = ((totalram_pages - memreserve) * (PAGE_SIZE / 1024)) / 10;
+	memreserve = min(memreserve, totalram_pages() - 1);
+	n = ((totalram_pages() - memreserve) * (PAGE_SIZE / 1024)) / 10;
 
 	files_stat.max_files = max_t(unsigned long, n, NR_FILE);
 } 
