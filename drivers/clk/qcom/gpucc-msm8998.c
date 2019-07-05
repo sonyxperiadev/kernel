b560c158a685d43ddccd3c897a5434913ff1600b
Angelo G. Del Regno <kholk11@gmail.com>
2019-07-05 18:17:15 +0200

    clk: qcom: gpucc-msm8998: Also remove SUPER_TURBO from declared corners
    The removal was incomplete.
    Now it's good.

--- drivers/clk/qcom/gpucc-msm8998.c
+++ drivers/clk/qcom/gpucc-msm8998.c
@@ -61,7 +61,6 @@ static int vdd_gpucc_corner[] = {
 	VDD_GFX_SVS_PLUS,	/* LOW:  SVS+		*/
 	VDD_GFX_NOMINAL,	/*       NOMINAL	*/
 	VDD_GFX_TURBO,		/* HIGH: TURBO		*/
-	VDD_GFX_SUPER_TURBO,	/* HIGH: SUPER_TURBO	*/
 };
 
 static DEFINE_VDD_REGULATORS(vdd_dig, VDD_DIG_NUM, 1, vdd_corner);
