4b2247f81aec0287e811416091c27c3c033860bb
Angelo G. Del Regno <kholk11@gmail.com>
2019-07-11 15:58:04 +0200

    iommu: arm-smmu: Implement QCOM MMU-500 TLB context invalidation errata
    Some Qualcomm MMU-500 implementation need to execute context
    invalidation in a remote spinlock: copy the implementation from
    Open Devices k4.9 and fix it to use the upstream hwspinlocks
    present in kernel 4.14.
    
    Also, in the kernel 4.9 implementation this was "errata 1", but
    Qualcomm has moved the min_iova align errata (errata 2 in k4.9)
    as errata 1 in k4.14, so, rename the TLB errata to errata 0 in
    this implementation to avoid confusion.

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -55,6 +55,7 @@
 #include <linux/msm-bus.h>
 #include <trace/events/iommu.h>
 #include <dt-bindings/msm/msm-bus-ids.h>
+#include <linux/hwspinlock.h>
 #include <linux/irq.h>
 #include <linux/wait.h>
 
@@ -111,6 +112,9 @@
 
 #define MSI_IOVA_BASE			0x8000000
 #define MSI_IOVA_LENGTH			0x100000
+
+/* Timeout (ms) for the trylock of remote spinlocks */
+#define HWSPINLOCK_TIMEOUT	1000
 
 static int force_stage;
 module_param(force_stage, int, S_IRUGO);
@@ -261,6 +265,7 @@ struct arm_smmu_device {
 #define ARM_SMMU_OPT_MIN_IOVA_ALIGN	(1 << 8)
 #define ARM_SMMU_OPT_NO_SMR_CHECK	(1 << 9)
 #define ARM_SMMU_OPT_NO_ACTLR_READ	(1 << 10)
+#define ARM_SMMU_OPT_MMU500_ERRATA0	(1 << 11)
 	u32				options;
 	enum arm_smmu_arch_version	version;
 	enum arm_smmu_implementation	model;
@@ -379,6 +384,8 @@ struct arm_smmu_domain {
 	/* nonsecure pool protected by pgtbl_lock */
 	struct list_head		nonsecure_pool;
 	struct iommu_domain		domain;
+	bool				qsmmuv500_errata0_init;
+	bool				qsmmuv500_errata0_client;
 	bool				qsmmuv500_errata1_min_iova_align;
 };
 
@@ -400,6 +407,7 @@ static struct arm_smmu_option_prop arm_smmu_options[] = {
 	{ ARM_SMMU_OPT_NO_ASID_RETENTION, "qcom,no-asid-retention" },
 	{ ARM_SMMU_OPT_STATIC_CB, "qcom,enable-static-cb"},
 	{ ARM_SMMU_OPT_DISABLE_ATOS, "qcom,disable-atos" },
+	{ ARM_SMMU_OPT_MMU500_ERRATA0, "qcom,mmu500-errata-0" },
 	{ ARM_SMMU_OPT_MIN_IOVA_ALIGN, "qcom,min-iova-align" },
 	{ ARM_SMMU_OPT_NO_SMR_CHECK, "qcom,no-smr-check" },
 	{ ARM_SMMU_OPT_NO_ACTLR_READ, "qcom,no-actlr-read" },
@@ -425,6 +433,7 @@ static int arm_smmu_enable_s1_translations(struct arm_smmu_domain *smmu_domain);
 static int arm_smmu_alloc_cb(struct iommu_domain *domain,
 				struct arm_smmu_device *smmu,
 				struct device *dev);
+static struct iommu_gather_ops qsmmuv500_errata0_smmu_gather_ops;
 
 static bool arm_smmu_is_static_cb(struct arm_smmu_device *smmu);
 static bool arm_smmu_is_master_side_secure(struct arm_smmu_domain *smmu_domain);
@@ -2034,6 +2043,9 @@ static int arm_smmu_init_domain_context(struct iommu_domain *domain,
 	     (quirks & IO_PGTABLE_QUIRK_QCOM_USE_LLC_NWA)) &&
 		(smmu->model == QCOM_SMMUV500))
 		quirks |= IO_PGTABLE_QUIRK_QSMMUV500_NON_SHAREABLE;
+
+	if (smmu->options & ARM_SMMU_OPT_MMU500_ERRATA0)
+		tlb_ops = &qsmmuv500_errata0_smmu_gather_ops;
 
 	if (arm_smmu_is_slave_side_secure(smmu_domain))
 		tlb_ops = &msm_smmu_gather_ops;
@@ -5159,6 +5171,11 @@ struct qsmmuv500_archdata {
 	u32				version;
 	struct actlr_setting		*actlrs;
 	u32				actlr_tbl_size;
+
+	struct arm_smmu_smr		*errata0_clients;
+	u32				num_errata0_clients;
+	struct hwspinlock		*errata0_lock;
+	ktime_t				last_tlbi_ktime;
 };
 #define get_qsmmuv500_archdata(smmu)				\
 	((struct qsmmuv500_archdata *)(smmu->archdata))
@@ -5204,6 +5221,135 @@ static bool arm_smmu_fwspec_match_smr(struct iommu_fwspec *fwspec,
 	}
 	return false;
 }
+
+#define ERRATA0_TLBI_INTERVAL_US		10
+static bool
+qsmmuv500_errata0_required(struct arm_smmu_domain *smmu_domain,
+				 struct qsmmuv500_archdata *data)
+{
+	bool ret = false;
+	int j;
+	struct arm_smmu_smr *smr;
+	struct iommu_fwspec *fwspec;
+
+	if (smmu_domain->qsmmuv500_errata0_init)
+		return smmu_domain->qsmmuv500_errata0_client;
+
+	fwspec = smmu_domain->dev->iommu_fwspec;
+	for (j = 0; j < data->num_errata0_clients; j++) {
+		smr = &data->errata0_clients[j];
+		if (arm_smmu_fwspec_match_smr(fwspec, smr)) {
+			ret = true;
+			break;
+		}
+	}
+
+	smmu_domain->qsmmuv500_errata0_init = true;
+	smmu_domain->qsmmuv500_errata0_client = ret;
+	return ret;
+}
+
+#define SCM_CONFIG_ERRATA0_CLIENT_ALL 0x2
+#define SCM_CONFIG_ERRATA0 0x3
+static void __qsmmuv500_errata0_tlbiall(struct arm_smmu_domain *smmu_domain)
+{
+	struct arm_smmu_device *smmu = smmu_domain->smmu;
+	struct device *dev = smmu_domain->dev;
+	struct arm_smmu_cfg *cfg = &smmu_domain->cfg;
+	void __iomem *base;
+	int ret;
+	ktime_t cur;
+	u32 val;
+	struct scm_desc desc = {
+		.args[0] = SCM_CONFIG_ERRATA0_CLIENT_ALL,
+		.args[1] = false,
+		.arginfo = SCM_ARGS(2, SCM_VAL, SCM_VAL),
+	};
+
+	base = ARM_SMMU_CB(smmu, cfg->cbndx);
+	writel_relaxed(0, base + ARM_SMMU_CB_S1_TLBIALL);
+	writel_relaxed(0, base + ARM_SMMU_CB_TLBSYNC);
+	if (!readl_poll_timeout_atomic(base + ARM_SMMU_CB_TLBSTATUS, val,
+				      !(val & TLBSTATUS_SACTIVE), 0, 100))
+		return;
+
+	ret = scm_call2_atomic(SCM_SIP_FNID(SCM_SVC_SMMU_PROGRAM,
+					    SCM_CONFIG_ERRATA0),
+			       &desc);
+	if (ret) {
+		dev_err(smmu->dev, "Calling into TZ to disable ERRATA0 failed - IOMMU hardware in bad state\n");
+		BUG();
+		return;
+	}
+
+	cur = ktime_get();
+	msm_bus_noc_throttle_wa(true);
+
+	if (readl_poll_timeout_atomic(base + ARM_SMMU_CB_TLBSTATUS, val,
+			      !(val & TLBSTATUS_SACTIVE), 0, 10000)) {
+		dev_err(smmu->dev, "ERRATA0 TLBSYNC timeout - IOMMU hardware in bad state");
+		trace_tlbsync_timeout(dev, 0);
+		BUG();
+	}
+
+	msm_bus_noc_throttle_wa(false);
+
+	desc.args[1] = true;
+	ret = scm_call2_atomic(SCM_SIP_FNID(SCM_SVC_SMMU_PROGRAM,
+					    SCM_CONFIG_ERRATA0),
+			       &desc);
+	if (ret) {
+		dev_err(smmu->dev, "Calling into TZ to reenable ERRATA0 failed - IOMMU hardware in bad state\n");
+		BUG();
+	}
+}
+
+/* Must be called with clocks/regulators enabled */
+static void qsmmuv500_errata0_tlb_inv_context(void *cookie)
+{
+	struct arm_smmu_domain *smmu_domain = cookie;
+	struct device *dev = smmu_domain->dev;
+	struct qsmmuv500_archdata *data =
+			get_qsmmuv500_archdata(smmu_domain->smmu);
+	ktime_t cur;
+	unsigned long flags;
+	bool errata;
+	int ret;
+
+	cur = ktime_get();
+	trace_tlbi_start(dev, 0);
+
+	errata = qsmmuv500_errata0_required(smmu_domain, data);
+	ret = hwspin_lock_timeout_irqsave(data->errata0_lock,
+					  HWSPINLOCK_TIMEOUT,
+					  &flags);
+	if (ret)
+		WARN(1, "Cannot lock remote spinlock. BUG() here?\n");
+
+	if (errata) {
+		s64 delta;
+
+		delta = ktime_us_delta(ktime_get(), data->last_tlbi_ktime);
+		if (delta < ERRATA0_TLBI_INTERVAL_US)
+			udelay(ERRATA0_TLBI_INTERVAL_US - delta);
+
+		__qsmmuv500_errata0_tlbiall(smmu_domain);
+
+		data->last_tlbi_ktime = ktime_get();
+	} else {
+		__qsmmuv500_errata0_tlbiall(smmu_domain);
+	}
+
+	hwspin_unlock_irqrestore(data->errata0_lock, &flags);
+
+	trace_tlbi_end(dev, ktime_us_delta(ktime_get(), cur));
+}
+
+static struct iommu_gather_ops qsmmuv500_errata0_smmu_gather_ops = {
+	.tlb_flush_all	= qsmmuv500_errata0_tlb_inv_context,
+	.alloc_pages_exact = arm_smmu_alloc_pages_exact,
+	.free_pages_exact = arm_smmu_free_pages_exact,
+};
 
 static int qsmmuv500_tbu_halt(struct qsmmuv500_tbu_device *tbu,
 				struct arm_smmu_domain *smmu_domain)
@@ -5616,6 +5762,48 @@ static int qsmmuv500_tbu_register(struct device *dev, void *cookie)
 	return 0;
 }
 
+static int qsmmuv500_parse_errata0(struct arm_smmu_device *smmu)
+{
+	int len, hwlock_id, i;
+	struct device *dev = smmu->dev;
+	struct qsmmuv500_archdata *data = get_qsmmuv500_archdata(smmu);
+	struct arm_smmu_smr *smrs;
+	const __be32 *cell;
+
+	cell = of_get_property(dev->of_node, "qcom,mmu500-errata-0", NULL);
+	if (!cell)
+		return 0;
+
+	hwlock_id = of_hwspin_lock_get_id(dev->of_node, 0);
+	if (hwlock_id < 0) {
+		if (hwlock_id != -EPROBE_DEFER)
+			dev_err(dev, "failed to retrieve hwlock\n");
+		return hwlock_id;
+	}
+
+	data->errata0_lock = hwspin_lock_request_specific(hwlock_id);
+	if (!data->errata0_lock)
+		return -ENXIO;
+
+	len = of_property_count_elems_of_size(
+			dev->of_node, "qcom,mmu500-errata-0", sizeof(u32) * 2);
+	if (len < 0)
+		return 0;
+
+	smrs = devm_kzalloc(dev, sizeof(*smrs) * len, GFP_KERNEL);
+	if (!smrs)
+		return -ENOMEM;
+
+	for (i = 0; i < len; i++) {
+		smrs[i].id = of_read_number(cell++, 1);
+		smrs[i].mask = of_read_number(cell++, 1);
+	}
+
+	data->errata0_clients = smrs;
+	data->num_errata0_clients = len;
+	return 0;
+}
+
 static int qsmmuv500_read_actlr_tbl(struct arm_smmu_device *smmu)
 {
 	int len, i;
@@ -5679,6 +5867,10 @@ static int qsmmuv500_arch_init(struct arm_smmu_device *smmu)
 
 	if (arm_smmu_is_static_cb(smmu))
 		return 0;
+
+	ret = qsmmuv500_parse_errata0(smmu);
+	if (ret)
+		return ret;
 
 	ret = qsmmuv500_read_actlr_tbl(smmu);
 	if (ret)
