c16b9b2e76de063cb48a5914524d8c4ea1096488
Ritesh Kumar <riteshk@codeaurora.org>
2019-07-29 14:01:15 +0530

    drm: msm: dsi-staging: Fix error handling in case of EPROBE_DEFER
    When mdss pll probe fails with status EPROBE_DEFER, mdss pll probe is
    called again and frees dfps data memory again causing the double free
    and the crash. To avoid this, parse dfps data memory when mdss pll
    probe is successful.
    
    When devm_clk_get returns error in dsi_ctrl_clocks_init, initialise the
    clk to NULL to avoid NULL pointer dereference in dsi_ctrl_clocks_deinit.
    
    Change-Id: I4179b711f70c033729a3c8a57f93737e414bd02c
    Signed-off-by: Ritesh Kumar <riteshk@codeaurora.org>

--- drivers/gpu/drm/msm/dsi-staging/dsi_ctrl.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_ctrl.c
@@ -575,24 +575,24 @@ static int dsi_ctrl_clocks_init(struct platform_device *pdev,
 	hs_link->byte_clk = devm_clk_get(&pdev->dev, "byte_clk");
 	if (IS_ERR(hs_link->byte_clk)) {
 		rc = PTR_ERR(hs_link->byte_clk);
-		hs_link->byte_clk = NULL;
 		pr_err("failed to get byte_clk, rc=%d\n", rc);
+		hs_link->byte_clk = NULL;
 		goto fail;
 	}
 
 	hs_link->pixel_clk = devm_clk_get(&pdev->dev, "pixel_clk");
 	if (IS_ERR(hs_link->pixel_clk)) {
 		rc = PTR_ERR(hs_link->pixel_clk);
-		hs_link->pixel_clk = NULL;
 		pr_err("failed to get pixel_clk, rc=%d\n", rc);
+		hs_link->pixel_clk = NULL;
 		goto fail;
 	}
 
 	lp_link->esc_clk = devm_clk_get(&pdev->dev, "esc_clk");
 	if (IS_ERR(lp_link->esc_clk)) {
 		rc = PTR_ERR(lp_link->esc_clk);
-		lp_link->esc_clk = NULL;
 		pr_err("failed to get esc_clk, rc=%d\n", rc);
+		lp_link->esc_clk = NULL;
 		goto fail;
 	}
 
@@ -605,16 +605,16 @@ static int dsi_ctrl_clocks_init(struct platform_device *pdev,
 	rcg->byte_clk = devm_clk_get(&pdev->dev, "byte_clk_rcg");
 	if (IS_ERR(rcg->byte_clk)) {
 		rc = PTR_ERR(rcg->byte_clk);
-		rcg->byte_clk = NULL;
 		pr_err("failed to get byte_clk_rcg, rc=%d\n", rc);
+		rcg->byte_clk = NULL;
 		goto fail;
 	}
 
 	rcg->pixel_clk = devm_clk_get(&pdev->dev, "pixel_clk_rcg");
 	if (IS_ERR(rcg->pixel_clk)) {
 		rc = PTR_ERR(rcg->pixel_clk);
-		rcg->pixel_clk = NULL;
 		pr_err("failed to get pixel_clk_rcg, rc=%d\n", rc);
+		rcg->pixel_clk = NULL;
 		goto fail;
 	}
 
