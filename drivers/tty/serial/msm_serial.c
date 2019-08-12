677dd1f13c99a52009a25d16e0034f5193a08215
Nathan Chancellor <natechancellor@gmail.com>
2019-08-12 11:15:32 -0700

    treewide: Linux 4.9.187
    source kernel.org
    https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.9.186-187.xz
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/tty/serial/msm_serial.c
+++ drivers/tty/serial/msm_serial.c
@@ -395,7 +395,7 @@ no_rx:
 
 static inline void msm_wait_for_xmitr(struct uart_port *port)
 {
-	u32 count = 500000;
+	unsigned int timeout = 500000;
 
 	while (!(msm_read(port, UART_SR) & UART_SR_TX_EMPTY)) {
 		if (msm_read(port, UART_ISR) & UART_ISR_TX_READY)
@@ -406,7 +406,7 @@ static inline void msm_wait_for_xmitr(struct uart_port *port)
 		 * TX ready, have a 500ms timeout to avoid stuck here
 		 * and only miss some log to uart.
 		 */
-		if (count-- == 0) {
+		if (!timeout--) {
 			msm_write(port, UART_CR_CMD_RESET_TX, UART_CR);
 			printk_deferred("uart may lost data, resetting TX!\n");
 			break;
