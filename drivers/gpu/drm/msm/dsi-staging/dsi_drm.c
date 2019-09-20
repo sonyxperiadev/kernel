e0aadde37ed9537986f7a452b51a98a51e9de375
Kalyan Thota <kalyant@codeaurora.org>
2019-09-20 20:40:46 +0530

    drm:msm:sde: update avr mode config during commit prepare
    Add changes to support avr mode config update during
    prepare commit which happens before gpu fence wait
    for input buffers.
    
    Change-Id: Ib2cb5b7e1f10501914c003f6cf066b85048f79d4
    Signed-off-by: Kalyan Thota <kalyant@codeaurora.org>

--- drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
@@ -776,6 +776,17 @@ int dsi_conn_pre_kickoff(struct drm_connector *connector,
 	return dsi_display_pre_kickoff(connector, display, params);
 }
 
+int dsi_conn_prepare_commit(void *display,
+		struct msm_display_conn_params *params)
+{
+	if (!display || !params) {
+		pr_err("Invalid params\n");
+		return -EINVAL;
+	}
+
+	return dsi_display_pre_commit(display, params);
+}
+
 void dsi_conn_enable_event(struct drm_connector *connector,
 		uint32_t event_idx, bool enable, void *display)
 {
@@ -791,7 +802,7 @@ void dsi_conn_enable_event(struct drm_connector *connector,
 }
 
 int dsi_conn_post_kickoff(struct drm_connector *connector,
-	struct msm_display_kickoff_params *params)
+	struct msm_display_conn_params *params)
 {
 	struct drm_encoder *encoder;
 	struct dsi_bridge *c_bridge;
