838f38d3f3d08ae3b054058ec2a887418b4ffe92
Vishnuvardhan Prodduturi <vproddut@codeaurora.org>
2019-10-21 14:52:59 +0530

    disp: msm: dsi: add check before buffer copy
    This change adds a check for the length of the buffer
    before copying it to avoid a buffer overflow.
    
    Change-Id: I146895660be4060d9896706636257a57c6aef94f
    Signed-off-by: Vishnuvardhan Prodduturi <vproddut@codeaurora.org>

--- drivers/gpu/drm/msm/dsi-staging/dsi_ctrl.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_ctrl.c
@@ -121,7 +121,7 @@ static ssize_t debugfs_state_info_read(struct file *file,
 			dsi_ctrl->clk_freq.pix_clk_rate,
 			dsi_ctrl->clk_freq.esc_clk_rate);
 
-	/* TODO: make sure that this does not exceed 4K */
+	len = min_t(size_t, len, SZ_4K);
 	if (copy_to_user(buff, buf, len)) {
 		kfree(buf);
 		return -EFAULT;
@@ -176,8 +176,7 @@ static ssize_t debugfs_reg_dump_read(struct file *file,
 		return rc;
 	}
 
-
-	/* TODO: make sure that this does not exceed 4K */
+	len = min_t(size_t, len, SZ_4K);
 	if (copy_to_user(buff, buf, len)) {
 		kfree(buf);
 		return -EFAULT;
