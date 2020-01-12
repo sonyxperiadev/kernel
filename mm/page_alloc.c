a78e257ccc00ab72dc04e85100d7de603037e525
Nathan Chancellor <natechancellor@gmail.com>
2020-01-12 14:04:03 -0700

    treewide: Linux 4.14.111
    source kernel.org
    https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.14.110-111.xz
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1779,8 +1779,8 @@ inline void post_alloc_hook(struct page *page, unsigned int order,
 	set_page_refcounted(page);
 
 	arch_alloc_page(page, order);
-	kasan_alloc_pages(page, order);
 	kernel_map_pages(page, 1 << order, 1);
+	kasan_alloc_pages(page, order);
 	kernel_poison_pages(page, 1 << order, 1);
 	set_page_owner(page, order, gfp_flags);
 }
