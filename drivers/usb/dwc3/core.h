b3cdc9dadb7ae675a00ad950f71e5a411001ef77
Nathan Chancellor <natechancellor@gmail.com>
2020-01-12 22:46:22 -0700

    treewide: Linux 4.14.154
    source kernel.org
    https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.14.153-154.xz
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -970,6 +970,7 @@ struct dwc3_scratchpad_array {
  * 	1	- -3.5dB de-emphasis
  * 	2	- No de-emphasis
  * 	3	- Reserved
+ * @dis_metastability_quirk: set to disable metastability quirk.
  * @err_evt_seen: previous event in queue was erratic error
  * @usb3_u1u2_disable: if true, disable U1U2 low power modes in Superspeed mode
  * @in_lpm: indicates if controller is in low power mode (no clocks)
@@ -1166,6 +1167,8 @@ struct dwc3 {
 	atomic_t		in_lpm;
 	bool			b_suspend;
 	unsigned int		vbus_draw;
+
+	unsigned		dis_metastability_quirk:1;
 
 	u16			imod_interval;
 	struct workqueue_struct *dwc_wq;
