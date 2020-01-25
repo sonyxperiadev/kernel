7c429de20761eddb319e85e1f68e538ffbf4bcb6
AngeloGioacchino Del Regno <kholk11@gmail.com>
2020-01-25 12:44:27 +0100

    soc: qcom: peripheral-loader: Disable minidump ToC search for Sony
    Sony production platforms have minidump disabled: disable its
    ToC search in SMEM in order to stop a warning at boot.

--- drivers/soc/qcom/peripheral-loader.c
+++ drivers/soc/qcom/peripheral-loader.c
@@ -60,6 +60,14 @@
 #define MAX_LEN 96
 #define NUM_OF_ENCRYPTED_KEY	3
 
+/* Sony production phones have minidump disabled */
+#if defined(CONFIG_ARCH_SONY_LOIRE) || defined(CONFIG_ARCH_SONY_TONE) || \
+    defined(CONFIG_ARCH_SONY_YOSHINO) || defined(CONFIG_ARCH_SONY_NILE) || \
+    defined(CONFIG_ARCH_SONY_GANGES) || defined(CONFIG_ARCH_SONY_TAMA) || \
+    defined(CONFIG_ARCH_SONY_KUMANO)
+ #define TARGET_NO_MINIDUMP_SUPPORT
+#endif
+
 static void __iomem *pil_info_base;
 static struct md_global_toc *g_md_toc;
 
@@ -1671,7 +1679,7 @@ static int __init msm_pil_init(void)
 		writel_relaxed(0, pil_info_base + (i * sizeof(u32)));
 
 	/* Get Global minidump ToC*/
-#ifndef CONFIG_ARCH_MSM8916
+#ifndef TARGET_NO_MINIDUMP_SUPPORT
 	g_md_toc = qcom_smem_get(QCOM_SMEM_HOST_ANY, SBL_MINIDUMP_SMEM_ID,
 				 &size);
 	pr_debug("Minidump: g_md_toc is %pa\n", &g_md_toc);
