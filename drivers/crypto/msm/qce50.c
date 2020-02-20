2256002fd978dd86999ac226653fd471d585b1a5
Nick Desaulniers <ndesaulniers@google.com>
2020-02-20 09:40:03 -0800

    crypto: msm: fix -Wbool-operation
    Fixes the warning:
    drivers/crypto/msm/qce50.c:4669:28: error: bitwise negation of a boolean
    expression always evaluates to 'true'; did you mean logical negation?
    [-Werror,-Wbool-operation]
    pce_dev->cadence_flag = ~pce_dev->cadence_flag;
                            ^~~~~~~~~~~~~~~~~~~~~~
                            !
    
    This is correct because a _Bool is one byte, so bitwise negation (~) (as
    opposed to logical negation (!)) will produce true when applied to false
    as expected, but will still produce true when applied to true. If the
    programmer intended for pce_dev->cadence_flag to always be true at that
    statement, it would have been more concise and clearer to just assign
    true.
    
    Bug: 148287141
    Bug: 149786337
    Change-Id: I9d82c7a8667228f5cc1c968da6f0ab980de14d9c
    Reported-by: Jason Chiu <jasoncschiu@google.com>
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>

--- drivers/crypto/msm/qce50.c
+++ drivers/crypto/msm/qce50.c
@@ -4676,7 +4676,7 @@ again:
 			pce_dev->intr_cadence = 0;
 			atomic_set(&pce_dev->bunch_cmd_seq, 0);
 			atomic_set(&pce_dev->last_intr_seq, 0);
-			pce_dev->cadence_flag = ~pce_dev->cadence_flag;
+			pce_dev->cadence_flag = !pce_dev->cadence_flag;
 		}
 	}
 
