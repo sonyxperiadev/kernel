becf724b6cdb67ab37db7bf8dbb132909829e1f9
AngeloGioacchino Del Regno <kholk11@gmail.com>
2020-05-31 18:55:01 +0200

    drm: msm: somc_panel: Vary short circuit IRQ flags for Seine
    This is temporary and rather dirty, but Seine needs these flags
    or it starts complaining about unexistant short circuits.
    
    The code that handles this IRQ should be refactored soon.

--- drivers/gpu/drm/msm/dsi-staging/somc_panel/somc_panel_exts.h
+++ drivers/gpu/drm/msm/dsi-staging/somc_panel/somc_panel_exts.h
@@ -262,9 +262,13 @@ struct dsi_samsung_hbm {
 #define SHORT_WORKER_ACTIVE		true
 #define SHORT_WORKER_PASSIVE		false
 #define SHORT_IRQF_DISABLED		0x00000020
-#define SHORT_IRQF_FLAGS		(SHORT_IRQF_DISABLED | IRQF_ONESHOT | \
-					 IRQF_TRIGGER_HIGH | IRQF_TRIGGER_RISING)
 
+#ifdef CONFIG_ARCH_SONY_SEINE
+ #define SHORT_IRQF_FLAGS		(IRQF_ONESHOT | IRQF_TRIGGER_RISING)
+#else
+ #define SHORT_IRQF_FLAGS		(SHORT_IRQF_DISABLED | IRQF_ONESHOT | \
+					 IRQF_TRIGGER_HIGH | IRQF_TRIGGER_RISING)
+#endif
 struct short_detection_ctrl {
 	struct delayed_work check_work;
 	int current_chatter_cnt;
