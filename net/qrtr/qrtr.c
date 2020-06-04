8eb22b1c844de88853e89bc15da6ef1312678aab
Stefan Rücker <s.ruecker@gmx.de>
2020-06-04 20:50:01 +0200

    treewide: Linux 4.14.183
    source kernel.org
    https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.14.182-183.xz
    
    Signed-off-by: Stefan Rücker <s.ruecker@gmx.de>

--- net/qrtr/qrtr.c
+++ net/qrtr/qrtr.c
@@ -1457,7 +1457,7 @@ static int qrtr_bcast_enqueue(struct qrtr_node *node, struct sk_buff *skb,
 	}
 	up_read(&qrtr_node_lock);
 
-	qrtr_local_enqueue(node, skb, type, from, to, flags);
+	qrtr_local_enqueue(NULL, skb, type, from, to, flags);
 
 	return 0;
 }
