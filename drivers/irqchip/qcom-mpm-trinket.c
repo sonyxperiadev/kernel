d011e9b72071fe379cc31d04f3bacef47deed985
AngeloGioacchino Del Regno <angelogioacchino.delregno@somainline.org>
2020-07-11 21:54:23 +0200

    irqchip: qcom-mpm: Add support for SM6125 SoC
    Partially import the support for the SM6125 SoC from the Open
    Devices kernel 4.14.
    The GPIO MPM mappings have been moved again to the DT, like
    kernel 3.4 days.

---
+++ drivers/irqchip/qcom-mpm-trinket.c
@@ -0,0 +1,24 @@
+/* Copyright (c) 2019, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include "mpm.h"
+
+const struct mpm_pin mpm_trinket_gic_chip_data[] = {
+	{2, 222},
+	{12, 454}, /* b3_lfps_rxterm_irq */
+	{86, 215}, /* mpm_wake,spmi_m */
+	{90, 292}, /* eud_p0_dpse_int_mx */
+	{91, 292}, /* eud_p0_dmse_int_mx */
+	{-1},
+};
+
