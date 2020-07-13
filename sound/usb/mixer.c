def10bfd6aa0b78ca9121cf73c2b8ccb38e8b9ab
chris42 <list-christian@web.de>
2020-07-13 14:56:02 +0200

    treewide: Linux 4.9.226 source kernel.org https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.9.225-226.xz
    Merge conflict resolved in
    - fs/binfmt_elf.c
    - mm/vmalloc.c
    - sound/usb/mixer.c
    
    Signed-off-by: Christian Saga <accounts@christiansaga.de>

--- sound/usb/mixer.c
+++ sound/usb/mixer.c
@@ -1050,7 +1050,14 @@ static void volume_control_quirks(struct usb_mixer_elem_info *cval,
 			cval->res = 384;
 		}
 		break;
-
+	case USB_ID(0x0495, 0x3042): /* ESS Technology Asus USB DAC */
+		if ((strstr(kctl->id.name, "Playback Volume") != NULL) ||
+			strstr(kctl->id.name, "Capture Volume") != NULL) {
+			cval->min >>= 8;
+			cval->max = 0;
+			cval->res = 1;
+		}
+		break;
 	case USB_ID(0x1130, 0x1620): /* Logitech Speakers S150 */
 	/* This audio device has 2 channels and it explicitly requires the
 	 * host to send SET_CUR command on the volume control of both the
