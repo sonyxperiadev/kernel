e6cf61b80737cc9d28ac9e6278faff8b761502a9
Stefan Rücker <s.ruecker@gmx.de>
2020-07-28 11:55:26 +0200

    treewide: Linux 4.14.189
    source kernel.org
    https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.14.188-189.xz
    
    Signed-off-by: Stefan Rücker <s.ruecker@gmx.de>

--- sound/core/compress_offload.c
+++ sound/core/compress_offload.c
@@ -727,8 +727,10 @@ static int snd_compr_stop(struct snd_compr_stream *stream)
 
 	retval = stream->ops->trigger(stream, SNDRV_PCM_TRIGGER_STOP);
 	if (!retval) {
-		stream->runtime->state = SNDRV_PCM_STATE_SETUP;
-		wake_up(&stream->runtime->sleep);
+		/* clear flags and stop any drain wait */
+		stream->partial_drain = false;
+		stream->metadata_set = false;
+		snd_compr_drain_notify(stream);
 		stream->runtime->total_bytes_available = 0;
 		stream->runtime->total_bytes_transferred = 0;
 	}
@@ -865,6 +867,7 @@ static int snd_compr_partial_drain(struct snd_compr_stream *stream)
 	if (stream->next_track == false)
 		return -EPERM;
 
+	stream->partial_drain = true;
 	retval = stream->ops->trigger(stream, SND_COMPR_TRIGGER_PARTIAL_DRAIN);
 
 	stream->next_track = false;
