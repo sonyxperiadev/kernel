684c017e4fbeda3c40029066a999741349e84dd8
sjllls <2012ji123@gmail.com>
2020-08-11 09:32:08 +0800

    treewide: Linux 4.4.196 source kernel.org https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.4.195-196.xz

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -3741,6 +3741,26 @@ static int binder_free_thread(struct binder_proc *proc,
 	binder_queue_for_zombie_cleanup(proc);
 	binder_proc_unlock(thread->proc, __LINE__);
 
+	/*
+	 * If this thread used poll, make sure we remove the waitqueue
+	 * from any epoll data structures holding it with POLLFREE.
+	 * waitqueue_active() is safe to use here because we're holding
+	 * the global lock.
+	 */
+	if ((thread->looper & BINDER_LOOPER_STATE_POLL) &&
+	    waitqueue_active(&thread->wait)) {
+		wake_up_poll(&thread->wait, POLLHUP | POLLFREE);
+	}
+
+	/*
+	 * This is needed to avoid races between wake_up_poll() above and
+	 * and ep_remove_waitqueue() called for other reasons (eg the epoll file
+	 * descriptor being closed); ep_remove_waitqueue() holds an RCU read
+	 * lock, so we can be sure it's done after calling synchronize_rcu().
+	 */
+	if (thread->looper & BINDER_LOOPER_STATE_POLL)
+		synchronize_rcu();
+
 	if (send_reply)
 		binder_send_failed_reply(send_reply, BR_DEAD_REPLY);
 	binder_stats_zombie(BINDER_STAT_THREAD);
