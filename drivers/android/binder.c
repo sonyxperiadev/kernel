8031ce9d1eda6dfda5f919e12556aeb5d0522ed5
Marco Ballesio <balejs@google.com>
2020-09-10 13:39:20 -0700

    binder: introduce the BINDER_GET_FROZEN_INFO ioctl
    User space needs to know if binder transactions occurred to frozen
    processes. Introduce a new BINDER_GET_FROZEN ioctl and keep track of
    transactions occurring to frozen proceses. Also, allow async
    transactions toward frozen processes and improve error hendling.
    
    Bug: 143717177
    Test: atest testBinderLib
    Signed-off-by: Marco Ballesio <balejs@google.com>
    
    Change-Id: I9ee1c2e5fe3d4ab31fc1a137d840bd4cd38a8704

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -469,8 +469,9 @@ struct binder_priority {
  * @is_dead:              process is dead and awaiting free
  *                        when outstanding transactions are cleaned up
  *                        (protected by @inner_lock)
- * @is_frozen:            process is frozen and unable to service
- *                        binder transactions
+ * @sync_recv:            process received sync transactions since last frozen
+ *                        (protected by @inner_lock)
+ * @async_recv:           process received async transactions since last frozen
  *                        (protected by @inner_lock)
  * @freeze_wait:          waitqueue of processes waiting for all outstanding
  *                        transactions to be processed
@@ -520,6 +521,8 @@ struct binder_proc {
 	int outstanding_txns;
 	bool is_dead;
 	bool is_frozen;
+	bool sync_recv;
+	bool async_recv;
 	wait_queue_head_t freeze_wait;
 
 	struct list_head todo;
@@ -2874,8 +2877,13 @@ static int binder_proc_transaction(struct binder_transaction *t,
 	}
 
 	binder_inner_proc_lock(proc);
+	if (proc->is_frozen) {
+		proc->sync_recv |= !oneway;
+		proc->async_recv |= oneway;
+	}
 
-	if (proc->is_frozen || proc->is_dead || (thread && thread->is_dead)) {
+	if ((proc->is_frozen && !oneway) || proc->is_dead ||
+			(thread && thread->is_dead)) {
 		bool proc_is_dead = proc->is_dead
 			|| (thread && thread->is_dead);
 		binder_inner_proc_unlock(proc);
@@ -3522,7 +3530,7 @@ static void binder_transaction(struct binder_proc *proc,
 		binder_enqueue_thread_work(thread, tcomplete);
 		binder_inner_proc_lock(target_proc);
 		if (target_thread->is_dead || target_proc->is_frozen) {
-			return_error = target_proc->is_dead ?
+			return_error = target_thread->is_dead ?
 				BR_DEAD_REPLY : BR_FROZEN_REPLY;
 			binder_inner_proc_unlock(target_proc);
 			goto err_dead_proc_or_thread;
@@ -5007,6 +5015,37 @@ static int binder_ioctl_get_node_debug_info(struct binder_proc *proc,
 	return 0;
 }
 
+static int binder_ioctl_get_freezer_info(
+				struct binder_frozen_status_info *info)
+{
+	struct binder_proc *target_proc;
+	bool found = false;
+
+	mutex_lock(&binder_procs_lock);
+	hlist_for_each_entry(target_proc, &binder_procs, proc_node) {
+		if (target_proc->pid == info->pid) {
+			found = true;
+			binder_inner_proc_lock(target_proc);
+			target_proc->tmp_ref++;
+			binder_inner_proc_unlock(target_proc);
+			break;
+		}
+	}
+	mutex_unlock(&binder_procs_lock);
+
+	if (!found)
+		return -EINVAL;
+
+	binder_inner_proc_lock(target_proc);
+	info->sync_recv = target_proc->sync_recv;
+	info->async_recv = target_proc->async_recv;
+	binder_inner_proc_unlock(target_proc);
+
+	binder_proc_dec_tmpref(target_proc);
+
+	return 0;
+}
+
 static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
 	int ret;
@@ -5151,6 +5190,8 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		}
 		if (!info.enable) {
 			binder_inner_proc_lock(target_proc);
+			target_proc->sync_recv = false;
+			target_proc->async_recv = false;
 			target_proc->is_frozen = false;
 			binder_inner_proc_unlock(target_proc);
 			binder_proc_dec_tmpref(target_proc);
@@ -5162,6 +5203,8 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		 * for transactions to drain.
 		 */
 		binder_inner_proc_lock(target_proc);
+		target_proc->sync_recv = false;
+		target_proc->async_recv = false;
 		target_proc->is_frozen = true;
 		binder_inner_proc_unlock(target_proc);
 		if (info.timeout_ms > 0)
@@ -5180,6 +5223,24 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		binder_proc_dec_tmpref(target_proc);
 		if (ret < 0)
 			goto err;
+		break;
+	}
+	case BINDER_GET_FROZEN_INFO: {
+		struct binder_frozen_status_info info;
+
+		if (copy_from_user(&info, ubuf, sizeof(info))) {
+			ret = -EFAULT;
+			goto err;
+		}
+
+		ret = binder_ioctl_get_freezer_info(&info);
+		if (ret < 0)
+			goto err;
+
+		if (copy_to_user(ubuf, &info, sizeof(info))) {
+			ret = -EFAULT;
+			goto err;
+		}
 		break;
 	}
 	default:
@@ -5518,6 +5579,8 @@ static void binder_deferred_release(struct binder_proc *proc)
 
 	proc->is_dead = true;
 	proc->is_frozen = false;
+	proc->sync_recv = false;
+	proc->async_recv = false;
 	threads = 0;
 	active_transactions = 0;
 	while ((n = rb_first(&proc->threads))) {
