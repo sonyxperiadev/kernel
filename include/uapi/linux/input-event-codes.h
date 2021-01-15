e64695d7add174bbf4d87b87f8a6734b4a9eb10a
Konrad Dybcio <konrad.dybcio@somainline.org>
2021-01-15 15:46:28 +0100

    include/uapi/linux: input-event-codes: Add missing key events from downstream
    KEY_G_ASSIST is used on PDX206, other ones might come in handy too.
    
    Signed-off-by: Konrad Dybcio <konrad.dybcio@somainline.org>

--- include/uapi/linux/input-event-codes.h
+++ include/uapi/linux/input-event-codes.h
@@ -520,6 +520,10 @@
 #define KEY_DEL_EOS		0x1c1
 #define KEY_INS_LINE		0x1c2
 #define KEY_DEL_LINE		0x1c3
+#define KEY_SIDE_GESTURE	0x1c6
+#define KEY_BLACK_UI_GESTURE	0x1c7
+#define KEY_MULTI		0x1c8
+#define KEY_G_ASSIST		0x1c9
 
 #define KEY_FN			0x1d0
 #define KEY_FN_ESC		0x1d1
