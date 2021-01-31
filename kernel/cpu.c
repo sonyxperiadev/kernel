8dcce468e0b205be80d43b4eb78c2b9bd05b8012
voidanix <voidanix@keyedlimepie.org>
2021-01-31 14:37:39 -0500

    treewide: Linux 4.14.285
    Applied cleanly.
    
    Link: https://lore.kernel.org/r/20220623164343.132308638@linuxfoundation.org
    Signed-off-by: voidanix <voidanix@keyedlimepie.org>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -33,6 +33,7 @@
 #include <uapi/linux/sched/types.h>
 #include <linux/highmem.h>
 #include <linux/cpuset.h>
+#include <linux/random.h>
 
 #include <trace/events/power.h>
 #define CREATE_TRACE_POINTS
@@ -1488,6 +1489,11 @@ static struct cpuhp_step cpuhp_bp_states[] = {
 		.startup.single		= perf_event_init_cpu,
 		.teardown.single	= perf_event_exit_cpu,
 	},
+	[CPUHP_RANDOM_PREPARE] = {
+		.name			= "random:prepare",
+		.startup.single		= random_prepare_cpu,
+		.teardown.single	= NULL,
+	},
 	[CPUHP_WORKQUEUE_PREP] = {
 		.name			= "workqueue:prepare",
 		.startup.single		= workqueue_prepare_cpu,
@@ -1611,6 +1617,11 @@ static struct cpuhp_step cpuhp_ap_states[] = {
 		.name			= "workqueue:online",
 		.startup.single		= workqueue_online_cpu,
 		.teardown.single	= workqueue_offline_cpu,
+	},
+	[CPUHP_AP_RANDOM_ONLINE] = {
+		.name			= "random:online",
+		.startup.single		= random_online_cpu,
+		.teardown.single	= NULL,
 	},
 	[CPUHP_AP_RCUTREE_ONLINE] = {
 		.name			= "RCU/tree:online",
