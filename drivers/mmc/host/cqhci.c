eabb224e88e7d7e6d07b16174304c57d73663c44
Phanindra Babu Pabba <pabba@codeaurora.org>
2021-02-03 23:57:08 +0530

    mmc: cqhci: Handle cqhci crypto configurations correctly
    Enable/Disable cqhci crypto from cqhci_enable/cqhci_disable
    instead of __cqhci_enable/__cqhci_disable to avoid mulitple
    changes to crypto configs. These APIs are more frequently
    called & the intermittent disablement of crypto config is
    leading to failure of keyslot programming, their by blocking
    the encryption of content.
    
    Change-Id: If7bafaad4ec90b6f97ab1ca6006335d6f58220e4
    Signed-off-by: Phanindra Babu Pabba <pabba@codeaurora.org>

--- drivers/mmc/host/cqhci.c
+++ drivers/mmc/host/cqhci.c
@@ -280,7 +280,6 @@ static void __cqhci_enable(struct cqhci_host *cq_host)
 		cqcfg |= CQHCI_TASK_DESC_SZ;
 
 	if (cqhci_host_is_crypto_supported(cq_host)) {
-		cqhci_crypto_enable(cq_host);
 		cqcfg |= CQHCI_ICE_ENABLE;
 		/* For SDHC v5.0 onwards, ICE 3.0 specific registers are added
 		 * in CQ register space, due to which few CQ registers are
@@ -323,9 +322,6 @@ static void __cqhci_enable(struct cqhci_host *cq_host)
 static void __cqhci_disable(struct cqhci_host *cq_host)
 {
 	u32 cqcfg;
-
-	if (cqhci_host_is_crypto_supported(cq_host))
-		cqhci_crypto_disable(cq_host);
 
 	cqcfg = cqhci_readl(cq_host, CQHCI_CFG);
 	cqcfg &= ~CQHCI_ENABLE;
@@ -371,6 +367,9 @@ static int cqhci_enable(struct mmc_host *mmc, struct mmc_card *card)
 	err = cqhci_host_alloc_tdl(cq_host);
 	if (err)
 		return err;
+
+	if (cqhci_host_is_crypto_supported(cq_host))
+		cqhci_crypto_enable(cq_host);
 
 	__cqhci_enable(cq_host);
 
@@ -431,6 +430,9 @@ static void cqhci_disable(struct mmc_host *mmc)
 		return;
 
 	cqhci_off(mmc);
+
+	if (cqhci_host_is_crypto_supported(cq_host))
+		cqhci_crypto_disable(cq_host);
 
 	__cqhci_disable(cq_host);
 
