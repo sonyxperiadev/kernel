d84230dd2159ac28e9c7a10e280ce8668985aa8c
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2021-07-13 19:14:29 +0200

    treewide: 4.14.233
    Skipped: drivers/mmc/core/core.c

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -3510,9 +3510,6 @@ int usb_port_resume(struct usb_device *udev, pm_message_t msg)
 		 * sequence.
 		 */
 		status = hub_port_status(hub, port1, &portstatus, &portchange);
-
-		/* TRSMRCY = 10 msec */
-		usleep_range(10000, 10500);
 	}
 
  SuspendCleared:
@@ -3527,6 +3524,9 @@ int usb_port_resume(struct usb_device *udev, pm_message_t msg)
 				usb_clear_port_feature(hub->hdev, port1,
 						USB_PORT_FEAT_C_SUSPEND);
 		}
+
+		/* TRSMRCY = 10 msec */
+		msleep(10);
 	}
 
 	if (udev->persist_enabled)
