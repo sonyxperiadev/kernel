a6586469ac5372cfea483791ac95511a6debd256
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2021-10-27 17:12:32 +0200

    linux: 4.19.179
    Just Documentation/devicetree/bindings/net/btusb.txt missing.
    Otherwise all fine

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1129,7 +1129,7 @@ static ssize_t mm_stat_show(struct device *dev,
 			zram->limit_pages << PAGE_SHIFT,
 			max_used << PAGE_SHIFT,
 			(u64)atomic64_read(&zram->stats.same_pages),
-			pool_stats.pages_compacted,
+			atomic_long_read(&pool_stats.pages_compacted),
 			(u64)atomic64_read(&zram->stats.huge_pages),
 			zram_dedup_dup_size(zram),
 			zram_dedup_meta_size(zram));
