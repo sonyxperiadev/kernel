8bd67828344c2f4a7ae1e9a96d8f654fdb2f87a5
Konrad Dybcio <konrad.dybcio@somainline.org>
2021-11-20 02:09:52 +0100

    pinctrl: qcom: lahaina: Reserve correct pins on Sagami
    Hey, we are "only" forbidden from touching 4 this time!

--- drivers/pinctrl/qcom/pinctrl-lahaina.c
+++ drivers/pinctrl/qcom/pinctrl-lahaina.c
@@ -1938,7 +1938,11 @@ static const struct msm_pingroup lahaina_groups[] = {
 };
 
 static const int lahaina_reserved_gpios[] = {
+#if defined(CONFIG_ARCH_SONY_SAGAMI)
+	44, 45, 46, 47, -1
+#else
 	52, 53, 54, 55, 56, 57, 58, 59, -1
+#endif
 };
 
 static struct pinctrl_qup lahaina_qup_regs[] = {
