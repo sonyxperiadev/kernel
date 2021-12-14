2854a54379c4f4e88715dcbf2c5bacb24d55f129
Alin Jerpelea <alin.jerpelea@sony.com>
2021-12-14 07:30:09 +0000

    include: uapi: esoc_ctrl.h: update SPDX license
    The license exported headers must be updated. We are following the license change
    https://portland.source.codeaurora.org/quic/la/kernel/msm-5.4/commit/include/uapi/linux/esoc_ctrl.h?h=LA.AU.0.2.0.r4.1&id=bb06fdbcd02fb0d51b3282f0b3edec55ab7d73fb
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- include/uapi/linux/esoc_ctrl.h
+++ include/uapi/linux/esoc_ctrl.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0 */
+/* SPDX-License-Identifier: GPL-2.0-only WITH Linux-syscall-note */
 /*
  * Copyright (c) 2015-2019, The Linux Foundation. All rights reserved.
  */
