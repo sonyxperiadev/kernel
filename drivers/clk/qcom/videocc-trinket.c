f9b68b6313e447a2c2f8ebcf8e2814b7b6cf9de9
Pavel Dubrova <pashadubrova@gmail.com>
2022-01-26 09:22:59 +0200

    clk: qcom: trinket: Add missing clk_rcg2_ops on DFS clocks
    clk_rcg2_ops pointer on DFS clocks was accidentally lost in commit
    7a93e75 ("clk: qcom: gcc-trinket: Update RCG clocks and DFS
    registration").
    
    Also uncomment test_ctl_hi_mask register.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/clk/qcom/videocc-trinket.c
+++ drivers/clk/qcom/videocc-trinket.c
@@ -94,7 +94,7 @@ static const struct alpha_pll_config video_pll0_config = {
 	.vco_mask = 0x3 << 20,
 	.main_output_mask = BIT(0),
 	.config_ctl_val = 0x4001055b,
-	/* .test_ctl_hi_mask = 0x1, */
+	.test_ctl_hi_mask = 0x1,
 };
 
 static struct clk_alpha_pll video_pll0_out_main = {
