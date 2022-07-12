5a6988fe24217d4aff951c66d77dad186d62bba1
voidanix <voidanix@keyedlimepie.org>
2022-07-12 14:00:05 +0800

    treewide: Linux 4.14.289
    Conflict with f4a5311dfd1cbf9440f0 ("xhci: make xhci_handshake timeout for
    xhci_reset() adjustable") resolved.
    
    Link: https://lore.kernel.org/r/20220719114521.868169025@linuxfoundation.org
    Signed-off-by: voidanix <voidanix@keyedlimepie.org>

--- sound/soc/soc-ops.c
+++ sound/soc/soc-ops.c
@@ -536,7 +536,7 @@ int snd_soc_put_volsw_range(struct snd_kcontrol *kcontrol,
 		return -EINVAL;
 	if (mc->platform_max && tmp > mc->platform_max)
 		return -EINVAL;
-	if (tmp > mc->max - mc->min + 1)
+	if (tmp > mc->max - mc->min)
 		return -EINVAL;
 
 	if (invert)
@@ -557,7 +557,7 @@ int snd_soc_put_volsw_range(struct snd_kcontrol *kcontrol,
 			return -EINVAL;
 		if (mc->platform_max && tmp > mc->platform_max)
 			return -EINVAL;
-		if (tmp > mc->max - mc->min + 1)
+		if (tmp > mc->max - mc->min)
 			return -EINVAL;
 
 		if (invert)
