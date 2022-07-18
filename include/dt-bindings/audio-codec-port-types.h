4ec200d6ec658a85369dc53a902ed514fa9b1e4d
Konrad Dybcio <konrad.dybcio@somainline.org>
2022-07-18 15:10:21 +0200

    dt-bindings: Pull in techpack headers

---
+++ include/dt-bindings/audio-codec-port-types.h
@@ -0,0 +1,75 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Copyright (c) 2018-2021, The Linux Foundation. All rights reserved. */
+
+#ifndef __AUDIO_CODEC_PORT_TYPES_H
+#define __AUDIO_CODEC_PORT_TYPES_H
+
+#define OFFSET1_VAL0 0
+#define OFFSET1_VAL1 1
+#define OFFSET1_VAL2 2
+#define OFFSET1_VAL3 3
+#define OFFSET1_VAL4 4
+#define OFFSET1_VAL5 5
+#define OFFSET1_VAL6 6
+#define OFFSET1_VAL7 7
+#define OFFSET1_VAL8 8
+#define OFFSET1_VAL9 9
+#define OFFSET1_VAL10 10
+#define OFFSET1_VAL11 11
+#define OFFSET1_VAL12 12
+#define OFFSET1_VAL13 13
+#define OFFSET1_VAL14 14
+#define OFFSET1_VAL15 15
+
+#define LANE0 0
+#define LANE1 1
+#define LANE2 2
+
+#define SPKR_L 1
+#define SPKR_L_COMP 2
+#define SPKR_L_BOOST 3
+#define SPKR_L_VI 4
+#define SPKR_R 5
+#define SPKR_R_COMP 6
+#define SPKR_R_BOOST 7
+#define SPKR_R_VI 8
+#define HPH_L 9
+#define HPH_R 10
+#define COMP_L 11
+#define COMP_R 12
+#define CLSH 13
+#define LO 14
+#define DSD_L 15
+#define DSD_R 16
+#define MBHC 17
+#define ADC1 18
+#define ADC2 19
+#define ADC3 20
+#define ADC4 21
+#define DMIC0 22
+#define DMIC1 23
+#define DMIC2 24
+#define DMIC3 25
+#define DMIC4 26
+#define DMIC5 27
+#define DMIC6 28
+#define DMIC7 29
+#define DMIC8 30
+#define DMIC9 31
+#define DMIC10 32
+#define PCM_OUT1 33
+#define SWRM_TX1_CH1 34
+#define SWRM_TX1_CH2 35
+#define SWRM_TX1_CH3 36
+#define SWRM_TX1_CH4 37
+#define SWRM_TX2_CH1 38
+#define SWRM_TX2_CH2 39
+#define SWRM_TX2_CH3 40
+#define SWRM_TX2_CH4 41
+#define SWRM_TX3_CH1 42
+#define SWRM_TX3_CH2 43
+#define SWRM_TX3_CH3 44
+#define SWRM_TX3_CH4 45
+#define SWRM_PCM_IN 46
+
+#endif /* __AUDIO_CODEC_PORT_TYPES_H */
