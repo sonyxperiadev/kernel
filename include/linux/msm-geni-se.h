8acf07d7fcc5792d5b09acd20f5476e7c2e674bf
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2022-07-19 22:43:42 +0200

    include: linux: Add QN5965F gpio_suspend

--- include/linux/msm-geni-se.h
+++ include/linux/msm-geni-se.h
@@ -71,6 +71,9 @@ struct se_geni_rsc {
 	struct pinctrl_state *geni_gpio_shutdown;
 	struct pinctrl_state *geni_gpio_active;
 	struct pinctrl_state *geni_gpio_sleep;
+	/*PDX225 code for QN5965F-837  at 2021/11/17 start*/
+	struct pinctrl_state *geni_gpio_suspend;
+	/*PDX225 code for QN5965F-837  at 2021/11/17 end*/
 	int	clk_freq_out;
 	unsigned int num_clk_levels;
 	unsigned long *clk_perf_tbl;
