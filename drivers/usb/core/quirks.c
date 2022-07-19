3a92ca148cc55292d44f07aa02703903faf61c19
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2022-07-19 22:27:49 +0200

    [SONY code] Add Fibercom LTE modem USB quirk
    Imported from 65.0.A.0.1094 SoMC Murray XQ-CC72

--- drivers/usb/core/quirks.c
+++ drivers/usb/core/quirks.c
@@ -520,6 +520,10 @@ static const struct usb_device_id usb_quirk_list[] = {
 	/* VCOM device */
 	{ USB_DEVICE(0x4296, 0x7570), .driver_info = USB_QUIRK_CONFIG_INTF_STRINGS },
 
+	/* Fibocom L850-GL LTE Modem */
+	{ USB_DEVICE(0x2cb7, 0x0007), .driver_info =
+			USB_QUIRK_IGNORE_REMOTE_WAKEUP },
+
 	/* INTEL VALUE SSD */
 	{ USB_DEVICE(0x8086, 0xf1a5), .driver_info = USB_QUIRK_RESET_RESUME },
 
