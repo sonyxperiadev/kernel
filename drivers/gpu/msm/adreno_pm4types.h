4d3a779d1f65774cc6a9dc2fdde2c2eb8a047693
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2022-07-19 13:11:04 +0200

    [SONY code] drm/msm: Extend Adreno perf counter support Imported from 65.0.A.0.1094 SoMC Murray XQ-CC72

--- drivers/gpu/msm/adreno_pm4types.h
+++ drivers/gpu/msm/adreno_pm4types.h
@@ -47,6 +47,9 @@
 /* switches SMMU pagetable, used on a5xx only */
 #define CP_SMMU_TABLE_UPDATE 0x53
 
+/* Designate command streams to be executed before/after CP does state restore during preemption */
+#define CP_SET_AMBLE		0x55
+
 /*  Set internal CP registers, used to indicate context save data addresses */
 #define CP_SET_PSEUDO_REGISTER      0x56
 
@@ -156,6 +159,9 @@
 #define CP_LOADSTATE_NUMOFUNITS_SHIFT 0x00000016
 #define CP_LOADSTATE_STATETYPE_SHIFT 0x00000000
 #define CP_LOADSTATE_EXTSRCADDR_SHIFT 0x00000002
+
+/* Used to define amble type in SET_AMBLE packet to execute during preemption */
+#define CP_KMD_AMBLE_TYPE 3
 
 static inline uint pm4_calc_odd_parity_bit(uint val)
 {
