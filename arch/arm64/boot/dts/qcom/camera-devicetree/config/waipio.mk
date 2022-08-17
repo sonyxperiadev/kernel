02ab40677f40ac01cf8206b884b869d93afca7fb
Alin Jerpelea <alin.jerpelea@sony.com>
2022-08-17 12:59:16 +0200

    dts: move Sony devices to a separate folder
    for easy rebase we keep the Sony devices in a separate folder
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- arch/arm64/boot/dts/qcom/camera-devicetree/config/waipio.mk
+++
@@ -1,4 +0,0 @@
-dtbo-$(CONFIG_ARCH_WAIPIO)		+= waipio-camera.dtbo
-dtbo-$(CONFIG_ARCH_WAIPIO)		+= waipio-camera-overlay-v2.dtbo
-
-dtbo-$(CONFIG_ARCH_WAIPIO)		+= waipio-camera-sensor-nagara-pdx223.dtbo
