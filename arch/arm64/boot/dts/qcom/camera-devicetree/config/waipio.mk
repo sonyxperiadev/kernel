281a069bd2dd046bfa3963b515d8b809632c41dc
Konrad Dybcio <konrad.dybcio@somainline.org>
2022-08-18 17:31:19 +0200

    Revert "dts: move Sony devices to a separate folder"
    This reverts commit 02ab40677f40ac01cf8206b884b869d93afca7fb.
    
    It makes sense to keep all the qcom things in one place and the
    "ease of rebasing" mentioned in the original commit makes little
    sense considering the nature of DTB OSS drops, the differences
    between SODP and stock kernel and the hacks required to build the
    DTB/Os properly without some magic internal qc tools. The only
    real way to update these against new releases is careful manual
    diffing and examination of what should and shouldn't be merged in.

---
+++ arch/arm64/boot/dts/qcom/camera-devicetree/config/waipio.mk
@@ -0,0 +1,4 @@
+dtbo-$(CONFIG_ARCH_WAIPIO)		+= waipio-camera.dtbo
+dtbo-$(CONFIG_ARCH_WAIPIO)		+= waipio-camera-overlay-v2.dtbo
+
+dtbo-$(CONFIG_ARCH_WAIPIO)		+= waipio-camera-sensor-nagara-pdx223.dtbo
