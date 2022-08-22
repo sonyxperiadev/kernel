9cf33b92411e9b5561580349607eaf3291519a2b
Björn Bidar <bjorn.bidar@jolla.com>
2022-08-22 19:43:01 +0300

    treewide: 4.19.191
    Applied cleanly except:
    - 7381dcf620182b4b986894ff36a832de479e1491 skipped since already applied through
      downstream commit 34cc2c83dc809fc1914d499c7b796bb76b58d3e3
    - 659ae4a027e2f8538303e700d3374860e330a768 skipped since already
      applied through downstream commit 65873c2988257179a464f896caf0084ba970c77f
    - e132c5ca8f64fb9b52434130151a976f82dbfd46 required manual edit
      because of downstream commit dd914d68a01066757f77cc99d93feb5487481b8e
    - 0747a1d043c2c0c062fd40966f642c43247e2477 required manual edit
      because of downstream commit
      1aacdcf9a11fcabeac8f104a393cb91b4de21cb6 and a073798a2b5b084ab175ad065010e4658a11d6ab
    
    CVE's addressed:
    - CVE-2021-23133
    - CVE-2021-23134
    
    Signed-off-by: Björn Bidar <bjorn.bidar@jolla.com>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -2550,6 +2550,12 @@ static int mmc_test_awake_ext_csd(struct mmc_host *host)
 	return err;
 }
 
+static bool _mmc_cache_enabled(struct mmc_host *host)
+{
+	return host->card->ext_csd.cache_size > 0 &&
+	       host->card->ext_csd.cache_ctrl & 1;
+}
+
 static int _mmc_suspend(struct mmc_host *host, bool is_suspend)
 {
 	int err = 0;
@@ -2970,6 +2976,7 @@ static const struct mmc_bus_ops mmc_ops = {
 	.alive = mmc_alive,
 	.shutdown = mmc_shutdown,
 	.hw_reset = _mmc_hw_reset,
+        .cache_enabled = _mmc_cache_enabled,
 	.change_bus_speed = mmc_change_bus_speed,
 	.change_bus_speed_deferred = mmc_change_bus_speed_deferred,
 };
