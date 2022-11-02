0293c42e8ad9ac17a1ecc0ca2e25519a13dc8b52
voidanix <voidanix@keyedlimepie.org>
2022-11-02 14:40:40 +0100

    xhci: Remove xhci_handshake_check_state API
    Because of commit f4a5311dfd1cbf9440f0 ("xhci: make xhci_handshake timeout
    for xhci_reset() adjustable"), the reset timeout is now back to a sane
    default and also became configurable: in turn it should avoid making the
    watchdog timeout again.
    
    All of this made the CAF-issued xhci_handshake_check_state() redundant,
    therefore it is best to remove it.
    
    Fixes: 089fc989d534 ("usb: XHCI: Implement xhci_handshake_check_state() API")
    Signed-off-by: voidanix <voidanix@keyedlimepie.org>

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -91,27 +91,6 @@ int xhci_handshake(void __iomem *ptr, u32 mask, u32 done, u64 timeout_us)
 	return ret;
 }
 
-int xhci_handshake_check_state(struct xhci_hcd *xhci,
-		void __iomem *ptr, u32 mask, u32 done, int usec)
-{
-	u32	result;
-
-	do {
-		result = readl_relaxed(ptr);
-		if (result == ~(u32)0)	/* card removed */
-			return -ENODEV;
-		/* host removed. Bail out */
-		if (xhci->xhc_state & XHCI_STATE_REMOVING)
-			return -ENODEV;
-		result &= mask;
-		if (result == done)
-			return 0;
-		udelay(1);
-		usec--;
-	} while (usec > 0);
-	return -ETIMEDOUT;
-}
-
 /*
  * Disable interrupts and begin the xHCI halting process.
  */
