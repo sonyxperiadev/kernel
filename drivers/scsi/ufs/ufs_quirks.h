30190858558231436a30d9dec05c7bc701ceb446
Alin Jerpelea <alin.jerpelea@sony.com>
2022-11-22 15:39:10 +0100

    Revert "scsi: ufs: add Murray UFS quirks"
    This reverts commit bb2c0905329c8ddac64ca8aea96f1211d81e37ae.

--- drivers/scsi/ufs/ufs_quirks.h
+++ drivers/scsi/ufs/ufs_quirks.h
@@ -8,7 +8,7 @@
 
 #if defined(CONFIG_ARCH_SONY_YOSHINO) || defined(CONFIG_ARCH_SONY_TAMA) || \
     defined(CONFIG_ARCH_SONY_KUMANO)  || defined(CONFIG_ARCH_SONY_EDO) || \
-    defined(CONFIG_ARCH_SONY_LENA) || defined(CONFIG_ARCH_SONY_MURRAY)
+    defined(CONFIG_ARCH_SONY_LENA)
  #ifndef UFS_TARGET_SONY_PLATFORM
   #define UFS_TARGET_SONY_PLATFORM
  #endif
