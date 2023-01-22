70fb65f06a826202d72f39e8e7ec5ef306b5c275
voidanix <voidanix@keyedlimepie.org>
2023-01-22 20:13:36 +0100

    treewide: Linux 4.14.274
    Conflict resolved when merging upstream d908d2776464a8021a1f ("nfc:
    st21nfca: Fix potential buffer overflows in EVT_TRANSACTION") and
    2c8abafd6c72ef04bc97 ("esp: Fix possible buffer overflow in ESP
    transformation"), due to ACK and msm changes respectively.
    
    Link: https://lore.kernel.org/r/20220325150416.756136126@linuxfoundation.org
    Tested-by: voidanix <voidanix@keyedlimepie.org>
    Signed-off-by: voidanix <voidanix@keyedlimepie.org>

--- include/net/sock.h
+++ include/net/sock.h
@@ -2460,4 +2460,7 @@ static inline void sk_pacing_shift_update(struct sock *sk, int val)
 int sockev_register_notify(struct notifier_block *nb);
 int sockev_unregister_notify(struct notifier_block *nb);
 
+/* On 32bit arches, an skb frag is limited to 2^15 */
+#define SKB_FRAG_PAGE_ORDER	get_order(32768)
+
 #endif	/* _SOCK_H */
