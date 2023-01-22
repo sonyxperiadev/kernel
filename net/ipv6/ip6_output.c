2e902fd5844d48fdf70d6de533e16a41579839a3
voidanix <voidanix@keyedlimepie.org>
2023-01-22 19:43:32 +0100

    treewide: Linux 4.14.270
    Handle IPv6 MTUs the way commit d4d5190b942ac7a0f048 ("xfrm: fix MTU
    regression") does, instead of doing downstream's MTU check.
    
    Fixes: 9277207d5e809fdbef0c ("ipv6: remove min MTU check for ipsec tunnels")
    Link: https://lore.kernel.org/r/20220307091636.146155347@linuxfoundation.org
    Signed-off-by: voidanix <voidanix@keyedlimepie.org>

--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -1266,8 +1266,6 @@ static int ip6_setup_cork(struct sock *sk, struct inet_cork_full *cork,
 		if (np->frag_size)
 			mtu = np->frag_size;
 	}
-	if (!(rt->dst.flags & DST_XFRM_TUNNEL) && mtu < IPV6_MIN_MTU)
-		return -EINVAL;
 	cork->base.fragsize = mtu;
 	cork->base.gso_size = sk->sk_type == SOCK_DGRAM &&
 			      sk->sk_protocol == IPPROTO_UDP ? ipc6->gso_size : 0;
@@ -1321,14 +1319,19 @@ static int __ip6_append_data(struct sock *sk,
 
 	fragheaderlen = sizeof(struct ipv6hdr) + rt->rt6i_nfheader_len +
 			(opt ? opt->opt_nflen : 0);
-	maxfraglen = ((mtu - fragheaderlen) & ~7) + fragheaderlen -
-		     sizeof(struct frag_hdr);
 
 	headersize = sizeof(struct ipv6hdr) +
 		     (opt ? opt->opt_flen + opt->opt_nflen : 0) +
 		     (dst_allfrag(&rt->dst) ?
 		      sizeof(struct frag_hdr) : 0) +
 		     rt->rt6i_nfheader_len;
+
+	if (mtu < fragheaderlen ||
+	    ((mtu - fragheaderlen) & ~7) + fragheaderlen < sizeof(struct frag_hdr))
+		goto emsgsize;
+
+	maxfraglen = ((mtu - fragheaderlen) & ~7) + fragheaderlen -
+		     sizeof(struct frag_hdr);
 
 	/* as per RFC 7112 section 5, the entire IPv6 Header Chain must fit
 	 * the first fragment
