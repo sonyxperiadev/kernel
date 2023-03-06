b3c9df5c1dd7edd8f97b2d3dcd2fe79662b01ddb
voidanix <voidanix@keyedlimepie.org>
2023-03-06 13:54:50 -0500

    treewide: Linux 4.14.310
    Conflict with 74f30fa7c92e61e51c5e (ext4: fix cgroup writeback accounting
    with fs-layer encryption) resolved.
    
    Link: https://lore.kernel.org/r/20230315115718.796692048@linuxfoundation.org
    Link: https://lore.kernel.org/r/20230316083335.429724157@linuxfoundation.org
    Signed-off-by: voidanix <voidanix@keyedlimepie.org>

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -1426,11 +1426,10 @@ static struct buffer_head * ext4_find_entry (struct inode *dir,
 		int has_inline_data = 1;
 		ret = ext4_find_inline_entry(dir, &fname, res_dir,
 					     &has_inline_data);
-		if (has_inline_data) {
-			if (inlined)
-				*inlined = 1;
+		if (inlined)
+			*inlined = has_inline_data;
+		if (has_inline_data)
 			goto cleanup_and_exit;
-		}
 	}
 
 	if ((namelen <= 2) && (name[0] == '.') &&
@@ -3493,7 +3492,8 @@ static void ext4_resetent(handle_t *handle, struct ext4_renament *ent,
 	 * so the old->de may no longer valid and need to find it again
 	 * before reset old inode info.
 	 */
-	old.bh = ext4_find_entry(old.dir, &old.dentry->d_name, &old.de, NULL);
+	old.bh = ext4_find_entry(old.dir, &old.dentry->d_name, &old.de,
+				 &old.inlined);
 	if (IS_ERR(old.bh))
 		retval = PTR_ERR(old.bh);
 	if (!old.bh)
@@ -3661,7 +3661,8 @@ static int ext4_rename(struct inode *old_dir, struct dentry *old_dentry,
 			return retval;
 	}
 
-	old.bh = ext4_find_entry(old.dir, &old.dentry->d_name, &old.de, NULL);
+	old.bh = ext4_find_entry(old.dir, &old.dentry->d_name, &old.de,
+				 &old.inlined);
 	if (IS_ERR(old.bh))
 		return PTR_ERR(old.bh);
 	/*
