1c76a070ba458bda5f4b12dde494c3e5fd7fc4ab
Pavel Dubrova <pashadubrova@gmail.com>
2023-09-08 01:28:43 +0300

    input: ts: fts: Parse and control avdd gpio
    Parse and control avdd gpio during power on/off sequence.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/input/touchscreen/focaltech_touch/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_core.c
@@ -2323,6 +2323,7 @@ static int fts_power_source_ctrl(struct fts_ts_data *ts_data, int enable)
 			FTS_DEBUG("regulator enable !");
 			gpio_direction_output(ts_data->pdata->reset_gpio, 0);
 			msleep(1);
+			gpio_direction_output(ts_data->pdata->avdd_gpio, 1);
 			ret = fts_ts_enable_reg(ts_data, true);
 			if (ret)
 				FTS_ERROR("Touch reg enable failed\n");
@@ -2333,6 +2334,7 @@ static int fts_power_source_ctrl(struct fts_ts_data *ts_data, int enable)
 			FTS_DEBUG("regulator disable !");
 			gpio_direction_output(ts_data->pdata->reset_gpio, 0);
 			msleep(1);
+			gpio_direction_output(ts_data->pdata->avdd_gpio, 0);
 			ret = fts_ts_enable_reg(ts_data, false);
 			if (ret)
 				FTS_ERROR("Touch reg disable failed");
@@ -2476,15 +2478,34 @@ static int fts_gpio_configure(struct fts_ts_data *data)
 		}
 	}
 
+	/* request avdd gpio */
+	if (gpio_is_valid(data->pdata->avdd_gpio)) {
+		ret = gpio_request(data->pdata->avdd_gpio, "fts_avdd_gpio");
+		if (ret) {
+			FTS_ERROR("[GPIO]avdd gpio request failed");
+			goto err_irq_gpio_req;
+		}
+
+		ret = gpio_direction_input(data->pdata->avdd_gpio);
+		if (ret) {
+			FTS_ERROR("[GPIO]set_direction for avdd gpio failed");
+			goto err_reset_gpio_dir;
+		}
+	}
+
 	FTS_FUNC_EXIT();
 	return 0;
 
 err_reset_gpio_dir:
 	if (gpio_is_valid(data->pdata->reset_gpio))
 		gpio_free(data->pdata->reset_gpio);
+	if (gpio_is_valid(data->pdata->avdd_gpio))
+		gpio_free(data->pdata->avdd_gpio);
 err_irq_gpio_dir:
 	if (gpio_is_valid(data->pdata->irq_gpio))
 		gpio_free(data->pdata->irq_gpio);
+	if (gpio_is_valid(data->pdata->avdd_gpio))
+		gpio_free(data->pdata->avdd_gpio);
 err_irq_gpio_req:
 	FTS_FUNC_EXIT();
 	return ret;
@@ -2588,6 +2609,11 @@ static int fts_parse_dt(struct device *dev, struct fts_ts_platform_data *pdata)
 			0, &pdata->irq_gpio_flags);
 	if (pdata->irq_gpio < 0)
 		FTS_ERROR("Unable to get irq_gpio");
+
+	pdata->avdd_gpio = of_get_named_gpio_flags(np, "focaltech,avdd-gpio",
+					  0, &pdata->avdd_gpio_flags);
+	if (pdata->avdd_gpio < 0)
+		FTS_ERROR("Unable to get avdd_gpio");
 
 	ret = of_property_read_u32(np, "focaltech,max-touch-number", &temp_val);
 	if (ret < 0) {
@@ -3005,6 +3031,9 @@ static int fts_ts_remove_entry(struct fts_ts_data *ts_data)
 
 	if (gpio_is_valid(ts_data->pdata->irq_gpio))
 		gpio_free(ts_data->pdata->irq_gpio);
+
+	if (gpio_is_valid(ts_data->pdata->avdd_gpio))
+		gpio_free(ts_data->pdata->avdd_gpio);
 
 #if FTS_POWER_SOURCE_CUST_EN
 	fts_power_source_exit(ts_data);
