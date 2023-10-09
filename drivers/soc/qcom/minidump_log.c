40494dcd90ef76f8728fef49686c13a7188764f7
Marijn Suijten <marijn.suijten@somainline.org>
2023-10-09 21:36:35 +0200

    soc/qcom/minidump: Provide type of `md_align_offset` `static` global var
        kernel/sony/msm-5.10/kernel/drivers/soc/qcom/minidump_log.c:111:8: error: type specifier missing, defaults to 'int' [-Werror,-Wimplicit-int]
        static md_align_offset;
        ~~~~~~ ^
        int
        1 error generated.
    
    Signed-off-by: Marijn Suijten <marijn.suijten@somainline.org>

--- drivers/soc/qcom/minidump_log.c
+++ drivers/soc/qcom/minidump_log.c
@@ -116,7 +116,7 @@ static bool minidump_ftrace_dump = true;
 static bool md_in_oops_handler;
 static atomic_t md_handle_done;
 static struct seq_buf *md_runq_seq_buf;
-static md_align_offset;
+static int md_align_offset;
 
 /* CPU context information */
 #ifdef CONFIG_QCOM_MINIDUMP_PANIC_CPU_CONTEXT
