8f7950558435ecbbd9077ee2f8b00979b828b76a
Pavel Dubrova <pashadubrova@gmail.com>
2023-12-31 07:22:07 +0200

    interconnect: qcom: Do not log an error on EPROBE_DEFER
    Probe deferral is not an error, so don't log this as an error:
    [    0.310441] qnoc-waipio soc:interconnect@0: failed to register ICC provider, -517
    [    0.311362] qnoc-waipio soc:interconnect@1: failed to register ICC provider, -517
    [    0.312247] qnoc-waipio 1500000.interconnect: failed to register ICC provider, -517
    [    0.313103] qnoc-waipio 1680000.interconnect: failed to register ICC provider, -517
    [    0.313945] qnoc-waipio 16c0000.interconnect: failed to register ICC provider, -517
    [    0.314915] qnoc-waipio 1740000.interconnect: failed to register ICC provider, -517
    [    0.315752] qnoc-waipio 19100000.interconnect: failed to register ICC provider, -517
    [    0.316627] qnoc-waipio 320c0000.interconnect: failed to register ICC provider, -517
    [    0.317501] qnoc-waipio 3c40000.interconnect: failed to register ICC provider, -517
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/interconnect/qcom/lemans.c
+++ drivers/interconnect/qcom/lemans.c
@@ -3231,7 +3231,7 @@ static int qnoc_probe(struct platform_device *pdev)
 
 	ret = qcom_icc_rpmh_probe(pdev);
 	if (ret)
-		dev_err(&pdev->dev, "failed to register ICC provider: %d\n", ret);
+		dev_err_probe(&pdev->dev, ret, "failed to register ICC provider\n");
 	else
 		dev_info(&pdev->dev, "Registered LEMANS ICC\n");
 
