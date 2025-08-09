873f1e5dfbe4326e0e40559d932f335de9f4c4c2
sagar-m-softing <sagar.makhar.ext@softing.com>
2025-08-09 23:08:47 +0530

    Fix build errors

--- drivers/hwtracing/coresight/coresight-tmc-core.c
+++ drivers/hwtracing/coresight/coresight-tmc-core.c
@@ -418,10 +418,10 @@ static ssize_t out_mode_store(struct device *dev,
 			      const char *buf, size_t size)
 {
 	struct tmc_drvdata *drvdata = dev_get_drvdata(dev->parent);
-	char str[10] = "";
+	char str[11] = "";
 	int ret;
 
-	if (strlen(buf) >= 10)
+	if (strlen(buf) >= 11)
 		return -EINVAL;
 	if (sscanf(buf, "%10s", str) != 1)
 		return -EINVAL;
