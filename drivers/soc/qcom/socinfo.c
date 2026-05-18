853639e473a5ee55ba0524f8f6ad697d08f43a90
Pavel Dubrova <pashadubrova@gmail.com>
2026-05-18 14:10:23 +0300

    soc: qcom: socinfo: Use Kalama codename for SM8550
    Some BSP parts expect the codename.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -600,7 +600,7 @@ static const struct soc_id soc_id[] = {
 	{ qcom_board_id(IPQ9554) },
 	{ qcom_board_id(IPQ9570) },
 	{ qcom_board_id(IPQ9574) },
-	{ qcom_board_id(SM8550) },
+	{ qcom_board_id(KALAMA) },
 	{ qcom_board_id(IPQ5016) },
 	{ qcom_board_id(IPQ9510) },
 	{ qcom_board_id(QRB4210) },
